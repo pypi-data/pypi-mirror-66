d6229814200e92b12179b548b26db7a11e84c92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package clubear/0.0.9/clubear-0.0.9.tar.gz

---
+++ packages/clubear/clubear-0.0.9.tar.gz/clubear-0.0.9/clubear/dm.py
@@ -0,0 +1,298 @@
+import pandas as pd
+import glob,os,time,IPython
+import numpy as np
+
+'''
+This module is develped for a quick management for multiple datafiles.
+We assume that a number of big data files are contained in one common
+directory. Then, we wish to answer the following important questions:
+    
+(1) How many CSV files do we have in the target directory? what about 
+their sizes in G and lines? How about their shapes?
+
+(2) Do they have headers? If so, are their headers consistent with each
+other or note? Do they have an nonempty intersection.
+
+According to the the answers of the above questions, we propose the following
+two methods to combine all the files into one single BIGDATA file for 
+next stage analysis. 
+
+(1) The 1st method is write(), it is useful if all the data files have 
+appropriately defined headers. Accordingly, write() merges all the CSV files 
+into one BIGDATA file. Only those columns exists in all CSV files are included.
+Those columns are not included by **every** csv file is dropped.
+
+(2) The 2nd method is dump(), it is useful if all the data files have no 
+headers. However, the contains the exactly the same number of columns.
+Then, dump() simply merge all the files into one single BIGDATA file.
+'''
+
+class manager(object):
+    
+    def demo():
+        
+        greeting='''
+To demonstrate the data manager method, we need to creat a folder
+named 'demodata' in your current directory. In the directory, we 
+will simulate 10 CSV datasets, named: CluBearDemoData.X.csv. For 
+each CSV file there contains a number of variables and the 'sales' 
+one can be treated for response for regression.
+
+**WARNING**: 'demodata' is a folder created for clubear demo only. As
+a result, please do *NOT* place any personal file in this 'demodadta' 
+folder for they are to be *DELETED* completely. Once your files are 
+deleted, they can NEVER be recovered! By answering 'YES', any files
+in the demodata folder (if exists) will be completely removed.
+'''
+        print(greeting);print('')
+        userinput=input('Do you want to proceed [YES/No]:')
+        if userinput != 'YES': return('manager.demo: demo data cannot be generated without user permission.')
+        
+        print('')
+        if not os.path.exists('demodata'): os.makedirs('demodata')
+        files=os.listdir('demodata')
+        [os.remove('demodata/'+each) for each in files]
+
+        nfiles=10;ss=10000;
+        heads=['gender','age','height','weight','region','company','brand','price','sales']
+        ncov=len(heads)
+        headline=','.join(heads)+'\n'
+        for j in range(ncov):
+            filename='CluBearDemoData.'+str(j)+'.csv'
+            f=open('demodata/'+filename,'w')
+            f.write(headline)
+            for i in range(ss):
+                gender='M'; 
+                if np.random.normal(0,1,1)>0: gender='F'
+                age=int(np.round(20+50*np.random.uniform()))
+                height=np.round(1.5+0.5*np.random.uniform(),2)
+                weight=int(100+100*np.random.uniform())
+                region=chr(int(65+25*np.random.uniform()))
+                company=chr(int(70+5*np.random.uniform()))+chr(int(65+5*np.random.uniform()))
+                brand=chr(int(75+10*np.random.uniform()))+chr(int(80+10*np.random.uniform()))
+                price=np.round(np.random.uniform()*100)
+                sales=np.random.normal(0,1,1)+0.5*np.sqrt(age)+0.3*(gender=='F')
+                sales=sales-0.2*np.log(price)+0.2*(region=='Z')-0.35*(region=='A')
+                sales=np.round(np.exp(sales))[0]
+                line=[gender,age,height,weight,region,company,brand,price,sales]
+                line=[str(each) for each in line]
+                line=','.join(line)+'\n'
+                f.write(line)
+            f.close()
+            print('Demo datasets generated [',j+1,'/',ncov,'] :',filename)
+        
+
+        demostr='''
+import clubear as cb
+dm=cb.manager("demodata/") #initiate a dm object. 
+dm.files #the file names without path.
+dm.sizes #the file sizes in bytes.
+dm.nr #the number of rows in each file.
+dm.nc #the number of columns in each file.
+dm.heads #the columns contained in each file.
+dm.Heads #the columns contained in EVERY file.
+dm.write() #do a test run for write
+dm.write("clubear.csv") #do a full run with headers
+'''
+        print(demostr)
+    
+    
+    def __init__(self,path):
+        
+        if not os.path.exists(path): print('manager: The directory dose not exists!'); return
+        if not os.path.isdir(path): print('manager: This is not a director at all!'); return
+        
+        '''List all the CSV files under the director'''
+        csvfiles = glob.glob(path+"*csv")
+        csvfiles = sorted(csvfiles)
+        isfiles=list(map(os.path.isfile,csvfiles))
+        csvfiles = [csvfiles[each] for each in range(len(csvfiles)) if isfiles[each] == True] 
+        if len(csvfiles)==0: print('manager: No CSV file found in the director!'); return
+        
+        '''short file names without path information'''
+        shortfiles=[each[len(path):] for each in csvfiles]
+
+        
+        '''Count the number of lines in each file'''
+        IPython.display.clear_output(wait=True)
+        print('Total',len(csvfiles),'files found in path=',path)
+        print('')
+        nr = np.array([0 for each in csvfiles])
+        for kk in range(len(csvfiles)):
+            nr[kk]=sum(1 for line in open(csvfiles[kk],encoding='iso8859-1'))
+            nr[kk]=nr[kk]-1 #eliminate the header line
+            print(csvfiles[kk],nr[kk],'(',kk+1,'/',len(csvfiles),')')
+        mlines=np.round(nr/10**3,2)
+
+        '''Open each CSV file and get its first row and assume they are headers'''
+        openfiles=[open(each,encoding='iso8859-1') for each in csvfiles]
+        heads = [each.readline().replace("\n","").replace('\"','').replace("\'","").split(',') for each in openfiles]
+        
+        '''The number of columns in each CSV file'''
+        nc = [len(each) for each in heads]
+        
+        '''Output at most 3 column headers for inspection. The number of characters'''
+        '''contained in each header is constrainted to be no more than maxstrlen.'''
+        maxstrlen=10
+        maxm=np.min([np.min(nc),3])
+        topheads=[each[0:maxm] for each in heads]
+        for i in range(len(topheads)):
+            for j in range(maxm):
+                topheads[i][j]=topheads[i][j][:maxstrlen]
+                
+        '''Heads contains those columns commonly shared by EVERY file'''
+        '''the order fo Heads is determined by its order in the 1st CSV file'''
+        Heads = sorted(list(set.intersection(*map(set,heads))))
+        Heads = [each for each in heads[0] if each in Heads]
+        
+        '''each file sizes in G bytes'''
+        filesizes=np.round(np.array(list(map(os.path.getsize,csvfiles)))/(2.0**20),3)
+        
+        '''output to screen for user inspection'''
+        output=pd.DataFrame(list(zip(shortfiles,filesizes,mlines,nc,topheads)))
+        output.columns=['files','sizes','nr','nc','heads']
+        IPython.display.clear_output(wait=True)
+        print('Total',len(csvfiles),'files found with ',len(Heads),'common columns and ',np.sum(nr),'lines.')
+        print('')
+        print(output)
+        print('')
+        print('* files: a list of csvfile names without path information; sizes: the sizes of the files in M bytes')
+        print('* nr: the number of rows contained in the file in 10**3; nc: the number of columns contained in the file')
+        print('');
+        print('The following',len(Heads),'Heads are found in EVERY file and can be used for output:')
+        print('\n',Heads)
+        print('')
+        
+        '''global variables shared by the class'''
+        self.csvfiles = csvfiles
+        self.totlines=np.sum(nr)        
+        self.path = path
+        
+        self.nc = np.array(nc)
+        self.nr = np.array(nr)
+        self.heads = heads
+        self.files = shortfiles
+        self.sizes = list(map(os.path.getsize,csvfiles))
+        self.Heads = Heads
+    
+    '''The write() method is used to merge CSV files **with** headers'''
+    def write(self,pathfile='_CluBearTest_.csv'):
+        
+        if len(self.Heads)==0: print('manager: No common column can be writen out!'); return
+        
+        '''Always remove the target file so that a new file can be created'''
+        if os.path.exists(pathfile): os.remove(pathfile)
+        
+        '''If no filename given by user then do a test run write() get from each file '''
+        '''its 1st observation (not header) line and then output them to a target file.'''
+        if pathfile=='_CluBearTest_.csv':
+            is_first_file = True       
+            for each in self.csvfiles:
+                reader=pd.read_csv(each,iterator=True,encoding='iso8859-1')
+                chunk=reader.get_chunk(1)[self.Heads]
+                chunk.to_csv(pathfile,mode='a',index=0,header=is_first_file)
+                is_first_file=False
+
+        '''If filename is given by user then do a test run write() get from each file '''
+        '''its all observation lines and then output them to a target file.'''
+        if pathfile!='_CluBearTest_.csv':
+            
+            '''This is the file for output'''
+            f=open(pathfile,'w',encoding='iso8859-1')
+            
+            '''output the header line'''
+            f.write(','.join(self.Heads)+'\n')
+            
+            '''oklines counts how many lines have been ouputed'''
+            oklines=0
+            
+            '''output each CSV file'''
+            for each in self.csvfiles:
+                reader=open(each,encoding='iso8859-1')
+                
+                '''read in the line and this is the headerline'''
+                firstline=next(reader).replace('\n','').replace('\"','').replace("\'","").split(',')
+                
+                '''find the column positions of those variables in Heads'''
+                pos=[firstline.index(each) for each in self.Heads]
+                
+                '''remember oklines should add one'''
+                oklines=oklines+1
+                
+                '''start to read and write data lines'''
+                for line in reader:
+                    
+                    '''read in data line and keep those columns in Heads'''
+                    newline=line.replace('\n','').replace('\"','').replace("\'","").split(',')
+                    data=[newline[each] for each in pos]
+                    
+                    '''output to the target file'''
+                    data=','.join(data)+'\n'
+                    f.write(data)
+                    oklines=oklines+1
+                    
+                    '''for every 10**5 lines the progress % is updated'''
+                    if (oklines%10**5==0)|(oklines==self.totlines):
+                        progress='%.2f'%(100*oklines/self.totlines)
+                        IPython.display.clear_output(wait=True)
+                        print("Mission accomplished: ", progress,'% for a total of ',self.totlines,'lines.' )                    
+            f.close()
+            
+        '''data target data is created. we sould read its top 10 lines and '''
+        '''output them to the screen for user inspection'''
+        df=pd.read_csv(pathfile,iterator=True,encoding='iso8859-1')
+        df=df.get_chunk(10)
+        if os.path.exists('_CluBearTest_.csv'): os.remove(pathfile)
+        return df
+
+    
+    '''The dump() method is used to merge CSV files **without** headers'''
+    def dump(self,pathfile='_CluBearTest_.csv'):
+        
+        '''Always remove the target file so that a new file can be created'''
+        if os.path.exists(pathfile): os.remove(pathfile)
+        
+        '''This is the target file for output'''
+        f=open(pathfile,'w',encoding='iso8859-1')
+        
+        '''Since those files have no headers, we are going to create headers for'''
+        '''them. How many headers are needed? This is determined by the maximum '''
+        '''number of columns contained in each CSV file.'''
+        max_num_columns=np.max(self.nc)
+        column_names=",".join(["V"+str(i) for i in range(max_num_columns)])+'\n'
+        f.write(column_names)
+        
+        '''If no user input is given then do a test run'''
+        if pathfile=='_CluBearTest_.csv': [f.write(next(open(each,encoding='iso8859-1'))) for each in self.csvfiles]
+            
+        '''If user input is given, we then do a full size run'''
+        if pathfile!='_CluBearTest_.csv':
+            
+            '''start to record how many lines outputed'''
+            oklines=0
+            
+            '''do for each CSV file'''
+            for each in self.csvfiles:
+                
+                '''do for each line'''
+                for line in open(each,encoding='iso8859-1'):
+                    
+                    '''write to the target file'''
+                    f.write(line)
+                    
+                    '''update how many lines outputed'''
+                    oklines=oklines+1
+                    
+                    '''update the progress % for every 10**5 lines'''
+                    if (oklines%10**5==0)|(oklines==self.totlines):
+                        progress='%.2f'%(100*oklines/self.totlines)
+                        IPython.display.clear_output(wait=True)
+                        print("Mission accomplished: ", progress,'% for a total of ',self.totlines,'lines.' )                    
+        f.close()
+
+        '''data target data is created. we sould read its top 10 lines and '''
+        '''output them to the screen for user inspection'''
+        df=pd.read_csv(pathfile,iterator=True,encoding='iso8859-1')
+        df=df.get_chunk(10)
+        if os.path.exists('_CluBearTest_.csv'): os.remove(pathfile)
+        return df
---
+++ packages/clubear/clubear-0.0.9.tar.gz/clubear-0.0.9/clubear/sf.py
@@ -0,0 +1,347 @@
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
+
+import pandas as pd 
+from sklearn.utils import shuffle as sk_shuffle
+import random
+import os
+import sys
+import shutil
+import time
+import numpy as np
+import re
+import warnings
+warnings.filterwarnings('ignore')
+
+class shuffle(object):
+
+    def demo():
+        """
+        给出接口使用示例
+        """
+        demo_str = '''
+import clubear as cb
+pathfile = '/clubear/data/single_files/airline.csv'
+shuffle = cb.shuffle(pathfile)
+shuffle.go()        '''
+        print(demo_str)
+
+    def __init__(self, original_data_path):
+        """
+        初始化方法
+        """
+        super(shuffle, self).__init__()
+
+        # 原始数据存放路径
+        self.original_data_path = original_data_path
+
+        # shuffle后的文件存放路径
+        self.shuffled_path = None
+
+        # 数据是否OK（存在，为file）
+        self.is_data_exits = False
+        # 数据是否为空
+        self.is_empty = False
+        if os.path.exists(self.original_data_path) == False:
+            print("shuffle: [%s] does not exist! Please check the path." % self.original_data_path)
+        elif os.path.isfile(self.original_data_path) == False:
+            print("shuffle: [%s] is not a file! Please check the path." % self.original_data_path)
+        else:
+            # 文件是否为空
+            self.is_empty = self.is_file_empy()
+
+            # shuffle后的文件存放路径，默认为原始路径+.csv.shuffle
+            # 若为windows路径，须将\替换为/
+            _convert_original_data_path = re.sub(r'\\', '/', original_data_path)
+            file_pat = re.compile(r'/([^/]*)\.csv')
+            file_name_middles = file_pat.findall('/'+_convert_original_data_path)
+            self.shuffled_path = './%s.csv.shuffle' % file_name_middles[0]
+
+            self.is_data_exits = True
+
+    def is_file_empy(self):
+        """
+        判断文件是否为空
+        """
+        with open(self.original_data_path, 'r', errors='ignore', encoding='ISO-8859-1') as f:
+            # 记录表头
+            line = f.readline()
+            header_str = line
+
+            # 记录原始数据字节总长度
+            f.seek(0, 2)
+            file_num = f.tell()
+        if len(header_str) == file_num:
+            return True
+        else:
+            return False
+
+    def go(self, **kwargs):
+        """
+        执行shuffle操作
+        根据输入的参数（shuffle方式）不同，调用不同的方法进行shuffle
+        """
+        if self.is_data_exits:
+            if self.is_empty:
+                print("shuffle.go: [%s] is empty." % self.original_data_path)
+            else:
+                if 'method' not in kwargs:
+                    # 若未指定shuffle方式
+                    self.shuffle_divide_conque(None, 200)
+                else:
+                    # 若指定采用 divide-conque 方式进行shuffle
+                    if kwargs['method'] == 'divide-conque':
+                        if 'output_path' in kwargs:
+                            new_path = kwargs['output_path']
+                        else:
+                            new_path = None
+                        if 'cache_file_num' in kwargs:
+                            cache_file_num = kwargs['cache_file_num']
+                        else:
+                            cache_file_num = 200
+                        self.shuffle_divide_conque(new_path, cache_file_num)
+                    # 若采用 XX 方式进行shuffle
+                    elif kwargs['method'] == 'XX':
+                        pass
+        else:
+            print("shuffle.go: Please check the path.")
+
+
+    def shuffle_divide_conque(self, new_path=None, cache_file_num = 200):
+        """
+        随机排序方法
+        """
+        # 若没有指定存放位置，则将shuffle后文件放到默认位置
+        if new_path == None:
+            new_path = self.shuffled_path
+        else:
+            self.shuffled_path = new_path
+
+        # 指定工作文件夹，保存中间文件
+        self.work_space = './_CluBear_file/'
+        # 日志
+        self.log_path = self.work_space + '_log.txt'
+
+        # 禁止将shuffle后文件路径设置为与原始文件路径相同
+        if new_path == self.original_data_path:
+            print("Warning: Are you sure to rewrite the original file?")
+        else:
+            if self.is_data_exits:
+                #print('[ Shuffling ]')
+                t0_shuffle = time.time()
+                _output = sys.stdout
+
+                # 创建中间文件文件夹
+                if not os.path.exists(self.work_space):
+                    os.mkdir(self.work_space)
+
+                self._log('[ Shuffling ] %s' % self._now_time())
+
+                # 创建文件夹，保存排序的中间文件
+                self.cache_fold = self.work_space + '_cache/'
+                # 刷新cache文件夹
+                if os.path.exists(self.cache_fold):
+                    shutil.rmtree(self.cache_fold)
+                os.mkdir(self.cache_fold)      
+
+                # [step-1].把原数据每一行随机分配给若干个cache
+                t0 = time.time()
+                header_str = self._shuffle_assign(cache_file_num, _output)
+                t1 = time.time()
+                self._log('** shuffle - cache assignment: %.2fs' % (t1 - t0))
+
+                # [step-2].对每个cache，打乱内部顺序
+                t0 = time.time()
+                #print('\n[ Shuffling Step-2/3]')
+
+                for i in range(0, cache_file_num):
+                    d = self._input(self.cache_fold + str(i))
+                    # 随机排序
+                    d = sk_shuffle(d)
+                    self._output(d, self.cache_fold + str(i))
+
+                    status = 100 * (i+1) / float(cache_file_num)
+                    _output.write('\rShuffle stage (2/3) accomplished: %.2f %%   ' % (status))
+     
+                # 将标准输出一次性刷新
+                #_output.flush()
+                #"""
+
+                t1 = time.time()
+                self._log('** shuffle - cache shuffling: %.2fs' % (t1 - t0))
+
+                # 拼接cache，组成最终文件
+                t0 = time.time()
+                #print('\n[ Shuffling Step-3/3]')
+
+                # 先对所有cache，产生一个随机排序
+                concat_order = list(pd.Series(range(cache_file_num)).sample(cache_file_num, replace = False))
+
+                # 刷新 new_path 指定的文件
+                with open(new_path, 'w', encoding='ISO-8859-1') as f:
+                    f.write(header_str)
+
+                with open(self.original_data_path, 'r', encoding='ISO-8859-1', errors='ignore') as of:
+
+                    with open(new_path, 'a', encoding='ISO-8859-1') as f:
+                        # 按照随机顺序，写入cache内容
+                        for i in range(0, len(concat_order)):
+                            with open(self.cache_fold + str(concat_order[i]), 'r') as fc:
+                                line = fc.readline()
+                                while line:
+                                    # 当前读取的line，只是一个行index
+                                    line = fc.readline()
+                                    # 利用行index寻找原始文件里那一行
+                                    try:
+                                        of.seek(int(line), 0)
+                                        original_line = of.readline()
+                                        if len(original_line) > 0:
+                                            if '\n' not in original_line:
+                                                original_line = original_line + '\n'
+                                            f.write(original_line)
+                                    except:
+                                        pass
+                            # 写入完毕后，删除cache文件
+                            os.remove(self.cache_fold + str(concat_order[i]))
+
+                            status = 100 * (i+1) / float(cache_file_num)
+                            _output.write('\rShuffle stage (3/3) accomplished: %.2f %%   ' % (status))
+
+                t1_shuffle = time.time()
+                _output.write('\rShuffling accomplished (%s) and shuffled file is generated as: %s \n' % (self._count_time(t1_shuffle - t0_shuffle), self.shuffled_path))
+
+                # 将标准输出一次性刷新
+                _output.flush()
+
+                # 最终删除cache文件夹
+                shutil.rmtree(self.cache_fold)
+
+                t1 = time.time()
+                self._log('** shuffle - cache concating: %.2fs' % (t1 - t0))
+                #print("\nShuffling finished!")
+
+                # 0411增加：删除工作文件夹
+                shutil.rmtree(self.work_space)
+
+
+    """
+    * 内部辅助方法
+    """
+    def _input(self, input_path):
+        """
+        获取csv数据方法
+        """
+        d = pd.read_csv(input_path, encoding='ISO-8859-1')
+        return d
+
+    def _output(self, data, output_path):
+        """
+        写出csv数据方法
+        """
+        data.to_csv(output_path, encoding='ISO-8859-1', index=False)
+
+    def _shuffle_assign(self, cache_file_num, _output):
+        """
+        打乱原始文件排序，打散成若干个文件
+        """
+
+        # 获取标准输出
+        #_output = sys.stdout
+        # 行计数器
+        cnt = 0
+        with open(self.original_data_path, 'r', encoding='ISO-8859-1', errors='ignore') as f:
+            # 记录原始数据字节总长度
+            f.seek(0, 2)
+            file_num = f.tell()
+
+            # 单行大概长度
+            f.seek(0, 0)
+            line = f.readline()
+            line = f.readline()
+            byte_line = len(line)
+
+            # 估计行数
+            line_num_est = int(file_num / byte_line)
+            #line_num_est = self.get_lines_num()
+            #print(line_num_est)
+            # 刷新频率
+            flush = int(max(line_num_est / 1000, 10)) 
+
+        with open(self.original_data_path, 'r', encoding='ISO-8859-1', errors='ignore') as f:
+            # 头一行为表头
+            line = f.readline()
+            header_str = line
+            cul_pos = len(line)
+
+            # 创建多个cache文件
+            cache_list = []
+            for i in range(0, cache_file_num):
+                fc = open(self.cache_fold + str(i), 'w')
+                fc.write('row_index\n')
+                cache_list.append(fc)
+
+            while line: # cnt < 100:
+                # 读取一行
+                line = f.readline()
+                # 随机放入一个cache
+                cache_no = int(random.random() * cache_file_num)
+
+                """
+                修改！不再写入一行，而是写入行的开头位置
+                """
+                # 追加写入某个cache
+                cache_list[cache_no].write('%d\n' % cul_pos)  
+                # 更新指针位置
+                cul_pos = cul_pos + len(line)   
+
+                # 记录已经处理的行数
+                cnt = cnt + 1
+                if cnt % flush == 0:
+                    # 估计当前进度
+                    status = min(100 * (cul_pos+1) / float(file_num), 100)
+                    _output.write('\rShuffle stage (1/3) accomplished: %.2f %%' % status)
+            _output.write('\rShuffle stage (1/3) accomplished: 100.00 %')
+
+
+        # 将标准输出一次性刷新
+        #_output.flush()
+        
+        # 关闭所有cache文件
+        for i in range(0, cache_file_num):
+            cache_list[i].close()
+        return header_str
+
+
+    def _log(self, info):
+        """
+        记录日志
+        """
+        with open(self.log_path, 'a') as f:
+            f.write(info + '\n')
+
+    def _now_time(self):
+        """
+        当前时间
+        """
+        return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+    def _count_time(self, _t):
+        """
+        整理时间格式
+        """
+        _hour = int(_t/3600)
+        _t = _t - 3600*_hour
+        _min = int(_t/60)
+        _sec = _t - 60*_min
+        time_str = ''
+        if _hour > 0:
+            time_str = time_str + str(_hour) + 'h'
+        if _min > 0:
+            time_str = time_str + str(_min) + 'm'
+
+        if _hour > 0 or _min > 0:
+            time_str = time_str + str(int(_sec)) + 's'
+        else:
+            time_str = '%.2fs' % _sec
+        return time_str
+
