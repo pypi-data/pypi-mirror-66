f926380556ca5fa97ad840f88353a1327077e2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:17 +0000

    Add package domdf-python-tools/0.1.22/domdf_python_tools-0.1.22-py3-none-any.whl

---
+++ packages/domdf-python-tools/domdf_python_tools-0.1.22-py3-none-any.whl/domdf_python_tools/__init__.py
@@ -0,0 +1,29 @@
+#  Compiled 2018-2019 by Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  terminal.py
+#  		Copyright © 2014-2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  		get_terminal_size, _get_terminal_size_windows, _get_terminal_size_tput and _get_terminal_size_linux
+# 			from https://gist.github.com/jtriley/1108174
+#  			Copyright © 2011 jtriley
+#
+#  paths.py
+# 		Copyright © 2018-2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  		copytree based on https://stackoverflow.com/a/12514470/3092681
+# 			Copyright © 2012 atzz
+# 			Licensed under CC-BY-SA
+#
+
+
+from domdf_python_tools.utils import *
+from domdf_python_tools import paths, terminal, utils, doctools
+
+__all__ = ["paths", "terminal", "utils", "dates"]
+
+__author__ = "Dominic Davis-Foster"
+__copyright__ = "2014-2019 Dominic Davis-Foster"
+
+__license__ = "LGPLv3+"
+__version__ = "0.1.22"
+__email__ = "dominic@davis-foster.co.uk"
