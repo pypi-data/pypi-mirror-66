6c0665569aecef8d1bd73c4b1dc4ed7925cbc6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:08 +0000

    Add package clubear/0.0.5/clubear-0.0.5.tar.gz

---
+++ packages/clubear/clubear-0.0.5.tar.gz/clubear-0.0.5/clubear/__init__.py
@@ -0,0 +1,13 @@
+AUTHOR="CluBear"  
+VERSION="2020-04-17" 
+
+from .dm import manager
+from .pp import pump
+from .reg import subreg
+from .sf import shuffle
+from .sub import subsample
+from .pp import check
+
+from .fun import demo
+from .fun import dummy
+from .fun import ispump
