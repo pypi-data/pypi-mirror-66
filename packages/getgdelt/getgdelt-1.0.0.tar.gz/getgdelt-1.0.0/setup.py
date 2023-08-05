4b68d6d183917342f165ed3d3f23956081fc2596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:56 +0000

    Add package getgdelt/1.0.0/getgdelt-1.0.0.tar.gz

---
+++ packages/getgdelt/getgdelt-1.0.0.tar.gz/getgdelt-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'getgdelt',
+    version="1.0.0",
+    author='Sun Yufei',
+    author_email='syfyufei@gmail.com',
+    description='Get and use gdelt data more reliably without hardware limitation.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/syfyufei/getgdelt',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
