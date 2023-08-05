710cba1db0b75ffc0925c2f89a1a9dd895dec9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:07 +0000

    Add package varsom-flood-client/1.0.0/varsom_flood_client-1.0.0-py3-none-any.whl

---
+++ packages/varsom-flood-client/varsom_flood_client-1.0.0-py3-none-any.whl/varsom_flood_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from varsom_flood_client.api.cap_api import CapApi
+from varsom_flood_client.api.region_api import RegionApi
+from varsom_flood_client.api.warning_api import WarningApi
