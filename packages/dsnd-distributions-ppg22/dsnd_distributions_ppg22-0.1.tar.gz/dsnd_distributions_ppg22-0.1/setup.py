4fd27bb7f917fe6aa2e220c0a6200141eb0378b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package dsnd-distributions-ppg22/0.1/dsnd_distributions_ppg22-0.1.tar.gz

---
+++ packages/dsnd-distributions-ppg22/dsnd_distributions_ppg22-0.1.tar.gz/dsnd_distributions_ppg22-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_ppg22',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['dsnd_distributions_ppg22'],
+      zip_safe=False)
