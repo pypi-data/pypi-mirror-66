9bb5b79e3cfbce0c90465b242b36e4f574a89937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package markusapi/0.1.0/markusapi-0.1.0.tar.gz

---
+++ packages/markusapi/markusapi-0.1.0.tar.gz/markusapi-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="markusapi",
+    version="0.1.0",
+    author="Alessio Di Sandro, Misha Schwartz",
+    author_email="mschwa@cs.toronto.edu",
+    description="Interface to interact with MarkUs API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarkUsProject/markus-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
