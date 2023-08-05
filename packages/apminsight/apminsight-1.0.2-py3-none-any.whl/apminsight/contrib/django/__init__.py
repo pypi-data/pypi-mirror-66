643dfedac2a7bc9216c93e0ae2d93873e56b8144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package apminsight/1.0.2/apminsight-1.0.2-py3-none-any.whl

---
+++ packages/apminsight/apminsight-1.0.2-py3-none-any.whl/apminsight/contrib/django/__init__.py
@@ -0,0 +1,2 @@
+
+default_app_config = "apminsight.contrib.django.apps.ApminsightConfig"
---
+++ packages/apminsight/apminsight-1.0.2-py3-none-any.whl/apminsight/instrumentation/packages/__init__.py
@@ -0,0 +1,12 @@
+
+
+__all__ = [
+    'django',
+    'flask',
+    'jinja2',
+    'mysql',
+    'psycopg2',
+    'pymemcache',
+    'redis',
+    'sqlite'
+]
