ff755c6209208c927381c0cacbaeb34696916d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package tmux-list-learner/0.1.0/tmux_list_learner-0.1.0.tar.gz

---
+++ packages/tmux-list-learner/tmux_list_learner-0.1.0.tar.gz/tmux_list_learner-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='tmux_list_learner',
+    version='0.1.0',
+    description='List learning tool using tmux windows',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/flaxandteal/tmux_list_learner',
+    author='Phil Weir',
+    author_email='phil.weir@flaxandteal.co.uk',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords='tmux learning',
+    install_requires=[
+        'Click',
+        'pyyaml',
+        'appdirs'
+    ],
+    entry_points='''
+        [console_scripts]
+        tmux-list-learner=tmux_list_learner.scripts.tmux_list_learner:cli
+    '''
+)
