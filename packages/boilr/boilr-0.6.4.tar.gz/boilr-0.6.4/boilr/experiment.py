c0485acda3e48497380b3664abffb0ecc35a0542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package boilr/0.6.4/boilr-0.6.4.tar.gz

---
+++ packages/boilr/boilr-0.6.4.tar.gz/boilr-0.6.4/boilr/experiment.py
@@ -0,0 +1,416 @@
+import argparse
+from numbers import Number
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+from .summarize import SummarizerCollection
+from .utils import print_num_params
+
+
+class BaseExperimentManager:
+    """
+    Experiment manager.
+
+    Data attributes:
+    - 'args': argparse.Namespace containing all config parameters. When
+      initializing this object, if 'args' is not given, all config
+      parameters are set based on experiment defaults and user input, using
+      argparse.
+    - 'run_description': string description of the run that includes a timestamp
+      and can be used e.g. as folder name for logging.
+    - 'model': the model.
+    - 'device': torch.device that is being used
+    - 'dataloaders': DataLoaders, with attributes 'train' and 'test'
+    - 'optimizer': the optimizer
+    """
+
+    def __init__(self, args=None):
+        self.device = None
+        self.dataloaders = None
+        self.model = None
+        self.optimizer = None
+        self.args = args
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            allow_abbrev=False)
+        if args is None:
+            self.args = self._parse_args(parser)
+        self.run_description = self._make_run_description(self.args)
+
+
+    def _parse_args(self, parser):
+        """
+        Parse command-line arguments defining experiment settings.
+
+        :return: args: argparse.Namespace with experiment settings
+        """
+        raise NotImplementedError
+
+
+    def add_required_args(self,
+                          parser,
+                          batch_size,
+                          test_batch_size,
+                          lr,
+                          max_epochs=1000000,
+                          max_steps=2147483647,
+                          seed=54321,
+                          log_interval=10000,
+                          test_log_interval=10000,
+                          checkpoint_interval=100000,
+                          resume="",
+                          ):
+
+        parser.add_argument('--batch-size',
+                            type=int,
+                            default=batch_size,
+                            metavar='N',
+                            dest='batch_size',
+                            help='training batch size')
+
+        parser.add_argument('--test-batch-size',
+                            type=int,
+                            default=test_batch_size,
+                            metavar='N',
+                            dest='test_batch_size',
+                            help='test batch size')
+
+        parser.add_argument('--lr',
+                            type=float,
+                            default=lr,
+                            metavar='LR',
+                            dest='lr',
+                            help='learning rate')
+
+        parser.add_argument('--seed',
+                            type=int,
+                            default=seed,
+                            metavar='N',
+                            dest='seed',
+                            help='random seed')
+
+        parser.add_argument('--tr-log-interv',
+                            type=int,
+                            default=log_interval,
+                            metavar='N',
+                            dest='log_interval',
+                            help='number of batches before logging train status')
+
+        parser.add_argument('--ts-log-interv',
+                            type=int,
+                            default=test_log_interval,
+                            metavar='N',
+                            dest='test_log_interval',
+                            help='number of batches before logging test status')
+
+        parser.add_argument('--ckpt-interv',
+                            type=int,
+                            default=checkpoint_interval,
+                            metavar='N',
+                            dest='checkpoint_interval',
+                            help='number of batches before saving model checkpoint')
+
+        parser.add_argument('--max-steps',
+                            type=int,
+                            default=max_steps,
+                            metavar='N',
+                            dest='max_steps',
+                            help='max number of training steps')
+
+        parser.add_argument('--max-epochs',
+                            type=int,
+                            default=max_epochs,
+                            metavar='N',
+                            dest='max_epochs',
+                            help='max number of training epochs')
+
+        parser.add_argument('--nocuda',
+                            action='store_true',
+                            dest='no_cuda',
+                            help='do not use cuda')
+
+        parser.add_argument('--descr',
+                            type=str,
+                            default='',
+                            metavar='STR',
+                            dest='additional_descr',
+                            help='additional description for experiment name')
+
+        parser.add_argument('--dry-run',
+                            action='store_true',
+                            dest='dry_run',
+                            help='do not save anything to disk')
+
+        parser.add_argument('--resume',
+                            type=str,
+                            metavar='NAME',
+                            default=resume,
+                            dest='resume',
+                            help="load the run with this name and resume training")
+
+
+    @staticmethod
+    def _make_run_description(args):
+        """
+        Create a string description of the run. It is used in the names of the
+        logging folders.
+
+        :param args: experiment config
+        :return: the run description
+        """
+        raise NotImplementedError
+
+
+    def make_datamanager(self):
+        """
+        Create a DatasetManager object. To be overridden.
+        :return: DatasetManager
+        """
+        raise NotImplementedError
+
+
+    def make_model(self):
+        """
+        Create a model. To be overridden.
+        :return: model
+        """
+        raise NotImplementedError
+
+
+    def make_optimizer(self):
+        """
+        Create an optimizer. To be overridden.
+        :return: optimizer
+        """
+        raise NotImplementedError
+
+
+    def setup(self, checkpoint_folder=None):
+        """
+        Setup experiment: load dataset, create model, load weights if a
+        checkpoint is given, create optimizer.
+        """
+
+        # If checkpoint folder is given, load model from there to resume
+        resume = checkpoint_folder is not None
+
+        # Dataset
+        print("Getting dataset ready...")
+        self.dataloaders = self.make_datamanager()
+        print("Data shape: {}".format(self.dataloaders.data_shape))
+        print("Train/test set size: {}/{}".format(
+            len(self.dataloaders.train.dataset),
+            len(self.dataloaders.test.dataset),
+        ))
+
+        # Model
+        print("Creating model...")
+        self.model = self.make_model().to(self.device)
+        print_num_params(self.model)
+
+        # Load weights if resuming training
+        if resume:
+            self.load_model(checkpoint_folder, step=None)
+
+        # Optimizer
+        self.optimizer = self.make_optimizer()
+
+
+    def load_model(self, checkpoint_folder, step=None):
+        """
+        Loads model weights from a checkpoint in the specified folder.
+        If step is given, it attempts to load the checkpoint at that step.
+        The weights are loaded with map_location=device, where device is the
+        current device of this experiment.
+        """
+        self.model.load(checkpoint_folder, self.device, step=step)
+
+
+    def forward_pass(self, x, y=None):
+        """
+        Simple single-pass model evaluation. It consists of a forward pass
+        and computation of all necessary losses and metrics.
+        """
+        raise NotImplementedError
+
+
+    def post_backward_callback(self):
+        pass
+
+
+    @classmethod
+    def get_metrics_dict(cls, results):
+        """
+        Given a dict of results, return a dict of metrics to be given to
+        summarizers. Keys are also used as names for tensorboard logging.
+
+        In the base implementation, keys are simply copyed and used as names
+        for tensorboard.
+
+        Only scalars accepted, non-scalars are discarded. Actually, anything
+        that either is a scalar or has the method item(). That should include
+        Python scalars, numpy scalars, torch scalars, numpy and torch
+        arrays/tensors with one element.
+
+        Override to customize translation from results to dictionary of
+        scalar metrics.
+        """
+        metrics_dict = {}
+        for k in results:
+            x = results[k]
+            if isinstance(x, Number):
+                metrics_dict[k] = x
+                continue
+            try:
+                metrics_dict[k] = x.item()
+            except (AttributeError, ValueError):  # is this enough?
+                pass
+        return metrics_dict
+
+
+    @classmethod
+    def train_log_str(cls, summaries, step, epoch=None):
+        s = "       [step {}]".format(step)
+        for k in summaries:
+            s += "  {key}={value:.5g}".format(key=k, value=summaries[k])
+        return s
+
+
+    @classmethod
+    def test_log_str(cls, summaries, step, epoch=None):
+        s = "       "
+        if epoch is not None:
+            s += "[step {}, epoch {}]".format(step, epoch)
+        for k in summaries:
+            s += "  {key}={value:.5g}".format(key=k, value=summaries[k])
+        return s
+
+
+    def test_procedure(self, **kwargs):
+        """
+        Execute test procedure for the experiment. This typically includes
+        collecting metrics on the test set using forward_pass().
+        For example in variational inference we might be interested in
+        repeating this many times to derive the importance-weighted ELBO.
+
+        :return: summaries (dict)
+        """
+        raise NotImplementedError
+
+
+    def additional_testing(self, img_folder):
+        """
+        Perform additional testing, including possibly generating images.
+
+        :param img_folder: folder to store images
+        """
+        raise NotImplementedError
+
+
+class VIExperimentManager(BaseExperimentManager):
+    """
+    Variational inference experiment manager. This version implements a default
+    test_procedure for variational inference.
+
+    Data attributes:
+    - 'args': argparse.Namespace containing all config parameters. When
+      initializing this object, if 'args' is not given, all config
+      parameters are set based on experiment defaults and user input, using
+      argparse.
+    - 'run_description': string description of the run that includes a timestamp
+      and can be used e.g. as folder name for logging.
+    - 'model': the model.
+    - 'device': torch.device that is being used
+    - 'dataloaders': DataLoaders, with attributes 'train' and 'test'
+    - 'optimizer': the optimizer
+    """
+
+    def test_procedure(self, iw_samples=None):
+        """
+        Execute test procedure for the experiment. This typically includes
+        collecting metrics on the test set using forward_pass().
+        Repeat this many times to derive the importance-weighted ELBO.
+
+        :param iw_samples: number of samples for the importance-weighted ELBO.
+                The other metrics are also averaged over all these samples,
+                yielding a more accurate estimate.
+        :return: summaries (dict)
+        """
+
+        # Shorthand
+        test_loader = self.dataloaders.test
+        step = self.model.global_step
+        args = self.args
+        n_test = len(test_loader.dataset)
+
+        # If it's time to estimate log likelihood, use many samples.
+        # If given, use the given number.
+        if iw_samples is None:
+            iw_samples = 1
+            if step % args.loglik_interval == 0 and step > 0:
+                iw_samples = args.loglik_samples
+
+        # Setup
+        summarizers = SummarizerCollection(mode='sum')
+        progress = tqdm(total=len(test_loader) * iw_samples, desc='test ')
+        all_elbo_sep = torch.zeros(n_test, iw_samples)
+        for batch_idx, (x, y) in enumerate(test_loader):
+            for i in range(iw_samples):
+                outputs = self.forward_pass(x, y)
+
+                # elbo_sep shape (batch size,)
+                i_start = batch_idx * args.test_batch_size
+                i_end = (batch_idx + 1) * args.test_batch_size
+                all_elbo_sep[i_start: i_end, i] = outputs['elbo_sep'].detach()
+
+                metrics_dict = self.get_metrics_dict(outputs)
+                multiplier = (x.size(0) / n_test) / iw_samples
+                for k in metrics_dict:
+                    metrics_dict[k] *= multiplier
+                summarizers.add(metrics_dict)
+
+                progress.update()
+        progress.close()
+
+        if iw_samples > 1:
+            # Shape (test set size,)
+            elbo_iw = torch.logsumexp(all_elbo_sep, dim=1)
+            elbo_iw = elbo_iw - np.log(iw_samples)
+
+            # Mean over test set (scalar)
+            elbo_iw = elbo_iw.mean().item()
+            key = 'elbo/elbo_IW_{}'.format(iw_samples)
+            summarizers.add({key: elbo_iw})
+
+        summaries = summarizers.get_all(reset=True)
+
+        return summaries
+
+
+    def add_required_args(self,
+                          parser,
+                          *args,
+                          ll_every=50000,
+                          loglik_samples=100,
+                          **kwargs
+                          ):
+
+        super().add_required_args(parser, *args, **kwargs)
+
+        parser.add_argument('--ll-interv',
+                            type=int,
+                            default=ll_every,
+                            metavar='N',
+                            dest='loglik_interval',
+                            help='number of batches before evaluating '
+                                 'log likelihood')
+
+        parser.add_argument('--ll-samples',
+                            type=int,
+                            default=loglik_samples,
+                            metavar='N',
+                            dest='loglik_samples',
+                            help='number of importance samples to evaluate '
+                                 'log likelihood')
---
+++ packages/boilr/boilr-0.6.4.tar.gz/boilr-0.6.4/boilr/trainer.py
@@ -0,0 +1,266 @@
+import os
+import pickle
+import warnings
+
+import torch
+try:
+    from torch.utils import tensorboard
+    have_tensorboard = True
+except ImportError as e:
+    msg = "{}: {}".format(e.__class__.__name__, e)
+    msg = "Could not import tensorboard.\n" + msg
+    warnings.warn(msg)
+    have_tensorboard = False
+from tqdm import tqdm
+
+from . import viz
+from .summarize import History, SummarizerCollection
+from .utils import set_rnd_seed, get_date_str
+
+
+class Trainer:
+    """
+    Generic tool for training models. All model- and experiment-specific work is
+    defined and performed in an experiment object, which is provided at init.
+    """
+
+    def __init__(self, experiment):
+        self.experiment = experiment
+        args = experiment.args
+        resume = args.resume != ""
+
+        if resume:
+            # Folder string = run name = resume argument
+            folder_str = args.resume
+            print("Resume from '{}'".format(folder_str))
+            msg = "When resuming training, the optimizer's state is not restored"
+            warnings.warn(msg)
+            msg = ("When resuming training, the relative time on tensorboard "
+                   "has a gap due to wall clock timestamps")
+            warnings.warn(msg)
+
+            # Get all folder names to resume saving results
+            result_folder = os.path.join('results', folder_str)
+            self.img_folder = os.path.join(result_folder, 'imgs')
+            self.checkpoint_folder = os.path.join('checkpoints', folder_str)
+            self.log_path = os.path.join(result_folder, 'log.pkl')
+            tboard_folder = os.path.join('tensorboard_logs', folder_str)
+            self.tb_writer = None
+            if have_tensorboard:  # maybe we didn't have tboard originally
+                os.makedirs(tboard_folder, exist_ok=True)
+                self.tb_writer = tensorboard.SummaryWriter(tboard_folder)
+
+            # Forget about all arguments, load all of them from saved config
+            # (the 'resume' argument is overwritten in the process)
+            config_path = os.path.join(self.checkpoint_folder, 'config.pkl')
+            with open(config_path, 'rb') as file:
+                args = pickle.load(file)
+            assert not args.dry_run  # this would not make sense
+            experiment.args = args  # copy them over to the experiment
+
+            # Load training and test history from log.pkl
+            with open(self.log_path, 'rb') as file:
+                history = pickle.load(file)
+                self.train_history = History(history['train'])
+                self.test_history = History(history['test'])
+
+        assert args.checkpoint_interval % args.test_log_interval == 0
+
+
+        # Pick device (cpu/cuda)
+        use_cuda = not args.no_cuda and torch.cuda.is_available()
+        self.device = torch.device("cuda" if use_cuda else "cpu")
+        experiment.device = self.device  # copy device to experiment manager
+
+        if not resume:
+
+            # To save training and test metrics
+            self.train_history = History()
+            self.test_history = History()
+
+            # Random seed
+            set_rnd_seed(args.seed)
+
+            # Get starting time and date for logging
+            date_str = get_date_str()
+
+            # Print info
+            print('Device: {}, start time: {}'.format(self.device, date_str))
+
+            # Create folders for logs and results, save config
+            folder_str = date_str + '_' + experiment.run_description
+            result_folder = os.path.join('results', folder_str)
+            self.img_folder = os.path.join(result_folder, 'imgs')
+            self.checkpoint_folder = os.path.join('checkpoints', folder_str)
+            self.log_path = os.path.join(result_folder, 'log.pkl')
+            tboard_folder = os.path.join('tensorboard_logs', folder_str)
+            self.tb_writer = None
+            if not args.dry_run:
+                os.makedirs(result_folder)
+                os.makedirs(self.img_folder)
+                os.makedirs(self.checkpoint_folder)
+                if have_tensorboard:
+                    os.makedirs(tboard_folder)
+                    self.tb_writer = tensorboard.SummaryWriter(tboard_folder)
+                config_path = os.path.join(self.checkpoint_folder, 'config.pkl')
+                with open(config_path, 'wb') as fd:
+                    pickle.dump(args, fd)
+
+        viz.img_folder = self.img_folder
+        experiment.setup(self.checkpoint_folder if resume else None)
+
+        # Check everything is initialized properly
+        self._check_experiment(experiment)
+
+
+    def run(self):
+
+        # This is needed when loading to resume training
+        first_step = True
+
+        # Setup
+        e = self.experiment
+        train_loader = e.dataloaders.train
+        train_summarizers = SummarizerCollection(
+            mode='moving_average', ma_length=1000)
+        progress = None
+
+        # Training mode
+        e.model.train()
+
+        # Main loop
+        for epoch in range(1, e.args.max_epochs + 1):
+            for batch_idx, (x, y) in enumerate(train_loader):
+
+                step = e.model.global_step
+
+                if step >= e.args.max_steps:
+                    break
+
+                if step % e.args.test_log_interval == 0:
+
+                    # Test model (unless we just resumed training)
+                    if not first_step or step == 0:
+                        with torch.no_grad():
+                            self._test(epoch)
+
+                    # Save model checkpoint (unless we just started/resuming training)
+                    if not first_step and step % e.args.checkpoint_interval == 0:
+                        print("* saving model checkpoint at step {}".format(step))
+                        e.model.checkpoint(self.checkpoint_folder)
+
+                    # Restart progress bar
+                    progress = tqdm(total=e.args.test_log_interval, desc='train')
+
+                # Reset gradients
+                e.optimizer.zero_grad()
+
+                # Forward pass: get loss and other info
+                outputs = e.forward_pass(x, y)
+
+                # Compute gradients (backward pass)
+                outputs['loss'].backward()
+
+                e.post_backward_callback()
+
+                # Add batch metrics to summarizers
+                metrics_dict = e.get_metrics_dict(outputs)
+                train_summarizers.add(metrics_dict)
+
+                # Update progress bar
+                progress.update()
+
+                # Close progress bar if test occurs at next loop iteration
+                if (step + 1) % e.args.test_log_interval == 0:
+                    if progress is not None:
+                        progress.close()
+
+                if (step + 1) % e.args.log_interval == 0:
+                    # step+1 because we already did a forward/backward step
+
+                    # Get moving average of training metrics and reset summarizers
+                    summaries = train_summarizers.get_all(reset=True)
+
+                    # Print summaries
+                    print(e.train_log_str(summaries, step + 1, epoch))
+
+                    # Add train summaries (smoothed) to history and dump it to
+                    # file and to tensorboard if available
+                    self.train_history.add(summaries, step + 1)
+                    if not e.args.dry_run:
+                        with open(self.log_path, 'wb') as fd:
+                            pickle.dump(self._history_dict(), fd)
+                        if self.tb_writer is not None:
+                            for k, v in summaries.items():
+                                self.tb_writer.add_scalar('train_' + k, v, step + 1)
+
+                # Optimization step
+                e.optimizer.step()
+
+                # Increment model's global step variable
+                e.model.increment_global_step()
+
+                first_step = False
+
+            if step >= e.args.max_steps:
+                break
+
+        if progress is not None:
+            progress.close()
+        if step < e.args.max_steps:
+            print("Reached epochs limit ({} epochs)".format(e.args.max_epochs))
+        else:
+            print("Reached steps limit ({} steps)".format(e.args.max_steps))
+
+    def _test(self, epoch):
+        e = self.experiment
+        step = e.model.global_step
+
+        # Evaluation mode
+        e.model.eval()
+
+        # Get test results
+        summaries = e.test_procedure()
+
+        # Additional testing, possibly including saving images
+        e.additional_testing(self.img_folder)
+
+        # Print log string with test results (experiment-specific)
+        print(e.test_log_str(summaries, step, epoch))
+
+        # Save summaries to history
+        self.test_history.add(summaries, step)
+
+        if not e.args.dry_run:
+
+            # Save summaries to tensorboard
+            if self.tb_writer is not None:
+                for k, v in summaries.items():
+                    self.tb_writer.add_scalar('validation_' + k, v, step)
+
+            # Save history to file
+            with open(self.log_path, 'wb') as fd:
+                pickle.dump(self._history_dict(), fd)
+
+        # Training mode
+        e.model.train()
+
+
+    def _history_dict(self):
+        return {
+            'train': self.train_history.get_dict(),
+            'test': self.test_history.get_dict(),
+        }
+
+
+    @staticmethod
+    def _check_experiment(e):
+        attributes = [
+            e.device,
+            e.dataloaders,
+            e.model,
+            e.args,
+            e.run_description,
+            e.optimizer,
+        ]
+        assert not (None in attributes)
