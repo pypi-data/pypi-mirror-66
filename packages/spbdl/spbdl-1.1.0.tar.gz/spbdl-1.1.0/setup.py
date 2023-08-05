57a8d7adeff40cbc79b406d0981743d9775018dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:36 +0000

    Add package spbdl/1.1.0/spbdl-1.1.0.tar.gz

---
+++ packages/spbdl/spbdl-1.1.0.tar.gz/spbdl-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+  name = 'spbdl',
+  packages = ['spbdl'],
+  version = '1.1.0', 
+  license='MIT',
+  description = 'Download images from shitpostbot database',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Boidushya Bhattacharya',
+  author_email = 'boidushyabhattacharya@gmail.com', 
+  url = 'https://github.com/Boidushya/spbdl',
+  download_url = 'https://github.com/Boidushya/spbdl/archive/v_01.tar.gz', 
+  keywords = ['shitpostbot', 'spb', 'spbdl'], 
+  install_requires=[
+          'requests',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
