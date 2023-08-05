6e021ab306046947d3cab72b2690558af73f562f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:30 +0000

    Add package domdf-python-tools/0.2.0/domdf_python_tools-0.2.0-py3-none-any.whl

---
+++ packages/domdf-python-tools/domdf_python_tools-0.2.0-py3-none-any.whl/domdf_python_tools/__init__.py
@@ -0,0 +1,35 @@
+#  Compiled 2018-2019 by Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  terminal.py
+#  		Copyright © 2014-2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  		get_terminal_size, _get_terminal_size_windows, _get_terminal_size_tput and _get_terminal_size_linux
+# 			from https://gist.github.com/jtriley/1108174
+#  			Copyright © 2011 jtriley
+#
+#  paths.py
+# 		Copyright © 2018-2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  		copytree based on https://stackoverflow.com/a/12514470/3092681
+# 			Copyright © 2012 atzz
+# 			Licensed under CC-BY-SA
+#
+#  Parts of the docstrings based on the Python 3.8.2 Documentation
+#  Licensed under the Python Software Foundation License Version 2.
+#  Copyright © 2001-2020 Python Software Foundation. All rights reserved.
+#  Copyright © 2000 BeOpen.com . All rights reserved.
+#  Copyright © 1995-2000 Corporation for National Research Initiatives . All rights reserved.
+#  Copyright © 1991-1995 Stichting Mathematisch Centrum . All rights reserved.
+#
+
+from domdf_python_tools.utils import *
+from domdf_python_tools import paths, terminal, utils, doctools
+
+__all__ = ["paths", "terminal", "utils", "dates"]
+
+__author__ = "Dominic Davis-Foster"
+__copyright__ = "2014-2019 Dominic Davis-Foster"
+
+__license__ = "LGPLv3+"
+__version__ = "0.2.0"
+__email__ = "dominic@davis-foster.co.uk"
