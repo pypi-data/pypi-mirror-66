739e555ffb4e67a2ea906a8b81af342aad6df278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:46 +0000

    Add package startpkg/0.1.9/startpkg-0.1.9.tar.gz

---
+++ packages/startpkg/startpkg-0.1.9.tar.gz/startpkg-0.1.9/src/templates/__version__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#                            _                _____
+#      /\                   | |       /\     |_   _|
+#     /  \     ____   __ _  | |_     /  \      | |
+#    / /\ \   |_  /  / _` | | __|   / /\ \     | |
+#   / ____ \   / /  | (_| | | |_   / ____ \   _| |_
+#  /_/    \_\ /___|  \__,_|  \__| /_/    \_\ |_____|
+#
+#
+
+__title__ = "{{ pkg_name }}"
+__description__ = "{{ pkg_description }}"
+__url__ = "{{ pkg_url }}"
+__version__ = "{{ pkg_version }}"
+__author__ = "{{author}}"
+__author_email__ = "{{author_email}}"
+__license__ = "{{license}}"
+__copyright__ = "{{copyright}}"
