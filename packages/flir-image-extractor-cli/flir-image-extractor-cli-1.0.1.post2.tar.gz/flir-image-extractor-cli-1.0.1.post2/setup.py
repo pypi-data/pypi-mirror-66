9dd0ee691d246196a16fc16a5002c0ac32811fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:47 +0000

    Add package flir-image-extractor-cli/1.0.1.post2/flir-image-extractor-cli-1.0.1.post2.tar.gz

---
+++ packages/flir-image-extractor-cli/flir-image-extractor-cli-1.0.1.post2.tar.gz/flir-image-extractor-cli-1.0.1.post2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flir-image-extractor-cli",
+    version="1.0.1.post2",
+    author="National Drones",
+    author_email="development@nationaldrones.com",
+    description="A cli-tool to get thermal information out of FLIR radiometric JPGs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/nationaldronesau/FlirImageExtractor",
+    packages=["flir_image_extractor_cli"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "flir-image-extractor-cli=flir_image_extractor_cli.__main__:main"
+        ]
+    },
+    install_requires=["numpy", "pillow", "matplotlib", "loguru"],
+)
