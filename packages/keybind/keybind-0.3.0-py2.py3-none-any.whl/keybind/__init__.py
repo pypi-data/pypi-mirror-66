59defa175ad196f84d133e0842afc3b07e6f846b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:11 +0000

    Add package keybind/0.3.0/keybind-0.3.0-py2.py3-none-any.whl

---
+++ packages/keybind/keybind-0.3.0-py2.py3-none-any.whl/keybind/__init__.py
@@ -0,0 +1,8 @@
+from .binder import KeyBinder, configure_logging
+
+
+VERSION = (0, 3, 0)
+"""Application version number tuple."""
+
+VERSION_STR = '.'.join(map(str, VERSION))
+"""Application version number string."""
