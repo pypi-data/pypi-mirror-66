1e4b1681294d37399dd704f280f5bbe598dbd3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package amlnx-adapter/0.0.0.4.2/amlnx_adapter-0.0.0.4.2.tar.gz

---
+++ packages/amlnx-adapter/amlnx_adapter-0.0.0.4.2.tar.gz/amlnx_adapter-0.0.0.4.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+# The directory containing this file
+#HERE = pathlib.Path(__file__).resolve().parents[1]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# This call to setup() does all the work
+setup(
+    name="amlnx_adapter",
+    version="0.0.0.4.2",
+    description="package for creating networkx adapters for arangodb",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arangoml/arangopipe",
+    author="ArangoDB",
+    author_email="rajiv@arangodb.com",
+    license="Apache",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"],
+    packages=["amlnx_adapter", "amlnx_adapter.dgl", "amlnx_adapter.node2vec"],
+    package_data={'amlnx_adapter.dgl': ['amlnx_adapter/dgl/cert/ca-b9b556df.crt'], 'amlnx_adapter':['amlnx_adapter/cert/ca-b9b556df.crt']},
+    include_package_data=True )
+
+
