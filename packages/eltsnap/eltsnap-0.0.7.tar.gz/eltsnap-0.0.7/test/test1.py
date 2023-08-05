49439709cfe4538bc3eb1b8691bf2dd5bdf05d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package eltsnap/0.0.7/eltsnap-0.0.7.tar.gz

---
+++ packages/eltsnap/eltsnap-0.0.7.tar.gz/eltsnap-0.0.7/test/test1.py
@@ -0,0 +1,7 @@
+from eltsnap.eltPackage import *
+create_execute_process_package('DESKTOP-D7VB61Q\SQLEXPRESS', 'eltsnap_v2',
+'kokos2df421545',
+'executable expression',
+'arguments expession',
+' default',
+' place values in ELT_Data default(0)')
