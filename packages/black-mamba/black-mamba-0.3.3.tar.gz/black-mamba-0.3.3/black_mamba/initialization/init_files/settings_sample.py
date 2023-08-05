c9c10b500631444a7e7692fc78226ca844bec2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package black-mamba/0.3.3/black-mamba-0.3.3.tar.gz

---
+++ packages/black-mamba/black-mamba-0.3.3.tar.gz/black-mamba-0.3.3/black_mamba/initialization/init_files/settings_sample.py
@@ -0,0 +1,33 @@
+networks = {
+    "development": {
+        "mode": "HTTP",
+        "host": "localhost",
+        "port": 7545,
+        "network_id": "*"
+    }
+
+    #"development": {
+    #    "mode": "IPC",
+    #    "url": "/home/sarahconnor/ethereum/data/geth.ipc"
+    #}
+
+    #"development": {
+    #    "mode": "Websocket",
+    #    "host": "localhost",
+    #    "port": 8546,
+    #    "network_id": "*"
+    #}
+
+    #"development": {
+    #    "mode": "IPC",
+    #    "url": "/home/sarahconnor/ethereum/data/geth.ipc"
+    #}
+
+    #"development": {
+    #    "mode": "Infura",
+    #    "project_id": "FILL_IT_WITH_PROJECT_ID",
+    #    "api_secret": "FILL_IT_WITH_API_SECRET",
+    #    "scheme": "https / wss",
+    #    "endpoints": "mainnet / ropsten / goerly / rinkeby / kovan"
+    #}
+}
