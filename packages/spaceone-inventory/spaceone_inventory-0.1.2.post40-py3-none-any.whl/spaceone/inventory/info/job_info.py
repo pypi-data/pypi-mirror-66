ef2b2322817cf2a323905ba9572292b9dadd6327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package spaceone-inventory/0.1.2.post40/spaceone_inventory-0.1.2.post40-py3-none-any.whl

---
+++ packages/spaceone-inventory/spaceone_inventory-0.1.2.post40-py3-none-any.whl/spaceone/inventory/info/job_info.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import functools
+
+from spaceone.api.inventory.v1 import collector_pb2, job_pb2
+from spaceone.core.pygrpc.message_type import *
+from spaceone.inventory.model.job_model import Job
+from spaceone.inventory.info.collector_info import CollectorInfo
+
+__all__ = ['JobInfo', 'JobsInfo']
+
+
+def JobInfo(job_vo: Job, minimal=False):
+    info = {
+        'job_id': job_vo.job_id,
+        'created_at': change_timestamp_type(job_vo.created_at),
+        'finished_at': change_timestamp_type(job_vo.finished_at),
+        'state': job_vo.state
+    }
+
+    if not minimal:
+        info.update({
+            'collector_info': CollectorInfo(job_vo.collector, minimal=True)
+        })
+
+    return collector_pb2.JobInfo(**info)
+
+def JobsInfo(vos, total_count, **kwargs):
+    return job_pb2.JobsInfo(results=list(map(functools.partial(JobInfo, **kwargs), vos)), total_count=total_count) 
