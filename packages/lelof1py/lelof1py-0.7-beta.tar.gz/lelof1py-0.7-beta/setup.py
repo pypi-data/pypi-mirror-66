6236d3eba34ca32049234cadf11d2c03988cece8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:03 +0000

    Add package lelof1py/0.7-beta/lelof1py-0.7-beta.tar.gz

---
+++ packages/lelof1py/lelof1py-0.7-beta.tar.gz/lelof1py-0.7-beta/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+setup(
+	name = 'lelof1py',
+	packages = ['lelof1py'],
+	version = '0.7-beta',
+	license = 'MIT',
+	description = 'Python client for LELO F1 SDK device',
+    long_description = 'See https://github.com/fabiofenoglio/lelo-f1-python-sdk',
+	author = 'Fabio Fenoglio',
+	author_email = 'development@fabiofenoglio.it',
+	url = 'https://github.com/fabiofenoglio/lelo-f1-python-sdk',
+	download_url = 'https://github.com/fabiofenoglio/lelo-f1-python-sdk/archive/v0.7-beta.tar.gz',
+	keywords = ['LELO', 'BLUETOOTH', 'REMOTE'],
+	install_requires = [
+		'asyncio',
+		'bleak',
+        'appdirs'
+	],
+	classifiers = [
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Build Tools',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+)
