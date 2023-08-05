75b4cf40b8cdb291793db8e205e9a2572328b194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:04 +0000

    Add package buttercms-python/1.2/buttercms-python-1.2.tar.gz

---
+++ packages/buttercms-python/buttercms-python-1.2.tar.gz/buttercms-python-1.2/butter_cms/client.py
@@ -0,0 +1,37 @@
+import requests
+
+from .__version__ import __version__
+
+
+class Client(object):
+    """Client"""
+    def __init__(self, auth_token):
+        self.auth_token = auth_token
+        self.url = 'https://api.buttercms.com/v2/'
+
+    def api_get(self, slug='', params=None, path_override=None):
+        payload = {
+            'auth_token': self.auth_token,
+        }
+        if params:
+            payload.update(params)
+
+        headers = {
+            'X-Butter-Client': 'Python/{}'.format(__version__),
+            'Accept-Encoding': 'gzip',
+        }
+
+        if path_override:
+            path = path_override
+        else:
+            path = self.path
+
+        response = requests.get(
+            url=self.url + path + str(slug),
+            params=payload,
+            headers=headers,
+        )
+        return response.json()
+
+    def get(self, slug='', params=None):
+        return self.api_get(slug=slug, params=params)
---
+++ packages/buttercms-python/buttercms-python-1.2.tar.gz/buttercms-python-1.2/setup.py
@@ -0,0 +1,51 @@
+import sys
+
+from butter_cms.__version__ import __version__
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+
+install_requires = []
+if sys.version_info < (2, 7, 9):
+    raise Exception('ButterCMS uses the requests library to securely talk to https://buttercms.com '
+        'which requires Python 2.7.9 or later.\n'
+        'Please take a few seconds to upgrade to Python 2.7.9 and try again.\n'
+        'https://www.python.org/downloads/')
+    # TODO: Add support for < Python 2.7.9
+    # install_requires.append('requests[security]')
+else:
+    install_requires.append('requests')
+
+
+setup(
+    name = 'buttercms-python',
+    packages = ['butter_cms'],
+    version = __version__,
+    description = 'API First Blogging and CMS platform built for developers',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author = 'Adam Yala',
+    author_email = 'adam@adamyala.com',
+    url = 'https://github.com/buttercms/buttercms-python',
+    download_url = 'https://github.com/buttercms/buttercms-python/tarball/0.1',
+    py_modules=['butter_cms'],
+    install_requires=install_requires,
+    keywords = ['foo', 'bar', 'baz'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
