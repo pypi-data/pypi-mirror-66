8c8ef209a10c59fb3a24093386619f349b71f741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package jadn/0.5.0b1/jadn-0.5.0b1.tar.gz

---
+++ packages/jadn/jadn-0.5.0b1.tar.gz/jadn-0.5.0b1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='jadn',
+    version='0.5.0b1',
+    author='David Kemp',
+    author_email='dk190a@gmail.com',
+    description='JADN schema tools',
+    long_description='long_description',
+    long_description_content_type='text/x-rst',
+    url='https://github.com/davaya/jadn-software',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    python_requires='>=3.4',
+    install_requires=['jsonschema'],
+)
