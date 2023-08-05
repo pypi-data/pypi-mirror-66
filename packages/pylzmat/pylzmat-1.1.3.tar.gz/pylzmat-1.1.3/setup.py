b2e994ff6c91e1198a382ce3eb8b76bebf5386ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:50 +0000

    Add package pylzmat/1.1.3/pylzmat-1.1.3.tar.gz

---
+++ packages/pylzmat/pylzmat-1.1.3.tar.gz/pylzmat-1.1.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+VERSION = open('VERSION', 'r').read().strip()
+
+setup(
+    name='pylzmat',
+    version=VERSION,
+    description='lzmat bindings.',
+    author='Marcos Agüero',
+    author_email='wiredrat@gmail.com',
+    url='https://github.com/wiredrat/pylzmat',
+    long_description='''
+        Bindings for lzmat library (http://www.matcode.com/lzmat.htm).
+''',
+    license="GNU GPLv2",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    py_modules=['pylzmat'],
+    setup_requires=["cffi>1.0.0"],
+    cffi_modules=["pylzmat_build.py:ffibuilder"],
+    install_requires=["cffi>1.0.0"],
+    test_suite="test"
+)
