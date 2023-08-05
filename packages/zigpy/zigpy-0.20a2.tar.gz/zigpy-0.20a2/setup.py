cb1927b17c7f025d3193187971b06e8440b1014a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package zigpy/0.20a2/zigpy-0.20a2.tar.gz

---
+++ packages/zigpy/zigpy-0.20a2.tar.gz/zigpy-0.20a2/setup.py
@@ -0,0 +1,17 @@
+"""Setup module for zigpy"""
+
+from setuptools import find_packages, setup
+import zigpy
+
+setup(
+    name="zigpy",
+    version=zigpy.__version__,
+    description="Library implementing a ZigBee stack",
+    url="http://github.com/zigpy/zigpy",
+    author="Russell Cloran",
+    author_email="rcloran@gmail.com",
+    license="GPL-3.0",
+    packages=find_packages(exclude=["*.tests"]),
+    install_requires=["aiohttp", "crccheck", "pycryptodome", "voluptuous"],
+    tests_require=["asynctest", "pytest"],
+)
