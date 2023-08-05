25a9c98a11c0e77a0d26546c6d064dc0efa7b3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:27 +0000

    Add package meta1/0.2.9/meta1-0.2.9.tar.gz

---
+++ packages/meta1/meta1-0.2.9.tar.gz/meta1-0.2.9/meta1base/chains.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+known_chains = {
+    "META1": {
+        "chain_id": "3dbdca4078e2988c2a31202e2aa3cbeae7a8aa755a31e0f49d6f4b2a262e9c3e",
+        "core_symbol": "META1",
+        "prefix": "META1",
+    },
+    "CTS": {
+        "chain_id": "53952fc10d52a3af6d572a298e8f99a0a2e7afd0f7dbad354d8d65e8c36c962d",
+        "core_symbol": "CTS",
+        "prefix": "CTS",
+    },
+    "TEST1": {
+        "chain_id": "713bae54009ab0a8da9b67aac7e97052428fd210b9c3afb521bfad6a15f1da6f",
+        "core_symbol": "TEST1",
+        "prefix": "TEST1",
+    },
+}
