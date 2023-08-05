5136817b178ad0c36cc9b693e40c15d5eabd3ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:06 +0000

    Add package mmc-distributions/0.1/mmc_distributions-0.1.tar.gz

---
+++ packages/mmc-distributions/mmc_distributions-0.1.tar.gz/mmc_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mmc_distributions',
+      version='0.1',
+      description='Probability distributions',
+      packages=['mmc_distributions'],
+      author = 'Michael McMahon',
+      author_email = 'michael-mcmahon94@hotmail.com',
+      zip_safe=False)
