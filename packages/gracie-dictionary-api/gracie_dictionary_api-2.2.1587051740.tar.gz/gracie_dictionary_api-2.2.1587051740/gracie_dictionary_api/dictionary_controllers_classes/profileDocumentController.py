7f2fabe70e5dadc10e64f19e92919f19e0dcdcb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:36 +0000

    Add package gracie-dictionary-api/2.2.1587051740/gracie_dictionary_api-2.2.1587051740.tar.gz

---
+++ packages/gracie-dictionary-api/gracie_dictionary_api-2.2.1587051740.tar.gz/gracie_dictionary_api-2.2.1587051740/gracie_dictionary_api/dictionary_controllers_classes/profileDocumentController.py
@@ -0,0 +1,54 @@
+from gracie_dictionary_api import GracieBaseAPI
+
+
+class profileDocumentController(GracieBaseAPI):
+    """Profile-documents"""
+
+    _controller_name = "profileDocumentController"
+
+    def list(self, profileClassId, **kwargs):
+        """
+
+        Args:
+            languageId: (string): * - request documents in all languages.
+            limit: (integer): limit
+            offset: (integer): offset
+            orderAsc: (boolean): orderAsc
+            orderBy: (string): orderBy
+            profileClassId: (string): profileClassId
+
+        Consumes:
+            application/json
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'languageId': {'name': 'languageId', 'required': False, 'in': 'query'}, 'limit': {'name': 'limit', 'required': False, 'in': 'query'}, 'offset': {'name': 'offset', 'required': False, 'in': 'query'}, 'orderAsc': {'name': 'orderAsc', 'required': False, 'in': 'query'}, 'orderBy': {'name': 'orderBy', 'required': False, 'in': 'query'}, 'profileClassId': {'name': 'profileClassId', 'required': True, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/profileDocument/list'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def retrieve(self, id):
+        """
+
+        Args:
+            id: (string): id
+
+        Consumes:
+            application/json
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'id': {'name': 'id', 'required': True, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/profileDocument/retrieve'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
