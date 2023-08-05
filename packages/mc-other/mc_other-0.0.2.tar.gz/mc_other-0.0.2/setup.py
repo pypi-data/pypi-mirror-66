b36466c371984d07b82f89f39764fdeec48a0ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:56 +0000

    Add package mc-other/0.0.2/mc_other-0.0.2.tar.gz

---
+++ packages/mc-other/mc_other-0.0.2.tar.gz/mc_other-0.0.2/setup.py
@@ -0,0 +1,12 @@
+﻿from setuptools import setup
+
+setup(
+    name='mc_other',
+    version='0.0.2',
+    author='Hugn',
+    author_email='wang1183478375@outlook.com',
+    url ='https://www.coding4fun.com.cn/',
+    data_files=[('',['mc_other.py'])],
+    include_package_data = True, 
+    zip_safe=False,
+    )
