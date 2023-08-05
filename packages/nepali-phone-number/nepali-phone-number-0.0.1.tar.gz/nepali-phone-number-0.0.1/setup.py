23ebea3c1e86b088b5ccbe6ed5e09a9e7e79f839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:39 +0000

    Add package nepali-phone-number/0.0.1/nepali-phone-number-0.0.1.tar.gz

---
+++ packages/nepali-phone-number/nepali-phone-number-0.0.1.tar.gz/nepali-phone-number-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nepali-phone-number",
+    version="0.0.1",
+    author="Bishnu Sharma",
+    author_email="sbishnu019@gmail.com",
+    description="A package for nepali mobile number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sbishnu019/nepali-phone-number",
+    download_url='https://github.com/sbishnu019/nepali-phone-number/archive/v0.0.1.tar.gz',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
