95d9f7a979700de3befec1b271292fa215334e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package varsom-avalanche-client/1.0.0/varsom_avalanche_client-1.0.0-py3-none-any.whl

---
+++ packages/varsom-avalanche-client/varsom_avalanche_client-1.0.0-py3-none-any.whl/varsom_avalanche_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from varsom_avalanche_client.api.archive_api import ArchiveApi
+from varsom_avalanche_client.api.avalanche_warning_by_coordinates_api import AvalancheWarningByCoordinatesApi
+from varsom_avalanche_client.api.avalanche_warning_by_region_api import AvalancheWarningByRegionApi
+from varsom_avalanche_client.api.cap_api import CapApi
+from varsom_avalanche_client.api.region_api import RegionApi
+from varsom_avalanche_client.api.region_summary_api import RegionSummaryApi
+from varsom_avalanche_client.api.warning_api import WarningApi
