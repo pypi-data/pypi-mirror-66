7828765214c135081dd2ded9df50e3e46638fbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:21 +0000

    Add package yinasrun/0.1.1/yinasrun-0.1.1.tar.gz

---
+++ packages/yinasrun/yinasrun-0.1.1.tar.gz/yinasrun-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yinasrun']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fire>=0.3.1,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['yin = yinasrun:run']}
+
+setup_kwargs = {
+    'name': 'yinasrun',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'italian roast',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
