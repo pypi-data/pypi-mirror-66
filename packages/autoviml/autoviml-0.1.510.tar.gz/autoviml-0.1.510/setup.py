58edd4aec15a53d235741bf403aea54b4938b5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package autoviml/0.1.510/autoviml-0.1.510.tar.gz

---
+++ packages/autoviml/autoviml-0.1.510.tar.gz/autoviml-0.1.510/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autoviml",
+    version="0.1.510",
+    author="Ram Seshadri",
+    # author_email="author@example.com",
+    description="Automatically Build Variant Interpretable ML models fast - now with CatBoost!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    url="https://github.com/AutoViML/Auto_ViML",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+        "ipython",
+        "jupyter",
+        "xgboost",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn",
+        "catboost"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
