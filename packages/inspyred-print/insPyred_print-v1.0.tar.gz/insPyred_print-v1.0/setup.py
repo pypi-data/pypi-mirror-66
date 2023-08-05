ae88331c71e0f0b2b3d4003388b49e0db6f70264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:49 +0000

    Add package inspyred-print/1.0/insPyred_print-v1.0.tar.gz

---
+++ packages/inspyred-print/insPyred_print-v1.0.tar.gz/insPyred_print-v1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='insPyred_print',
+    version='v1.0',
+    packages=['inspyred_print'],
+    url='https://github.com/Inspyre-Softworks/inspyred_print',
+    license='WTFYW',
+    author='Taylor-Jayde J. Blackstone',
+    author_email='t.blackstone@inspyre.tech',
+    description='A very simple module containing some color codes for line printing in Python'
+)
