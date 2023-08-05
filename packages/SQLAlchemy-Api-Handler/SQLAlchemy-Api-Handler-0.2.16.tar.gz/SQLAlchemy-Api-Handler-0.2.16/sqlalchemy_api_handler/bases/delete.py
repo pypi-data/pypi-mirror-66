574a0856064d2661e62ed8d97a0f93eef1ad6d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:58 +0000

    Add package SQLAlchemy-Api-Handler/0.2.16/SQLAlchemy-Api-Handler-0.2.16.tar.gz

---
+++ packages/SQLAlchemy-Api-Handler/SQLAlchemy-Api-Handler-0.2.16.tar.gz/SQLAlchemy-Api-Handler-0.2.16/sqlalchemy_api_handler/bases/delete.py
@@ -0,0 +1,16 @@
+from sqlalchemy_api_handler.bases.accessor import Accessor
+
+class DeletedRecordException(Exception):
+    pass
+
+
+class Delete(Accessor):
+
+    def delete(self):
+        Accessor.get_db().session.delete(self)
+        return self
+
+    @staticmethod
+    def delete_objects(*objects):
+        delete_objects = list(map(Delete.delete, objects))
+        return delete_objects
