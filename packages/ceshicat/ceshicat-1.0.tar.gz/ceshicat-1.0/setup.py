4cbc99068300aaa231bdab35b2439a7690ce3737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:13 +0000

    Add package ceshicat/1.0/ceshicat-1.0.tar.gz

---
+++ packages/ceshicat/ceshicat-1.0.tar.gz/ceshicat-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'ceshicat',
+    version= '1.0',
+    description='11',
+    author='ca',
+    author_email='11@163.com',
+    py_modules=['SuperMath.demo1']
+
+)
