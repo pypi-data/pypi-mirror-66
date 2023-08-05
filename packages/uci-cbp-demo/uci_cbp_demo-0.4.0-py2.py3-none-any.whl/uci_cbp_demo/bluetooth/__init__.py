b61c13ac285807794b009c97438427632d98b957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package uci-cbp-demo/0.4.0/uci_cbp_demo-0.4.0-py2.py3-none-any.whl

---
+++ packages/uci-cbp-demo/uci_cbp_demo-0.4.0-py2.py3-none-any.whl/uci_cbp_demo/bluetooth/__init__.py
@@ -0,0 +1,7 @@
+#  MIT License
+#  Copyright (C) Michael Tao-Yi Lee (taoyil AT UCI EDU)
+
+from .callbacks import CapData
+from .list_characteristics import list_char
+from .scan import scan, check_adaptor
+from .sensorBoard import SensorBoard
