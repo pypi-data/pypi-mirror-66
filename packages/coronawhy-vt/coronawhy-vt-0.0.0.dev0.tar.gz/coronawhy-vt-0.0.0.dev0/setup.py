9a8bf5fb746a788f7ef2cfa49e5031f0ee874920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:21 +0000

    Add package coronawhy-vt/0.0.0.dev0/coronawhy-vt-0.0.0.dev0.tar.gz

---
+++ packages/coronawhy-vt/coronawhy-vt-0.0.0.dev0.tar.gz/coronawhy-vt-0.0.0.dev0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ],
+    name='coronawhy-vt',
+    version='0.0.0.dev0',
+    zip_safe=False,
+)
