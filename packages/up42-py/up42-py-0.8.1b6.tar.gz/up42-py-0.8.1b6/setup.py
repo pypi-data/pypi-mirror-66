bfc05b8f4079b18dc4ff4d87e9be5728d60e8f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:36 +0000

    Add package up42-py/0.8.1b6/up42-py-0.8.1b6.tar.gz

---
+++ packages/up42-py/up42-py-0.8.1b6.tar.gz/up42-py-0.8.1b6/setup.py
@@ -0,0 +1,29 @@
+import io
+from pathlib import Path
+from setuptools import setup, find_packages
+
+parent_dir = Path(__file__).resolve().parent
+
+version = io.open(parent_dir / "up42/_version.txt", encoding="utf-8").read()
+
+
+setup(
+    name="up42-py",
+    version=version,
+    description="Python SDK for UP42",
+    long_description=Path(parent_dir / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/up42/up42-py",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs", "examples")),
+    package_data={
+        "": ["_version.txt", "data/aoi_berlin.geojson", "data/aoi_washington.geojson"]
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=Path(parent_dir / "requirements.txt").read_text().splitlines(),
+    entry_points="""
+        [console_scripts]
+        up42=up42.cli:main
+    """,
+)
