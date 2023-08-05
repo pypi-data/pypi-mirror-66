346b65813693868d188e35007c0a0fd1af4c9455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package inline-importer/0.0.1/inline_importer-0.0.1-py3-none-any.whl

---
+++ packages/inline-importer/inline_importer-0.0.1-py3-none-any.whl/inline_importer/__init__.py
@@ -0,0 +1,9 @@
+"""
+The inliner package assembles the various components of the InlineImporter library.
+
+The inline_importer.inline_importer module should be kept clean, as it will be inlined with any script produced by this.
+"""
+
+from inline_importer._version import __version__
+
+__all__ = ['__version__']
