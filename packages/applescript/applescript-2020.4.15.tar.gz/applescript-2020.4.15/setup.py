67cd937d8d994771973109f93a6f847b802eb38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package applescript/2020.4.15/applescript-2020.4.15.tar.gz

---
+++ packages/applescript/applescript-2020.4.15.tar.gz/applescript-2020.4.15/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='applescript',
+    version='2020.4.15',
+    install_requires=[
+        'only',
+        'public',
+        'setuptools',
+    ],
+    packages=[
+        'applescript',
+    ],
+)
