e0eeedffa8895238a0ca8489551209beb7b4b0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:13 +0000

    Add package surk/0.0.40/surk-0.0.40.tar.gz

---
+++ packages/surk/surk-0.0.40.tar.gz/surk-0.0.40/setup.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='surk',
+    version='0.0.40',
+    description='Surk dilini derleyip çalıştırmak üzere hazırlanıp dağıtılan program.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Emre ŞURK',
+    author_email='emre.surk@gmail.com',
+    packages=setuptools.find_packages(),
+    scripts=['bin/surk.py'],
+    # scripts=['surk/__main__.py'],
+    install_requires=[],  # external packages as dependencies
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+# pip3 uninstall surk && pip3 install surk && surk -y
+
+# pip3 uninstall surk && python3 setup.py sdist bdist_wheel && twine upload dist/* && pip3 install surk && surk -y
+
+# python3 setup.py sdist && twine upload dist/*
+# pip3 uninstall surk && pip3 install surk && surk -y
