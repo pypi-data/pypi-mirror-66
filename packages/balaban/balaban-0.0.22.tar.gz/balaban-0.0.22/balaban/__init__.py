a5f7a0115a10b9ffa7b3cb3b1b2d0c65aa0ab2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:25 +0000

    Add package balaban/0.0.22/balaban-0.0.22.tar.gz

---
+++ packages/balaban/balaban-0.0.22.tar.gz/balaban-0.0.22/balaban/__init__.py
@@ -0,0 +1,2 @@
+from balaban.balaban import bosko
+from balaban.balaban import scrape_top_five_leagues
---
+++ packages/balaban/balaban-0.0.22.tar.gz/balaban-0.0.22/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="balaban",
+    version="0.0.22",
+    author="Will Thomson",
+    author_email="willthomson1991@gmail.com",
+    description="Bayesian hierarchical models for football",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anenglishgoat/balaban",
+    packages=setuptools.find_packages(),
+    install_requires=['importlib.resources', 'numpy', 'pymc3', 'pandas', 'matplotlib','selenium'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.2',
+)
