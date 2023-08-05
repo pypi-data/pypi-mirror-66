ff4bf7e64e8ab0eb753351b4b52c8e72ff1ee09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package pydaikin/1.6.3/pydaikin-1.6.3.tar.gz

---
+++ packages/pydaikin/pydaikin-1.6.3.tar.gz/pydaikin-1.6.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='pydaikin',
+    version='1.6.3',
+    description='Python Daikin HVAC appliances interface',
+    author='Yari Adan',
+    author_email='mustang@yadan.org',
+    license='GPL',
+    url='https://bitbucket.org/mustang51/pydaikin',
+    python_requires='>3.5',
+    packages=['pydaikin'],
+    keywords=['homeautomation', 'daikin'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Home Automation",
+    ],
+    install_requires=['netifaces', 'aiohttp'],
+    scripts=['bin/pydaikin'],
+)
