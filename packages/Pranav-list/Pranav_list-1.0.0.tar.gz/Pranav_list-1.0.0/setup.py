4663c99d7433f05d60f597733ddb52546050d878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package Pranav-list/1.0.0/Pranav_list-1.0.0.tar.gz

---
+++ packages/Pranav-list/Pranav_list-1.0.0.tar.gz/Pranav_list-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Pranav_list',
+    version='1.0.0',
+    py_modules=['Pranav_list'],
+    author='Alphapython',
+    author_email='pranav.jenus@gmail.com'
+    )
