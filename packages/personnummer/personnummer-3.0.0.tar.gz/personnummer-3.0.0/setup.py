fd600b59f53c6e208702b8e8aa67f4ee2739e5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:42 +0000

    Add package personnummer/3.0.0/personnummer-3.0.0.tar.gz

---
+++ packages/personnummer/personnummer-3.0.0.tar.gz/personnummer-3.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+      name='personnummer',
+      version='3.0.0',
+      description='Validate Swedish social security numbers',
+      url='http://github.com/personnummer/python',
+      author='Personnummer and Contributors',
+      author_email='fredrik.forsmo+personnummer@gmail.com',
+      license='MIT',
+      packages=['personnummer'],
+      test_suite='nose.collector',
+      tests_require=['nose', 'mock'],
+)
