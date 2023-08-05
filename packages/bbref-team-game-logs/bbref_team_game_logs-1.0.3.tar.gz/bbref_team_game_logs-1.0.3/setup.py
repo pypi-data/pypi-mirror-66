770f98424333488a2cf1ce20020a6a4a6c2af545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:41 +0000

    Add package bbref-team-game-logs/1.0.3/bbref_team_game_logs-1.0.3.tar.gz

---
+++ packages/bbref-team-game-logs/bbref_team_game_logs-1.0.3.tar.gz/bbref_team_game_logs-1.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bbref_team_game_logs",
+    version="1.0.3",
+    author="Siddharth Mehta",
+    author_email="siddharthm2350@gmail.com",
+    description="Retrieves a team's regular season game log and stats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sherpan/bbref_team_game_logs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'beautifulsoup4==4.8.2',
+        'pandas==0.25.3',
+        'requests==2.22.0',
+        'lxml==4.4.2'
+    ],
+)
