a0ae1e15b6c4b12f24b80dd66e34ee32bf64b26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package pytest-salt-factories/0.9.9/pytest_salt_factories-0.9.9-py2.py3-none-any.whl

---
+++ packages/pytest-salt-factories/pytest_salt_factories-0.9.9-py2.py3-none-any.whl/tests/integration/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+tests.integration.conftest
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# from saltfactories.plugin import *  # pylint: disable=wildcard-import,unused-wildcard-import
