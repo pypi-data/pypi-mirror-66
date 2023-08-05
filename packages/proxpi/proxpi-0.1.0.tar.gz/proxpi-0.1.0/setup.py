9b456b383249bba0656e256aa16634da0c8b7cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:06 +0000

    Add package proxpi/0.1.0/proxpi-0.1.0.tar.gz

---
+++ packages/proxpi/proxpi-0.1.0.tar.gz/proxpi-0.1.0/setup.py
@@ -0,0 +1,5 @@
+"""Install script for ``proxpi``."""
+
+import setuptools
+
+setuptools.setup()
---
+++ packages/proxpi/proxpi-0.1.0.tar.gz/proxpi-0.1.0/src/proxpi/server.py
@@ -0,0 +1,85 @@
+"""Cached package index server."""
+
+import sys
+import logging
+from urllib import parse as urllib_parse
+
+import flask
+import jinja2
+
+from . import _cache
+
+fmt = "%(asctime)s [%(levelname)8s] %(name)s: %(message)s"
+try:
+    import coloredlogs
+except ImportError:  # pragma: no cover
+    logging.basicConfig(level=logging.INFO, format=fmt)
+else:  # pragma: no cover
+    coloredlogs.install(
+        level=logging.INFO,
+        fmt=fmt,
+        field_styles={
+            "asctime": {"faint": True, "color": "white"},
+            "levelname": {"bold": True, "color": "blue"},
+            "name": {"bold": True, "color": "yellow"},
+        },
+    )
+logger = logging.getLogger(__name__)
+
+app = flask.Flask("proxpi")
+app.jinja_loader = jinja2.PackageLoader("proxpi")
+cache = _cache.Cache.from_config()
+if "--help" not in sys.argv:
+    _cache_init_thread = _cache.Thread(target=cache.list_packages)
+    _cache_init_thread.start()
+    app.before_first_request_funcs.append(_cache_init_thread.join)
+if app.debug or app.testing:
+    logging.root.setLevel(logging.DEBUG)
+    for handler in logging.root.handlers:
+        if handler.level > logging.DEBUG:
+            handler.level = logging.DEBUG
+logger.info("Cache: %r", cache)
+
+
+@app.route("/index/")
+def list_packages():
+    """List all packages in index(es)."""
+    package_names = cache.list_packages()
+    return flask.render_template("packages.html", package_names=package_names)
+
+
+@app.route("/index/<package_name>/")
+def list_files(package_name: str):
+    """List all files for a package."""
+    try:
+        files = cache.list_files(package_name)
+    except _cache.NotFound:
+        flask.abort(404)
+    return flask.render_template("files.html", package_name=package_name, files=files)
+
+
+@app.route("/index/<package_name>/<file_name>")
+def get_file(package_name: str, file_name: str):
+    """Download a file."""
+    try:
+        path = cache.get_file(package_name, file_name)
+    except _cache.NotFound:
+        flask.abort(404)
+    scheme = urllib_parse.urlparse(path).scheme
+    if scheme and scheme != "file":
+        return flask.redirect(path)
+    return flask.send_file(path)
+
+
+@app.route("/cache/list", methods=["DELETE"])
+def invalidate_list():
+    """Invalidate package list cache."""
+    cache.invalidate_list()
+    return {"status": "success", "data": None}
+
+
+@app.route("/cache/<package_name>", methods=["DELETE"])
+def invalidate_package(package_name):
+    """Invalidate package file list cache."""
+    cache.invalidate_package(package_name)
+    return {"status": "success", "data": None}
