9a082079669e8e6808026dd08c7e92b55221ae1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:44 +0000

    Add package vra8testlib/3/vra8testlib-3.tar.gz

---
+++ packages/vra8testlib/vra8testlib-3.tar.gz/vra8testlib-3/vra8testlib/vra8testlib.py
@@ -0,0 +1,24 @@
+import logging
+import argparse
+import sys
+
+
+#parser = argparse.ArgumentParser(description='Write to the pipeline log')
+#parser.add_argument('--logpath', required=True, type=str, help='Path of the file to log to')
+#parser.add_argument('-m', '--message', required=True, type=str, help='Message to log')
+#args = parser.parse_args()
+
+
+
+class JenkinsLogger:
+    def __init__(self):
+        self.logger = logging.getLogger(__file__)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s|%(name)s|%(levelname)s ****  %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def send(self, message):
+        self.logger.info(message + 'final final final version')
