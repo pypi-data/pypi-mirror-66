6ae46cc582d8480b60eb32ee5fcdcef59d826c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:29 +0000

    Add package pysme-astro/0.4.2/pysme-astro-0.4.2.tar.gz

---
+++ packages/pysme-astro/pysme-astro-0.4.2.tar.gz/pysme-astro-0.4.2/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import json
+import os
+from os.path import exists, expanduser, join
+
+import versioneer
+from setuptools import setup
+
+# Create folder structure
+directory = expanduser("~/.sme/")
+conf = join(directory, "config.json")
+atmo = join(directory, "atmospheres")
+nlte = join(directory, "nlte_grids")
+cache_atmo = join(atmo, "cache")
+cache_nlte = join(nlte, "cache")
+
+os.makedirs(directory, exist_ok=True)
+os.makedirs(atmo, exist_ok=True)
+os.makedirs(nlte, exist_ok=True)
+os.makedirs(cache_atmo, exist_ok=True)
+os.makedirs(cache_nlte, exist_ok=True)
+
+
+# Create config file if it does not exist
+if not exists(conf):
+    # Hardcode default settings?
+    defaults = {
+        "data.file_server": "http://sme.astro.uu.se/atmos",
+        "data.atmospheres": "~/.sme/atmospheres",
+        "data.nlte_grids": "~/.sme/nlte_grids",
+        "data.cache.atmospheres": "~/.sme/atmospheres/cache",
+        "data.cache.nlte_grids": "~/.sme/nlte_grids/cache",
+        "data.pointers.atmospheres": "datafiles_atmospheres.json",
+        "data.pointers.nlte_grids": "datafiles_nlte.json",
+    }
+
+    # Save file to disk
+    with open(conf, "w") as f:
+        json.dump(defaults, f)
+
+# TODO: Have smelib compiled before distribution
+
+# Setup package
+setup(
+    name="pysme-astro",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Spectroscopy Made Easy",
+    author="Ansgar Wehrhahn, Jeff A. Valenti",
+    author_email="ansgar.wehrhahn@physics.uu.se, valenti@stsci.edu",
+    packages=["pysme", "pysme.gui", "pysme.atmosphere", "pysme.linelist"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=["numpy", "scipy", "matplotlib", "plotly", "pandas", "wget"],
+)
---
+++ packages/pysme-astro/pysme-astro-0.4.2.tar.gz/pysme-astro-0.4.2/test/test_largefilestorage.py
@@ -0,0 +1,59 @@
+import pytest
+
+from os import listdir, makedirs, stat
+from os.path import dirname, islink, exists
+from shutil import rmtree
+
+from pysme.large_file_storage import LargeFileStorage, Server
+from pysme.config import Config
+
+
+def lfs_available():
+    config = Config()
+    server = Server(config["data.file_server"])
+    return server.isUp()
+
+
+skipif_lfs = pytest.mark.skipif(lfs_available(), reason="LFS not available")
+
+
+@pytest.fixture
+def lfs_nlte():
+    config = Config()
+    server = config["data.file_server"]
+    pointers = config["data.pointers.nlte_grids"]
+    storage = "./lfs_test"
+    cache = "./lfs_test/cache"
+    makedirs(cache, exist_ok=True)
+    lfs_nlte = LargeFileStorage(server, pointers, storage, cache)
+    yield lfs_nlte
+    rmtree(storage, ignore_errors=True)
+
+
+@pytest.fixture
+def lfs_atmo():
+    config = Config()
+    server = config["data.file_server"]
+    pointers = config["data.pointers.atmospheres"]
+    storage = "./lfs_test"
+    cache = "./lfs_test/cache"
+    makedirs(cache, exist_ok=True)
+    lfs_atmo = LargeFileStorage(server, pointers, storage, cache)
+    yield lfs_atmo
+    rmtree(storage, ignore_errors=True)
+
+
+@skipif_lfs
+def test_load_atmo(lfs_atmo):
+    fname = "marcs2012p_t0.0.sav"
+    dname = lfs_atmo.get(fname)
+    assert fname in listdir(dirname(dname))
+    assert exists(dname)
+
+
+@skipif_lfs
+def test_load_nlte(lfs_nlte):
+    fname = "marcs2012_Na.grd"
+    dname = lfs_nlte.get(fname)
+    assert fname in listdir(dirname(dname))
+    assert exists(dname)
