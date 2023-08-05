4f8433fec2aa75a4b850e6f6ee44eb21e6760075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package LyricsPy/1.2.0/LyricsPy-1.2.0.tar.gz

---
+++ packages/LyricsPy/LyricsPy-1.2.0.tar.gz/LyricsPy-1.2.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='LyricsPy',
+    version='1.2.0',
+    packages=["lyricspy"],
+    url='https://github.com/amanoteam/LyricsPy',
+    author='AmanoTeam',
+    install_requires=['urllib3', 'bs4'],
+    author_email='contact@amanoteam.com',
+    description='search lyrics on musixmatch.com',
+    long_description=long_desc,
+    long_description_content_type="text/markdown"
+)
