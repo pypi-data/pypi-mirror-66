711927ef9bfed28237a2f772dd7dea0bd4b55e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package otools/0.1.1/otools-0.1.1.tar.gz

---
+++ packages/otools/otools-0.1.1.tar.gz/otools-0.1.1/otools/core/Swarm.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'Swarm'
+]
+
+class Swarm ():
+    """
+    A Swarm helps deploying multiple similar Service objects
+    by handling everything the user should do in order to deploy
+    them with much fewer lines of code.
+    """
+
+    # TODO: Implement
+    pass
---
+++ packages/otools/otools-0.1.1.tar.gz/otools-0.1.1/otools/core/TriggerCondition.py
@@ -0,0 +1,31 @@
+__all__ = ['TriggerCondition']
+
+from otools.status.StatusCode import StatusTrigger
+from otools.core.Service import Service
+
+class TriggerCondition (Service):
+  """
+  TriggerCondition is a class for encapsulating other classes in order
+  to identify it as a condition not as anything else.
+  """
+
+  def __init__ (self, obj):
+    Service.__init__(self, obj)
+    self.__obj = obj
+
+  def main (self):
+    if self.active:
+      try:
+        if (self.__obj.main(self.__obj)):
+          return StatusTrigger.TRIGGERED
+        else:
+          return StatusTrigger.NOT_TRIGGERED
+      except:
+        self.MSG_WARNING("TriggerCondition with name {} failed to check trigger status".format(self.name))
+        return StatusTrigger.NOT_TRIGGERED
+  
+  def loop (self):
+    return
+
+  def __str__ (self):
+    return "<OTools TriggerCondition (name={})>".format(self.name)
