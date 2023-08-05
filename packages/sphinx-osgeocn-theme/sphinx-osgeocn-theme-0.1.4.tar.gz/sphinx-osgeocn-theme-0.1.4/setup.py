0e4e001772779baebdf911c194eb509584f9eab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:18 +0000

    Add package sphinx-osgeocn-theme/0.1.4/sphinx-osgeocn-theme-0.1.4.tar.gz

---
+++ packages/sphinx-osgeocn-theme/sphinx-osgeocn-theme-0.1.4.tar.gz/sphinx-osgeocn-theme-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = 'sphinx-osgeocn-theme',
+    version = '0.1.4',
+    author = 'OSGeo CN',
+    author_email= '486936@qq.com',
+    url="https://github.com/osgeocn/sphinx-osgeocn-theme",
+    docs_url="https://github.com/osgeocn/sphinx-osgeocn-theme",
+    description='Sphinx Bootstrap4 Theme of OSGeo China Chapter Documents',
+    py_modules = ['sphinx_osgeocn_theme'],
+    packages = ['themes'],
+    include_package_data=True,
+    license= 'MIT License',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        "Topic :: Internet",
+        "Topic :: Software Development :: Documentation"
+    ],
+)
+
