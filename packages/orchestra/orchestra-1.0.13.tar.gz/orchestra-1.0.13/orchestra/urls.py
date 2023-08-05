4878ceee4e629f172f88c96b3b15c40ea25d257f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package orchestra/1.0.13/orchestra-1.0.13.tar.gz

---
+++ packages/orchestra/orchestra-1.0.13.tar.gz/orchestra-1.0.13/orchestra/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import include
+from django.conf.urls import url
+
+from orchestra.views import index
+from orchestra.views import status
+
+app_name = 'orchestra'
+
+urlpatterns = [
+    url(r'^api/',
+        include('orchestra.api_urls', namespace='orchestra')),
+    url(r'^communication/',
+        include('orchestra.communication.urls',
+                namespace='communication')),
+    url(r'^app/?', index, name='index'),
+    url(r'', include('orchestra.accounts.urls')),
+    url(r'^bots/', include('orchestra.bots.urls', namespace='bots')),
+    url(r'^todos/', include('orchestra.todos.urls', namespace='todos')),
+
+    # Health check status
+    url(r'^status/', status, name='status'),
+]
