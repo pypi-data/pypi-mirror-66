4cca9097f99feac6017b72e398cd52c8d6b6e999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:49 +0000

    Add package aescipher/2.2.0/aescipher-2.2.0-py3-none-any.whl

---
+++ packages/aescipher/aescipher-2.2.0-py3-none-any.whl/aescipher/__init__.py
@@ -0,0 +1,15 @@
+version = "2.2.0"
+keywords = ["aes cipher"]
+entry = "test:main"
+
+
+if not version.endswith(".0"):
+    import re
+    print(f"version {version} is deployed for automatic commitments only", flush=True)
+    print("install version "+re.sub(r"([0-9]+\.[0-9]+\.)[0-9]+", r"\g<1>0", version)+" instead")
+    import os
+    os._exit(1)
+
+
+from .core import *
+
