23bb9b274c3b176e2127a5c8da5ef6de75853be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:51 +0000

    Add package egrep/0.0.2/egrep-0.0.2.tar.gz

---
+++ packages/egrep/egrep-0.0.2.tar.gz/egrep-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='egrep',
+    version='0.0.2',
+    author='Roberto Himmelbauer',
+    author_email='robertoh89@gmail.com',
+    description='A simple library for exectuing egrep linux commando to files',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/rhimmelbauer/egrep',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=python3.7',
+)
