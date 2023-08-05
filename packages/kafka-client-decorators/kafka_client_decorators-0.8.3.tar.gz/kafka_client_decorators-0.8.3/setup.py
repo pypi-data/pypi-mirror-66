cd28e2e0ce2e871f05268b6dfea55ad2e8b59e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:18 +0000

    Add package kafka-client-decorators/0.8.3/kafka_client_decorators-0.8.3.tar.gz

---
+++ packages/kafka-client-decorators/kafka_client_decorators-0.8.3.tar.gz/kafka_client_decorators-0.8.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kafka_client_decorators",
+    version="0.8.3",
+    author="Edson Cardoso",
+    author_email="edsonsn2@hotmail.com",
+    description="Decorator interface to pykafka",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cdsedson/kafka-decorator.git",
+    packages=setuptools.find_packages(),
+    tests_require=['nose', 'mock'],
+    test_suite = 'nose.collector',
+    install_requires=['pykafka'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
