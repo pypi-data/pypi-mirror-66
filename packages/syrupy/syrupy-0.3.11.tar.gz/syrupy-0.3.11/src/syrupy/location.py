a2c0d5a7b84eff94dabcebb54452edb9a5eaea98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:35 +0000

    Add package syrupy/0.3.11/syrupy-0.3.11.tar.gz

---
+++ packages/syrupy/syrupy-0.3.11.tar.gz/syrupy-0.3.11/src/syrupy/location.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from typing import (
+    Any,
+    Optional,
+)
+
+
+class TestLocation:
+    def __init__(self, node: Any):
+        self._node = node
+        self.filepath = self._node.fspath
+        self.modulename = self._node.obj.__module__
+        self.methodname = self._node.obj.__name__
+        self.nodename = getattr(self._node, "name", None)
+        self.testname = self.nodename or self.methodname
+
+    @property
+    def classname(self) -> Optional[str]:
+        classes = self._node.obj.__qualname__.split(".")[:-1]
+        return ".".join(classes) if classes else None
+
+    @property
+    def filename(self) -> str:
+        return Path(self.filepath).stem
+
+    @property
+    def snapshot_name(self) -> str:
+        if self.classname is not None:
+            return f"{self.classname}.{self.testname}"
+        return str(self.testname)
+
+    def __valid_id(self, name: str) -> str:
+        valid_id = ""
+        for char in name:
+            new_valid_id = f"{valid_id}{char}"
+            if not new_valid_id.isidentifier():
+                break
+            valid_id = new_valid_id
+        return valid_id
+
+    def __parse(self, name: str) -> str:
+        valid_ids = (self.__valid_id(n) for n in name.split("."))
+        return ".".join(valid_id for valid_id in valid_ids if valid_id)
+
+    def matches_snapshot_name(self, snapshot_name: str) -> bool:
+        return self.__parse(self.snapshot_name) == self.__parse(snapshot_name)
+
+    def matches_snapshot_location(self, snapshot_location: str) -> bool:
+        return self.filename in snapshot_location
