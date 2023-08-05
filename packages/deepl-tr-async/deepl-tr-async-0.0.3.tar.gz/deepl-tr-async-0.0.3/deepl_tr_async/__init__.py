42ff4d83884580ae6c39f4e40cca3e6e707184df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:42 +0000

    Add package deepl-tr-async/0.0.3/deepl-tr-async-0.0.3.tar.gz

---
+++ packages/deepl-tr-async/deepl-tr-async-0.0.3.tar.gz/deepl-tr-async-0.0.3/deepl_tr_async/__init__.py
@@ -0,0 +1,9 @@
+""" deepl_tr_async via pyppeteer and proxy support """
+from deepl_tr_async.deepl_tr_async import deepl_tr_async, BROWSER, get_ppbrowser, HEADFUL, DEBUG  # noqa: F401
+# from deepl_tr_async.google_langpair import google_langpair
+
+# version__ = """0.0.1"""
+# date__ = "2020.3.24"
+__version__ = """0.0.3"""
+__date__ = "2020.4.17"
+VERSION = tuple(__version__.split("."))
