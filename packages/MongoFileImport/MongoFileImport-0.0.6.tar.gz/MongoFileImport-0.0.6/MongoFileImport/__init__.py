255583140e5df86f60ede70a76ec02853298adf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:35 +0000

    Add package MongoFileImport/0.0.6/MongoFileImport-0.0.6.tar.gz

---
+++ packages/MongoFileImport/MongoFileImport-0.0.6.tar.gz/MongoFileImport-0.0.6/MongoFileImport/__init__.py
@@ -0,0 +1,26 @@
+
+import argparse
+from MongoFileImport import MongoFileImport
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mongourl', type=str, default=None)
+    parser.add_argument("--file",type=str, default=None)
+    parser.add_argument("--dir",type=str,default=None)
+    parser.add_argument("--db",type=str,default=None)
+
+
+    args = parser.parse_args()
+
+
+
+    if(args.mongourl!=None):
+        if(args.dir!=None and args.db!=None):
+            MongoFileImport.pichuli(args.dir,args.mongourl,args.db)
+        elif(args.file!=None and args.db!=None):
+            MongoFileImport.filepprocess(args.file,args.mongourl,args.db)
+        else:
+            print("args error")
+
+
