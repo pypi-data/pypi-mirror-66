a4f356fce404f32552e1bc0f1a3eafa229c6ace6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:32 +0000

    Add package pyshuttle/0.2.0a1/pyshuttle-0.2.0a1-py3-none-any.whl

---
+++ packages/pyshuttle/pyshuttle-0.2.0a1-py3-none-any.whl/shuttle/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from .providers import bitcoin, bytom, config
+
+
+# Shuttle Information's
+__version__ = "0.2.0a1"
+__license__ = "AGPL-3.0"
+__author__ = "Meheret Tesfaye"
+__email__ = "meherett@zoho.com"
+
+
+__all__ = [
+    "__version__",
+    "__license__",
+    "__author__",
+    "__email__",
+    "bitcoin",
+    "bytom",
+    "config"
+]
