e1bb2c5c885124a2b82ad3b12a2e24aed805ac96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:09 +0000

    Add package insights-core/3.0.160/insights_core-3.0.160-py3.6.egg

---
+++ packages/insights-core/insights_core-3.0.160-py3.6.egg/insights/core/hydration.py
@@ -0,0 +1,44 @@
+import logging
+import os
+
+from insights.core import archives
+from insights.core.context import (ClusterArchiveContext,
+                                   ExecutionContextMeta,
+                                   HostArchiveContext)
+
+log = logging.getLogger(__name__)
+
+
+def get_all_files(path):
+    all_files = []
+    for f in archives.get_all_files(path):
+        if os.path.isfile(f) and not os.path.islink(f):
+            all_files.append(f)
+    return all_files
+
+
+def identify(files):
+    common_path, ctx = ExecutionContextMeta.identify(files)
+    if ctx:
+        return common_path, ctx
+
+    common_path = os.path.dirname(os.path.commonprefix(files))
+    if not common_path:
+        raise archives.InvalidArchive("Unable to determine common path")
+
+    return common_path, HostArchiveContext
+
+
+def create_context(path, context=None):
+    top = os.listdir(path)
+    arc = [os.path.join(path, f) for f in top if f.endswith(archives.COMPRESSION_TYPES)]
+    if arc:
+        return ClusterArchiveContext(path, all_files=arc)
+
+    all_files = get_all_files(path)
+    if not all_files:
+        raise archives.InvalidArchive("No files in archive")
+
+    common_path, ctx = identify(all_files)
+    context = context or ctx
+    return context(common_path, all_files=all_files)
