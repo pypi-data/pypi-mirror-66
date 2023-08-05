b3e38f638873b031d6bb28fa71f59a7ecb993a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package ldstore/0.1.4/ldstore-0.1.4.tar.gz

---
+++ packages/ldstore/ldstore-0.1.4.tar.gz/ldstore-0.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open( 'README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name                           = 'ldstore',
+    version                        = '0.1.4',
+    author                         = 'Christian Benner',
+    author_email                   = 'christian.benner@helsinki.fi',
+    description                    = 'A package for reading files created by LDstore',
+    url                            = 'http://www.finemap.me',
+    data_files                     = [("", ["LICENSE"])],
+    packages                       = setuptools.find_packages(),
+    install_requires               = [ 'numpy', 'pandas', 'zstd' ],
+    classifiers                    = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires                = '>= 3.8',
+)
