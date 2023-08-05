874ababf5e2c6adfe1b85bbdb6aa1a98c9bb5f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:19 +0000

    Add package mkv-this/0.1.6/mkv-this-0.1.6.tar.gz

---
+++ packages/mkv-this/mkv-this-0.1.6.tar.gz/mkv-this-0.1.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file as long_description:
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(name='mkv-this',
+      version='0.1.6',
+      description='markovify user-provided text and output the results to a text file.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='',
+      author='mousebot',
+      author_email='mousebot@riseup.net',
+      license='AGPLv3',
+      packages=find_packages(),
+      install_requires=[
+          'markovify',
+          'argparse',
+          'sys',
+      ],
+      zip_safe=False)
