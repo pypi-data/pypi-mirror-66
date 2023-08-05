801ffca7c1090ffd9c4cdee972861e216a1982b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:05 +0000

    Add package dosk/0.4.8/dosk-0.4.8-py3-none-any.whl

---
+++ packages/dosk/dosk-0.4.8-py3-none-any.whl/tests/min_max_test.py
@@ -0,0 +1,13 @@
+from dosk.algo import algo
+
+def test_min():
+    values = (2, 3, 1, 4, 6)
+
+    val = algo.min(values)
+    assert val == 1
+
+def test_max():
+    values = (2, 3, 1, 4, 6)
+
+    val = algo.max(values)
+    assert val == 6
