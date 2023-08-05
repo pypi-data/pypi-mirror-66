a0dee00181d274fee1532a65f74acb5cd250f93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:20 +0000

    Add package AutoNode/0.0.4/AutoNode-0.0.4.tar.gz

---
+++ packages/AutoNode/AutoNode-0.0.4.tar.gz/AutoNode-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import subprocess
+from setuptools import setup
+
+
+setup(
+    name='AutoNode',
+    version=f"0.0.4",
+    description="AutoNode Python Library (this library is only used inside the sentry docker image)",
+    author='Daniel Van Der Maden',
+    author_email='daniel@harmony.one',
+    url="http://harmony.one/auto-node",
+    packages=['AutoNode'],
+    install_requires=[
+        'requests',
+        'pexpect',
+        'pyhmy',
+    ]
+)
