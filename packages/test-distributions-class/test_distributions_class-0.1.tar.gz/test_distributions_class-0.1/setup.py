c777e184702517d03a64f47bb7e5dedc0ed356c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package test-distributions-class/0.1/test_distributions_class-0.1.tar.gz

---
+++ packages/test-distributions-class/test_distributions_class-0.1.tar.gz/test_distributions_class-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name ='test_distributions_class',
+      version ='0.1',
+      description ='Gaussian distributions and Binomial distribution',
+      packages =['test_distributions_class'],
+      author = 'Irfan sp',
+      zip_safe=False)
