b63e59aa554599118dec22c2b56aaec52b472fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:43 +0000

    Add package mypy-boto3-elastic-inference/1.12.41.0/mypy-boto3-elastic-inference-1.12.41.0.tar.gz

---
+++ packages/mypy-boto3-elastic-inference/mypy-boto3-elastic-inference-1.12.41.0.tar.gz/mypy-boto3-elastic-inference-1.12.41.0/mypy_boto3_elastic_inference/__init__.py
@@ -0,0 +1,23 @@
+"""
+Main interface for elastic-inference service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.elastic_inference import (
+        Client,
+        ElasticInferenceClient,
+        )
+
+    session = boto3.Session()
+
+    client: ElasticInferenceClient = boto3.client("elastic-inference")
+    session_client: ElasticInferenceClient = session.client("elastic-inference")
+"""
+from mypy_boto3_elastic_inference.client import (
+    ElasticInferenceClient as Client,
+    ElasticInferenceClient,
+)
+
+
+__all__ = ("Client", "ElasticInferenceClient")
