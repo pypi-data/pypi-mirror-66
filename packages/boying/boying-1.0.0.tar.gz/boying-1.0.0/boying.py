c2c1c35779738c7d98c7af12c40e9529f8622be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package boying/1.0.0/boying-1.0.0.tar.gz

---
+++ packages/boying/boying-1.0.0.tar.gz/boying-1.0.0/boying.py
@@ -0,0 +1,1 @@
+print('Python牛人')
---
+++ packages/boying/boying-1.0.0.tar.gz/boying-1.0.0/setup.py
@@ -0,0 +1,25 @@
+﻿from distutils.core import setup
+
+#从python的发布工具导入setup函数
+
+
+
+setup(
+
+    name= 'boying',
+
+    version= '1.0.0',
+
+    #关联模块
+
+    py_modules =['boying'],
+
+    author= 'lqh',
+
+    author_email='11@189.com',
+
+    url='https://upload.pypi.org/1egacy/',
+
+    description= '处理列表数据'
+
+)
