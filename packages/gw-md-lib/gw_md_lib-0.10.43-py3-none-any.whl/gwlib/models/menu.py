5034e75a3df31c4987d711272e7dc15b41970bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package gw-md-lib/0.10.43/gw_md_lib-0.10.43-py3-none-any.whl

---
+++ packages/gw-md-lib/gw_md_lib-0.10.43-py3-none-any.whl/gwlib/models/menu.py
@@ -0,0 +1,12 @@
+from sqlalchemy import func
+
+from gwlib.models import db, ModelJson
+
+
+class Menu(db.Model, ModelJson):
+    __tablename__ = "gw_menu"
+
+    menu_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
+    time_updated = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
