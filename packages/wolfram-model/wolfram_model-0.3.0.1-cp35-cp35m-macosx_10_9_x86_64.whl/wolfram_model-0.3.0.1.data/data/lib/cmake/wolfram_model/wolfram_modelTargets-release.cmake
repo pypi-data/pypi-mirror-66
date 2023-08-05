52ca35b5fd096f99de677716561b2e90c595b463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:23 +0000

    Add package wolfram-model/0.3.0.1/wolfram_model-0.3.0.1-cp35-cp35m-macosx_10_9_x86_64.whl

---
+++ packages/wolfram-model/wolfram_model-0.3.0.1-cp35-cp35m-macosx_10_9_x86_64.whl/wolfram_model-0.3.0.1.data/data/lib/cmake/wolfram_model/wolfram_modelTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "wolfram_model::SetReplace" for configuration "Release"
+set_property(TARGET wolfram_model::SetReplace APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(wolfram_model::SetReplace PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libSetReplace.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS wolfram_model::SetReplace )
+list(APPEND _IMPORT_CHECK_FILES_FOR_wolfram_model::SetReplace "${_IMPORT_PREFIX}/lib/libSetReplace.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
