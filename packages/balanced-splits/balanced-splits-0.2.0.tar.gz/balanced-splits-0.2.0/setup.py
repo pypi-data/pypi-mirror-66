54e75354f6a6edc4dc6a0147bf6647138fe80ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package balanced-splits/0.2.0/balanced-splits-0.2.0.tar.gz

---
+++ packages/balanced-splits/balanced-splits-0.2.0.tar.gz/balanced-splits-0.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="balanced-splits",
+    version="0.2.0",
+    author="andersource",
+    author_email="hi@andersource.dev",
+    description="Balanced splitting utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andersource/balanced-splits",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas'
+    ]
+)
