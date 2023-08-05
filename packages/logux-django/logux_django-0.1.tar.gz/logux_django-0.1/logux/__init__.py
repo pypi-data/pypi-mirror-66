e4e123098d7b5664195b0c8e7d8c77fcb2f69b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:25 +0000

    Add package logux-django/0.1/logux_django-0.1.tar.gz

---
+++ packages/logux-django/logux_django-0.1.tar.gz/logux_django-0.1/logux/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'Django Logux integration engine'
+__version__ = "0.1"
+__author__ = 'Vadim Iskuchekov @egregors'
+__license__ = 'MIT License'
+
+# Synonyms
+VERSION = __version__
+AUTHOR = __author__
+
+# Logux protocol version: https://logux.io/protocols/ws/spec/
+LOGUX_PROTOCOL_VERSION = 3
