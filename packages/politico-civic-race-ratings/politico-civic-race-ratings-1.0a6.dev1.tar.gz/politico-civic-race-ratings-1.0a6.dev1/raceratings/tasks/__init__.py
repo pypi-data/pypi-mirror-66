e3370a967f12ffecc4d8d8b34597e4f4627a6a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:11 +0000

    Add package politico-civic-race-ratings/1.0a6.dev1/politico-civic-race-ratings-1.0a6.dev1.tar.gz

---
+++ packages/politico-civic-race-ratings/politico-civic-race-ratings-1.0a6.dev1.tar.gz/politico-civic-race-ratings-1.0a6.dev1/raceratings/tasks/__init__.py
@@ -0,0 +1,29 @@
+# Imports from race_ratings.
+from raceratings.tasks.bake_race_ratings import bake_all_race_ratings
+from raceratings.tasks.bake_race_ratings import bake_electoral_college_ratings
+from raceratings.tasks.bake_race_ratings import bake_governor_ratings
+from raceratings.tasks.bake_race_ratings import bake_house_ratings
+from raceratings.tasks.bake_race_ratings import bake_senate_ratings
+from raceratings.tasks.bake_race_ratings import bake_sitemap
+from raceratings.tasks.bake_rating_deltas import bake_all_race_rating_deltas
+from raceratings.tasks.bake_rating_deltas import bake_electoral_college_deltas
+from raceratings.tasks.bake_rating_deltas import bake_governor_deltas
+from raceratings.tasks.bake_rating_deltas import bake_house_deltas
+from raceratings.tasks.bake_rating_deltas import bake_overall_deltas
+from raceratings.tasks.bake_rating_deltas import bake_senate_deltas
+
+
+__all__ = [
+    "bake_all_race_rating_deltas",
+    "bake_all_race_ratings",
+    "bake_electoral_college_deltas",
+    "bake_electoral_college_ratings",
+    "bake_governor_deltas",
+    "bake_governor_ratings",
+    "bake_house_deltas",
+    "bake_house_ratings",
+    "bake_overall_deltas",
+    "bake_senate_deltas",
+    "bake_senate_ratings",
+    "bake_sitemap",
+]
