ef9da24178fedac1c37cf8a732a3408813f5ee05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package easy-parallel/0.1.6/easy-parallel-0.1.6.tar.gz

---
+++ packages/easy-parallel/easy-parallel-0.1.6.tar.gz/easy-parallel-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='easy-parallel',
+    py_modules=['parallel'],
+    url='https://github.com/raphtown/easy-parallel',
+    version='0.1.6',
+    description='Parallel wrapper for easy multi-threading.',
+    long_description=open("README.rst").read(),
+    author='Raphael Townshend',
+    license='MIT',
+    install_requires=['pathos'],
+)
