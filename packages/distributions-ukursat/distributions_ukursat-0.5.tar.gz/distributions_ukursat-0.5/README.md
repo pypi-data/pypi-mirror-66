417691a9bffa2ca0a839c05d245cb761c40712ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package distributions-ukursat/0.5/distributions_ukursat-0.5.tar.gz

---
+++ packages/distributions-ukursat/distributions_ukursat-0.5.tar.gz/distributions_ukursat-0.5/README.md
@@ -0,0 +1,3 @@
+# Data Scientist Nanodegree (Term 2)
+
+# Exercise on uploading package on to PyPi Repository
---
+++ packages/distributions-ukursat/distributions_ukursat-0.5.tar.gz/distributions_ukursat-0.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_ukursat',
+      version='0.5',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
