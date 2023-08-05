d1c266eb735028ae6a9a66dc970688d157855ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package treedb/1.0/treedb-1.0-py3-none-any.whl

---
+++ packages/treedb/treedb-1.0-py3-none-any.whl/treedb/shortcuts.py
@@ -0,0 +1,38 @@
+# shortcuts.py - sqlalchemy and pandas convenience short-cut functions
+
+import functools
+import warnings
+
+import sqlalchemy as sa
+
+from . import ENGINE
+
+__all__ = ['count', 'select', 'text',
+           'pd_read_sql']
+
+PANDAS = None
+
+
+count = sa.func.count
+
+
+select = functools.partial(sa.select, bind=ENGINE)
+
+
+text = functools.partial(sa.text, bind=ENGINE)
+
+
+def pd_read_sql(sql=None, *args, con=ENGINE, **kwargs):
+    global PANDAS
+    if PANDAS is None:
+        try:
+            import pandas as PANDAS
+        except ImportError as e:
+            warnings.warn(f'failed to import pandas: {e}')
+            return None
+
+    if sql is None:
+        from . import queries
+        sql = queries.get_query(bind=con)
+
+    return PANDAS.read_sql_query(sql, *args, con=con, **kwargs)
