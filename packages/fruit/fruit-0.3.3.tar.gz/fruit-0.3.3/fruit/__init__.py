feddae422e58403f4321cd850a60a030e60781fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package fruit/0.3.3/fruit-0.3.3.tar.gz

---
+++ packages/fruit/fruit-0.3.3.tar.gz/fruit-0.3.3/fruit/__init__.py
@@ -0,0 +1,11 @@
+# Import console logging / printing functions
+from fruit.modules.console import echo, error, warning
+
+# Export decorators
+from fruit.api.decorators import target, step, provider
+
+# Export exceptions
+from fruit.api.api import abort, finish, skip, fail
+
+# Export the shell module
+from fruit.api.shell import shell
