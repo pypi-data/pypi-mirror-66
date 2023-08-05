a89b81f020369fce97b002368fbd9d2d771c3149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package pyshape/0.1.0/pyshape-0.1.0.tar.gz

---
+++ packages/pyshape/pyshape-0.1.0.tar.gz/pyshape-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyshape']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyshape',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'yafeunteun',
+    'author_email': 'yfe.github@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
