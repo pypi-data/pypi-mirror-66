9f3e33b2413f0fa8289f0e455263544d060e4076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package constants-and-utils/0.0.1/constants-and-utils-0.0.1.tar.gz

---
+++ packages/constants-and-utils/constants-and-utils-0.0.1.tar.gz/constants-and-utils-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="constants-and-utils",
+    version="0.0.1",
+    author="Terminus",
+    author_email="jose.salas@zinobe.com",
+    description="Constants and utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/terminus-zinobe/constants-and-utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
