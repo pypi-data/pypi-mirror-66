0b7f43a9e74d1b6a94ab119b3526efbe68f699d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package landlord-ai/0.1.37/landlord_ai-0.1.37.tar.gz

---
+++ packages/landlord-ai/landlord_ai-0.1.37.tar.gz/landlord_ai-0.1.37/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='landlord_ai',
+      version='0.1.37',
+      description='Landlord AI Core',
+      license='GNU',
+      packages=['landlordai.game', 'landlordai.sim'],
+      author_email='seokhohong02@gmail.com',
+      install_requires=[],
+      zip_safe=False)
