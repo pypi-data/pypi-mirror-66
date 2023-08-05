6f70ffa630534a562274dbe7cc84dda925ab0a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:41 +0000

    Add package formio-data/0.1.0.dev4/formio-data-0.1.0.dev4.tar.gz

---
+++ packages/formio-data/formio-data-0.1.0.dev4.tar.gz/formio-data-0.1.0.dev4/formiodata/__init__.py
@@ -0,0 +1,9 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from . import builder
+from . import submission
+from . import components
