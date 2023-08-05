4ed275bba3accbd65b671d1373971ca5272a755e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:52 +0000

    Add package clihandler/0.6/clihandler-0.6.tar.gz

---
+++ packages/clihandler/clihandler-0.6.tar.gz/clihandler-0.6/clihandler/__init__.py
@@ -0,0 +1,12 @@
+from clihandler.clihandler import CliHandler
+
+cli = CliHandler()
+
+call = cli.call
+get = cli.get
+capture = cli.capture
+kill = cli.kill
+return_code = cli.return_code
+reset = cli.reset
+list_process = cli.list_process
+__version__ = '0.6'
