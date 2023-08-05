7542cbf0af14387a050acc8c563b118ce0c1a219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:20 +0000

    Add package divacat/1.0.0/divacat-1.0.0.tar.gz

---
+++ packages/divacat/divacat-1.0.0.tar.gz/divacat-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name = 'divacat',
+version = '1.0.0',
+py_modules = ['divacat'],
+author = 'aishvarya',
+author_email = 'aishvarya.kumar@gmail.com',
+url = 'https://pypi.org/project/diva/',
+description = 'A simple printer of nested lists',
+)
