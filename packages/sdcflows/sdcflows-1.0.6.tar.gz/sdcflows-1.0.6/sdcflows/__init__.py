3b5bbb839d2c30eb3b5db3105b26d8014c717047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:26 +0000

    Add package sdcflows/1.0.6/sdcflows-1.0.6.tar.gz

---
+++ packages/sdcflows/sdcflows-1.0.6.tar.gz/sdcflows-1.0.6/sdcflows/__init__.py
@@ -0,0 +1,14 @@
+"""SDCflows - :abbr:`SDC (susceptibility distortion correction)` by DUMMIES, for dummies."""
+from .__about__ import (
+    __version__,
+    __copyright__,
+    __credits__,
+    __packagename__,
+)
+
+__all__ = [
+    '__version__',
+    '__copyright__',
+    '__credits__',
+    '__packagename__',
+]
