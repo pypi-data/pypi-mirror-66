af35fd727e4fae319ebf7ec4a4d43b43e3008d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package text-tracker/1.0.1/text-tracker-1.0.1.tar.gz

---
+++ packages/text-tracker/text-tracker-1.0.1.tar.gz/text-tracker-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+setuptools.setup(
+    name='text-tracker',
+    version='1.0.1',
+    author='Cooper Sanders',
+    author_email='tromboneguy@coopersanders.com',
+    description='Keep track of text locations!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/supercooper6558/Text-Tracker',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6',
+)
