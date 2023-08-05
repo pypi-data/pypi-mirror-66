4061aff2203487bb13e6aa6cf4d30bc2c1d2c0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package mp3toolsce/1.0.3/mp3toolsce-1.0.3-py3-none-any.whl

---
+++ packages/mp3toolsce/mp3toolsce-1.0.3-py3-none-any.whl/mp3toolsce/__init__.py
@@ -0,0 +1,7 @@
+from mp3toolsce.mp3tools import (
+    mergemp3, 
+    packsubdirs, 
+    unpackdirs)
+
+__version__ = "1.0.3"
+__all__ = ["mergemp3", "packsubdirs", "unpackdirs"]
---
+++ packages/mp3toolsce/mp3toolsce-1.0.3-py3-none-any.whl/mp3toolsce/__main__.py
@@ -0,0 +1,3 @@
+from mp3toolsce.mp3tools import main
+if __name__ == "__main__":
+    main()
