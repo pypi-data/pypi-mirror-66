39a8606e0ff32a18e2f1c7896081857ad2ab6dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:53 +0000

    Add package key-people/0.1.7.1.1/key_people-0.1.7.1.1-py3-none-any.whl

---
+++ packages/key-people/key_people-0.1.7.1.1-py3-none-any.whl/key_people/__init__.py
@@ -0,0 +1,43 @@
+import os
+import pkg_resources
+import sqlite3
+from .database_management import open_connection
+from .database_management import find_ticker
+
+class Key_people:
+	def __init__(self):
+		DB_FILE = pkg_resources.resource_filename('key_people', 'DB/Stocks.db')
+		self.conn, self.c = open_connection(DB_FILE)
+
+	def search(self, ticker):
+		ticker = ticker.upper()
+		ticker = (ticker, )
+		results = find_ticker(ticker, self.c)[0]
+		ticker = results[0]
+		company_name = results[1]
+		people = []
+		for x in results[2:]:
+			if x != 'NULL':
+				people.append(x)
+		return Entry(ticker, company_name, people)
+
+class Entry:
+	def __init__(self, ticker, company_name, people):
+		self.key_people = people
+		self.company_name = company_name
+		self.ticker = ticker
+
+def search(ticker):
+	ticker = ticker.upper()
+	DB_FILE = pkg_resources.resource_filename('key_people', 'DB/Stocks.db')
+	conn, c = open_connection(DB_FILE)
+	ticker = (ticker, )
+	results = find_ticker(ticker, c)[0]
+	ticker = results[0]
+	company_name = results[1]
+	people = []
+	for x in results[2:]:
+		if x != 'NULL':
+			people.append(x)
+	conn.close()
+	return people
