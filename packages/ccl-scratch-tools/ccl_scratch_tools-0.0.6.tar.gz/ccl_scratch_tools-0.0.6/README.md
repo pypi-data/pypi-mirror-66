92a9b32190d62172ef01fb846ca6505f9b4c91e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package ccl-scratch-tools/0.0.6/ccl_scratch_tools-0.0.6.tar.gz

---
+++ packages/ccl-scratch-tools/ccl_scratch_tools-0.0.6.tar.gz/ccl_scratch_tools-0.0.6/README.md
@@ -0,0 +1,18 @@
+# Scratch Tools
+A Python package with which to work with Scratch JSON files.
+
+This project has been relocated to [https://github.com/GSE-CCL/scratch-tools](https://github.com/GSE-CCL/scratch-tools).
+
+
+## Setup
+
+Make sure you have Python 3.7 or higher. Then install the package by running ```pip install ccl-scratch-tools```.
+
+Check it out on PyPI: [https://pypi.org/project/ccl-scratch-tools/](https://pypi.org/project/ccl-scratch-tools/).
+
+## Documentation
+
+Read the documentation of the package [here](https://ccl-scratch-tools.readthedocs.io/en/latest/).
+
+## Resources
+[https://github.com/LLK/scratch-rest-api/wiki/](https://github.com/LLK/scratch-rest-api/wiki/) for API documentation
---
+++ packages/ccl-scratch-tools/ccl_scratch_tools-0.0.6.tar.gz/ccl_scratch_tools-0.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ccl_scratch_tools",
+    version="0.0.6",
+    author="Creative Computing Lab",
+    author_email="jarchibald@college.harvard.edu",
+    description="Tools by which to work with Scratch JSON files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GSE-CCL/scratch-tools",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
