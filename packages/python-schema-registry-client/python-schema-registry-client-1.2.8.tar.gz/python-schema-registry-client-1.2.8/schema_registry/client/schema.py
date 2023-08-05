2714ed88d3fba5883c5bbca4d6e109fde51ea9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:34 +0000

    Add package python-schema-registry-client/1.2.8/python-schema-registry-client-1.2.8.tar.gz

---
+++ packages/python-schema-registry-client/python-schema-registry-client-1.2.8.tar.gz/python-schema-registry-client-1.2.8/schema_registry/client/schema.py
@@ -0,0 +1,37 @@
+import json
+import typing
+
+import fastavro
+
+
+class AvroSchema:
+    def __init__(self, schema: str) -> None:
+        if isinstance(schema, str):
+            schema = json.loads(schema)
+        self.schema = fastavro.parse_schema(schema, _force=True)
+        self.generate_hash()
+
+    def generate_hash(self) -> None:
+        self._hash = hash(json.dumps(self.schema))
+
+    @property
+    def name(self) -> str:
+        return self.schema.get("name")
+
+    def __hash__(self) -> int:
+        return self._hash
+
+    def __str__(self) -> str:
+        return str(self.schema)
+
+    def __eq__(self, other: typing.Any) -> bool:
+        if not isinstance(other, AvroSchema):
+            return NotImplemented
+        return self.__hash__() == other.__hash__()
+
+
+def load(fp: str) -> AvroSchema:
+    """Parse a schema from a file path"""
+    with open(fp, mode="r") as f:
+        content = f.read()
+        return AvroSchema(content)
