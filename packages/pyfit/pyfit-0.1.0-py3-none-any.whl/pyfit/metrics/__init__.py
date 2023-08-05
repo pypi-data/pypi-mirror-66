1678aa7227cea45a1c6545a3009ba832d7db6285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:52 +0000

    Add package pyfit/0.1.0/pyfit-0.1.0-py3-none-any.whl

---
+++ packages/pyfit/pyfit-0.1.0-py3-none-any.whl/pyfit/metrics/__init__.py
@@ -0,0 +1,7 @@
+"""
+Regression and classification metrics
+"""
+
+from ._distance import Distance, euclidean_distance
+from ._regression import mean_squared_error
+from ._classification import accuracy
