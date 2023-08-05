5f42274967fbfd000788862129e676ec2feae7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:43 +0000

    Add package omnitools/0.0.73/omnitools-0.0.73-py3-none-any.whl

---
+++ packages/omnitools/omnitools-0.0.73-py3-none-any.whl/omnitools/__init__.py
@@ -0,0 +1,12 @@
+from .stdout import *
+from .xtypes import *
+from .encoding import *
+from .hashing import *
+from .rng import *
+from .js import *
+from .inspecting import *
+from .tracing import *
+
+version = "0.0.73"
+keywords = ["omnitools utilities"]
+entry = "test:main"
