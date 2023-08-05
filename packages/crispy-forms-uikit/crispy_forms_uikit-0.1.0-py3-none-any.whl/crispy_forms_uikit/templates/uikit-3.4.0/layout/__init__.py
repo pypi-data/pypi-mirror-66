69f13df4a4b0b540b0bc8ace480c0e0be5a04aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:39 +0000

    Add package crispy-forms-uikit/0.1.0/crispy_forms_uikit-0.1.0-py3-none-any.whl

---
+++ packages/crispy-forms-uikit/crispy_forms_uikit-0.1.0-py3-none-any.whl/crispy_forms_uikit/templates/uikit-3.4.0/layout/__init__.py
@@ -0,0 +1,2 @@
+from .base import Layout
+from .grid import Row, RowFluid, Column
---
+++ packages/crispy-forms-uikit/crispy_forms_uikit-0.1.0-py3-none-any.whl/crispy_forms_uikit/templates/uikit-3.4.0/layout/base.py
@@ -0,0 +1,31 @@
+from crispy_forms import layout as crispy_forms_layout
+
+
+class Layout(crispy_forms_layout.Layout):
+    pass
+
+
+class Empty(crispy_forms_layout.Div):
+    """
+    It wraps fields inside a ``<div>`` element.
+    You can set ``css_id`` for element id and ``css_class`` for a element
+    class names.
+    Example:
+    .. sourcecode:: python
+        Div('form_field_1', 'form_field_2', css_id='div-example',
+            css_class='divs')
+    """
+    template = "%s/layout/empty.html"
+
+
+class Div(crispy_forms_layout.Div):
+    """
+    It wraps fields inside a ``<div>`` element.
+    You can set ``css_id`` for element id and ``css_class`` for a element
+    class names.
+    Example:
+    .. sourcecode:: python
+        Div('form_field_1', 'form_field_2', css_id='div-example',
+            css_class='divs')
+    """
+    template = "%s/layout/div.html"
