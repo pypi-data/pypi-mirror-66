b1995a2b216dda9dd8e5e18b51e382012c93293e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:18 +0000

    Add package appinventor-tfjs/0.1.4/appinventor-tfjs-0.1.4.tar.gz

---
+++ packages/appinventor-tfjs/appinventor-tfjs-0.1.4.tar.gz/appinventor-tfjs-0.1.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from appinventor.tfjs.__main__ import __version__
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='appinventor-tfjs',
+      version=__version__,
+      description='Tool for generating App Inventor extension skeletons from Tensorflow.js models',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/mit-cml/tfjs-extension-tool',
+      author='Evan W. Patton',
+      author_email='ewpatton@mit.edu',
+      license='Apache',
+      packages=find_packages(),
+      entry_points={'console_scripts': ['appinventor-tfjs = appinventor.tfjs.__main__:main']},
+      python_requires='>=3.0',
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'License :: OSI Approved :: Apache Software License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: JavaScript',
+            'Programming Language :: Python :: 3',
+            'Topic :: Education',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Code Generators'
+      ],
+      include_package_data=True,
+      install_requires=['certifi'])
