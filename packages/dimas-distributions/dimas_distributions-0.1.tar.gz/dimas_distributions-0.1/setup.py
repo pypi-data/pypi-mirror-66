7b0b70c1df84a1f13bf2050092cad13fca9b59a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:16 +0000

    Add package dimas-distributions/0.1/dimas_distributions-0.1.tar.gz

---
+++ packages/dimas-distributions/dimas_distributions-0.1.tar.gz/dimas_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dimas_distributions',
+      version='0.1',
+      description='Gaussian distribution and Binomial Distributions',
+      packages=['dimas_distributions'],
+      author = 'Dimas Torres',
+      zip_safe=False)
