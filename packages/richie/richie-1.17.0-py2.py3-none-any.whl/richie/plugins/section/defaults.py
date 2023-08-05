97a1520228c5ba1738614547e69d49ec088de4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package richie/1.17.0/richie-1.17.0-py2.py3-none-any.whl

---
+++ packages/richie/richie-1.17.0-py2.py3-none-any.whl/richie/plugins/section/defaults.py
@@ -0,0 +1,15 @@
+"""
+Section plugin default settings
+"""
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+SECTION_TEMPLATES = getattr(
+    settings,
+    "RICHIE_SECTION_TEMPLATES",
+    [
+        ("richie/section/section.html", _("Default")),
+        ("richie/section/section_cadenced.html", _("Highlighted items")),
+        ("richie/section/section_list.html", _("Unordered list")),
+    ],
+)
