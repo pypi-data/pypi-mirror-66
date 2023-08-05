29a0816865ada7a720d7aa39b1605d60f12baabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:32 +0000

    Add package pyshuttle/0.2.0a1/pyshuttle-0.2.0a1.tar.gz

---
+++ packages/pyshuttle/pyshuttle-0.2.0a1.tar.gz/pyshuttle-0.2.0a1/shuttle/cli/providers/bitcoin/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# IMPORT ALL PACKAGES
+from shuttle.cli import click
+
+from .htlc import htlc
+from .signature import sign
+from .decode import decode
+from .submit import submit
+
+
+@click.group("bitcoin", options_metavar="[OPTIONS]",
+             short_help="Select Bitcoin provider.")
+def bitcoin():
+    pass
+
+
+# Adding bitcoin htlc
+bitcoin.add_command(htlc)
+# Adding bitcoin sign
+bitcoin.add_command(sign)
+# Adding bitcoin decoder
+bitcoin.add_command(decode)
+# Adding bitcoin submit
+bitcoin.add_command(submit)
---
+++ packages/pyshuttle/pyshuttle-0.2.0a1.tar.gz/pyshuttle-0.2.0a1/shuttle/cli/providers/bytom/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# IMPORT ALL PACKAGES
+from shuttle.cli import click
+
+from .htlc import htlc
+from .signature import sign
+from .decode import decode
+from .submit import submit
+
+
+@click.group("bytom", options_metavar="[OPTIONS]",
+             short_help="Select Bytom provider.")
+def bytom():
+    pass
+
+
+# Adding bitcoin htlc
+bytom.add_command(htlc)
+# Adding bytom sign
+bytom.add_command(sign)
+# Adding bytom decoder
+bytom.add_command(decode)
+# Adding bytom submit
+bytom.add_command(submit)
