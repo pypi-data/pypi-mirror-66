d976bef624b879cb1c0734c0243d84363363257c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package predictionscorer/0.1.0/predictionscorer-0.1.0.tar.gz

---
+++ packages/predictionscorer/predictionscorer-0.1.0.tar.gz/predictionscorer-0.1.0/CONTRIBUTING.md
@@ -0,0 +1,27 @@
+# Create virtual environment
+
+`python3 -m venv venv`
+
+# Activate virtual environment
+
+`source venv/bin/activate.fish`
+
+# Install dependencies
+
+`pip install -r dev-requirements.txt`
+
+# Set up pre-commit hooks
+
+`pre-commit install`
+
+# Check pre-commit hooks
+
+`pre-commit run --all-files`
+
+# Prepare for tests
+
+`pip install -e .`
+
+# Run tests
+
+`pytest`
---
+++ packages/predictionscorer/predictionscorer-0.1.0.tar.gz/predictionscorer-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    author="Yngve Hoiseth",
+    author_email="yngve@hoiseth.net",
+    description="Python library to score predictions",
+    include_package_data=True,
+    license="MIT",
+    name="predictionscorer",
+    packages=find_packages(),
+    url="https://github.com/yhoiseth/python-prediction-scorer",
+    version="0.1.0",
+    zip_safe=False,
+)
