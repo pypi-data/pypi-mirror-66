c8c09dca8e0d21d7f4d9335254a6548170d1db94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:02 +0000

    Add package cterasdk/1.1.1/cterasdk-1.1.1-py3-none-any.whl

---
+++ packages/cterasdk/cterasdk-1.1.1-py3-none-any.whl/cterasdk/core/login.py
@@ -0,0 +1,33 @@
+import logging
+
+from .base_command import BaseCommand
+from . import session
+
+
+class Login(BaseCommand):
+    """
+    Portal Login APIs
+    """
+
+    def login(self, username, password):
+        """
+        Log into the portal
+
+        :param str username: User name to log in
+        :param str password: User password
+        """
+        self._portal.form_data('/login', {'j_username': username, 'j_password': password})
+
+        logging.getLogger().info("User logged in. %s", {'host': self._portal.host(), 'user': username})
+
+        session.activate(self._portal)
+
+    def logout(self):
+        """
+        Log out of the portal
+        """
+        self._portal.form_data('/logout', {})
+
+        logging.getLogger().info("User logged out. %s", {'host': self._portal.host()})
+
+        session.terminate(self._portal)
