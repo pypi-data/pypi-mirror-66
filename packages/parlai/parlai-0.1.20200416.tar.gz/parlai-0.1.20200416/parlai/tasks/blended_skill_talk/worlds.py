46fb1b2d215882ba67a02e7799b7ed020c55142a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package parlai/0.1.20200416/parlai-0.1.20200416.tar.gz

---
+++ packages/parlai/parlai-0.1.20200416.tar.gz/parlai-0.1.20200416/parlai/tasks/blended_skill_talk/worlds.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import json
+import random
+import os
+
+from parlai.tasks.interactive.worlds import InteractiveWorld as InteractiveBaseWorld
+from parlai.tasks.self_chat.worlds import SelfChatWorld as SelfChatBaseWorld
+
+
+def load_personas(opt):
+    print('[ loading personas.. ]')
+    fname = os.path.join(
+        opt['datapath'], 'blended_skill_talk', opt['datatype'].split(':')[0] + '.json'
+    )
+    with open(fname) as json_file:
+        data = json.load(json_file)
+    contexts = []
+    for d in data:
+        context1 = []
+        context2 = []
+        if opt.get('include_personas', True):
+            context1.append('your persona: ' + d['personas'][0][0])
+            context1.append('your persona: ' + d['personas'][0][1])
+            context2.append('your persona: ' + d['personas'][1][0])
+            context2.append('your persona: ' + d['personas'][1][1])
+        if d['context_dataset'] == 'wizard_of_wikipedia':
+            context1.append(d['additional_context'])
+            context2.append(d['additional_context'])
+        if opt.get('include_initial_utterances', True):
+            context1.append(d['free_turker_utterance'])
+            context2.append(d['free_turker_utterance'])
+            context1.append(d['guided_turker_utterance'])
+            context2.append(d['guided_turker_utterance'])
+        c1 = '\n'.join(context1)
+        c2 = '\n'.join(context2)
+        contexts.append([c1, c2])
+    return contexts
+
+
+class InteractiveWorld(InteractiveBaseWorld):
+    @staticmethod
+    def add_cmdline_args(argparser):
+        parser = argparser.add_argument_group('BST Interactive World')
+        parser.add_argument(
+            '--display-partner-persona',
+            type='bool',
+            default=True,
+            help='Display your partner persona at the end of the chat',
+        )
+        parser.add_argument(
+            '--include-personas',
+            type='bool',
+            default=True,
+            help='Include personas as input context, or not',
+        )
+        parser.add_argument(
+            '--include-initial-utterances',
+            type='bool',
+            default=True,
+            help='Include context conversation at beginning or not',
+        )
+
+    def __init__(self, opt, agents, shared=None):
+        super().__init__(opt, agents, shared)
+        self.display_partner_persona = self.opt['display_partner_persona']
+
+    def init_contexts(self, shared=None):
+        self.contexts_data = load_personas(self.opt)
+
+    def get_contexts(self):
+        random.seed()
+        p = random.choice(self.contexts_data)
+        return p[0], p[1]
+
+    def finalize_episode(self):
+        print("\nCHAT DONE.\n")
+        if self.display_partner_persona:
+            partner_persona = self.p2.replace('your persona:', 'partner\'s persona:')
+            print(f"Your partner was playing the following persona:\n{partner_persona}")
+        print("[ Preparing new chat ... ]\n")
+
+
+class SelfChatWorld(SelfChatBaseWorld):
+    def add_cmdline_args(argparser):
+        parser = argparser.add_argument_group('BST SelfChat World')
+        parser.add_argument(
+            '--include-personas',
+            type='bool',
+            default=True,
+            help='Include personas as input context, or not',
+        )
+        parser.add_argument(
+            '--include-initial-utterances',
+            type='bool',
+            default=True,
+            help='Include context conversation at beginning or not',
+        )
+
+    def init_contexts(self, shared=None):
+        self.contexts_data = load_personas(self.opt)
+
+    def get_contexts(self):
+        random.seed()
+        p = random.choice(self.contexts_data)
+        return [p[0], p[1]]
