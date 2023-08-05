1cfd2b0f636e1a0b21d17e6294179303ac5e08d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package py-pde/0.6.1/py-pde-0.6.1.tar.gz

---
+++ packages/py-pde/py-pde-0.6.1.tar.gz/py-pde-0.6.1/docs/source/manual/index.rst
@@ -0,0 +1,12 @@
+User manual
+===========
+
+
+.. toctree::
+   :maxdepth: 3
+    
+   
+   mathematical_basics
+   advanced_usage
+   contributing
+   code_of_conduct
---
+++ packages/py-pde/py-pde-0.6.1.tar.gz/py-pde-0.6.1/examples/poisson_eq_1d.py
@@ -0,0 +1,14 @@
+"""
+Solving Poisson's equation in 1d
+================================
+
+This example shows how to solve a 1d Poisson equation with boundary conditions.
+"""
+
+from pde import CartesianGrid, ScalarField
+
+grid = CartesianGrid([[0, 1]], 32, periodic=False)
+field = ScalarField(grid, 1)
+result = field.solve_poisson([{'value': 0}, {'derivative': 1}])
+
+result.plot()
