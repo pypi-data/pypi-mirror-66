dba489561713a2dd6485ab71b2c72b168d3b5b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:40 +0000

    Add package tfkit/0.2.23/tfkit-0.2.23-py3-none-any.whl

---
+++ packages/tfkit/tfkit-0.2.23-py3-none-any.whl/tfkit/test/test_zeval.py
@@ -0,0 +1,23 @@
+import unittest
+
+import os
+
+
+class TestEval(unittest.TestCase):
+
+    def testHelp(self):
+        result = os.system('tfkit-eval -h')
+        assert (result == 0)
+
+    def testEvalOnGen(self):
+        result = os.system(
+            'tfkit-eval --model ./cache/10.pt --valid ../demo_data/generate.csv --metric classification --print')
+        print(result)
+        result = os.system(
+            'tfkit-eval --model ./cache/10.pt --valid ../demo_data/generate.csv --metric emf1 --print --beamsearch --outfile')
+        print(result)
+
+    def testEvalClassify(self):
+        result = os.system(
+            'tfkit-eval --model ./cache/10.pt --valid ../demo_data/classification.csv --metric classification --print')
+        print(result)
