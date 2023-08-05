85bd468b1ee2dfdaf73d838f8a10cc8ab6865922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package ssmode/0.1.6/ssmode-0.1.6.tar.gz

---
+++ packages/ssmode/ssmode-0.1.6.tar.gz/ssmode-0.1.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ssmode",
+    version="0.1.6",
+    author="SkySelect, Inc.",
+    author_email="oss@skyselect.com",
+    description="Tools for use in Mode Analytics Jupyter Notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skyselect/ssmode",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/ssmode/ssmode-0.1.6.tar.gz/ssmode-0.1.6/ssmode/line_chart.py
@@ -0,0 +1,20 @@
+from .constants import colors
+
+def style_line_chart(ptl_fig, ytitle=''):
+  # Add axis title and style the legend
+  ptl_fig.layout.yaxis = {"title": ytitle, "titlefont": {"size": 12}}
+  ptl_fig.layout.legend = {"xanchor":"center", "yanchor":"top", "x":0.5,"y":-0.15, "orientation":"h"}
+  ptl_fig.layout.font = dict(family='Graphik, Arial, sans-serif', size=11, color='#666666')
+
+  # Color the bars, need to modify color library with more colors
+  i=0
+  for line in ptl_fig.data:
+      line.marker = {"color": colors[i % len(colors)]}
+      line.textfont={"size": 11, "color": "#FFFFFF", "family": "Graphik, Arial, sans-serif"}
+      line.hoverinfo='text'
+      i+=1
+
+  # Format hovering
+  ptl_fig.layout.hovermode = "x"
+  ptl_fig.layout.hoverlabel = {"namelength": -1, "bgcolor":"#F1F2F5"}
+  return ptl_fig
