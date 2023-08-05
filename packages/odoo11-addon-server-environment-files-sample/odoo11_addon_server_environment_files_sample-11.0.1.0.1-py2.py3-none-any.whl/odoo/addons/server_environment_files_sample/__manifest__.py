66d580a35ee64f1d3684c17c5071daabc4fe14ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:09 +0000

    Add package odoo11-addon-server-environment-files-sample/11.0.1.0.1/odoo11_addon_server_environment_files_sample-11.0.1.0.1-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-server-environment-files-sample/odoo11_addon_server_environment_files_sample-11.0.1.0.1-py2.py3-none-any.whl/odoo/addons/server_environment_files_sample/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright Wingo SA
+# Copyright 2018 Camptocamp (https://www.camptocamp.com).
+# License GPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Example server configuration environment files repository module",
+    "version": "11.0.1.0.1",
+    "depends": ["base"],
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "summary": "sample config file for server_environment",
+    "website": "http://github.com/OCA/server-env",
+    "license": "GPL-3 or any later version",
+    "category": "Tools",
+    "preloadable": False,
+    "data": [],
+}
