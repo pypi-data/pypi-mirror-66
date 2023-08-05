5af0564f1a306b1f85450377b88a19c578b95647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package django-jbank/3.2.11/django-jbank-3.2.11.tar.gz

---
+++ packages/django-jbank/django-jbank-3.2.11.tar.gz/django-jbank-3.2.11/jbank/management/commands/parse_xp.py
@@ -0,0 +1,31 @@
+import logging
+from django.core.management import CommandParser
+from jbank.files import list_dir_files
+from jbank.helpers import process_pain002_file_content
+from jbank.models import PayoutStatus
+from jutil.command import SafeCommand
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(SafeCommand):
+    help = 'Parses pain.002 payment response .XP files and updates Payout status'
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument('path', type=str)
+        parser.add_argument('--test', action='store_true')
+        parser.add_argument('--verbose', action='store_true')
+        parser.add_argument('--suffix', type=str, default='XP')
+
+    def do(self, *args, **options):
+        files = list_dir_files(options['path'], '.' + options['suffix'])
+        for f in files:
+            if PayoutStatus.objects.is_file_processed(f):
+                if options['verbose']:
+                    print('Skipping processed payment status file', f)
+                continue
+            if options['verbose']:
+                print('Importing payment status file', f)
+            with open(f, 'rb') as fp:
+                process_pain002_file_content(fp.read(), f)
