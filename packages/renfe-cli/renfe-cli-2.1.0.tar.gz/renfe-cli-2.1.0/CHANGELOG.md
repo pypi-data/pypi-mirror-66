98b9d2f4828c4ae812b2072d9b1a3fd861d82053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:47 +0000

    Add package renfe-cli/2.1.0/renfe-cli-2.1.0.tar.gz

---
+++ packages/renfe-cli/renfe-cli-2.1.0.tar.gz/renfe-cli-2.1.0/CHANGELOG.md
@@ -0,0 +1,63 @@
+<!--
+ Copyright 2019 @gerardcl. All rights reserved.
+ Use of this source code is governed by a BSD-style
+ license that can be found in the LICENSE file.
+-->
+
+# Changelog
+
+## v2.1.0 (2020-04-18)
+
+* Security fixes
+
+* Stable versioning
+
+* HTTPS calls
+
+
+## v2.0.0 (2019-05-08)
+
+* Python lib-like refactor
+
+* Users can define custom origin and destination stations defaults. Closes [#2](https://github.com/gerardcl/renfe-cli/issues/2)
+
+* Enhanced code coverage
+
+
+## v1.1.7 (2019-05-05)
+
+* Codecoverage: Travis CI with SonarCloud integration
+
+## v1.1.6 (2019-05-05)
+
+* Travis CI automated deployment to PyPI through Linux job when new tag released. Closes [#6](https://github.com/gerardcl/renfe-cli/issues/6)
+
+## v1.1.2 (2019-05-05)
+
+* Reverting Travis CI deploy stage from previous release and opened issue [#5](https://github.com/gerardcl/renfe-cli/issues/5)
+
+## v1.1.1 (2019-05-05)
+
+* Travis CI deploying releases through git tags to PyPI
+
+## v1.1.0 (2019-05-05)
+
+* Enhanced logging and debug options
+* Error handling
+* Pre-checks
+* PyPi with RST formatted readme [#3](https://github.com/gerardcl/renfe-cli/issues/3)
+
+## v1.0.2 (2019-05-04)
+
+* Defaults overwritting solved [#4](https://github.com/gerardcl/renfe-cli/issues/4)
+
+## v1.0.1 (2019-05-04)
+
+* Travis CI integration
+* Travis CI tests working within Windows, OSX and Linux
+* Readme update
+
+## v1.0.0 (2019-05-03)
+
+* Creation of python package.
+* Test commands.
---
+++ packages/renfe-cli/renfe-cli-2.1.0.tar.gz/renfe-cli-2.1.0/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+
+try:
+    from pypandoc import convert_file
+    read_md = lambda f: convert_file(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+setup(
+    name='renfe-cli',
+    version='2.1.0',
+    description='Get faster RENFE Spanish Trains timetables in your terminal',
+    long_description=read_md('README.md'),
+    keywords='Get faster RENFE Spanish Trains timetables terminal',
+    author='Gerard Castillo',
+    author_email='gerardcl@gmail.com',
+    url='https://github.com/gerardcl/renfe-cli',
+    license='BSD',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    py_modules=['renfe-cli'],
+    include_package_data=True,
+    install_requires=[
+        'setuptools<46.2.0',
+        'lxml<=4.5.0',
+        'numpy<1.18.3',
+        'pandas<1.0.4',
+        'python-dateutil<=2.8.1',
+        'pytz<=2019.3',
+        'six<=1.14.0',
+        'colorama<=0.4.3'
+    ],
+    entry_points="""
+        [console_scripts]
+        renfe-cli = renfe.cli:main
+        """,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Terminals',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    tests_require=['nose'],
+    test_suite = 'nose.collector',
+)
