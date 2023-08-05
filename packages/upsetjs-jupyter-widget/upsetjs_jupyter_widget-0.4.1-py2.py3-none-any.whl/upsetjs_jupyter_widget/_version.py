d7d979b1a422c1b41584d5c938538ff2114cb04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:28 +0000

    Add package upsetjs-jupyter-widget/0.4.1/upsetjs_jupyter_widget-0.4.1-py2.py3-none-any.whl

---
+++ packages/upsetjs-jupyter-widget/upsetjs_jupyter_widget-0.4.1-py2.py3-none-any.whl/upsetjs_jupyter_widget/_version.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# Copyright (c) Samuel Gratzl.
+"""
+UpSet.js Jupyter Widget
+"""
+
+version_info = (0, 4, 1)  # pylint: disable=C0103
+__version__ = ".".join(map(str, version_info))
