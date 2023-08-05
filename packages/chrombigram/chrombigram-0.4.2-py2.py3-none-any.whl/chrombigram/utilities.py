68927242a215f330872132e4edbf52ecba3dcd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:10 +0000

    Add package chrombigram/0.4.2/chrombigram-0.4.2-py2.py3-none-any.whl

---
+++ packages/chrombigram/chrombigram-0.4.2-py2.py3-none-any.whl/chrombigram/utilities.py
@@ -0,0 +1,34 @@
+import mido
+
+def getpcs_music21(m21):
+    allpcs = []
+    for x in m21.chordify().flat.notes:
+        pcs = []
+        for n in x:
+            pcs.append(n.pitch.pitchClass)
+        pcs = frozenset(pcs)
+        allpcs.append(pcs)
+    return allpcs
+
+
+def getpcs_midi(filename):
+    """ Returns a list of notes from the note_on events of a MIDI file. """
+    mid = mido.MidiFile(filename)
+    slices = []
+    slic = []
+    for msg in mid:
+        if hasattr(msg, 'time') and msg.time > 0.0:
+            if slic:
+                slices.append(slic)
+                slic = []
+        elif msg.type == 'note_on' and msg.velocity > 0:
+            # Add to the current slice
+            if slic:
+                slic.append(msg.note % 12)
+            # Start a new slice
+            else:
+                slic = [msg.note % 12]
+    if slic:
+        slices.append(slic)
+    pcs = [frozenset(slic) for slic in slices]
+    return pcs
