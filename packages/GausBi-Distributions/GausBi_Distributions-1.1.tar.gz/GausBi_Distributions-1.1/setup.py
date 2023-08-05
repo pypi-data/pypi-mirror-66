83573df4eaa0b1ca30a18df9e1afcb412b7239a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:23 +0000

    Add package GausBi-Distributions/1.1/GausBi_Distributions-1.1.tar.gz

---
+++ packages/GausBi-Distributions/GausBi_Distributions-1.1.tar.gz/GausBi_Distributions-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='GausBi_Distributions',
+      version='1.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['GausBi_Distributions'],
+      author = 'CM',
+      zip_safe=False)
