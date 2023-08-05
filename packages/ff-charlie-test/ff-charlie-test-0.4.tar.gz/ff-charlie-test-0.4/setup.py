45457f405c1b0827dc6e0cb94b91dd391eea7679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:27 +0000

    Add package ff-charlie-test/0.4/ff-charlie-test-0.4.tar.gz

---
+++ packages/ff-charlie-test/ff-charlie-test-0.4.tar.gz/ff-charlie-test-0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'ff-charlie-test',
+  version = '0.4',
+  author = 'Tom Maslen',
+  author_email = 'tom_maslen@hotmail.com',
+  description = 'Service agnostic featureflag client',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/tmaslen/featureflagclient-python',
+  keywords = ['feature flag', 'feature toggle'],
+  packages = setuptools.find_packages(),
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License", #update
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
