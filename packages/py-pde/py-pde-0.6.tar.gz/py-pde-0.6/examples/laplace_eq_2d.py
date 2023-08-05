2a674235625096cb98e1a550c1e726dc5adffa05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package py-pde/0.6/py-pde-0.6.tar.gz

---
+++ packages/py-pde/py-pde-0.6.tar.gz/py-pde-0.6/examples/laplace_eq_2d.py
@@ -0,0 +1,17 @@
+"""
+Solving Laplace's equation in 2d
+================================
+
+This example shows how to solve a 2d Laplace equation with spatially varying
+boundary conditions.
+"""
+
+import numpy as np
+from pde import CartesianGrid, ScalarField
+
+grid = CartesianGrid([[0, 2 * np.pi]] * 2, 64)
+bcs = [{'value': 'sin(y)'}, {'value': 'sin(x)'}]
+
+field = ScalarField(grid)
+res = field.solve_poisson(bcs)
+res.plot()
