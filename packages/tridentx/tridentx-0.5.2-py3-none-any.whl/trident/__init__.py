092fffa56b7ba294b0750d0ea54d95aacc60fc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:11 +0000

    Add package tridentx/0.5.2/tridentx-0.5.2-py3-none-any.whl

---
+++ packages/tridentx/tridentx-0.5.2-py3-none-any.whl/trident/__init__.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+
+import sys
+from importlib import reload
+from sys import stderr
+
+defaultencoding = 'utf-8'
+if sys.getdefaultencoding() != defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
+
+
+
+__version__ = '0.5.2'
+stderr.write('trident {0}\n'.format(__version__))
+from .backend import *
+from trident import models
+from trident import misc
+from trident import callbacks
+from trident import data
+
+import threading
+
+
+
