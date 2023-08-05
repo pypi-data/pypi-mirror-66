0465a514856998c492b7817547e6a9b6af2093d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package bpda/10.0/bpda-10.0.tar.gz

---
+++ packages/bpda/bpda-10.0.tar.gz/bpda-10.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(name='bpda',
+      version='10.0',
+      description='BingPanDevApi.py: Make Old Bing Pics Can Access',
+      author='Jack',
+      author_email='admin@rdpstudo.top',
+      maintainer='Jack',
+      maintainer_email='admin@rdpstudo.top',
+      url=' http://rdpstudio.top',
+      packages=setuptools.find_packages(),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license="Public domain",
+      install_requires=['requests'],
+      platforms=["any"],
+     )
