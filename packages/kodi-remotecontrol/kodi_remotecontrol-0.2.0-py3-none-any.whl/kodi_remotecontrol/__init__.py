4faa0c59b8a8d2f62440614bd6c2572d3ba275c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:35 +0000

    Add package kodi-remotecontrol/0.2.0/kodi_remotecontrol-0.2.0-py3-none-any.whl

---
+++ packages/kodi-remotecontrol/kodi_remotecontrol-0.2.0-py3-none-any.whl/kodi_remotecontrol/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.2.0"
+
+from kodi_remotecontrol.eventclient import EventClient
+from kodi_remotecontrol.httpclient import HttpClient
+from kodi_remotecontrol.navigation import Navigation
+from kodi_remotecontrol.player import Player
+from kodi_remotecontrol.subtitle import Subtitle
+from kodi_remotecontrol.audio import Audio
