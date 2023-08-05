768e722e89d23851d6c3a5d5ac823afbe4def2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:55 +0000

    Add package aiomirai/0.0.2/aiomirai-0.0.2-py3-none-any.whl

---
+++ packages/aiomirai/aiomirai-0.0.2-py3-none-any.whl/aiomirai/api.py
@@ -0,0 +1,34 @@
+"""
+此模块提供了 Mirai API HTTP 的接口类。
+"""
+
+import abc
+import functools
+from typing import Callable, Any, Union, Awaitable, Dict
+
+
+class Api:
+    @abc.abstractmethod
+    def call_action(self, action: str, **params) -> Union[Awaitable[Any], Any]:
+        pass
+
+    def __getattr__(self,
+                    item: str) -> Callable[..., Union[Awaitable[Any], Any]]:
+        """获取一个可调用对象，用于调用对应 API。"""
+        return functools.partial(self.call_action, item)
+
+
+class SessionApi(Api):
+    @abc.abstractmethod
+    def auth(self) -> Union[Awaitable[Dict[str, Any]], Dict[str, Any]]:
+        pass
+
+
+class AsyncApi(Api):
+    @abc.abstractmethod
+    async def call_action(self, action: str, **params) -> Any:
+        pass
+
+
+class AsyncSessionApi(SessionApi, AsyncApi):
+    pass
