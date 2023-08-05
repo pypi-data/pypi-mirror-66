9272990d69c11b78abd856cf41b0ad780454d5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:07 +0000

    Add package say22/0.1/say22-0.1.tar.gz

---
+++ packages/say22/say22-0.1.tar.gz/say22-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import sys
+
+pkg_data = {}
+
+if sys.platform == 'win32':
+  pkg_data = {'say_something': ['SETInterfaceCS.dll','interface_cpp.hpp','interface_cpp.so','c_models.h']}
+
+if sys.platform in ['darwin', 'linux', 'bsd', 'sunos']:
+  pkg_data = {'say_something': ['SETInterfaceCS2.dll','interface_cpp2.hpp','interface_cpp2.so','c_models2.h']}
+
+setup(
+    name="say22",
+    version="0.1",
+    packages = find_packages(exclude = ['say_something.test']),
+    package_data=pkg_data,
+)
