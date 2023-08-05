28d92a0872c8405649831d13ddfa6b98a889946e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package targeted/0.0.25/targeted-0.0.25-pp36-pypy36_pp73-manylinux2010_x86_64.whl

---
+++ packages/targeted/targeted-0.0.25-pp36-pypy36_pp73-manylinux2010_x86_64.whl/targeted/__about__.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+__name__        = "targeted"
+__version__     = "0.0.25"
+__license__     = "Apache Software License"
+__description__ = "Targeted inference"
+__author__      = u"Klaus Kähler Holst"
+__email__       = "klaus.holst@maersk.com"
+__url__         = "https://github.com/kkholts/target"
+__copyright__   = u"Copyright 2019-2020, Klaus Kähler Holst"
+__summary__     = "Targeted inference"
+__keywords__    = "semi-parametric inference, double robust estimator,\
+ model diagnostics, cumulative residuals, generalized linear models"
+
+__all__ = [
+    "__name__", "__summary__", "__description__",
+    "__url__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
