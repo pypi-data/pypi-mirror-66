5c1e77967d2b22f6e23ec089255caca056be7bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:42 +0000

    Add package NPC/1.0/NPC-1.0.tar.gz

---
+++ packages/NPC/NPC-1.0.tar.gz/NPC-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='NPC',
+    version='1.0',
+    description='Parallel Processing in Network Devices',
+    long_description="""
+         Documentation => https://ntp.perity.site/
+    """,
+    url='https://ntp.perity.site',
+    author='Dipnarayan Das',
+    author_email='dipnarayan.das35@gmail.com',
+    license='License.txt',
+    py_modules=[""],
+   	package=[''],
+   	include_package_data=True,
+    zip_safe=False
+)
