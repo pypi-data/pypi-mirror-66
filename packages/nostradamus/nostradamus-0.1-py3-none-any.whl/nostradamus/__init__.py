2622c059fefa70dd5f87345a9cc1a7a97f7fb298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:14 +0000

    Add package nostradamus/0.1/nostradamus-0.1-py3-none-any.whl

---
+++ packages/nostradamus/nostradamus-0.1-py3-none-any.whl/nostradamus/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2020-present, Naftali Cohen
+# All rights reserved.
+
+from nostradamus.forecaster import Nostradamus
+
+__version__ = '0.1'
---
+++ packages/nostradamus/nostradamus-0.1-py3-none-any.whl/nostradamus/example.py
@@ -0,0 +1,10 @@
+#
+
+import sys
+import numpy as np
+import pandas as pd
+
+sys.path.append('/Users/naftali/')  # path to nostradamus library
+from nostradamus.forecaster import Nostradamus
+
+Nostradamus()
