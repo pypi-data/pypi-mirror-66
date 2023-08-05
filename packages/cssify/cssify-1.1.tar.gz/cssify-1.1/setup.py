0e4bfe041139f369a5ae61b3dbad643047d06672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package cssify/1.1/cssify-1.1.tar.gz

---
+++ packages/cssify/cssify-1.1.tar.gz/cssify-1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cssify',
+      version='1.1',
+      license='MIT',
+      description='Convert XPATH locators to CSS',
+      author='Santiago Suarez Ordonez',
+      author_email='santiycr@gmail.com',
+      url = 'https://github.com/santiycr/cssify',
+      download_url = 'https://github.com/santiycr/cssify/archive/v_10.tar.gz',
+      packages=['cssify'],
+     )
