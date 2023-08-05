c529e0df4edbfb2d37565e6835f3756dc94f95ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:21 +0000

    Add package MKnester/1.0.0/MKnester-1.0.0.tar.gz

---
+++ packages/MKnester/MKnester-1.0.0.tar.gz/MKnester-1.0.0/nester.py
@@ -0,0 +1,18 @@
+movies = ["The Holy Grails", "1975", "Terry Jones & Terry Gillium", 91,
+          ["Graham Chapman",
+           ["Michael Palin", "John Cleese", "Terry Gillium", "Eric Idle", "Terry Johnes"]]]
+""" This is the "nester.py" module,
+and it provides one function called print_lol()
+which prints lists that may or may not include nested lists. """
+
+
+def print_lol(the_list):
+
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+
+print_lol(movies)
---
+++ packages/MKnester/MKnester-1.0.0.tar.gz/MKnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='MKnester',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='mskobal',
+    author_email='mskobal@gmail.com',
+    url='http://www.madhavkobal.com',
+    description='A simple printer of nested lists',
+)
