1763d06cfbf8b335e445e03df7f1c707550239be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:58 +0000

    Add package startpkg/0.1.5/startpkg-0.1.5.tar.gz

---
+++ packages/startpkg/startpkg-0.1.5.tar.gz/startpkg-0.1.5/src/__version__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#                            _                _____ 
+#      /\                   | |       /\     |_   _|
+#     /  \     ____   __ _  | |_     /  \      | |  
+#    / /\ \   |_  /  / _` | | __|   / /\ \     | |  
+#   / ____ \   / /  | (_| | | |_   / ____ \   _| |_ 
+#  /_/    \_\ /___|  \__,_|  \__| /_/    \_\ |_____|
+#                                         
+#                        
+
+
+__title__ = 'startpkg'
+__description__ = 'AzatAI Start Package Tool (startpkg)'
+__url__ = 'https://github.com/AzatAI/startpkg'
+__version__ = '0.1.5'
+__author__ = 'Yaakov Azat'
+__author_email__ = 'yaakovazat@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Azat Artificial Intelligence, LLP (AzatAI)'
