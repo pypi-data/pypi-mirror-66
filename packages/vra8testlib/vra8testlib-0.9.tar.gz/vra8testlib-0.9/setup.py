581a9a8797fc9e56f403c3788f2905d29f39f46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:39 +0000

    Add package vra8testlib/0.9/vra8testlib-0.9.tar.gz

---
+++ packages/vra8testlib/vra8testlib-0.9.tar.gz/vra8testlib-0.9/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name = 'vra8testlib',         # How you named your package folder (MyLib)
+    packages = ['vra8testlib'],   # Chose the same as "name"
+    version = '0.9',      # Start with a small number and increase it with every change you make
+    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description = 'HELLO WORLD',   # Give a short description about your library
+    author = 'MOLLMAN MOLLMAN',                   # Type in your name
+    author_email = 'mollman@uoregon.edu',      # Type in your E-Mail
+    url = 'https://git.uoregon.edu/projects/VRA/repos/jenkins-pipeline-test/browse',   # Provide either the link to your github or to your website
+    # download_url = 'NOT SURE YET',    # I explain this later on
+    keywords = ['HELLO', 'WORLD', 'TEST'],   # Keywords that define your package best
+    install_requires=[            # I get to this in a second
+            'beautifulsoup4',
+        ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    ],
+)
