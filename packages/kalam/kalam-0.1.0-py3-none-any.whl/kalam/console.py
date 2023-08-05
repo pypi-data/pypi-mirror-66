49fc11bff609e0289b01c74a2566a00a7ebf3652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:12 +0000

    Add package kalam/0.1.0/kalam-0.1.0-py3-none-any.whl

---
+++ packages/kalam/kalam-0.1.0-py3-none-any.whl/kalam/console.py
@@ -0,0 +1,12 @@
+"""Command-line interface."""
+
+import click
+
+from . import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main() -> None:
+    """Kalam console."""
+    click.secho("Hello World!", fg="green")
