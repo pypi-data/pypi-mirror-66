30a66dbccdee31c1ca6481d8d5c7b74634150ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:53 +0000

    Add package pymt5adapter/0.3.1/pymt5adapter-0.3.1-py3-none-any.whl

---
+++ packages/pymt5adapter/pymt5adapter-0.3.1-py3-none-any.whl/pymt5adapter/__init__.py
@@ -0,0 +1,15 @@
+import MetaTrader5 as _mt5
+
+from . import types
+from .const import *
+from .context import connected
+from .context import handle_exit
+from .core import *
+from .oem import *
+from .helpers import as_dict_all
+
+__version__ = _mt5.__version__
+__author__ = _mt5.__author__
+
+
+
