c332ee39b324e677ceb798f4b10beb8e76b5dafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:58 +0000

    Add package mphyspy/0.1.7/mphyspy-0.1.7-py3-none-any.whl

---
+++ packages/mphyspy/mphyspy-0.1.7-py3-none-any.whl/mphyspy/qmechanic/schrodinger.py
@@ -0,0 +1,33 @@
+#mphyspy/quantum/schrodinger.py
+import sympy as sy
+import numpy as np
+import mphyspy.qmechanic.constants as ct
+
+def test():
+    print("This is schrodinger module test")
+
+#Schrodinger's Probability Function
+def prob(function,a,b):
+    x = sy.Symbol('x')
+    prob = sy.integrate(function, (x, a,b))
+    return prob
+
+#Return uncertainty when one of values is given
+def uncertainty(x):
+    y = ct.barh/(2*x)
+    return y
+
+#Return velocity from uncertainty eq
+def vel(m,x):
+    v = ct.barh/(2*m*x)
+    return v
+
+#Return Kinetic energy from mass and momentum
+def kinetic(p,m):
+    k = p**2/(2*m)
+    return k
+
+#Return energy from given quantum number
+def energy(n,m,l):
+    e = n**2*np.pi**2*ct.hbar**2/(2*m*(l**2))
+    return e
