3b78ebb5d021f7d3a3b881ed8f3cefbb85b18d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:54 +0000

    Add package dist-udacity-test/0.1/dist_udacity_test-0.1.tar.gz

---
+++ packages/dist-udacity-test/dist_udacity_test-0.1.tar.gz/dist_udacity_test-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_udacity_test',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_udacity_test'],
+      Author = "Luis Rocha",
+      zip_safe=False)
