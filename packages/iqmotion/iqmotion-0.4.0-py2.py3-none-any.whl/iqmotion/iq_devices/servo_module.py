ac042042e011d5c232d1a37d0097ab95bb6192fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package iqmotion/0.4.0/iqmotion-0.4.0-py2.py3-none-any.whl

---
+++ packages/iqmotion/iqmotion-0.4.0-py2.py3-none-any.whl/iqmotion/iq_devices/servo_module.py
@@ -0,0 +1,43 @@
+from iqmotion.iq_devices.iq_module import IqModule
+from iqmotion.iq_devices.ramper import Ramper
+
+
+class ServoModule(IqModule):
+    """ Creates ServoModule object
+    
+    Arguments:
+        com {Communicator} -- The communicator object to interface with the IqModule
+    
+    Keyword Arguments:
+        module_idn {int} -- The idn of the module (default: {0})
+    """
+
+    _MODULE_FILE_NAME = "servo.json"
+
+    def ramp_velocity(self, final_velocity: float, total_time: float, time_steps=20):
+        velocity_client = "multi_turn_angle_control"
+        velocity_client_entry = "ctrl_velocity"
+        success = Ramper.ramp_velocity(
+            self,
+            velocity_client,
+            velocity_client_entry,
+            final_velocity,
+            total_time,
+            time_steps,
+        )
+
+        return success
+
+    def ramp_volts(self, final_volts: float, total_time: float, time_steps=20):
+        volts_client = "multi_turn_angle_control"
+        volts_client_entry = "ctrl_volts"
+        success = Ramper.ramp_volts(
+            self, volts_client, volts_client_entry, final_volts, total_time, time_steps,
+        )
+
+        return success
+
+    def ramp_volts_slew(self, final_volts: float, slew_rate: float):
+        success = Ramper.ramp_volts_slew(self, final_volts, slew_rate)
+
+        return success
