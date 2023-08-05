c7522245c872e015037cf482aabe51a5c6fce2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package stream-learn/0.8.10/stream_learn-0.8.10-py3-none-any.whl

---
+++ packages/stream-learn/stream_learn-0.8.10-py3-none-any.whl/strlearn/ensembles/__init__.py
@@ -0,0 +1,17 @@
+from .SEA import SEA
+from .AWE import AWE
+from .AUE import AUE
+from .WAE import WAE
+from .OnlineBagging import OnlineBagging
+from .OOB import OOB
+from .UOB import UOB
+
+__all__ = [
+    "OnlineBagging",
+    "OOB",
+    "SEA",
+    "UOB",
+    "WAE",
+    "AWE",
+    "AUE"
+]
