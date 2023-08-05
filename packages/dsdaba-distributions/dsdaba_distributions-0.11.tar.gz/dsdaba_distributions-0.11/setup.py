a3467813df63b105738657597459b2e884ad49f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:00 +0000

    Add package dsdaba-distributions/0.11/dsdaba_distributions-0.11.tar.gz

---
+++ packages/dsdaba-distributions/dsdaba_distributions-0.11.tar.gz/dsdaba_distributions-0.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsdaba_distributions',
+      version='0.11',
+      description='Gaussian and Binomial distributions',
+      packages=['dsdaba_distributions'],
+      author='Yuchen Xie',
+      author_email='xie.yuc@husky.neu.edu',
+      zip_safe=False)
