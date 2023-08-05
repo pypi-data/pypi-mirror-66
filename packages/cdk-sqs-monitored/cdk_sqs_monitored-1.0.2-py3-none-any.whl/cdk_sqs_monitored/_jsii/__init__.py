f4b7324638ceb1918312c39a49d5b9c54a90cb1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:33 +0000

    Add package cdk-sqs-monitored/1.0.2/cdk_sqs_monitored-1.0.2-py3-none-any.whl

---
+++ packages/cdk-sqs-monitored/cdk_sqs_monitored-1.0.2-py3-none-any.whl/cdk_sqs_monitored/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudwatch_actions
+import aws_cdk.aws_sns
+import aws_cdk.aws_sns_subscriptions
+import aws_cdk.aws_sqs
+import aws_cdk.core
+import constructs
+
+__all__ = []
+
+publication.publish()
