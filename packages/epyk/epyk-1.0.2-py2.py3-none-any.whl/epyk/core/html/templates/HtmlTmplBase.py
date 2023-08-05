76ac34f80f526a6d0eb1fae816cc62102182129d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package epyk/1.0.2/epyk-1.0.2-py2.py3-none-any.whl

---
+++ packages/epyk/epyk-1.0.2-py2.py3-none-any.whl/epyk/core/html/templates/HtmlTmplBase.py
@@ -0,0 +1,35 @@
+# <span class="fas fa-spinner fa-spin" style="padding:auto;margin:auto;font-size:65px;"></span>
+
+STATIC_PAGE = '''
+<!DOCTYPE html>
+<html lang="en" style="height:100%%">
+<head>
+%(header)s
+%(jsImports)s
+%(cssImports)s
+<style>
+%(cssStyle)s
+</style>
+</head>
+%(body)s
+<script>
+%(jsFrgs)s
+</script>
+</html>
+'''
+
+JUPYTER = '''
+<!DOCTYPE html>
+<HTML>
+<style>
+    %(cssStyle)s
+</style>
+%(cssImports)s
+%(jsImports)s
+
+%(content)s
+<script>
+    %(jsFrgs)s
+</script>
+</HTML>
+'''
