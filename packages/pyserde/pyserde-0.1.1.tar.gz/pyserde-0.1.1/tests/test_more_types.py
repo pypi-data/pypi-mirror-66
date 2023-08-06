8c2a0f90b7d5a5f0b2092b7e08cb2c8a9c5f02a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:36 +0000

    Add package pyserde/0.1.1/pyserde-0.1.1.tar.gz

---
+++ packages/pyserde/pyserde-0.1.1.tar.gz/pyserde-0.1.1/tests/test_more_types.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from decimal import Decimal
+
+import pytest
+
+from serde import deserialize, serialize
+from tests.test_basics import format_json, format_toml, format_yaml
+
+
+@deserialize
+@serialize
+@dataclass
+class Foo:
+    d: Decimal
+
+
+@pytest.mark.parametrize('se,de', format_json + format_yaml + format_toml)  # type: ignore
+def test_decimal(se, de):
+    f = Foo(d=Decimal('0.1'))
+    assert f == de(Foo, se(f))
+
+    f = Foo(d=Decimal(0.1))
+    assert f == de(Foo, se(f))
