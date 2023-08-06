e59552e6bebe36ac63d1d4d54b8ddbb69560f3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:42 +0000

    Add package legitindicators/0.0.18/legitindicators-0.0.18.tar.gz

---
+++ packages/legitindicators/legitindicators-0.0.18.tar.gz/legitindicators-0.0.18/tests/test_supersmoother.py
@@ -0,0 +1,15 @@
+import requests
+from src.legitindicators import super_smoother
+
+BINANCE_URL = "https://api.binance.com/api/v3/klines"
+SYMBOL = "BTCUSDT"
+INTERVAL = "1h"
+PARAMS = {"symbol":SYMBOL, "interval":INTERVAL}
+
+def test_supersmoother():
+    response = requests.get(url=BINANCE_URL, params=PARAMS)
+    data = response.json()
+    close = [float(d[4]) for d in data]
+    ssf = super_smoother(close,10)
+    print(ssf)
+    assert len(ssf) == len(close)
