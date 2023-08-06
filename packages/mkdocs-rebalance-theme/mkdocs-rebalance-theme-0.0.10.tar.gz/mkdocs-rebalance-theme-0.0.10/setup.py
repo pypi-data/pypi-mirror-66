988c187755ce2a3ea4f8d252921d5eda07aabeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:43 +0000

    Add package mkdocs-rebalance-theme/0.0.10/mkdocs-rebalance-theme-0.0.10.tar.gz

---
+++ packages/mkdocs-rebalance-theme/mkdocs-rebalance-theme-0.0.10.tar.gz/mkdocs-rebalance-theme-0.0.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+VERSION = '0.0.10'
+
+setup(
+    name="mkdocs-rebalance-theme",
+    version=VERSION,
+    url='https://github.com/kubilus1/mkdocs-rebalance-theme',
+    license='GPL',
+    description='Rebalance theme for mkdocs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Matt Kubilus',
+    author_email='mattkubilus@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'rebalance = rebalance',
+        ]
+    },
+    zip_safe=False
+)
