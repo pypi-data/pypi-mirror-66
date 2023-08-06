c27ed38a28bf2ed5a82a42429b26d6c165258a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:31 +0000

    Add package pdaltagent/0.1.0/PDaltagent-0.1.0-py3-none-any.whl

---
+++ packages/pdaltagent/PDaltagent-0.1.0-py3-none-any.whl/pdaltagent/tasks.py
@@ -0,0 +1,15 @@
+import pdaltagent.pd as pd
+from requests import HTTPError
+from pdaltagent.config import app
+from celery.utils.log import get_task_logger
+
+@app.task(autoretry_for=(HTTPError,),
+          retry_kwargs={'max_retries': 2,
+                        'countdown': 15})
+def send_to_pd(routing_key, payload):
+    logger = get_task_logger(__name__)
+    logger.info(f"sending to {routing_key}")
+    return pd.send_v2_event(routing_key, payload)
+
+def consume():
+    app.worker_main(['worker', '-A', 'pdaltagent.tasks', '-E', '-l', 'info'])
