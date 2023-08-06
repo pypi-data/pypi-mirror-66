cbdaaddf81fcd660bb807d3131aee4e439709c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:32 +0000

    Add package urequest/0.0.4/urequest-0.0.4-py3-none-any.whl

---
+++ packages/urequest/urequest-0.0.4-py3-none-any.whl/urequest/__init__.py
@@ -0,0 +1,6 @@
+"""Provides user-friendly HTTP client with clean objects."""
+
+__author__: str = "Volodymyr Yahello"
+__email__: str = "vyahello@gmail.com"
+__version__: str = "0.0.4"
+__license__: str = "MIT License"
