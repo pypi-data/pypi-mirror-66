ca59986a31636e6f88e1f88a7967f4d95f50fdf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:23 +0000

    Add package bpmappers/1.1/bpmappers-1.1.tar.gz

---
+++ packages/bpmappers/bpmappers-1.1.tar.gz/bpmappers-1.1/setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import setup
+
+
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
+
+
+setup(
+    name='bpmappers',
+    version='1.1',
+    description='bpmappers is a Python moudle that maps Python dictionary '
+    'values and object properties to different dictionary.',
+    long_description=read_file('README.rst'),
+    author='BeProud Inc.',
+    author_email='tokibito@gmail.com',
+    url='https://bpmappers.readthedocs.io/en/stable/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=['bpmappers'],
+    keywords=['model', 'mapper', 'django'],
+    license='MIT License',
+    install_requires=['six'],
+    extras_require={
+        'django': ['Django'],
+        'develop': [
+            'Django', 'pytest', 'flake8', 'pytest-django',
+            'pytest-pythonpath', 'tox',
+        ],
+    },
+)
