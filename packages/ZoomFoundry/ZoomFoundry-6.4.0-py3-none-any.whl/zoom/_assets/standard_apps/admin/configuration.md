cb6b9f3e6a8bd535d954ad042040dfadd1fc2818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:39 +0000

    Add package ZoomFoundry/6.4.0/ZoomFoundry-6.4.0-py3-none-any.whl

---
+++ packages/ZoomFoundry/ZoomFoundry-6.4.0-py3-none-any.whl/zoom/_assets/standard_apps/admin/configuration.md
@@ -0,0 +1,34 @@
+Configuration
+====
+
+Site
+----
+
+CSRF Validation
+: {request.site.csrf_validation}
+
+Profiling
+: {profiling}
+
+Paths
+----
+
+Instance
+: {request.instance}
+
+Site
+: {request.site.path}
+
+Theme
+: {request.site.theme_path}
+
+App
+: {request.app.path}
+
+Apps Paths
+: {apps_paths}
+
+
+Packages
+----
+: {packages}
---
+++ packages/ZoomFoundry/ZoomFoundry-6.4.0-py3-none-any.whl/zoom/browse.py
@@ -0,0 +1,171 @@
+"""
+    zoom.browse
+"""
+
+from zoom.utils import name_for, sorted_column_names, Record
+from zoom.components import as_actions
+from zoom.html import tag, div
+
+
+def browse(data, **kwargs):
+    """browse data"""
+
+    def getcol(item, index):
+        if isinstance(item, dict):
+            return item.get(index, None)
+        elif isinstance(item, (tuple, list)):
+            return item[index]
+        else:
+            return getattr(item, index)
+
+    labels = kwargs.get('labels')
+    fields = kwargs.get('fields')
+    columns = kwargs.get('columns')
+    title = kwargs.get('title')
+    actions = kwargs.get('actions', [])
+    header = kwargs.get('header')
+    footer = kwargs.get('footer', '')
+
+    items = list(data)
+
+    if labels:
+        if not columns:
+            if len(items) and hasattr(items[0], 'get'):
+                columns = [name_for(label).lower() for label in labels]
+
+            elif len(items) and hasattr(items[0], '__len__') and len(items[0]):
+                columns = range(len(labels))
+
+            else:
+                columns = [name_for(label).lower() for label in labels]
+
+    else:
+        if columns:
+            labels = columns
+        else:
+            if len(items) and isinstance(items[0], Record):
+                labels = columns = sorted_column_names(set([
+                    a for item in items
+                    for a in item.attributes()
+                    if not a.startswith('__')
+                ]))
+
+            elif (len(items) and hasattr(items[0], 'keys') and
+                  callable(getattr(items[0], 'keys'))):
+                # list of dicts
+                labels = columns = [
+                    a for a in sorted_column_names(items[0].keys())
+                    if not a.startswith('__')
+                ]
+
+            elif len(items) and hasattr(items[0], '__dict__'):
+                # list of objects
+                labels = columns = [items[0].__dict__.keys()]
+
+            elif hasattr(data, 'cursor'):
+                # Result object
+                labels = [c[0] for c in data.cursor.description]
+                columns = range(len(labels))
+
+            elif len(items) and hasattr(items[0], '__len__') and len(items[0]):
+                # list of lists?
+                labels = items[0]
+                columns = range(len(items[0]))
+                items = items[1:]
+
+            else:
+                if len(items):
+                    raise Exception('%s' % hasattr(items[0],'__len__'))
+                return '<div class="baselist"><table><tbody><tr><td>None</td></th></tbody></table></div>'
+
+    columns = list(columns)
+    labels = list(labels)
+    invisible = []
+
+    if fields:
+        invisible_labels = []
+        lookup = fields.as_dict()
+
+        for n, col in enumerate(columns):
+
+            if col in lookup:
+                field = lookup[col]
+                better_label = field.label
+                visible = field.visible and field.browse
+            else:
+                better_label = None
+                visible = True
+
+            if better_label:
+                if n > len(labels):
+                    labels.append(better_label)
+                else:
+                    labels[n] = better_label
+
+            if not visible:
+                invisible.append(col)
+                invisible_labels.append(n)
+
+        for n in reversed(invisible_labels):
+            del labels[n]
+
+        alist = []
+        for item in items:
+            fields.initialize(item)
+            flookup = fields.display_value()
+            row = [flookup.get(col, getcol(item, col)) for col in columns if col not in invisible]
+            alist.append(row)
+    else:
+        alist = [[getcol(item, col) for col in columns] for item in items]
+
+    t = []
+    if labels:
+        t.append('<thead><tr>')
+
+        colnum = 0
+        for label in labels:
+            colnum += 1
+            t.append('<th>%s</th>' % label)
+
+        t.append('</tr></thead>')
+
+    t.append('<tbody>')
+
+    count = 0
+    for row in alist:
+        count += 1
+        t.append('<tr id="row-%s">' % (count))
+
+        for item in row:
+            try:
+                value = str(item)
+            except Exception:
+                value = repr(item)
+            wrapping = len(value) < 80 and ' nowrap' or ''
+            cell_tpl = '<td{}>%s</td>'.format(wrapping)
+            t.append(cell_tpl % value)
+
+        t.append('</tr>')
+
+    t.append('</tbody>')
+
+    if not count:
+        t.append('<tr><td colspan=%s>None</td></tr>' % len(labels))
+
+    if not header:
+        if title:
+            header = '<div class="title">%s</div>' % title
+        if actions:
+            header += as_actions(actions)
+
+    header_body = header and ('<div class="header">%s</div>' % header) or ''
+    footer_body = footer and ('<div class="footer">%s</div>' % footer) or ''
+
+    result = '\n'.join(
+        ['<div class="baselist">'] +
+        [header_body] +
+        ['<table>'] + t + ['</table>'] +
+        [footer_body] +
+        ['</div>']
+    )
+    return result
