3ec6a64b8f897244af49f52f2eb0b75b52ba6840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:47 +0000

    Add package isos-environment-test/0.0.3/isos_environment_test-0.0.3-py3-none-any.whl

---
+++ packages/isos-environment-test/isos_environment_test-0.0.3-py3-none-any.whl/isos-env-test/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# IIIIII  SSSS  OOOOO  SSSS     Isos Technology
+#   II    S     O   O  S        https://www.isostech.com/
+#   II    SSSS  O   O  SSSS     April 2020
+#   II       S  O   O     S     sky.moore@isostech.com
+# IIIIII  SSSS  OOOOO  SSSS
