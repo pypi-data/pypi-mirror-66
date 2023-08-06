05c7c84ea8ad5726cabbb96547385f7eb47d9d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:53 +0000

    Add package python-reapy/0.6.0/python-reapy-0.6.0.tar.gz

---
+++ packages/python-reapy/python-reapy-0.6.0.tar.gz/python-reapy-0.6.0/reapy/errors.pyi
@@ -0,0 +1,56 @@
+"""Define custom errors."""
+
+import typing as ty
+
+
+class DisabledDistAPIError(Exception):
+    ...
+
+
+class DisabledDistAPIWarning(Warning):
+    ...
+
+
+class DisconnectedClientError(Exception):
+    ...
+
+
+class DistError(Exception):
+    ...
+
+
+class ExtensionNotFoundError(Exception):
+    def __init__(self, extension: str, url: str): ...
+
+
+class InsideREAPERError(Exception):
+    ...
+
+
+class OutsideREAPERError(Exception):
+    ...
+
+
+class RedoError(Exception):
+    ...
+
+
+class UndefinedEnvelopeError(Exception):
+    def __init__(self, index: ty.Optional[int], name: ty.Optional[str],
+                 chunk_name: ty.Optional[str]) -> None:
+        ...
+
+
+class UndefinedExtStateError(Exception):
+    def __init__(self, key: object) -> None:
+        ...
+
+
+class UndefinedFXParamError(Exception):
+    def __init__(self, fx_name: str, name: str) -> None:
+        ...
+
+
+class UndoError(Exception):
+    def __init__(self) -> None:
+        ...
