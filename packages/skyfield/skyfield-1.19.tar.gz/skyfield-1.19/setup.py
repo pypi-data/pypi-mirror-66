6977dc194bd412c44a29f63dc0c80138accc1ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:57 +0000

    Add package skyfield/1.19/skyfield-1.19.tar.gz

---
+++ packages/skyfield/skyfield-1.19.tar.gz/skyfield-1.19/setup.py
@@ -0,0 +1,52 @@
+from distutils.core import setup
+import skyfield  # safe, because __init__.py contains no import statements
+
+extras = {
+    'tests': [
+        # TODO: add assay
+        'pytz',
+    ],
+}
+
+setup(
+    name='skyfield',
+    version=skyfield.__version__,
+    description=skyfield.__doc__.split('\n', 1)[0],
+    long_description=open('README.rst', 'rb').read().decode('utf-8'),
+    license='MIT',
+    author='Brandon Rhodes',
+    author_email='brandon@rhodesmill.org',
+    url='http://github.com/brandon-rhodes/python-skyfield/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        ],
+    packages=[
+        'skyfield',
+        'skyfield.data',
+        'skyfield.tests',
+        ],
+    package_data = {
+        'skyfield': ['documentation/*.rst'],
+        'skyfield.data': ['Leap_Second.dat', 'deltat.data', 'deltat.preds',
+                          '*.npy', '*.npz'],
+        'skyfield.tests': ['data/*'],
+        },
+    install_requires=[
+        'certifi>=2017.4.17',
+        'jplephem>=2.11',
+        'numpy',
+        'sgp4>=2.2',
+        ],
+    extras_require=extras,          # support "pip install skyfield[tests]"
+)
