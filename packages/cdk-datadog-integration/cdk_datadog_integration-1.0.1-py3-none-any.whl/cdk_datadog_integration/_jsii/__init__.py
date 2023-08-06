517efb1cc602983fe5c80c0220c11a9c13c8817d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:35 +0000

    Add package cdk-datadog-integration/1.0.1/cdk_datadog_integration-1.0.1-py3-none-any.whl

---
+++ packages/cdk-datadog-integration/cdk_datadog_integration-1.0.1-py3-none-any.whl/cdk_datadog_integration/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudformation
+import aws_cdk.aws_s3
+import aws_cdk.aws_secretsmanager
+import aws_cdk.core
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("cdk-datadog-integration", "1.0.1", __name__[0:-6], "cdk-datadog-integration@1.0.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
