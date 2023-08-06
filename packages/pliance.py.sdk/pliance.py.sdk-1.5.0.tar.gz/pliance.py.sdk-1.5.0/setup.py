9cb58efcfd11707f27d8d024ce2b728512de46c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:01 +0000

    Add package pliance.py.sdk/1.5.0/pliance.py.sdk-1.5.0.tar.gz

---
+++ packages/pliance.py.sdk/pliance.py.sdk-1.5.0.tar.gz/pliance.py.sdk-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pliance.py.sdk',
+      version='1.5.0',
+      description='Pliance SDK',
+      url='http://github.com/pliance',
+      author='Pliance',
+      author_email='help@pliance.io',
+      license='MIT',
+      packages=['pliance_py_sdk'],
+      zip_safe=False)
