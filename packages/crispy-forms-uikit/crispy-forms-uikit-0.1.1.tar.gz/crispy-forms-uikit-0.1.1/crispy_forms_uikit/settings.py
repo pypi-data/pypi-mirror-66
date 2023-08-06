b826175981998a95220bcf83c5ad7b15bae3a86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:48 +0000

    Add package crispy-forms-uikit/0.1.1/crispy-forms-uikit-0.1.1.tar.gz

---
+++ packages/crispy-forms-uikit/crispy-forms-uikit-0.1.1.tar.gz/crispy-forms-uikit-0.1.1/crispy_forms_uikit/settings.py
@@ -0,0 +1,27 @@
+"""
+Settings
+========
+
+Default required settings. You can override them in your project settings
+file.
+
+"""
+
+# Allowed layout pack
+CRISPY_ALLOWED_TEMPLATE_PACKS = (
+    'bootstrap',
+    'uni_form',
+    'bootstrap3',
+    'bootstrap4',
+    'uikit-3.4.0'
+)
+
+
+# Default layout pack
+CRISPY_TEMPLATE_PACK = 'uikit-3.4.0'
+
+# Default class names on input
+# CRISPY_CLASS_CONVERTERS = {
+#     'inputelement': None,
+#     'errorcondition': 'is-invalid-input',
+# }
