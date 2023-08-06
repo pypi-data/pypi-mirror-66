3bfdfebb63bd843cd543d399e645c09b13df8905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:18 +0000

    Add package gridsim/0.2.1/gridsim-0.2.1.tar.gz

---
+++ packages/gridsim/gridsim-0.2.1.tar.gz/gridsim-0.2.1/gridsim/message.py
@@ -0,0 +1,92 @@
+# from __future__ import annotations  # For the type checking circular import
+
+from typing import Dict, Any, Optional, Type  # , TYPE_CHECKING
+
+from .robot import Robot
+
+# if TYPE_CHECKING:
+#     from .robot import Robot
+
+
+class Message:
+    def __init__(self, tx_id: Optional[int] = None,
+                 content: Optional[Dict[str, Any]] = None,
+                 rx_type: Type[Robot] = Robot,  # Default is any robot
+                 ):
+        """
+        A message sent by robots. Can be either a null (empty) message if no
+        arguments are provided to the constructor. Or it contains the sender's
+        ID, a dictionary of content, and (optionally) the type of robot that
+        receives the message.
+
+        Parameters
+        ----------
+        tx_id : Optional[int], optional
+            ID of the sending (transmitting) robot, by default None
+        content : Optional[Dict[str, Any]], optional
+            Dictionary of message keys and values, by default None. Keys must be
+            strings, but values can be of any type (incumbent on receiver for
+            interpretation)
+        rx_type : Type[Robot], optional
+            Type of the receiving robot, by default Robot (i.e., message will be
+            processed by any Robot.)
+        """
+        # Validate the message contents
+        if tx_id is None and content is None:
+            # Null message
+            self._tx_id = None
+            self._rx_type = None
+            self._content = None
+            self.is_null = True
+        else:
+            # Non-null message must include correct receiver type and dictionary
+            if not issubclass(rx_type, Robot):
+                raise TypeError('Receiver type must be a subclass of Robot')
+            if not isinstance(content, Dict) or \
+                    not all(map(lambda k: isinstance(k, str), content.keys())):
+                raise TypeError(
+                    'Content must be a dictionary with string keys')
+            else:
+                self._tx_id = tx_id
+                self._rx_type = rx_type
+                self._content = content
+                self.is_null = False
+
+    def get(self) -> Optional[Dict[str, Any]]:
+        """
+        Get the contents of the message
+
+        Returns
+        -------
+        Optional[Dict[str, Any]]
+            Dictionary of the message contents
+        """
+        return self._content
+
+    def tx_id(self) -> Optional[int]:
+        """
+        Get the ID (32-bit integer) of the robot that sent the message
+
+        Returns
+        -------
+        Optional[int]
+            ID of the sending (transmitting) robot
+        """
+        return self._tx_id
+
+    def __str__(self) -> str:
+        """Format the message as a human-readable string
+
+        Returns
+        -------
+        str
+            Friendly message format (or says it's an empty message)
+        """
+        if self.is_null:
+            return "NULL message"
+        else:
+            return '{id} -> {type}: {contents}'.format(
+                id=self._tx_id,
+                type=self._rx_type,
+                contents=self._content
+            )
---
+++ packages/gridsim/gridsim-0.2.1.tar.gz/gridsim-0.2.1/gridsim/viewer.py
@@ -0,0 +1,136 @@
+"""
+[Optional] For viewing the simulations, using Pygame
+"""
+
+import os
+
+import pygame
+
+from .world import World
+
+
+class Viewer:
+    def __init__(self, world: World, window_width: int = 1080,
+                 display_rate: int = 10, show_grid: bool = False):
+        """
+        Create a Viewer to display the simulation of a World.
+
+        This is optional (for debugging and visualization); simulations can be
+        run much faster if the Viewer is not used.
+
+        Parameters
+        ----------
+        world : World
+            World to display
+        window_width : int, optional
+            Width (in pixels) of the window to display the World, by default
+            1080
+        display_rate : int, optional
+            How fast to update the view (ticks/s), by default 10. In each tick,
+            robots will move by one cell, so keep this low to be able to
+            interpret what's going on.
+        show_grid : bool, optional
+            Whether to show the underlying grid in the World, by default False.
+        """
+        self._world = world
+        self._clock = pygame.time.Clock()
+        self._tick_rate = display_rate
+        self._show_grid = show_grid
+
+        self._world_dim = world.get_dimensions()
+        self._cell_size = window_width / self._world_dim[0]
+        self._window_dim = (window_width,
+                            int(self._cell_size * self._world_dim[1]))
+
+        # This allows setting an environment variable to avoid drawing stuff
+        # when no display is set up (e.g., Travis CI)
+        self._has_screen = bool(os.getenv('HAS_SCREEN', True))
+        if self._has_screen:
+            self._screen = pygame.display.set_mode(self._window_dim)
+
+        # Set up all of the sprites for all of the robots
+        [r._sprite_setup(self._cell_size) for r in self._world.get_robots()]
+
+        self._bg = self._create_bg()
+
+    def _has_new_environment(self) -> bool:
+        """
+        Check of the World's environment has changed.
+
+        Returns
+        -------
+        bool
+            Whether the World's environment has changed (or one has been added)
+        """
+        return self._world.has_environment and \
+            not self._world._environment.is_in_viewer
+
+    def _create_bg(self) -> pygame.Surface:
+        """
+        Create a background image for the Viewer, with the World's environment
+        (if it has one) and possibly with a grid
+
+        Returns
+        -------
+        pygame.Surface
+            Pygame image (surface) to use as a the Viewer background
+        """
+        if self._has_new_environment:
+            self._world._environment.add_to_viewer(self._window_dim)
+            bg_img = self._world._environment.viewer_img
+        else:
+            bg_img = None
+
+        # Use background image or solid color
+        if bg_img is None:
+            bg = pygame.Surface(self._window_dim)
+            bg.fill((20, 20, 20))
+        else:
+            bg = bg_img
+
+        # Set up background grid
+        if self._show_grid:
+            for col in range(1, self._world_dim[0]):
+                x = col * self._cell_size
+                pygame.draw.lines(bg, (50, 50, 50), False,
+                                  [(x, 0),
+                                   (x, self._window_dim[1])],
+                                  1)
+            for row in range(1, self._world_dim[1]):
+                y = row * self._cell_size
+                pygame.draw.lines(bg, (50, 50, 50,), False,
+                                  [(0, y),
+                                   (self._window_dim[0], y)],
+                                  1)
+        return bg
+
+    def draw(self):
+        """
+        Draw all of the robots in the world into the World and its environment.
+        """
+        if self._has_screen:
+            # Set the window title
+            pygame.display.set_caption(
+                'Gridsim (t={})'.format(self._world.get_time()))
+
+            # If the world has a new environment, change the viewer background
+            if self._has_new_environment:
+                self._bg = self._create_bg()
+            # Clear everything by drawing the background
+            self._screen.blit(self._bg, (0, 0))
+
+            # Draw all the robots
+            # Overrides sprite.Group.draw() method (see pygame source code)
+            robots = self._world.get_robots()
+            sprites = robots.sprites()
+            surface_blit = self._screen.blit
+            for spr in sprites:
+                if not spr.is_sprite_setup:
+                    spr._sprite_setup(self._cell_size)
+                robots.spritedict[spr] = \
+                    surface_blit(spr.image, spr.rect)
+            self.lostsprites = []
+
+            # Update the display
+            pygame.display.flip()
+            self._clock.tick(self._tick_rate)
