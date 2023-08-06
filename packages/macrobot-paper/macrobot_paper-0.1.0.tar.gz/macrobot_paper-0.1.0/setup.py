db937eff8e4de8c7f8ccd45fe7c60b33624dd458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:21 +0000

    Add package macrobot-paper/0.1.0/macrobot_paper-0.1.0.tar.gz

---
+++ packages/macrobot-paper/macrobot_paper-0.1.0.tar.gz/macrobot_paper-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['macrobot_paper']
+entry_points = \
+{'console_scripts': ['mb = macrobot_paper:main']}
+
+setup_kwargs = {
+    'name': 'macrobot-paper',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
