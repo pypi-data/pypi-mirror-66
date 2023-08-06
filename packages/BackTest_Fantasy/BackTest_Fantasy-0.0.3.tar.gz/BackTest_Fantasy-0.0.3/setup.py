41e1ea288350b51ef9f3912f5d4b8be09741833a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:39 +0000

    Add package BackTest_Fantasy/0.0.3/BackTest_Fantasy-0.0.3.tar.gz

---
+++ packages/BackTest_Fantasy/BackTest_Fantasy-0.0.3.tar.gz/BackTest_Fantasy-0.0.3/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='BackTest_Fantasy',
+    packages=['BackTest'],
+    description="BackTest",
+    version='0.0.3',
+    author='Fantasy',
+
+    )
