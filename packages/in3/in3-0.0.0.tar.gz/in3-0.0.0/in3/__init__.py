1e1a72b808be40ddc26c300db26037b70ca86e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:16 +0000

    Add package in3/0.0.0/in3-0.0.0.tar.gz

---
+++ packages/in3/in3-0.0.0.tar.gz/in3-0.0.0/in3/__init__.py
@@ -0,0 +1,12 @@
+from in3.exception import IN3BaseException, EthAddressFormatException, HashFormatException, \
+    NumberFormatException, PrivateKeyNotFoundException, ClientException
+from in3.model import ClientConfig
+from in3.libin3.enum import Chain
+from in3.client import Client
+
+__name__ = 'in3'
+__author__ = 'Slock.it <github.com/slockit>'
+__repository__ = 'github.com/slockit/in3-c'
+__status__ = "pre-alpha"
+__version__ = "2.1"
+__date__ = "March 2020"
---
+++ packages/in3/in3-0.0.0.tar.gz/in3-0.0.0/setup.py
@@ -0,0 +1,44 @@
+import pathlib
+import setuptools
+import os
+
+"""
+Information on how to build this file
+https://pypi.org/
+https://pypi.org/classifiers/
+"""
+
+envs = os.environ
+version = envs.get("version", "0.0.15")
+url = envs.get("url", "https://github.com/slockit/in3-c")
+download_url = envs.get("download_url","https://git.slock.it/in3/c/in3-python/-/archive/dev_0.1.0/in3-python-dev_0.1.0.tar.gz")
+license = envs.get("license", "LGPL")
+description = envs.get("description", "Incubed client and provider for web3. Based on in3-c for cython runtime.")
+keywords = envs.get("keywords", "in3").split(",")
+readme = (pathlib.Path(__file__).parent / "README.md").read_text()
+name = envs.get("name", "in3")
+author = envs.get("author", "github.com/slockit")
+author_email = envs.get("author_email", "products@slock.it")
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url=url,
+    packages=setuptools.find_packages(exclude=["docs", "tests"]),
+    install_requires=['requests>=2.23.0'],
+    keywords=keywords,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
+        "Environment :: Console",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+)
+
