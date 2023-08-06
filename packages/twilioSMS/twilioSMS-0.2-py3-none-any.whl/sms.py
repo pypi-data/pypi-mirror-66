0e28e58279ecf158081eb4e6c4b9735727c77e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:20 +0000

    Add package twilioSMS/0.2/twilioSMS-0.2-py3-none-any.whl

---
+++ packages/twilioSMS/twilioSMS-0.2-py3-none-any.whl/sms.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+from twilio.rest import Client
+import os
+from run import *
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=300, width=300,  bg='#6B5A00')
+canvas.pack()
+
+client = Client(account_sid, auth_token)
+
+message = tk.Entry(canvas, width=50, bg='#6B5A00', fg='#FFFFFF')
+message.pack()
+message.insert(0, 'message')
+
+cell = tk.Entry(canvas, width=50, bg='#6B5A00', fg='#FFFFFF')
+cell.pack()
+cell.insert(0, my_cell)
+
+
+def fsms():
+    client.messages.create(
+        to=cell.get(),
+        from_=my_twilio,
+        body=message.get()
+    )
+
+
+def fspam():
+    i = 0
+    while i < 5:
+        fsms()
+        i += 1
+
+
+Send = tk.Button(canvas, text='Send', padx=10,
+                 pady=5, fg='#FFFFFF', bg='#6B5A00',
+                 command=fsms)
+Send.pack()
+
+Spam = tk.Button(canvas, text='SPAM!', padx=10,
+                 pady=5, fg='#FFFFFF', bg='#6B5A00',
+                 command=fspam)
+Spam.pack()
+
+Quit = tk.Button(canvas, text='Quit', padx=10,
+                 pady=5, fg='#FFFFFF', bg='#6B5A00',
+                 command=root.destroy)
+Quit.pack()
+
+root.title('twilioSMS')
+root.mainloop()
