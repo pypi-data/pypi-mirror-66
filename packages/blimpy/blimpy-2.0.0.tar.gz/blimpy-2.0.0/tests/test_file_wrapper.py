348551faf3119d008912942089cf0b8d268c5c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:05 +0000

    Add package blimpy/2.0.0/blimpy-2.0.0.tar.gz

---
+++ packages/blimpy/blimpy-2.0.0.tar.gz/blimpy-2.0.0/tests/test_file_wrapper.py
@@ -0,0 +1,31 @@
+import blimpy as bl
+import pytest
+from tests.data import voyager_fil, voyager_h5, here
+
+
+def test_read_fns():
+    """ These read functions are currently not implemented. """
+    a = bl.Waterfall(voyager_fil)
+    b = bl.Waterfall(voyager_h5)
+    with pytest.raises(NotImplementedError):
+        a.container.read_all()
+        a.container.read_row(0)
+        a.container.read_rows(0, 2)
+
+        b.container.read_all()
+        b.container.read_row(0)
+        b.container.read_rows(0, 2)
+
+
+def test_file_wrapper_open_file():
+    from blimpy.io.file_wrapper import open_file
+    open_file(voyager_h5)
+    open_file(voyager_fil)
+
+    with pytest.raises(NotImplementedError):
+        open_file(here + '/run_tests.sh')
+
+
+if __name__ == "__main__":
+    test_read_fns()
+    test_file_wrapper_open_file()
