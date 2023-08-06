a214a9f13e0516c13259f5f4309714f6043b2939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:02 +0000

    Add package kedro-static-viz/0.1.3/kedro-static-viz-0.1.3.tar.gz

---
+++ packages/kedro-static-viz/kedro-static-viz-0.1.3.tar.gz/kedro-static-viz-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+NAME = "kedro-static-viz"
+
+README = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name=NAME,
+    version="0.1.3",
+    url="https://github.com/WaylonWalker/kedro-static-viz.git",
+    author="Waylon Walker",
+    author_email="waylon@waylonwalker.com",
+    description="Creates a static visualization of your pipeline",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    license="MIT",
+    install_requires=["kedro", "kedro-viz", "click"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # package_data={"public": ['**/public/**/*']},
+    entry_points={
+        "kedro.global_commands": ["kedro-static-viz = kedro_static_viz.cli:cli"],
+        "console_scripts": ["kedro-static-viz = kedro_static_viz.cli:cli"],
+    },
+)
