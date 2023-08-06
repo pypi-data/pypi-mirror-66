0e429e45f517086aa16e2d2d2a8da84504da48a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:06 +0000

    Add package algovis/0.1.1/algovis-0.1.1.tar.gz

---
+++ packages/algovis/algovis-0.1.1.tar.gz/algovis-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['algovis']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'algovis',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'hotshot07',
+    'author_email': 'aroram@tcd.ie',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
