3293949daf6515209a6d2ece1ca7c6e323417862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:25 +0000

    Add package py-swagger-generator/1.0.4/py_swagger_generator-1.0.4-py3-none-any.whl

---
+++ packages/py-swagger-generator/py_swagger_generator-1.0.4-py3-none-any.whl/swagger_generator/__init__.py
@@ -0,0 +1,12 @@
+from swagger_generator.template import *
+from swagger_generator.insert import *
+from swagger_generator import util
+from swagger_generator import swagger_info
+from swagger_generator.template_arg import TemplateArg
+from swagger_generator.data_type import DataType
+
+
+import sys
+assert sys.version_info >= (3, 7)
+
+__version__ = "1.0.4"
