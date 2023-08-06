7722846d17d3fb0681b5ca08926f1a551de416bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:25 +0000

    Add package SeaSor/0.1.3.2/SeaSor-0.1.3.2.tar.gz

---
+++ packages/SeaSor/SeaSor-0.1.3.2.tar.gz/SeaSor-0.1.3.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='SeaSor',
+        version='0.1.3.2',
+        description='Search & Sorting algorithms',
+        url='https://github.com/m1ghtfr3e/SearchSort',
+        author='m1ghtfr3e',
+        packages=setuptools.find_packages(),
+        zip_safe=False,
+        long_description = long_description,
+        long_description_content_type = 'text/markdown',
+        classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        ],
+)
