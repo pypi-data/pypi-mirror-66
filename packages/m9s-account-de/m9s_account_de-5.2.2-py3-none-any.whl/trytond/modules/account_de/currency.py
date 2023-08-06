be56e6f6aa68315953a94a616d7651b16fa800b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:05 +0000

    Add package m9s-account-de/5.2.2/m9s_account_de-5.2.2-py3-none-any.whl

---
+++ packages/m9s-account-de/m9s_account_de-5.2.2-py3-none-any.whl/trytond/modules/account_de/currency.py
@@ -0,0 +1,12 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from decimal import ROUND_HALF_UP
+from trytond.pool import PoolMeta
+
+
+class Currency:
+    __metaclass__ = PoolMeta
+    __name__ = 'currency.currency'
+
+    def round(self, amount, rounding=ROUND_HALF_UP):
+        return super(Currency, self).round(amount, rounding=rounding)
