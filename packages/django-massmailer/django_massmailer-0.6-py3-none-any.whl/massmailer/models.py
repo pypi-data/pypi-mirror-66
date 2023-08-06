a2958e3cef373d73f4ab3e191d983d077dc8ad34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:04 +0000

    Add package django-massmailer/0.6/django_massmailer-0.6-py3-none-any.whl

---
+++ packages/django-massmailer/django_massmailer-0.6-py3-none-any.whl/massmailer/models.py
@@ -0,0 +1,426 @@
+import bleach
+import enum
+import jinja2
+import jinja2.meta
+import jinja2.runtime
+import operator
+import re
+import uuid
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core import mail
+from django.core.exceptions import FieldDoesNotExist
+from django.urls import reverse
+from django.db import models
+from django.db.models import Count, F, BooleanField
+from django.utils import timezone
+from django.utils.text import slugify
+from django.utils.translation import ugettext_lazy as _
+from functools import reduce
+
+from massmailer.query_parser import QueryParser, ParseError
+from massmailer.utils import get_attr_rec, get_field_rec, filters as mfilters
+from massmailer.utils.db import ConditionalSum, CaseMapping
+from massmailer.utils.sandbox import SandboxedModelEnvironment
+
+TEMPLATE_OPTS = {
+    'autoescape': False,
+    'trim_blocks': True,
+    'undefined': jinja2.runtime.StrictUndefined,
+}
+
+VARIABLE_PLACEHOLDER = '<span class="placeholder">\u25cc</span>'
+RE_TAG = re.compile(
+    r'\{([%#])(.*?)\1\}|\{\{(.*?)\}\}', re.MULTILINE | re.DOTALL
+)
+
+
+class MailState(enum.IntEnum):
+    pending = 1
+    sending = 2
+    sent = 3
+    delivered = 4
+    bounced = 5
+    complained = 6
+
+    @classmethod
+    def bad(cls):
+        return {cls.bounced, cls.complained}
+
+
+class TemplateItem(enum.Enum):
+    subject = ('subject', {})
+    plain = ('plain_body', {})
+    html = ('html_body', {'autoescape': True})
+
+
+class Template(models.Model):
+    name = models.CharField(max_length=144, verbose_name=_("Name"))
+    description = models.TextField(blank=True, verbose_name=_("Description"))
+    is_marketing = models.BooleanField(
+        default=False,
+        verbose_name=_("This is a ‘marketing’ email"),
+        help_text=_(
+            "Adds an Unsubscribe header and makes sure the email contains the unsubscribe URL."
+        ),
+    )
+    subject = models.TextField(verbose_name=_("Subject template"))
+    plain_body = models.TextField(verbose_name=_("Plaintext body template"))
+    html_body = models.TextField(
+        blank=True, verbose_name=_("HTML body template")
+    )
+    language = models.CharField(max_length=10, verbose_name=_("Language"))
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _("Template")
+        verbose_name_plural = _("Templates")
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def template_opts(item: TemplateItem):
+        _, specific_opts = item.value
+        opts = TEMPLATE_OPTS.copy()
+        opts.update(specific_opts)
+        return opts
+
+    @property
+    def html_enabled(self):
+        return bool(self.html_body.strip())
+
+    def template_source(self, item: TemplateItem):
+        attr, _ = item.value
+        return getattr(self, attr)
+
+    def environment(self, item):
+        env = SandboxedModelEnvironment(**self.template_opts(item))
+        env.filters['format_datetime'] = mfilters.format_datetime
+        env.filters['format_date'] = mfilters.format_date
+        env.filters['format_time'] = mfilters.format_time
+        return env
+
+    def template(self, item: TemplateItem):
+        env = self.environment(item)
+        return env.from_string(source=self.template_source(item))
+
+    def variables(self, item: TemplateItem):
+        env = self.environment(item)
+        ast = env.parse(source=self.template_source(item))
+        return jinja2.meta.find_undeclared_variables(ast)
+
+    def render(self, item: TemplateItem, context: dict):
+        context['language'] = self.language
+        content = self.template(item).render(context)
+        if item is TemplateItem.html:
+            content = bleach.linkify(content)
+        return content
+
+    def preview(self, item: TemplateItem):
+        text = self.template_source(item)
+
+        def replace(match):
+            return jinja2.Markup(VARIABLE_PLACEHOLDER)
+
+        return RE_TAG.sub(replace, text)
+
+    def full_preview(self, context):
+        result = {}
+        for item in TemplateItem:
+            data = {}
+            try:
+                declared = self.variables(item)
+                rendered = self.render(item, context)
+                data['context'] = {
+                    'declared': list(context.keys() - declared),
+                    'missing': list(declared - context.keys()),
+                }
+                data['content'] = rendered
+            except jinja2.UndefinedError as error:
+                data['error'] = {'type': 'undefined', 'msg': error.message}
+            except jinja2.TemplateSyntaxError as error:
+                data['error'] = {'type': 'syntax', 'msg': error.message}
+            except Exception as error:
+                data['error'] = {'type': 'other', 'msg': str(error)}
+            result[item.name] = data
+        return result
+
+    def subject_preview(self):
+        return self.preview(TemplateItem.subject)
+
+    def plain_preview(self):
+        return self.preview(TemplateItem.plain)
+
+    def html_preview(self):
+        return self.preview(TemplateItem.html)
+
+    def get_absolute_url(self):
+        return reverse(
+            'massmailer:template:update',
+            kwargs={'id': self.pk, 'slug': slugify(self.name)},
+        )
+
+
+class Query(models.Model):
+    name = models.CharField(max_length=144, verbose_name=_("Name"))
+    description = models.TextField(blank=True, verbose_name=_("Description"))
+    query = models.TextField(verbose_name=_("Query"))
+    useful_with = models.ManyToManyField(
+        Template,
+        blank=True,
+        related_name='useful_queries',
+        verbose_name=_("Useful with templates"),
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _("Query")
+        verbose_name_plural = _("Queries")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'massmailer:query:update',
+            kwargs={'id': self.pk, 'slug': slugify(self.name)},
+        )
+
+    def get_results(self):
+        return self.execute(self.query)
+
+    def parse(self):
+        return QueryParser().parse_query(self.query)
+
+    @staticmethod
+    def execute(query):
+        result = QueryParser().parse_query(query)
+        qs = result.queryset
+        if len(qs) <= 0:
+            raise ParseError(_("The query must be non empty."))
+
+        user_qs = qs
+        User = get_user_model()
+        if user_qs.model != User and 'user' in result.aliases:
+            user_field = result.aliases['user']
+            try:
+                if get_field_rec(qs.model, user_field) != User:
+                    raise ParseError(
+                        _("%(label)s.%(field)s is not %(model)s")
+                        % {
+                            'label': qs_label,
+                            'field': user_field,
+                            'model': user_label,
+                        }
+                    )
+            except FieldDoesNotExist:
+                raise ParseError(
+                    _(
+                        "%(label)s has no field `%(field)s`"
+                        % {'label': qs_label, 'field': user_field}
+                    )
+                )
+            user_pks = set(qs.values_list(user_field, flat=True))
+            user_qs = User._default_manager.filter(pk__in=user_pks)
+
+        if 'email' not in result.aliases and hasattr(user_qs[0], 'email'):
+            result.aliases['email'] = 'email'
+        if 'email' not in result.aliases and not hasattr(user_qs[0], 'email'):
+            raise ParseError(
+                _(
+                    "The query must have an email field or declare an `email` alias."
+                )
+            )
+        return result, user_qs
+
+
+class BatchManager(models.Manager):
+    def get_queryset(self):
+        total = F('email_count')
+        ecount = lambda e: F('{}_email_count'.format(e))
+
+        qs = (
+            super()
+            .get_queryset()
+            .select_related('query', 'template', 'initiator')
+            .prefetch_related('emails')
+        )
+
+        def annotate(key, value):
+            nonlocal qs
+            qs = qs.annotate(**{key: value})
+
+        def annotate_count(name, expr):
+            count_field = ecount(name)
+            annotate(count_field.name, expr)
+            annotate('{}_percentage'.format(name), 100.0 * count_field / total)
+
+        annotate(total.name, Count('emails'))
+
+        for state in MailState:
+            annotate_count(
+                state.name, ConditionalSum(emails__state=state.value)
+            )
+
+        annotate_count('unsent', ecount('pending') + ecount('sending'))
+        annotate_count(
+            'erroneous',
+            reduce(
+                operator.add, (ecount(state.name) for state in MailState.bad())
+            ),
+        )
+        annotate(
+            'completed',
+            CaseMapping(
+                ecount('unsent').name,
+                [(0, True)],
+                default=False,
+                output_field=BooleanField(),
+            ),
+        )
+        return qs
+
+
+class Batch(models.Model):
+    name = models.CharField(
+        max_length=140, blank=True, verbose_name=_("Optional name")
+    )
+    template = models.ForeignKey(
+        Template,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='batches',
+        verbose_name=_("Template"),
+    )
+    query = models.ForeignKey(
+        Query,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='batches',
+        verbose_name=_("Query"),
+    )
+    initiator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='massmailer_batches',
+    )
+    date_created = models.DateTimeField(default=timezone.now, null=False)
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = _("Batch")
+        verbose_name_plural = _("Batches")
+
+    objects = BatchManager()
+
+    @property
+    def default_name(self):
+        return _("Batch %(id)s") % {'id': self.pk}
+
+    def __str__(self):
+        return self.name or self.default_name
+
+    def pending_emails(self):
+        return self.emails.filter(state=MailState.pending.value)
+
+    @property
+    def erroneous_emails(self):
+        return self.emails.filter(
+            state__in=[state.value for state in MailState.bad()]
+        )
+
+    def build_emails(self):
+        result, qs = self.query.get_results()
+        queryset = result.queryset.order_by('pk')
+
+        html_enabled = self.template.html_enabled
+
+        for object in queryset:
+            context = {
+                alias: get_attr_rec(object, field)
+                for alias, field in result.aliases.items()
+            }
+            context[result.model_name] = object
+            email = getattr(object, result.aliases['email'])
+
+            unsubscribe_url = ""
+            if self.template.is_marketing and hasattr(
+                object, 'get_unsubscribe_url'
+            ):
+                unsubscribe_url = '<{}>'.format(
+                    getattr(object, 'get_unsubscribe_url')
+                )
+
+            yield BatchEmail(
+                batch=self,
+                to=email,
+                unsubscribe_url=unsubscribe_url,
+                subject=self.template.render(TemplateItem.subject, context),
+                body=self.template.render(TemplateItem.plain, context),
+                html_body=self.template.render(TemplateItem.html, context)
+                if html_enabled
+                else "",
+            )
+
+
+class BatchEmail(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    state = models.PositiveIntegerField(
+        db_index=True, default=MailState.pending.value
+    )
+    batch = models.ForeignKey(
+        Batch, related_name='emails', on_delete=models.CASCADE
+    )
+    to = models.EmailField(blank=False)
+    unsubscribe_url = models.TextField(blank=True)
+    subject = models.TextField(blank=True)
+    body = models.TextField(blank=True)
+    html_body = models.TextField(blank=True, default="")
+
+    class Meta:
+        ordering = ['state']
+
+    def __str__(self):
+        return '[{}] {}'.format(self.state_display, self.id)
+
+    @property
+    def state_display(self):
+        return MailState(self.state).name
+
+    @property
+    def task_id(self):
+        return 'massmailer-{}'.format(self.pk)
+
+    @property
+    def pending(self):
+        return self.state == MailState.pending.value
+
+    def build_email(self):
+        assert self.subject
+        assert self.body
+        # add a custom header to resolve the mail ID from bounces/complaints
+        headers = {'X-MID': self.id}
+        if self.unsubscribe_url:
+            headers['List-Unsubscribe'] = self.unsubscribe_url
+
+        kwargs = {
+            'to': [self.to],
+            'subject': self.subject,
+            'body': self.body,
+            'headers': headers,
+        }
+
+        if self.html_body:
+            email = mail.EmailMultiAlternatives(**kwargs)
+            email.attach_alternative(self.html_body, 'text/html')
+        else:
+            email = mail.EmailMessage(**kwargs)
+        return email
+
+    def send_task(self):
+        from massmailer.tasks import send_email
+
+        return send_email.apply_async(args=[self.pk], task_id=self.task_id)
---
+++ packages/django-massmailer/django_massmailer-0.6-py3-none-any.whl/massmailer/views.py
@@ -0,0 +1,413 @@
+import bleach
+import json
+import inspect
+import markdown
+import traceback
+import pyparsing
+
+from django.apps import apps
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import (
+    PermissionRequiredMixin,
+    UserPassesTestMixin,
+)
+from django.core import serializers
+from django.core.exceptions import FieldError, ObjectDoesNotExist
+from django.db import models
+from django.db import transaction
+from django.db.models import Count
+from django.http.response import JsonResponse, Http404
+from django.urls import reverse
+from django.urls.base import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import (
+    UpdateView,
+    CreateView,
+    ModelFormMixin,
+    DeleteView,
+)
+from django.views.generic.list import ListView
+from reversion.views import RevisionMixin
+
+import massmailer.forms
+import massmailer.models
+import massmailer.tasks
+from massmailer.query_parser import QueryParser
+from massmailer.utils import JinjaEscapeExtension, get_attr_rec
+
+
+class MailerAdminMixin(UserPassesTestMixin):
+    def test_func(self):
+        # Like for the Django Admin, only staff users can see the mailing
+        # panel, whatever their permissions are.
+        return self.request.user.is_staff
+
+
+class ObjectByIdMixin:
+    def get_object(self, queryset=None):
+        try:
+            return (queryset or self.get_queryset()).get(pk=self.kwargs['id'])
+        except ObjectDoesNotExist:
+            raise Http404()
+
+
+class DashboardView(MailerAdminMixin, TemplateView):
+    template_name = 'massmailer/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['templates'] = massmailer.models.Template.objects.annotate(
+            query_count=Count('useful_queries')
+        )
+        context['queries'] = massmailer.models.Query.objects.annotate(
+            template_count=Count('useful_with')
+        )
+        return context
+
+
+class TemplateMixin(MailerAdminMixin, RevisionMixin):
+    template_name = 'massmailer/template/details.html'
+    context_object_name = 'template'
+    model = massmailer.models.Template
+    form_class = massmailer.forms.TemplateForm
+
+
+class CreateTemplateView(PermissionRequiredMixin, TemplateMixin, CreateView):
+    permission_required = 'massmailer.create_template'
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+    def form_valid(self, form):
+        self.object = template = form.save(commit=False)
+        template.author = self.request.user
+        template.save()
+        return super(ModelFormMixin, self).form_valid(form)
+
+    def get_object(self):
+        return None  # the template isn't in db yet
+
+
+class UpdateTemplateView(
+    PermissionRequiredMixin, TemplateMixin, ObjectByIdMixin, UpdateView
+):
+    permission_required = 'massmailer.change_template'
+
+    def form_valid(self, form):
+        if form.was_overwritten():
+            form.reset_overwritten()
+            form.add_error(
+                None,
+                _(
+                    "The form was modified and saved while you were editing. "
+                    "Please merge with current version."
+                ),
+            )
+            return self.form_invalid(form)
+        return super().form_valid(form)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class TemplatePreviewView(PermissionRequiredMixin, MailerAdminMixin, View):
+    permission_required = 'massmailer.view_template'
+
+    def post(self, request, *args, **kwargs):
+        # TODO: context from query builder
+        data = {}
+
+        if not request.user.has_perm('massmailer.view_query'):
+            data['error'] = _(
+                "You don't have the permission to view query results."
+            )
+            return JsonResponse(data)
+
+        html_enabled = request.POST.get('html_enabled') == 'true'
+
+        query = massmailer.models.Query.objects.get(pk=request.POST['query'])
+        page = int(request.POST['page'])
+
+        template = massmailer.models.Template()
+        template.is_marketing = request.POST.get('is_marketing') == 'true'
+        template.subject = request.POST['subject']
+        template.plain_body = request.POST['plain']
+        template.language = request.POST['language']
+
+        if html_enabled:
+            if request.POST.get('use_markdown') == 'true':
+                md = markdown.Markdown(extensions=[JinjaEscapeExtension()])
+                html = data['html_template'] = md.convert(
+                    bleach.clean(template.plain_body)
+                )
+            else:
+                html = request.POST['html']
+            template.html_body = html
+
+        results, user_qs = query.get_results()
+        qs = results.queryset
+        if template.is_marketing and not hasattr(
+            qs.model, 'get_unsubscribe_url'
+        ):
+            data['error'] = _(
+                'If this is a mailing the query model must have a get_unsubscribe_url method.'
+            )
+            return JsonResponse(data)
+        count = qs.count()
+        user_count = user_qs.count()
+        data['query'] = {
+            'count': count,
+            'user_count': user_count,
+            'page': min(count, page),
+        }
+        try:
+            object = qs[page]
+            context = {
+                alias: get_attr_rec(object, field)
+                for alias, field in results.aliases.items()
+            }
+            context[results.model_name] = object
+            data['render'] = template.full_preview(context)
+            data['render']['header'] = ''
+            if template.is_marketing:
+                data['render']['header'] = "List-Unsubscribe: " + getattr(
+                    object, 'get_unsubscribe_url'
+                )
+        except IndexError:
+            data['render'] = ''
+        return JsonResponse(data)
+
+
+class QueryMixin(MailerAdminMixin, RevisionMixin):
+    template_name = 'massmailer/query/details.html'
+    context_object_name = 'query'
+    model = massmailer.models.Query
+    form_class = massmailer.forms.QueryForm
+
+    @cached_property
+    def available_enums(self):
+        return sorted(
+            (
+                {'name': name, 'members': [m.name for m in enum]}
+                for name, enum in QueryParser().available_enums.items()
+            ),
+            key=lambda e: e['name'].lower(),
+        )
+
+    @cached_property
+    def available_funcs(self):
+        return [
+            {
+                'name': name,
+                'doc': inspect.getdoc(func),
+                'signature': inspect.signature(func),
+            }
+            for name, func in QueryParser().available_funcs.items()
+        ]
+
+    @cached_property
+    def available_models(self):
+        User = get_user_model()
+
+        def find_user_field(model):
+            for field in model._meta.fields:
+                try:
+                    if field.related_model is User:
+                        return field.name
+                except AttributeError:
+                    pass
+
+        models = apps.get_models()
+        models = (
+            {
+                'name': str(model._meta.verbose_name_plural.capitalize()),
+                'cls_name': model.__name__,
+                'app': model._meta.app_label,
+                'is_user': model is User,
+                'user_field': find_user_field(model),
+                'label': model._meta.label,
+                'doc': inspect.getdoc(model),
+            }
+            for model in models
+        )
+        return sorted(
+            models,
+            key=lambda e: (
+                not e['is_user'],
+                e['user_field'] is None,
+                e['app'].lower(),
+                e['name'].lower(),
+            ),
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['available_enums'] = self.available_enums
+        context['available_funcs'] = self.available_funcs
+        context['available_models'] = self.available_models
+        context['user_model'] = get_user_model().__name__
+        return context
+
+
+class CreateQueryView(PermissionRequiredMixin, QueryMixin, CreateView):
+    permission_required = 'massmailer.create_query'
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+    def form_valid(self, form):
+        self.object = query = form.save(commit=False)
+        query.author = self.request.user
+        query.save()
+        return super(ModelFormMixin, self).form_valid(form)
+
+    def get_object(self):
+        return None  # the query isn't in the db yet
+
+
+class UpdateQueryView(
+    PermissionRequiredMixin, QueryMixin, ObjectByIdMixin, UpdateView
+):
+    permission_required = 'massmailer.change_query'
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class QueryPreviewView(PermissionRequiredMixin, MailerAdminMixin, View):
+    permission_required = 'massmailer.view_query'
+
+    @staticmethod
+    def serialize(obj):
+        if obj is None:
+            return None
+        if not isinstance(obj, models.Model):
+            return None
+        return json.loads(
+            serializers.get_serializer('json')().serialize([obj])
+        )[0]
+
+    def post(self, request, *args, **kwargs):
+        query = request.POST['query']
+        page = int(request.POST['page'])
+        try:
+            # run the query
+            result, user_qs = massmailer.models.Query.execute(query)
+            qs = result.queryset
+            count = qs.count()
+            user_count = user_qs.count()
+            instance = None
+            if 0 <= page < count:
+                obj = qs[page]
+                instance = {result.model_name: self.serialize(obj)}
+                for name, field in result.aliases.items():
+                    serialized = self.serialize(get_attr_rec(obj, field))
+                    if serialized:
+                        instance[name] = serialized
+
+            data = {
+                'count': count,
+                'user_count': user_count,
+                'model': qs.model._meta.label,
+                'model_name': result.model_name,
+                'aliases': list(result.aliases.items()),
+                'query': str(qs.query),
+                'result': instance,
+            }
+        except Exception as e:
+            if isinstance(e, (massmailer.query_parser.ParseError, FieldError)):
+                error = str(e)
+            elif isinstance(e, pyparsing.ParseException):
+                error = _("Syntax error at position %(pos)s.") % {'pos': e.loc}
+            else:
+                error = traceback.format_exc(limit=2)
+            data = {'error': error}
+        return JsonResponse(data)
+
+
+class BatchListView(PermissionRequiredMixin, MailerAdminMixin, ListView):
+    model = massmailer.models.Batch
+    template_name = 'massmailer/batch-list.html'
+    context_object_name = 'batches'
+    paginate_by = 25
+    permission_required = 'massmailer.view_batch'
+
+
+class BatchCreateView(PermissionRequiredMixin, MailerAdminMixin, CreateView):
+    model = massmailer.models.Batch
+    form_class = massmailer.forms.CreateBatchForm
+    template_name = 'massmailer/batch-create.html'
+    permission_required = 'massmailer.create_batch'
+
+    def get_success_url(self):
+        return reverse('massmailer:batch:detail', args=[self.object.pk])
+
+    def form_valid(self, form):
+        with transaction.atomic():
+            # create the batch
+            batch = form.save(commit=False)
+            batch.initiator = self.request.user
+            batch.save()
+
+            emails = list(batch.build_emails())
+            # create the batch emails
+            massmailer.models.BatchEmail.objects.bulk_create(emails)
+
+        # create the tasks
+        for email in emails:
+            email.send_task()
+
+        return super().form_valid(form)
+
+    def get_object(self):
+        return None  # the batch isn't in the db yet
+
+
+class BatchDetailView(PermissionRequiredMixin, MailerAdminMixin, ListView):
+    model = massmailer.models.BatchEmail
+    template_name = 'massmailer/batch-emails.html'
+    context_object_name = 'emails'
+    paginate_by = 200
+    permission_required = 'massmailer.view_batch'
+
+    @property
+    def batch_id(self):
+        return self.kwargs['id']
+
+    @cached_property
+    def batch(self):
+        return (
+            massmailer.models.Batch.objects.prefetch_related('emails')
+            .annotate(email_count=Count('emails'))
+            .get(pk=self.batch_id)
+        )
+
+    def get_queryset(self):
+        return self.batch.emails.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['batch'] = self.batch
+        return context
+
+
+class BatchRetryView(PermissionRequiredMixin, MailerAdminMixin, UpdateView):
+    model = massmailer.models.Batch
+    pk_url_kwarg = 'id'
+    fields = []
+    success_url = reverse_lazy('massmailer:batch:list')
+    permission_required = 'massmailer.change_batch'
+
+    def form_valid(self, form):
+        # create the tasks
+        for email in self.get_object().pending_emails():
+            email.send_task()
+        return super(ModelFormMixin, self).form_valid(form)
+
+
+class BatchDeleteView(PermissionRequiredMixin, MailerAdminMixin, DeleteView):
+    model = massmailer.models.Batch
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('massmailer:batch:list')
+    permission_required = 'massmailer.delete_batch'
