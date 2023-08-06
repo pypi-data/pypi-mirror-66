90d3c67a7533662703ac68b985f20aad7581b594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:44 +0000

    Add package cartprograph/0.2.3/cartprograph-0.2.3-py3-none-any.whl

---
+++ packages/cartprograph/cartprograph-0.2.3-py3-none-any.whl/tracer/interaction_tracer.py
@@ -0,0 +1,111 @@
+import os
+import logging
+
+from .tracer import Tracer, TracerEvent, on_event
+
+
+l = logging.getLogger(__name__)
+logging.basicConfig(level=os.getenv("LOGLEVEL", "INFO"))
+
+
+class Desync(Exception):
+    def __init__(self, description, expected, received):
+        self.description = description
+        self.expected = expected
+        self.received = received
+
+    def __repr__(self):
+        return f'<Desync {repr(self.description)}, expected={repr(self.expected)}, received={repr(self.received)}>'
+
+    def __str__(self):
+        return repr(self)
+
+
+class InteractionTracer(Tracer):
+    def __init__(self, target_args, *, interaction=None, bb_trace=None):
+        self.interaction = interaction if interaction is not None else list()
+        self.interaction_index = 0
+        self.bb_trace = bb_trace if bb_trace is not None else list()
+        self.bb_trace_index = 0
+        super().__init__(target_args)
+
+    @property
+    def current_interaction(self):
+        if self.interaction_index < len(self.interaction):
+            return self.interaction[self.interaction_index]
+
+    @property
+    def prev_interaction(self):
+        if 0 < self.interaction_index <= len(self.interaction):
+            return self.interaction[self.interaction_index - 1]
+
+    @property
+    def prev_interactions(self):
+        if 0 < self.interaction_index <= len(self.interaction):
+            for i in range(self.interaction_index - 1, 0-1, -1):
+                yield self.interaction[i]
+
+    @property
+    def stdin(self):
+        return self.popen.stdin if hasattr(self, 'popen') else None
+
+    @property
+    def stdout(self):
+        return self.popen.stdout if hasattr(self, 'popen') else None
+
+    @property
+    def stderr(self):
+        return self.popen.stderr if hasattr(self, 'popen') else None
+
+    def run(self):
+        self.interaction_index = 0
+        super().run()
+
+    @on_event(TracerEvent.EXEC_BLOCK, ...)
+    def on_exec_block(self, addr):
+        if self.bb_trace_index < len(self.bb_trace):
+            if self.bb_trace[self.bb_trace_index] != addr:
+                raise Desync('exec addr', self.bb_trace[self.bb_trace_index], addr)
+        else:
+            self.bb_trace.append(addr)
+        self.bb_trace_index += 1
+
+    @on_event(TracerEvent.SYSCALL_START, '.*')
+    def on_syscall_start(self, syscall, args):
+        l.debug('syscall: %s %s', syscall, args)
+        if self.interaction_index < len(self.interaction):
+            if self.current_interaction['syscall'] != syscall:
+                raise Desync(f'syscall ({syscall}, {args}) start',
+                             self.current_interaction['syscall'], syscall)
+            if self.current_interaction['args'] != args:
+                raise Desync(f'syscall ({syscall}, {args}) args start',
+                             self.current_interaction['args'], args)
+            if self.current_interaction['bb_trace_index'] != self.bb_trace_index:
+                raise Desync(f'syscall ({syscall}, {args}) bb_trace_index start',
+                             self.current_interaction['bb_trace_index'], self.bb_trace_index)
+        else:
+            self.interaction.append({
+                'syscall': syscall,
+                'args': args,
+                'bb_trace_index': self.bb_trace_index,
+            })
+
+    @on_event(TracerEvent.SYSCALL_FINISH, '.*')
+    def on_syscall_finish(self, syscall, args, result):
+        l.debug('syscall: %s %s = %s', syscall, args, result)
+        if self.current_interaction['syscall'] != syscall:
+            raise Desync(f'syscall ({syscall}, {args}, {result}) finish',
+                         self.current_interaction['syscall'], syscall)
+        if self.current_interaction['args'] != args:
+            raise Desync(f'syscall ({syscall}, {args}, {result}) args finish',
+                         self.current_interaction['args'], args)
+        if self.current_interaction['bb_trace_index'] != self.bb_trace_index:
+            raise Desync(f'syscall ({syscall}, {args}, {result}) bb_trace_index finish',
+                         self.current_interaction['bb_trace_index'], self.bb_trace_index)
+        if 'result' in self.current_interaction:
+            if self.current_interaction['result'] != result:
+                raise Desync(f'syscall ({syscall}, {args}, {result}) result finish',
+                             self.current_interaction['result'], result)
+        else:
+            self.current_interaction['result'] = result
+        self.interaction_index += 1
---
+++ packages/cartprograph/cartprograph-0.2.3-py3-none-any.whl/tracer/tracer.py
@@ -0,0 +1,188 @@
+import os
+import re
+import subprocess
+import enum
+import json
+import logging
+
+from .qemu import qemu_path
+
+
+l = logging.getLogger(__name__)
+logging.basicConfig(level=os.getenv("LOGLEVEL", "INFO"))
+
+QEMU_PATH = qemu_path('x86_64')
+
+
+class RegexReader:
+    def __init__(self, fd, *, balanced=None):
+        self.fd = fd
+        self.balanced = balanced or []
+        self._buffer = b''
+
+    def unread(self, data):
+        self._buffer = data + self._buffer
+
+    def read_regex(self, *regexs):
+        while True:
+            matches = [
+                re.search(regex, self._buffer)
+                for regex in regexs
+            ]
+            for match in sorted([match for match in matches if match], key=lambda k: k.start()):
+                if any(len(set(match.group(0).count(e) for e in balance)) != 1
+                       for balance in self.balanced):
+                    continue
+                self._buffer = self._buffer[match.end():]
+                return match
+            data = os.read(self.fd, 4096)
+            if not data:
+                return
+            self._buffer += data
+
+
+class TracerEvent(enum.Enum):
+    SYSCALL_START = enum.auto()
+    SYSCALL_FINISH = enum.auto()
+    EXEC_BLOCK = enum.auto()
+
+
+event_count = 0
+def on_event(event, filter_):
+    global event_count
+    def wrapper(func):
+        if not hasattr(func, 'on_event'):
+            func.on_event = []
+        func.on_event.append({
+            'id': event_count,
+            'event': event,
+            'filter': filter_,
+        })
+        return func
+    event_count += 1
+    return wrapper
+
+
+class Tracer:
+    SYSCALL_ENABLED = True
+    EXEC_BLOCK_ENABLED = True
+
+    def __init__(self, target_args):
+        self.target_args = target_args
+
+        self.handlers = list()
+        for attr_name in dir(self):
+            attr = getattr(self, attr_name)
+            if hasattr(attr, 'on_event'):
+                for handler in attr.on_event:
+                    self.handlers.append((attr, handler))
+        self.handlers = sorted(self.handlers, key=lambda k: k[1]['id'])
+
+    def dispatch_event(self, event, *, syscall=None, args=None, result=None, addr=None):
+        l.debug('Dispatching %s (%s)', event, hex(addr) if addr else syscall)
+        for func, handler in self.handlers:
+            handler_event = handler['event']
+            filter_ = handler['filter']
+            if handler_event != event:
+                continue
+            if event == TracerEvent.SYSCALL_START and re.match('^' + filter_ + '$', syscall):
+                func(syscall, args)
+            elif event == TracerEvent.SYSCALL_FINISH and re.match('^' + filter_ + '$', syscall):
+                func(syscall, args, result)
+            elif event == TracerEvent.EXEC_BLOCK and (filter_ is ... or addr in filter_):
+                func(addr)
+
+    def start(self):
+        qemu_log_r, qemu_log_w = os.pipe()
+        qemu_log_path = f'/proc/{os.getpid()}/fd/{qemu_log_w}'
+
+        log_options = []
+        if self.SYSCALL_ENABLED:
+            log_options.append('strace')
+        if self.EXEC_BLOCK_ENABLED:
+            log_options.append('exec')
+            log_options.append('nochain')
+
+        popen = subprocess.Popen([QEMU_PATH, '-d', ','.join(log_options),
+                                  '-D', qemu_log_path,
+                                  '--', *self.target_args],
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+
+        self.popen = popen
+        self.qemu_log_r = qemu_log_r
+        self.qemu_log_w = qemu_log_w
+
+        return popen, qemu_log_r
+
+    def run(self):
+        popen, qemu_log_r = self.start()
+        self.popen = popen
+
+        reader = RegexReader(qemu_log_r, balanced=[(b'(', b')')])
+
+        try:
+            execve_args = (json.dumps(self.target_args[0]),
+                           json.dumps(self.target_args),
+                           json.dumps({}))
+            self.dispatch_event(TracerEvent.SYSCALL_START, syscall='execve', args=execve_args)
+            self.dispatch_event(TracerEvent.SYSCALL_FINISH, syscall='execve', args=execve_args, result=None)
+
+            while True:
+                # syscall_re is very delicate because args terminator ')' can and does appear inside an arg
+                # ?P<args> tries to find args that end with ' =', as this appears before the return result
+                # ?P<args_blocked> handles the case where the syscall is blocked, which will not end in ' ='
+                # in this case, the line must end with a ')'
+                # line end must occur because otherwise potential race condition could occur in non-blocked syscall midway through logging it
+                # in general, this should be mitigated by a ',' if there are more args
+                # however, this may not be the case inside of structs, and therefore we need to check for balanced parentheses
+                syscall_re = br'(?P<pid>\d+) (?P<syscall>\w+)((\((?P<args>.*?)\)(?= =))|(\((?P<args_blocked>.*?)\)$))'
+                syscall_result_re = br' = (?P<result>.*)\n'
+                bb_addr_re = br'Trace .*?: .*? \[.*?\/(?P<addr>.*?)\/.*?\]'
+                sig_re = br'--- (?P<signal>\w+) {.*?} ---'
+
+                match = reader.read_regex(syscall_re, bb_addr_re, sig_re)
+
+                if match.groupdict().get('addr'):
+                    bb_addr_match = match
+                    addr = int(bb_addr_match['addr'], 16)
+                    self.dispatch_event(TracerEvent.EXEC_BLOCK, addr=addr)
+
+                elif match.groupdict().get('syscall'):
+                    syscall_match = match
+                    pid = int(syscall_match['pid'].decode())
+                    syscall = syscall_match['syscall'].decode()
+                    if syscall_match['args'] is not None:
+                        args = syscall_match['args']
+                    else:
+                        args = syscall_match['args_blocked']
+                    args = tuple(arg.strip() for arg in args.decode().split(','))
+
+                    self.dispatch_event(TracerEvent.SYSCALL_START, syscall=syscall, args=args)
+
+                    if 'exit' in syscall:
+                        break
+
+                    syscall_result_match = reader.read_regex(syscall_result_re)
+                    result, _, result_info = syscall_result_match['result'].decode().partition(' ')
+                    result = int(result, 16) if result.startswith('0x') else int(result)
+
+                    self.dispatch_event(TracerEvent.SYSCALL_FINISH, syscall=syscall, args=args, result=result)
+
+                elif match.groupdict().get('signal'):
+                    signal_match = match
+                    signal = signal_match['signal'].decode()
+                    self.dispatch_event(TracerEvent.SYSCALL_START, syscall='signal', args=(signal,))
+                    break
+
+        finally:
+            self.stop()
+
+    def stop(self):
+        self.popen.kill()
+        self.popen.stdin.close()
+        self.popen.stdout.close()
+        self.popen.stderr.close()
+        os.close(self.qemu_log_r)
+        os.close(self.qemu_log_w)
