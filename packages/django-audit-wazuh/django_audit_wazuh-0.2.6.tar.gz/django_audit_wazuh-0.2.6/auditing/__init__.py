26acfd4eff8cca1ad7b46e0598492b9e94f9fe61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:12 +0000

    Add package django-audit-wazuh/0.2.6/django_audit_wazuh-0.2.6.tar.gz

---
+++ packages/django-audit-wazuh/django_audit_wazuh-0.2.6.tar.gz/django_audit_wazuh-0.2.6/auditing/__init__.py
@@ -0,0 +1,30 @@
+import logging
+
+from django.contrib.auth.signals import (user_logged_in,
+                                         user_login_failed,
+                                         user_logged_out)
+from django.dispatch import receiver
+from . utils import get_request_info
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(user_logged_in)
+def login_logger(sender, request, **kwargs):
+    _msg = '"Django Login successful", "username": "{}", {}'
+    logger.info(_msg.format(request.POST['username'],
+                            get_request_info(request)))
+
+
+@receiver(user_login_failed)
+def login_failed_logger(sender, request, **kwargs):
+    _msg = '"Django Login failed", "username": "{}", {}'
+    logger.warn(_msg.format(request.POST['username'],
+                            get_request_info(request)))
+
+
+@receiver(user_logged_out)
+def logout_logger(sender, request, **kwargs):
+    _msg = '"Django Logout successful", "username": "{}", {}'
+    logger.info(_msg.format(request.user,
+                            get_request_info(request)))
