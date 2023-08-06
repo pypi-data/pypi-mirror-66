4cf158e59a438ee52080c97d282027c9c6d6293d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:12 +0000

    Add package pyTSFoil/0.1.0/pyTSFoil-0.1.0.tar.gz

---
+++ packages/pyTSFoil/pyTSFoil-0.1.0.tar.gz/pyTSFoil-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pyTSFoil import __version__, __name__
+
+with open('README.md') as f:
+      long_description = f.read()
+
+setup(name=__name__,
+      version=__version__,
+      description='This is a python interface of TSFOIL2',
+#     long_description=long_description,
+      keywords=['CFD', 'TSFOIL2', 'transonic small disturbance theory'],
+      download_url='https://github.com/swayli94/pyTSFoil',
+      license='MIT',
+      author='Runze LI',
+      author_email='swayli94@gmail.com',
+      packages=find_packages(),
+      package_data={"":['*.f','*.exe']},
+      install_requires=['numpy', 'cst-modeling3d'],
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ]
+)
+
