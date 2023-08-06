f43ecb3a10c330a085f6118dc54a40fff10c3b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:13 +0000

    Add package pyairvisual/4.4.0/pyairvisual-4.4.0.tar.gz

---
+++ packages/pyairvisual/pyairvisual-4.4.0.tar.gz/pyairvisual-4.4.0/pyairvisual/api.py
@@ -0,0 +1,68 @@
+"""Define an object to interact with the AirVisual data API."""
+from typing import Awaitable, Callable, Optional, Union
+
+API_URL_SCAFFOLD = "https://api.airvisual.com/v2"
+
+
+class API:
+    """Define the "API" object."""
+
+    def __init__(self, request: Callable[..., Awaitable[dict]]) -> None:
+        """Iniitialize."""
+        self._request: Callable[..., Awaitable[dict]] = request
+
+    async def _nearest(
+        self,
+        kind: str,
+        latitude: Optional[Union[float, str]] = None,
+        longitude: Optional[Union[float, str]] = None,
+    ) -> dict:
+        """Return data from nearest city/station (IP or coordinates)."""
+        params: dict = {}
+        if latitude and longitude:
+            params.update({"lat": str(latitude), "lon": str(longitude)})
+
+        data: dict = await self._request("get", f"nearest_{kind}", params=params)
+        return data["data"]
+
+    async def city(self, city: str, state: str, country: str) -> dict:
+        """Return data for the specified city."""
+        data: dict = await self._request(
+            "get", "city", params={"city": city, "state": state, "country": country}
+        )
+        return data["data"]
+
+    async def nearest_city(
+        self,
+        latitude: Optional[Union[float, str]] = None,
+        longitude: Optional[Union[float, str]] = None,
+    ) -> dict:
+        """Return data from nearest city (IP or coordinates)."""
+        return await self._nearest("city", latitude, longitude)
+
+    async def nearest_station(
+        self,
+        latitude: Optional[Union[float, str]] = None,
+        longitude: Optional[Union[float, str]] = None,
+    ) -> dict:
+        """Return data from nearest station (IP or coordinates)."""
+        return await self._nearest("station", latitude, longitude)
+
+    async def ranking(self) -> dict:
+        """Return a sorted array of selected major cities in the world."""
+        data = await self._request("get", "city_ranking")
+        return data["data"]
+
+    async def station(self, station: str, city: str, state: str, country: str) -> dict:
+        """Return data for the specified city."""
+        data: dict = await self._request(
+            "get",
+            "station",
+            params={
+                "station": station,
+                "city": city,
+                "state": state,
+                "country": country,
+            },
+        )
+        return data["data"]
---
+++ packages/pyairvisual/pyairvisual-4.4.0.tar.gz/pyairvisual-4.4.0/pyairvisual/client.py
@@ -0,0 +1,83 @@
+"""Define a client to interact with AirVisual."""
+from json.decoder import JSONDecodeError
+from typing import Optional
+
+from aiohttp import ClientSession, ClientTimeout
+
+from .api import API, API_URL_SCAFFOLD
+from .errors import _raise_on_data_error
+from .node import Node
+from .supported import Supported
+
+DEFAULT_TIMEOUT: int = 10
+
+
+class Client:  # pylint: disable=too-few-public-methods
+    """Define the client."""
+
+    def __init__(
+        self, *, api_key: Optional[str] = None, session: Optional[ClientSession] = None
+    ) -> None:
+        """Initialize."""
+        self._api_key: Optional[str] = api_key
+        self._session: ClientSession = session
+
+        self.api: API = API(self.request)
+        self.node: Node = Node(self.request)
+        self.supported: Supported = Supported(self.request)
+
+    async def request(
+        self,
+        method: str,
+        endpoint: str,
+        *,
+        base_url: str = API_URL_SCAFFOLD,
+        headers: Optional[dict] = None,
+        params: Optional[dict] = None,
+        json: Optional[dict] = None,
+    ) -> dict:
+        """Make a request against AirVisual."""
+        _headers = headers or {}
+        _headers.update({"Content-Type": "application/json"})
+
+        _params = params or {}
+        if self._api_key:
+            _params.update({"key": self._api_key})
+
+        use_running_session = self._session and not self._session.closed
+
+        if use_running_session:
+            session = self._session
+        else:
+            session = ClientSession(timeout=ClientTimeout(total=DEFAULT_TIMEOUT))
+
+        try:
+            async with session.request(
+                method,
+                f"{base_url}/{endpoint}",
+                headers=_headers,
+                params=_params,
+                json=json,
+            ) as resp:
+                data = await resp.json(content_type=None)
+        except JSONDecodeError:
+            # The response can't be parsed as JSON, so we'll use its body text
+            # in an error:
+            response_text = await resp.text()
+            data = {"status": "fail", "data": {"message": response_text}}
+        finally:
+            if not use_running_session:
+                await session.close()
+
+        if isinstance(data, str):
+            # In some cases, the AirVisual API will return a quoted string in its
+            # response body (e.g., "\"node not found\""), which is technically valid
+            # JSON. Additionally, AirVisual sets that response's Content-Type header
+            # to application/json (#smh). Together, these factors will allow a
+            # non-true-JSON  payload to escape the try/except above. So, if we get
+            # here, we use the string value (with quotes removed) to raise an error:
+            response_text = data.replace('"', "")
+            data = {"status": "fail", "data": {"message": response_text}}
+
+        _raise_on_data_error(data)
+        return data
