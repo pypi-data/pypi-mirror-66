a5dbdd51b8713fd848f8177da6a618b9d5414c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package gaussian-binomial-pande/0.1/gaussian_binomial_pande-0.1.tar.gz

---
+++ packages/gaussian-binomial-pande/gaussian_binomial_pande-0.1.tar.gz/gaussian_binomial_pande-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_pande',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Kartik Pande',
+      zip_safe=False)
