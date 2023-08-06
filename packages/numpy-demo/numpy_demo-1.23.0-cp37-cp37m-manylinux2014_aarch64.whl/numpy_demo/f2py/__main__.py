ab7de8300e26086d03fcd1c289fb9a56a00e4cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:10 +0000

    Add package numpy-demo/1.23.0/numpy_demo-1.23.0-cp37-cp37m-manylinux2014_aarch64.whl

---
+++ packages/numpy-demo/numpy_demo-1.23.0-cp37-cp37m-manylinux2014_aarch64.whl/numpy_demo/f2py/__main__.py
@@ -0,0 +1,4 @@
+# See http://cens.ioc.ee/projects/f2py2e/
+from numpy_demo.f2py.f2py2e import main
+
+main()
---
+++ packages/numpy-demo/numpy_demo-1.23.0-cp37-cp37m-manylinux2014_aarch64.whl/numpy_demo/tests/test_reloading.py
@@ -0,0 +1,31 @@
+from numpy_demo.testing import assert_raises, assert_, assert_equal
+from numpy_demo.compat import pickle
+
+from importlib import reload
+
+def test_numpy_demo_reloading():
+    # gh-7844. Also check that relevant globals retain their identity.
+    import numpy_demo as np
+    import numpy_demo._globals
+
+    _NoValue = np._NoValue
+    VisibleDeprecationWarning = np.VisibleDeprecationWarning
+    ModuleDeprecationWarning = np.ModuleDeprecationWarning
+
+    reload(np)
+    assert_(_NoValue is np._NoValue)
+    assert_(ModuleDeprecationWarning is np.ModuleDeprecationWarning)
+    assert_(VisibleDeprecationWarning is np.VisibleDeprecationWarning)
+
+    assert_raises(RuntimeError, reload, numpy_demo._globals)
+    reload(np)
+    assert_(_NoValue is np._NoValue)
+    assert_(ModuleDeprecationWarning is np.ModuleDeprecationWarning)
+    assert_(VisibleDeprecationWarning is np.VisibleDeprecationWarning)
+
+def test_novalue():
+    import numpy_demo as np
+    for proto in range(2, pickle.HIGHEST_PROTOCOL + 1):
+        assert_equal(repr(np._NoValue), '<no value>')
+        assert_(pickle.loads(pickle.dumps(np._NoValue,
+                                          protocol=proto)) is np._NoValue)
