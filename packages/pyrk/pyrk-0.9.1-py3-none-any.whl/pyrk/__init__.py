400e3b1f4ea234eac2e534d5a060c89623fd6a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:07 +0000

    Add package pyrk/0.9.1/pyrk-0.9.1-py3-none-any.whl

---
+++ packages/pyrk/pyrk-0.9.1-py3-none-any.whl/pyrk/__init__.py
@@ -0,0 +1,17 @@
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2015 Kevin Walchko
+# see LICENSE for full details
+##############################################
+from .pyrk import RK4
+
+
+try:
+    from importlib.metadata import version # type: ignore
+except ImportError:
+    from importlib_metadata import version # type: ignore
+
+__license__ = 'MIT'
+__copyright__ = 'copyright (c) 2016 Kevin J. Walchko'
+__author__ = 'Kevin J. Walchko'
+__version__ = version("pyrk")
