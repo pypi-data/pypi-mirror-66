20d6403112fdf7e84b428c768939569fe8dd59ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:17 +0000

    Add package coinmetrics/0.2.5/coinmetrics-0.2.5.tar.gz

---
+++ packages/coinmetrics/coinmetrics-0.2.5.tar.gz/coinmetrics-0.2.5/coinmetrics/__init__.py
@@ -0,0 +1,9 @@
+"""
+Coin Metrics API Module Includes
+"""
+
+from .base import Base
+from .community import Community
+from .utils import cm_to_pandas, normalize, csv
+
+__version__ = '0.2.5'
---
+++ packages/coinmetrics/coinmetrics-0.2.5.tar.gz/coinmetrics-0.2.5/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Setup for Coin Metrics API.
+"""
+import os
+from coinmetrics import __version__
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('README.md') as r:
+	long_description = r.read()
+
+setup(
+	name='coinmetrics',
+	version=__version__,
+	author="Robert Rice",
+	author_email="h4110w33n@gmail.com",
+	url='https://github.com/h4110w33n/coinmetrics',
+	description='An unofficial library to standardize queries to the Coin Metrics (coinmetrics.io) API.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	keywords=['coinmetrics', 'crypto', 'cryptocurrency', 'rest', 'api'],
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Education',
+		'Intended Audience :: Financial and Insurance Industry',
+		'Intended Audience :: Information Technology',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python',
+		'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+		'Topic :: Scientific/Engineering :: Information Analysis',
+		'Topic :: Scientific/Engineering :: Mathematics',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+	install_requires=required,
+	provides=['coinmetrics'],
+	license='GNU General Public License v3 (GPLv3)',
+	packages=['coinmetrics'],
+)
