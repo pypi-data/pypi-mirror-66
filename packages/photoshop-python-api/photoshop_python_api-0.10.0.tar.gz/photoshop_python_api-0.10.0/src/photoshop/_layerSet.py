cd42f84cd95a4224ebc3b574a42d78a5c0b67afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:10 +0000

    Add package photoshop-python-api/0.10.0/photoshop_python_api-0.10.0.tar.gz

---
+++ packages/photoshop-python-api/photoshop_python_api-0.10.0.tar.gz/photoshop_python_api-0.10.0/src/photoshop/_layerSet.py
@@ -0,0 +1,49 @@
+from photoshop._artlayers import ArtLayers
+from photoshop._core import Photoshop
+from photoshop._layers import Layers
+
+
+class LayerSet(Photoshop):
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+
+    @property
+    def name(self):
+        return self.app.name
+
+    @property
+    def artLayers(self):
+        return ArtLayers(self.app.artLayers)
+
+    @property
+    def enabledChannels(self):
+        return self.app.enabledChannels
+
+    @property
+    def layerSets(self):
+        return self.app.layerSets
+
+    @property
+    def layers(self):
+        return Layers(self.app.layers)
+
+    @property
+    def parent(self):
+        return self.app.parent
+
+    def add(self):
+        self.app.add()
+
+    def merge(self):
+        self.app.merge()
+
+    def __iter__(self):
+        for layer in self.app:
+            yield layer
+
+    @property
+    def isBackgroundLayer(self):
+        index = "app.activeDocument.layers.length-1"
+        layers = "app.activeDocument.layers"
+        layer = self.eval_javascript(f"{layers}({index}).name")
+        return bool(layer == self.app.name)
---
+++ packages/photoshop-python-api/photoshop_python_api-0.10.0.tar.gz/photoshop_python_api-0.10.0/src/photoshop/save_options/__init__.py
@@ -0,0 +1,18 @@
+from photoshop.save_options.bmp import BMPSaveOptions
+from photoshop.save_options.gif import GIFSaveOptions
+from photoshop.save_options.jpg import JPEGSaveOptions
+from photoshop.save_options.pdf import PDFSaveOptions
+from photoshop.save_options.png import ExportOptionsSaveForWeb, PNGSaveOptions
+from photoshop.save_options.psd import PhotoshopSaveOptions
+from photoshop.save_options.tif import TiffSaveOptions
+
+__all__ = [
+    "BMPSaveOptions",
+    "GIFSaveOptions",
+    "JPEGSaveOptions",
+    "PDFSaveOptions",
+    "ExportOptionsSaveForWeb",
+    "PNGSaveOptions",
+    "PhotoshopSaveOptions",
+    "TiffSaveOptions",
+]
