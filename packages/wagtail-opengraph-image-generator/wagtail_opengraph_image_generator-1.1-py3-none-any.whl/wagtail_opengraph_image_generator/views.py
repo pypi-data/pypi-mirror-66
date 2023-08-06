88dd38b7bf7a1e3ba9c5c56ab5c754ac0fdfd522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:29 +0000

    Add package wagtail-opengraph-image-generator/1.1/wagtail_opengraph_image_generator-1.1-py3-none-any.whl

---
+++ packages/wagtail-opengraph-image-generator/wagtail_opengraph_image_generator-1.1-py3-none-any.whl/wagtail_opengraph_image_generator/views.py
@@ -0,0 +1,19 @@
+import base64
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from wagtail.core.models import Page
+
+from .conf import get_page_model
+from .generator import create_og_image
+
+
+@csrf_exempt
+def show_preview(request, page_id):
+    if page_id > 0:
+        page = get_page_model().objects.get(pk=page_id).specific
+    else:
+        page = None
+    buf = create_og_image(request, page, True, request.POST)
+    return HttpResponse(base64.b64encode(buf.getvalue()), content_type='text/plain')
