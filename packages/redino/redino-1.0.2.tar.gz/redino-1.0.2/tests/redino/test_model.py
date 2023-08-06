41d597bb3ac1335e3c360542df3ca7cc853128e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:27 +0000

    Add package redino/1.0.2/redino-1.0.2.tar.gz

---
+++ packages/redino/redino-1.0.2.tar.gz/redino-1.0.2/tests/redino/test_model.py
@@ -0,0 +1,47 @@
+import unittest
+
+import redis
+from redino import transactional, redis_connect
+
+
+class TestRedisReadWrite(unittest.TestCase):
+    """
+    Test the model if it can create entries in redis
+    """
+
+    def test_regular_writing(self):
+        @redis_connect
+        @transactional
+        def set_values(r: redis.Redis):
+            r.hset("a", "key", "3")
+            r.hset("b", "key", "3")
+
+        @redis_connect
+        def read_values(r: redis.Redis):
+            self.assertEqual("3", r.hget("a", "key").decode("utf-8"))
+            self.assertEqual("3", r.hget("b", "key").decode("utf-8"))
+
+        set_values()
+        read_values()
+
+    def test_exception_rolls_back(self):
+        @redis_connect
+        @transactional
+        def set_values(r: redis.Redis):
+            r.hset("ROLL", "key", "3")
+            raise Exception("ded")
+
+        @redis_connect
+        def read_values(r: redis.Redis):
+            self.assertIsNone(r.hget("ROLL", "key"))
+
+        try:
+            set_values()
+        except Exception:
+            pass
+
+        read_values()
+
+
+if __name__ == "__main__":
+    unittest.main()
