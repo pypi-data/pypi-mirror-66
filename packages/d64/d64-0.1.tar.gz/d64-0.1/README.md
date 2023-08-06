d9585b3ea47068ac7c15eba37edc94cef1b5c881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:10 +0000

    Add package d64/0.1/d64-0.1.tar.gz

---
+++ packages/d64/d64-0.1.tar.gz/d64-0.1/README.md
@@ -0,0 +1,3 @@
+# python-d64
+
+Python module to read and write to Commodore disk images.
---
+++ packages/d64/d64-0.1.tar.gz/d64-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='d64',
+      description='Read and write Commodore disk images',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      packages=['d64'],
+      author='Simon Rowe',
+      author_email='srowe@mose.org.uk',
+      url='https://eden.mose.org.uk/gitweb/?p=python-d64.git'
+      )
