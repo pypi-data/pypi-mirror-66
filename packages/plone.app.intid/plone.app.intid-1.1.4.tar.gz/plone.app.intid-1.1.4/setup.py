554240464868495279e5eb0e3a4f4f7d9679ab31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:28 +0000

    Add package plone.app.intid/1.1.4/plone.app.intid-1.1.4.tar.gz

---
+++ packages/plone.app.intid/plone.app.intid-1.1.4.tar.gz/plone.app.intid-1.1.4/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.4'
+
+setup(
+    name='plone.app.intid',
+    version=version,
+    description="Installation and migration support for five.intid within "
+                "Plone/CMF",
+    long_description='%s\n%s' % (
+        open("README.rst").read(),
+        open(os.path.join("CHANGES.rst")).read(),
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: Core",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords='plone zope five intid',
+    author='Alec Mitchell',
+    author_email='apm13@columbia.edu',
+    url='https://github.com/plone/plone.app.intid',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.intid',
+        'zope.lifecycleevent',
+        'five.intid>=1.0',
+        'Products.CMFCore',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.dexterity',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
