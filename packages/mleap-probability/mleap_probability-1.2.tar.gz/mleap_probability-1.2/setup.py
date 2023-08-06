52fe87a025b67e8d6bafaff4e8787124680a5a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:42 +0000

    Add package mleap-probability/1.2/mleap_probability-1.2.tar.gz

---
+++ packages/mleap-probability/mleap_probability-1.2.tar.gz/mleap_probability-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mleap_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['mleap_probability'],
+      author=['Anna Pastwa'],
+      zip_safe=False)
