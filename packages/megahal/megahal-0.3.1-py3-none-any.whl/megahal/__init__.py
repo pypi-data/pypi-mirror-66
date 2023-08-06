19fdfee5b84d0de17f6fd7ad6b1413ed45f63458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:29 +0000

    Add package megahal/0.3.1/megahal-0.3.1-py3-none-any.whl

---
+++ packages/megahal/megahal-0.3.1-py3-none-any.whl/megahal/__init__.py
@@ -0,0 +1,10 @@
+from megahal.megahal import (
+    DEFAULT_BANWORDS, DEFAULT_BRAINFILE, DEFAULT_HARD_TIMEOUT, DEFAULT_ORDER,
+    DEFAULT_SOFT_TIMEOUT, Brain, Dictionary, MegaHAL, Tree, __version__,
+)
+
+__all__ = [
+    "MegaHAL", "Dictionary", "Tree", "__version__", "DEFAULT_ORDER", "Brain",
+    "DEFAULT_BRAINFILE", "DEFAULT_HARD_TIMEOUT", "DEFAULT_SOFT_TIMEOUT",
+    "DEFAULT_BANWORDS",
+]
