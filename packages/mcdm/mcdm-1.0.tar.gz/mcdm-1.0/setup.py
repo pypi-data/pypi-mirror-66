3ea496ce51c2e02514048411ea987ac714762db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:56 +0000

    Add package mcdm/1.0/mcdm-1.0.tar.gz

---
+++ packages/mcdm/mcdm-1.0.tar.gz/mcdm-1.0/setup.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2020 Dimitrios-Georgios Akestoridis
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Setup script for the mcdm package
+"""
+
+import os
+import setuptools
+
+
+about = {}
+top_dirpath = os.path.dirname(os.path.abspath(__file__))
+about_filepath = os.path.join(top_dirpath, "mcdm", "__about__.py")
+with open(about_filepath, "r") as fp:
+    exec(fp.read(), about)
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    description=about["__description__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=about["__license__"],
+    url=about["__url__"],
+    keywords=about["__keywords__"],
+    classifiers=about["__classifiers__"],
+    install_requires=about["__install_requires__"],
+    python_requires=about["__python_requires__"],
+    include_package_data=True,
+    zip_safe=False,
+    packages=setuptools.find_packages()
+)
