bd7ee7788af9ac680600c1fc0d72318e7b7a91b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:25 +0000

    Add package aiojaeger/0.1.5/aiojaeger-0.1.5-py3-none-any.whl

---
+++ packages/aiojaeger/aiojaeger-0.1.5-py3-none-any.whl/aiojaeger/spancontext/__init__.py
@@ -0,0 +1,57 @@
+# possible span kinds
+import abc
+from typing import Optional
+
+from pydantic import BaseModel
+
+from aiojaeger.mypy_types import Headers, OptBool, OptStr
+
+CLIENT = "CLIENT"
+SERVER = "SERVER"
+PRODUCER = "PRODUCER"
+CONSUMER = "CONSUMER"
+
+
+class BaseTraceContext(BaseModel):
+    """Immutable class with trace related data that travels across
+    process boundaries.
+    """
+
+    trace_id: str
+    span_id: str
+    parent_id: OptStr
+    sampled: OptBool
+    debug: bool
+    shared: bool
+
+    @abc.abstractmethod
+    def make_headers(cls) -> Headers:
+        """Creates dict with headers from available context.
+
+        Resulting dict should be passed to HTTP client  propagate contest
+        to other services.
+        """
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def make_context(cls, headers: Headers) -> Optional["BaseTraceContext"]:
+        pass
+
+
+class DummyTraceContext(BaseTraceContext):
+    def make_headers(self) -> Headers:
+        return {}
+
+    @classmethod
+    def make_context(
+        cls, headers: Headers, sampled: bool = True
+    ) -> BaseTraceContext:
+        return cls(
+            trace_id="dummy-trace-id",
+            span_id="dummy-span-id",
+            parent_id="dummy-parent-id",
+            debug=False,
+            sampled=sampled,
+            shared=False,
+        )
---
+++ packages/aiojaeger/aiojaeger-0.1.5-py3-none-any.whl/aiozipkin/helpers/__init__.py
@@ -0,0 +1,54 @@
+import time
+from typing import Any, Dict, List, NamedTuple, Optional
+
+from aiojaeger.mypy_types import OptInt, OptStr, OptTs
+
+Endpoint = NamedTuple(
+    "Endpoint",
+    [
+        ("serviceName", OptStr),
+        ("ipv4", OptStr),
+        ("ipv6", OptStr),
+        ("port", OptInt),
+    ],
+)
+
+
+def create_endpoint(
+    service_name: str,
+    *,
+    ipv4: OptStr = None,
+    ipv6: OptStr = None,
+    port: OptInt = None
+) -> Endpoint:
+    """Factory function to create Endpoint object.
+    """
+    return Endpoint(service_name, ipv4, ipv6, port)
+
+
+def make_timestamp(ts: OptTs = None) -> int:
+    """Create zipkin timestamp in microseconds, or convert available one
+    from second. Useful when user supplies ts from time.time() call.
+    """
+    ts = ts if ts is not None else time.time()
+    return int(ts * 1000 * 1000)  # microseconds
+
+
+OptKeys = Optional[List[str]]
+
+
+def filter_none(data: Dict[str, Any], keys: OptKeys = None) -> Dict[str, Any]:
+    """Filter keys from dict with None values.
+
+    Check occurs only on root level. If list of keys specified, filter
+    works only for selected keys
+    """
+
+    def limited_filter(k: str, v: Any) -> bool:
+        return k not in keys or v is not None  # type: ignore
+
+    def full_filter(k: str, v: Any) -> bool:
+        return v is not None
+
+    f = limited_filter if keys is not None else full_filter
+    return {k: v for k, v in data.items() if f(k, v)}
