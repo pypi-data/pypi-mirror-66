afd8667da3607e777e4c1a36b570181351bac81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:47 +0000

    Add package pandemic/0.3.5/pandemic-0.3.5-py3-none-any.whl

---
+++ packages/pandemic/pandemic-0.3.5-py3-none-any.whl/pandemic/config_private.py
@@ -0,0 +1,6 @@
+# NOT A REAL CONFIG
+# Should you want to play around with your own REDIS instance,
+# you config might look something like this
+REDIS_CONFIG = {"password":"HqtnotarealkeyWkeF",
+                "host":"redis-someting-blah.com",
+                "port":"35406"}
