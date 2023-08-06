3d1857d64c95f4f78ccfacfe1b0844aebe609323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:03 +0000

    Add package Manteia/0.0.3/Manteia-0.0.3.tar.gz

---
+++ packages/Manteia/Manteia-0.0.3.tar.gz/Manteia-0.0.3/Manteia/Manteia.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  core.py
+#  
+#  Copyright 2020 Yves <yves@mercadier>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#
+ 
+"""
+    Implémentation de la proclamation de la bonne parole.
+ 
+    Usage:
+ 
+    >>> from Manteia import proclamer
+    >>> testManteia()
+"""
+__all__ = ['testManteia']
+
+from Manteia.Data import Data
+
+class Manteia:
+	def __init__(self,documents=None,labels=None):
+		self.data=Data(documents,labels)
+
+def testManteia():
+    print ("Hello, Mantéïa is alive.")
+ 
+
