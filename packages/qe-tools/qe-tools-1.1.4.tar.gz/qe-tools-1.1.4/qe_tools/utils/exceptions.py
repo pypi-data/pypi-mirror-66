4154e50570ab8b95d1c7ebb3d35b30aa50079e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:33 +0000

    Add package qe-tools/1.1.4/qe-tools-1.1.4.tar.gz

---
+++ packages/qe-tools/qe-tools-1.1.4.tar.gz/qe-tools-1.1.4/qe_tools/utils/exceptions.py
@@ -0,0 +1,13 @@
+class ParsingError(Exception):
+    """
+    Generic error raised when there is a parsing error
+    """
+    pass
+
+
+class InputValidationError(Exception):
+    """
+    The input data for a calculation did not validate (e.g., missing
+    required input data, wrong data, ...)
+    """
+    pass
