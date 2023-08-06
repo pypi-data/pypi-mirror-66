80c5db29361779c07afbd0ecfc1d9badbec817c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:12 +0000

    Add package python-lru/0.2.0/python-lru-0.2.0.tar.gz

---
+++ packages/python-lru/python-lru-0.2.0.tar.gz/python-lru-0.2.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-lru",
+    version="0.2.0",
+    author="Nathan Shearer",
+    author_email="shearern@gmail.com",
+    description="LRU Cache Implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shearern/lru",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
