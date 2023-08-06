38b918968ffea07eb03e6cdb057ded3347ab47d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:31 +0000

    Add package resquare/0.0.2/resquare-0.0.2.tar.gz

---
+++ packages/resquare/resquare-0.0.2.tar.gz/resquare-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['resquare']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=7.0.0,<8.0.0']
+
+setup_kwargs = {
+    'name': 'resquare',
+    'version': '0.0.2',
+    'description': 'Utility for expanding the frame size of a logo image while preserving the background color of the original image.',
+    'long_description': None,
+    'author': 'faizanbhat',
+    'author_email': 'faizan.bhat@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.4,<4.0',
+}
+
+
+setup(**setup_kwargs)
