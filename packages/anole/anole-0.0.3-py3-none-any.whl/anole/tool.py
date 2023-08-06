1963c551fa918a5573cf348bc1e32c07faab2bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:55 +0000

    Add package anole/0.0.3/anole-0.0.3-py3-none-any.whl

---
+++ packages/anole/anole-0.0.3-py3-none-any.whl/anole/tool.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+
+"""tools"""
+
+import json
+import os
+
+import requests
+from retrying import retry
+
+from anole import setting
+
+
+@retry(stop_max_attempt_number=setting.HTTP_RETRY)
+def get(url):
+    res = requests.get(url)
+    print("Occur get")
+    return res
+
+
+def update(path, data):
+    os.path.exists(path) and os.remove(path)
+    open(path, encoding="utf-8", mode="w").write(data)
+
+
+def read(path):
+    return json.loads(open(path, encoding="utf-8").read())
+
+
+def load_cached(path):
+    if not os.path.exists(path):
+        update(path, get(setting.CACHE_URL).text)
+    return read(path)
+
