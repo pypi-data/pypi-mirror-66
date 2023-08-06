2cefb0445ad37dd424024040c2599ba54b4d7c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:59 +0000

    Add package home-connect-sdk/1.0.0/home-connect-sdk-1.0.0.tar.gz

---
+++ packages/home-connect-sdk/home-connect-sdk-1.0.0.tar.gz/home-connect-sdk-1.0.0/home_connect_sdk/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from home_connect_sdk.api.commands_api import CommandsApi
+from home_connect_sdk.api.default_api import DefaultApi
+from home_connect_sdk.api.images_api import ImagesApi
+from home_connect_sdk.api.programs_api import ProgramsApi
+from home_connect_sdk.api.settings_api import SettingsApi
+from home_connect_sdk.api.status_events_api import StatusEventsApi
