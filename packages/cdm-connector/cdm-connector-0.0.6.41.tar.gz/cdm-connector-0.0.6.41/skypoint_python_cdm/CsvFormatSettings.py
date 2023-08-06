47823d3dcb321b03f5d85bd4d4a8e590ffb48369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:31 +0000

    Add package cdm-connector/0.0.6.41/cdm-connector-0.0.6.41.tar.gz

---
+++ packages/cdm-connector/cdm-connector-0.0.6.41.tar.gz/cdm-connector-0.0.6.41/skypoint_python_cdm/CsvFormatSettings.py
@@ -0,0 +1,29 @@
+from .Base import Base
+from .JsonEnum import JsonEnum
+from .utils import String
+from .FileFormatSettings import FileFormatSettings
+
+class CsvQuoteStyle(JsonEnum):
+    Csv = "QuoteStyle.Csv"
+    None_ = "QuoteStyle.None"
+
+
+class CsvStyle(JsonEnum):
+    QuoteAlways = "CsvStyle.QuoteAlways"
+    QuoteAfterDelimiter = "CsvStyle.QuoteAfterDelimiter"
+
+
+class CsvFormatSettings(FileFormatSettings):
+    def __init__(self, schema=[]):
+        self.schema = schema + [
+            SchemaEntry("columnHeaders", bool),
+            SchemaEntry("delimiter", String),
+            SchemaEntry("quoteStyle", CsvQuoteStyle),
+            SchemaEntry("csvStyle", CsvStyle),
+        ]
+        super().__init__(self.schema)
+
+    def toJson(self):
+        entity = super().toJson()
+        entity["$type"] = "CsvFormatSettings"
+        return entity
