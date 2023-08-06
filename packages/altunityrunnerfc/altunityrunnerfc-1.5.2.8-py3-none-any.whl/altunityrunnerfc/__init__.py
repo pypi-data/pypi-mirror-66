281429d4c04c5b497f90e2762184cc90dee6796f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:58 +0000

    Add package altunityrunnerfc/1.5.2.8/altunityrunnerfc-1.5.2.8-py3-none-any.whl

---
+++ packages/altunityrunnerfc/altunityrunnerfc-1.5.2.8-py3-none-any.whl/altunityrunnerfc/__init__.py
@@ -0,0 +1,5 @@
+from altunityrunnerfc.commands import *
+from altunityrunnerfc.altElement import AltElement
+from altunityrunnerfc.by import By
+from altunityrunnerfc.runner import *
+from altunityrunnerfc.player_pref_key_type import *
---
+++ packages/altunityrunnerfc/altunityrunnerfc-1.5.2.8-py3-none-any.whl/altunityrunnerfc/commands/InputActions/__init__.py
@@ -0,0 +1,11 @@
+from altunityrunnerfc.commands.InputActions.move_mouse_and_wait import *
+from altunityrunnerfc.commands.InputActions.move_mouse import *
+from altunityrunnerfc.commands.InputActions.press_key import *
+from altunityrunnerfc.commands.InputActions.press_key_and_wait import *
+from altunityrunnerfc.commands.InputActions.scroll_mouse import *
+from altunityrunnerfc.commands.InputActions.scroll_mouse_and_wait import *
+from altunityrunnerfc.commands.InputActions.swipe import *
+from altunityrunnerfc.commands.InputActions.swipe_and_wait import *
+from altunityrunnerfc.commands.InputActions.tilt import *
+from altunityrunnerfc.commands.InputActions.tap_at_coordinates import *
+
