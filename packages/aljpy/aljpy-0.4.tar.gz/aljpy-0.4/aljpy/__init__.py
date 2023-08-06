f2d3e03c9a6fea639e12ea461620520bc36080a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:36 +0000

    Add package aljpy/0.4/aljpy-0.4.tar.gz

---
+++ packages/aljpy/aljpy-0.4.tar.gz/aljpy-0.4/aljpy/__init__.py
@@ -0,0 +1,9 @@
+from . import email, log, notify, plot
+from .log import logger
+from .timer import timer
+from .humanhash import humanhash
+from .cache import autocache, memcache, timecache
+from .parallel import parallel
+from .dotdict import dotdict, mapping, starmapping
+from .debugging import extract
+from .download import download
---
+++ packages/aljpy/aljpy-0.4.tar.gz/aljpy-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aljpy',
+      version='0.4',
+      description='Andy\'s common tools',
+      author='Andy Jones',
+      author_email='andyjones.ed@gmail.com',
+      url='https://github.com/andyljones/aljpy',
+      packages=['aljpy'],
+      package_data={'aljpy': ['*.txt']},
+      include_package_data=True)
