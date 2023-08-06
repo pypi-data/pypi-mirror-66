c2771a016873c02f722c37089106ec16ad160859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:06 +0000

    Add package spherical-dev/0.0.4/spherical-dev-0.0.4.tar.gz

---
+++ packages/spherical-dev/spherical-dev-0.0.4.tar.gz/spherical-dev-0.0.4/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+setuptools.setup(
+    name='spherical-dev',
+
+    use_scm_version=True,
+    setup_requires=[
+        'setuptools_scm',
+    ],
+
+    author='Anton Patrushev',
+    author_email='ap@spherical.pm',
+    maintainer='spherical.pm',
+    maintainer_email='support@spherical.pm',
+
+    description='Set of tools used in Spherical development',
+    license='MIT',
+
+    packages=[
+        'spherical_dev',
+    ],
+    package_dir={
+        '': 'src',
+    },
+    extras_require={
+        'dev': [
+            'invoke',
+            'flake8',
+            'isort',
+            'pytest-cov',
+            'pytest',
+        ],
+    },
+    zip_safe=True,
+)
