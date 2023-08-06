178a1a94db964b8d4ea6ad02cf7e2e5eb3ea3fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:33 +0000

    Add package abdesign/0.0.1/abdesign-0.0.1-py3-none-any.whl

---
+++ packages/abdesign/abdesign-0.0.1-py3-none-any.whl/abdesign/data/cdr_library.py
@@ -0,0 +1,22 @@
+# TODO: Check imgt positioning for correctness
+
+library = {
+            "kabat":{
+                "light":{
+                    "start_1": 24, "start_2":50,"start_3":89,"end_1":{"num":34,"ext":""},"end_2":{"num":56,"ext":""},"end_3":{"num":97,"ext":""}},
+                "heavy":{
+                    "start_1": 31, "start_2":50,"start_3":95,"end_1":{"num":35,"ext":""},"end_2":{"num":65,"ext":"B"},"end_3":{"num":102,"ext":""}}
+                    },
+            "chothia":{
+                "light":{
+                    "start_1": 24, "start_2":50,"start_3":89,"end_1":{"num":34,"ext":""},"end_2":{"num":56,"ext":""},"end_3":{"num":97,"ext":""}},
+                "heavy":{
+                    "start_1": 26, "start_2":52,"start_3":95,"end_1":{"num":32,"ext":""},"end_2":{"num":56,"ext":""},"end_3":{"num":102,"ext":""}}
+                    },
+            "imgt":{
+                "light":{
+                    "start_1": 27, "start_2":50,"start_3":89,"end_1":{"num":32,"ext":""},"end_2":{"num":51,"ext":""},"end_3":{"num":97,"ext":""}},
+                "heavy":{
+                    "start_1": 26, "start_2":51,"start_3":93,"end_1":{"num":33,"ext":""},"end_2":{"num":56,"ext":""},"end_3":{"num":102,"ext":""}}
+                    },
+            }
