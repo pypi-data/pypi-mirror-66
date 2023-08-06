c4a69e5c17385600189ebaaeab4d04a18a7c9d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:14 +0000

    Add package sinagot/0.1.3/sinagot-0.1.3.tar.gz

---
+++ packages/sinagot/sinagot-0.1.3.tar.gz/sinagot-0.1.3/sinagot/models/step.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+
+import inspect
+from pathlib import Path
+import pandas as pd
+from sinagot.models import Model
+from sinagot.utils import StepStatus, LOG_STEP_LABEL, LOG_STEP_STATUS
+
+
+class Step(Model):
+
+    _REPR_ATTRIBUTES = ["scope", "task", "modality", "label"]
+    _MODEL_TYPE = "step"
+    label = None
+    id = None
+
+    def __init__(self, script, model):
+
+        self.model = model
+        self.task = model.task
+        self.modality = model.modality
+        super().__init__(model.dataset)
+
+        if inspect.isclass(script):
+            script_class = script
+            self.label = script.__class__.__name__
+        elif isinstance(script, str):
+            script_class = self._get_module("Script", self.modality, script)
+            self.label = script
+        else:
+            raise AttributeError("Type {} is not valid for script".format(type(script)))
+
+        if model._MODEL_TYPE == "record":
+            self.id = model.id
+
+        self.script_class = script_class
+        self.script = script_class(
+            data_path=self.dataset._data_path,
+            id_=self.id,
+            task=self.task,
+            logger_namespace=self.logger.name,
+        )
+
+    def _script_path_exists(self, position: str) -> bool:
+        path = getattr(self.script.path, position)
+        if isinstance(path, Path):
+            return path.exists()
+        if isinstance(path, dict):
+            return all([p.exists() for p in path.values()])
+
+    def status(self):
+        if self._script_path_exists("output"):
+            return StepStatus.DONE
+        try:
+            logs = self.logs()
+            status = logs[logs[LOG_STEP_STATUS].notna()].iloc[0][LOG_STEP_STATUS]
+        except:
+            status = None
+        if status in [StepStatus.PROCESSING, StepStatus.ERROR]:
+            return status
+        if self._script_path_exists("input"):
+            return StepStatus.DATA_READY
+        else:
+            return StepStatus.INIT
+
+    def run(self):
+        self.model.run()
+
+    def logs(self):
+        try:
+            return self.model.logs().query(
+                "{} == '{}'".format(LOG_STEP_LABEL, self.label)
+            )
+        except:
+            return pd.DataFrame()
