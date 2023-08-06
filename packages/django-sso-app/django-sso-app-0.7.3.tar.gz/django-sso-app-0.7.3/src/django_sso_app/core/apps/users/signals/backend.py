1195ce8ff5c8ef48b93dfca31363a5116ec1dc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:07 +0000

    Add package django-sso-app/0.7.3/django-sso-app-0.7.3.tar.gz

---
+++ packages/django-sso-app/django-sso-app-0.7.3.tar.gz/django-sso-app-0.7.3/src/django_sso_app/core/apps/users/signals/backend.py
@@ -0,0 +1,14 @@
+import logging
+
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+
+logger = logging.getLogger('django_sso_app.core.apps.users.signals')
+
+
+@receiver(user_logged_in)
+def post_user_login(**kwargs):
+    user = kwargs['user']
+
+    if not user.is_superuser:
+        logger.debug('Users, "{}" LOGGED IN!!!'.format(user))
