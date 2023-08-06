6a67a03107c182f5d6db022afb306277bfba6155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:51 +0000

    Add package python-gql/0.0.8/python_gql-0.0.8-py3-none-any.whl

---
+++ packages/python-gql/python_gql-0.0.8-py3-none-any.whl/gql/__init__.py
@@ -0,0 +1,4 @@
+from .enum import enum_type  # noqa
+from .resolver import field_resolver, mutate, query, subscribe, type_resolver  # noqa
+from .scalar import scalar_type  # noqa
+from .utils import gql  # noqa
