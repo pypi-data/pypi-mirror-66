bd0817a7f7e7e6ef76fadb1503067c25f1fad649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:42 +0000

    Add package web-and-file-utils/0.1.3/web_and_file_utils-0.1.3-py3-none-any.whl

---
+++ packages/web-and-file-utils/web_and_file_utils-0.1.3-py3-none-any.whl/web_and_file_utils/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 19:49:28 2020
+
+@author: shriy
+"""
+from . import filepack
+from . import mailstat
+from . import getstat
+from . import getdictstat
+
+__all__ = [
+    'filepack',
+    'mailstat',
+    'getstat',
+    'getdictstat'
+]
+
