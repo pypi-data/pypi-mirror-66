2c1e69ee55e5cbf1fca0741228552a26b01fa7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package dsp-probability/0.1/dsp_probability-0.1.tar.gz

---
+++ packages/dsp-probability/dsp_probability-0.1.tar.gz/dsp_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsp_probability',
+      version='0.1',
+      description='Gaussian, Binomial distributions',
+      packages=['dsp_probability'],
+      author='Jose A Mancilla',
+      author_email = 'jt.mancilla@gmail.com',
+      zip_safe=False)
