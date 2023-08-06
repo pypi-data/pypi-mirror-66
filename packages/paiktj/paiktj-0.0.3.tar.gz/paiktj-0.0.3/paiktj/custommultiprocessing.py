12053e209b0c7f0ec1e1bd30dff9568a05c9a62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:06 +0000

    Add package paiktj/0.0.3/paiktj-0.0.3.tar.gz

---
+++ packages/paiktj/paiktj-0.0.3.tar.gz/paiktj-0.0.3/paiktj/custommultiprocessing.py
@@ -0,0 +1,33 @@
+from multiprocessing import Pool
+
+
+def my_pool(func, iterable, processes, verbose=True, verbose_option="number", print_end_option="\n"):
+    if verbose:
+        global __wrapper
+        if verbose_option == "number":
+            num = len(iterable)
+
+            def __wrapper(i):
+                print("Doing multiprocessing : index : ", i, "/", num, end=print_end_option)
+                return func(iterable[i])
+
+            pool = Pool(processes=processes)
+            result = pool.map(__wrapper, range(num))
+            pool.close()
+            pool.join()
+        else:
+            def __wrapper(param):
+                print("Doing multiprocessing : param : ", param, end=print_end_option)
+                return func(param)
+
+            pool = Pool(processes=processes)
+            result = pool.map(__wrapper, iterable)
+            pool.close()
+            pool.join()
+        return result
+    else:
+        pool = Pool(processes=processes)
+        result = pool.map(func, iterable)
+        pool.close()
+        pool.join()
+        return result
