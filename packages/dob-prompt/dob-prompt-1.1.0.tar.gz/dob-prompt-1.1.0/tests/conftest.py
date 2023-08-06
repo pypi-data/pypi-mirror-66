36cdd2db6d885b87a32e970cf509b179f8fb2f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:00 +0000

    Add package dob-prompt/1.1.0/dob-prompt-1.1.0.tar.gz

---
+++ packages/dob-prompt/dob-prompt-1.1.0.tar.gz/dob-prompt-1.1.0/tests/conftest.py
@@ -0,0 +1,33 @@
+# This file exists within 'dob-prompt':
+#
+#   https://github.com/hotoffthehamster/dob-prompt
+#
+# Copyright © 2019-2020 Landon Bouma. All rights reserved.
+#
+# This program is free software:  you can redistribute it  and/or  modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3  of the License,  or  (at your option)  any later version  (GPLv3+).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU  General  Public  License  for  more  details.
+#
+# If you lost the GNU General Public License that ships with this software
+# repository (read the 'LICENSE' file), see <http://www.gnu.org/licenses/>.
+
+"""
+Fixtures available to the tests/.
+
+- In general, fixtures should return a single instance.
+
+- If a fixture is a factory, its name should reflect that.
+
+- A fixture that is parametrized should be suffixed with
+  ``_parametrized`` to imply it has increased complexity.
+"""
+
+# For the fixture to work, either import specifically, or glob it all.
+# noqa: F401 'foo.bar' imported but unused
+# noqa: F403 'from foo.bar import *' used; unable to detect undefined names
+from dob_bright.tests.conftest import *  # noqa: F401, F403
+
