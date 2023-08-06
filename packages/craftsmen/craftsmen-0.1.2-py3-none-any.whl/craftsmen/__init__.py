0a3094fcd37e70953902c7afbbed3a51b43c67a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:14 +0000

    Add package craftsmen/0.1.2/craftsmen-0.1.2-py3-none-any.whl

---
+++ packages/craftsmen/craftsmen-0.1.2-py3-none-any.whl/craftsmen/__init__.py
@@ -0,0 +1,14 @@
+from .identity import identity
+from .curry.curry import curry
+from .curry.placeholder import placeholder, p
+from .compose import compose
+
+__version__ = '0.1.2'
+
+__all__ = [
+    'identity',
+    'curry',
+    'placeholder',
+    'p',
+    'compose',
+]
