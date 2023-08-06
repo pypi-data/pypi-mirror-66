830c8407239e3a7a1210a5b47a0ee5eedb8f567c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:06 +0000

    Add package onelab/1.0/onelab-1.0.tar.gz

---
+++ packages/onelab/onelab-1.0.tar.gz/onelab-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="onelab",
+    version="1.0",
+    author="Jack Kennedy",
+    author_email="jackbkennedy@gmail.com",
+    description="A python package to use gmsh and ONELAB via their original python APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackbkennedy/onelab",
+    packages=setuptools.find_packages(),
+    license="GPL",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy', 'pandas']
+)
