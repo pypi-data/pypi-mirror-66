9318471e9e5fa20a58d8e5713b0d2340d2097127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:11 +0000

    Add package hedaro/0.0.4/hedaro-0.0.4.tar.gz

---
+++ packages/hedaro/hedaro-0.0.4.tar.gz/hedaro-0.0.4/hedaro/__init__.py
@@ -0,0 +1,15 @@
+﻿from __future__ import absolute_import
+from hedaro.core.main import (
+get_sublist3r,
+get_amass,
+get_subdomains,
+)
+ 
+
+"""hedaro - A pentesting library for Python"""
+
+__version__ = '0.0.4'
+__author__ = 'David Rojas <david@hedaro.com>'
+__all__ = []
+
+
