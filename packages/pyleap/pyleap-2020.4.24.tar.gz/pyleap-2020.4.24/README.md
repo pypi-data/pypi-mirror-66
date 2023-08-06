3485dff8e1409f261f678568b092261a3b10194e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:38 +0000

    Add package pyleap/2020.4.24/pyleap-2020.4.24.tar.gz

---
+++ packages/pyleap/pyleap-2020.4.24.tar.gz/pyleap-2020.4.24/README.md
@@ -0,0 +1,47 @@
+# pyleap使用手册
+
+### 安装 Install
+
+```shell
+pip install pyleap
+```
+
+### 使用 Usage
+
+在项目中引入pyleap即可使用pyleap相关的功能
+
+```python
+from pyleap import *
+```
+
+在每次使用code环境前，启动leapserver模块。
+
+```shell
+python -m leapserver
+```
+
+
+
+### 更新
+
+```shell
+pip install pyleap --upgrade
+或
+pip install pyleap -U
+```
+
+
+
+### 打包
+
+如果要将程序打包，可以安装pyinstaller
+
+```shell
+pip install pyinstaller
+```
+
+打包命令可以参照pyinstaller文档，如果要打成一个文件，按照以下指令，将my_file替换为自己的文件名。
+
+```shell
+$ pyinstaller -F my_file.py --windowed
+```
---
+++ packages/pyleap/pyleap-2020.4.24.tar.gz/pyleap-2020.4.24/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+# pip install twine wheel
+
+# window: rmdir /s/q dist
+# python setup.py sdist bdist_wheel
+# twine upload dist/* -u VicWang -p PYPI******
+
+setuptools.setup(
+    name="pyleap",
+    version="2020.4.24",
+    author="Vic Wang",
+    author_email="305880887@qq.com",
+    description='A simple 2D game engine base on pyglet',
+    long_description='A simple 2D game engine base on pyglet',
+    long_description_content_type="text/markdown",
+    url='http://github.com/XRDX/pyleap',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyglet==1.3.3',
+        'leaptask>=2020.4.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
