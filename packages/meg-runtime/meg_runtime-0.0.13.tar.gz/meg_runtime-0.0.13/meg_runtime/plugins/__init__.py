db2d0af55e9ccb90a84969fa574bde84d48cfa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:39 +0000

    Add package meg-runtime/0.0.13/meg_runtime-0.0.13.tar.gz

---
+++ packages/meg-runtime/meg_runtime-0.0.13.tar.gz/meg_runtime-0.0.13/meg_runtime/plugins/__init__.py
@@ -0,0 +1,4 @@
+""" Multimedia Extensible Git runtime plugin interfaces """
+
+from meg_runtime.plugins.plugin import Plugin, PluginInformation, PluginException
+from meg_runtime.plugins.manager import PluginManager
