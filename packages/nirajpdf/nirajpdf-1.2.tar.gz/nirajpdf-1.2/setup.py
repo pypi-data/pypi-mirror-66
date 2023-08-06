715097e69bf18f13595175fecf1c729e2e4e3ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:24 +0000

    Add package nirajpdf/1.2/nirajpdf-1.2.tar.gz

---
+++ packages/nirajpdf/nirajpdf-1.2.tar.gz/nirajpdf-1.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="nirajpdf",
+    version="1.2",
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+
+)
