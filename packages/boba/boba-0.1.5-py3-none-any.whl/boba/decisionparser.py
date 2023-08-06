f7566804427faaf5a08f07c6159044285882141b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package boba/0.1.5/boba-0.1.5-py3-none-any.whl

---
+++ packages/boba/boba-0.1.5-py3-none-any.whl/boba/decisionparser.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+
+import json
+from dataclasses import dataclass
+from .baseparser import ParseError, BaseParser
+
+
+@dataclass
+class Decision:
+    var: str
+    value: list
+    desc: str = ''
+
+
+class DecisionParser(BaseParser):
+    def __init__(self):
+        super(DecisionParser, self).__init__('')
+        self.decisions = {}
+
+    @staticmethod
+    def _is_syntax_start(ch):
+        return ch == '{'
+
+    @staticmethod
+    def _is_id_token(s):
+        if not BaseParser._is_id_start(s[0]):
+            return False
+
+        for ch in s:
+            if not BaseParser._is_id(ch):
+                return False
+
+        return True
+
+    @staticmethod
+    def _read_value(s):
+        try:
+            res = list(s)
+        except ValueError:
+            raise ParseError('Cannot handle value "{}"'.format(s))
+
+        if len(s) == 0:
+            raise ParseError('Cannot handle decision value "[]"')
+        return res
+
+    def _read_json_safe(self, obj, field):
+        if field not in obj:
+            raise ParseError('Cannot find "{}" in json'.format(field))
+        return obj[field]
+
+    def _check_type(self, var, fun, msg):
+        if not fun(var):
+            raise ParseError('Cannot handle {} "{}"'.format(msg, var))
+        return var
+
+    def verify_naming(self, reserved):
+        """
+        Verify if the decision names collide with any other variable names.
+        :param reserved: A list of reserved names.
+        :return:
+        """
+        for w in reserved:
+            if w in self.decisions:
+                raise ParseError('Duplicate variable/block name "{}"'.format(w))
+
+    def read_decisions(self, spec):
+        """
+        Read decisions from the JSON spec.
+        :return: a dict of decisions
+        """
+        dec_spec = spec['decisions'] if 'decisions' in spec else []
+        for d in dec_spec:
+            desc = d['desc'] if 'desc' in d else 'Decision {}'.format(d['var'])
+
+            var = self._check_type(self._read_json_safe(d, 'var'),
+                                   DecisionParser._is_id_token, 'id')
+            value = self._read_value(self._read_json_safe(d, 'options'))
+
+            # check if two variables have the same name
+            if var in self.decisions:
+                raise ParseError('Duplicate variable name "{}"'.format(var))
+
+            decision = Decision(var, value, desc)
+            self.decisions[var] = decision
+
+        return self.decisions
+
+    def get_num_alt(self, dec):
+        """
+        Return the number of possible alternatives.
+        For discrete type, it's the number of options.
+        :param dec: variable ID of a decision
+        :return: number
+        """
+        return len(self.decisions[dec].value)
+
+    def get_alt(self, dec, i_alt):
+        """
+        Return the i-th alternative. For discrete type, it's simply the i-th
+        option specified.
+        :param dec: variable ID of a decision
+        :param i_alt: which alternative
+        :return: value of the option
+        """
+        return self.decisions[dec].value[i_alt]
+
+    def get_cross_prod(self):
+        """
+        Get the maximum possible cardinality of options, computed as a cross
+        product of all decisions.
+        :return: number
+        """
+        ret = 1
+        for dec in self.decisions:
+            ret *= self.get_num_alt(dec)
+        return ret
+
+    def get_decs(self):
+        """Get a list of decision names."""
+        return [i for i in self.decisions.keys()]
+
+    def gen_code(self, template, dec_id, i_alt):
+        """
+        Replace the placeholder variable in a template chunk.
+        :param template: a chunk of code with only one placeholder
+        :param dec_id: variable ID of the decision
+        :param i_alt: which alternative
+        :return: {string, string} replaced code and the value at this parameter
+        """
+        v = self.get_alt(dec_id, i_alt)
+
+        # assuming the placeholder var is always at the end
+        # which is true given how we chop up the chunks
+        return template + str(v), str(v)
+
+    def parse_code(self, line):
+        """
+        Find placeholder variables in a line of code.
+        :param line: a line of code
+        :return: (variables, chunks) a list of found variables and a list of
+                 code chunks, each containing one variable
+        """
+        code = []
+        res = []
+        j = 0
+        i_start = 0
+        self.i = 0
+        self.line = line
+
+        while not self._is_end():
+            if self._is_syntax_start(self._peek_char()):
+                token = self._read_while(lambda ch: ch == '{')
+                if len(token) < 2:
+                    continue
+                i_start = self.i - 2
+
+                # read variable identifier
+                if not self._is_id_start(self._peek_char()):
+                    continue
+                val = self._read_while(self._is_id)
+                if len(val) == 0:
+                    continue
+
+                # read definition, if any
+                df = None
+                self._read_while(self._is_whitespace)
+                if not self._is_end() and self._peek_char() == '=':
+                    self._next_char()
+                    # problem: the variable value can't contain "}"
+                    df = self._read_while(lambda ch: ch != '}')
+                    self._read_while(self._is_whitespace)
+
+                token = self._read_while(lambda ch: ch == '}', max_len=2)
+                if len(token) < 2:
+                    continue
+
+                # read succeeds
+                code.append(line[j:i_start])
+                res.append(val)
+                j = self.i
+
+                # parse and save definition
+                if df:
+                    try:
+                        df = json.loads('[{}]'.format(df))
+                        decision = Decision(val, df, '')
+                        if val in self.decisions:
+                            msg = 'Duplicate variable definition "{}"'
+                            raise ParseError(msg.format(val))
+                        self.decisions[val] = decision
+                    except ValueError:
+                        msg = 'Cannot parse variable definition:\n{}'
+                        raise ParseError(msg.format(df))
+            else:
+                self._next_char()
+
+        code.append(line[j:])
+        return res, code
---
+++ packages/boba/boba-0.1.5-py3-none-any.whl/boba/wrangler.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import csv
+import json
+from dataclasses import dataclass
+from .baseparser import ParseError
+
+
+@dataclass
+class Output:
+    name: str
+    value: str
+
+
+exec_template = """\
+#!/bin/sh
+
+cleanup ()
+{{
+echo "kill -s TERM $!"
+kill -s TERM $!
+exit 0
+}}
+
+merge_log ()
+{{
+boba merge exit_status_{{}}.csv -b $DLOG --out $DLOG/exit_status.csv
+rm $DLOG/exit_status_*.csv
+}}
+
+{}
+
+DIR="$( cd "$( dirname "${{BASH_SOURCE[0]}}" )" >/dev/null 2>&1 && pwd )"
+DLOG=$DIR/boba_logs
+suffix={}
+num={}
+i=1
+
+# create a folder for logs
+rm -rf $DLOG
+mkdir $DLOG
+
+# if specified, change the range of universes to execute
+if [ "$1" != "" ]; then i=$1; num=$1; fi
+if [ "$2" != "" ]; then num=$2; fi
+
+cd $DIR/code
+
+trap cleanup SIGINT SIGTERM
+
+while [ $i -le $num ]
+do
+  # execute the universe
+  f="universe_$i$suffix"
+  echo "{} $f"
+  (set -o pipefail; {} $f 2>&1 | tee $DLOG/log_$i.txt)
+
+  # collect exit status
+  printf "%s\\n%d" exit_status $? > $DLOG/exit_status_$i.csv
+
+  # increment
+  i=$(( i+1 ))
+  wait $!
+done
+
+merge_log
+{}
+"""
+
+DIR_SCRIPT = 'code/'
+
+
+class Wrangler:
+    """Handles outputs."""
+    def __init__(self, spec, lang, out):
+        self.spec = spec
+        self.lang = lang
+        self.out = out
+        self.fn = os.path.abspath(os.path.join(out, 'summary.csv'))
+
+        self.outputs = {}
+        self.col = 0  # output column number, will be set by parser
+        self.counter = 0
+
+        self.pre_exe = ''
+        self.post_exe = ''
+
+        self._read_spec()
+
+    @staticmethod
+    def _read_json_safe(obj, field):
+        if field not in obj:
+            raise ParseError('Cannot find "{}" in json'.format(field))
+        return obj[field]
+
+    @staticmethod
+    def _read_optional(obj, field, df):
+        return obj[field] if field in obj else df
+
+    def _read_spec(self):
+        """Read misc fields from the JSON spec."""
+        sp = self._read_optional(self.spec, 'outputs', [])
+        for d in sp:
+            name = str(self._read_json_safe(d, 'name'))
+            value = str(self._read_json_safe(d, 'value'))
+            o = Output(name, value)
+            self.outputs[name] = o
+
+        self.pre_exe = self._read_optional(self.spec, 'before_execute', '')
+        self.post_exe = self._read_optional(self.spec, 'after_execute', '')
+
+    def _codegen_r(self):
+        """Generate output code for R scripts."""
+        if len(self.outputs) == 0:
+            return ''
+
+        # read csv
+        code = '\n\n# wrangles output\n' \
+            'df <- read.csv2("{}", sep = ",", stringsAsFactors = FALSE, ' \
+               'check.names=FALSE)'\
+            .format(self.fn)
+
+        # record outputs
+        ns = self.get_outputs()
+        col = self.col + 1
+        row = self.counter
+        for n in ns:
+            code += '\ndf[{}, {}] = {}'.format(row, col, self.outputs[n].value)
+            col += 1
+
+        # write csv
+        code += '\nwrite.csv(df, file="{}", row.names=FALSE)'.format(self.fn)
+        code += '\n'
+
+        return code
+
+    def _codegen_python(self):
+        if len(self.outputs) == 0:
+            return ''
+
+        # TODO
+
+    def _gen_code(self):
+        """Generate output code to be appended to the end of the script."""
+        if self.lang.is_r():
+            return self._codegen_r()
+        if self.lang.is_python():
+            return self._codegen_python()
+
+    def write_sh(self):
+        """Write a shell script for executing all universes."""
+        cmd = self.lang.get_cmd()
+        sh = exec_template.format(self.pre_exe,
+                                  self.lang.get_ext(), self.counter, cmd, cmd,
+                                  self.post_exe)
+        fn_exec = os.path.join(self.out, 'execute.sh')
+        with open(fn_exec, 'w') as f:
+            f.write(sh)
+        st = os.stat(fn_exec)
+        os.chmod(fn_exec, st.st_mode | 0o0111)
+
+    def write_universe(self, code):
+        """Write the generated code to a universe file."""
+
+        self.counter += 1
+        fn = 'universe_{}{}'.format(self.counter, self.lang.get_ext())
+
+        # replace the reserved keyword _n
+        code = code.replace('{{_n}}', str(self.counter))
+
+        # append output code
+        code += self._gen_code()
+
+        # write file
+        with open(os.path.join(self.out, DIR_SCRIPT, fn), 'w') as f:
+            f.write(code)
+            f.flush()
+
+        return fn
+
+    def write_summary(self, rows):
+        """Write the summary CSV file"""
+        with open(self.fn, 'w', newline='') as f:
+            wrt = csv.writer(f)
+            for row in rows:
+                wrt.writerow(row)
+
+    def write_overview_json(self, res):
+        """ Write the overview.json file"""
+        # append visualizer block
+        default_config = {
+            "files": [{"id": "est", "path": "estimates.csv"}],
+            "schema": {"point_estimate": {"file": "est", "field": "estimate"}}
+        }
+        vis = Wrangler._read_optional(self.spec, 'visualizer', None)
+
+        # if it is a string, read config file
+        if isinstance(vis, str):
+            try:
+                with open(vis) as f:
+                    vis = json.load(f)
+            except (IOError, json.JSONDecodeError) as e:
+                print(e)
+                print('Cannot read the visualizer config, using the default')
+                vis = default_config
+        # if user does not specify the config, use the default
+        vis = default_config if vis is None else vis
+        res['visualizer'] = vis
+
+        with open(os.path.join(self.out, 'overview.json'), 'w') as f:
+            obj = json.dumps(res, indent=2, sort_keys=True)
+            f.write(obj)
+
+    def create_dir(self):
+        """Create output directories."""
+        if os.path.exists(self.out):
+            shutil.rmtree(self.out)
+        os.makedirs(self.out)
+        os.makedirs(os.path.join(self.out, DIR_SCRIPT))
+
+    def get_outputs(self):
+        """Get a sorted list of output names."""
+        return sorted(list(self.outputs.keys()))
