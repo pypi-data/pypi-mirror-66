7120041aa37be1596e72ac882e15bf91144a7de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:13 +0000

    Add package custom-django-auth-backend/0.1.1/custom-django-auth-backend-0.1.1.tar.gz

---
+++ packages/custom-django-auth-backend/custom-django-auth-backend-0.1.1.tar.gz/custom-django-auth-backend-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from os.path import dirname, join
+from setuptools import setup, find_packages
+
+
+setup(
+    name='custom-django-auth-backend',
+    version='0.1.1',
+    license='MIT',
+    url='https://github.com/Chiorufarewerin/custom-django-auth-backend',
+    author='Artur Beltsov',
+    author_email='artur1998g@gmail.com',
+    description='Easy add some auth backends in django project',
+    long_description=open(join(dirname(__file__), 'README.md'), encoding='utf-8').read(),
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    keyword=['django', 'auth', 'backend'],
+    project_urls={
+        'GitHub': 'https://github.com/Chiorufarewerin/custom-django-auth-backend',
+    },
+    install_requires=[
+        'autowrapt>=1.0',
+        'django<3.0',
+        'lazy-object-proxy==1.4.*',
+    ],
+)
