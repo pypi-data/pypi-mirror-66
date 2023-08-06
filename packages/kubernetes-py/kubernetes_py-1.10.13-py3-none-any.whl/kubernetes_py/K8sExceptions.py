e4e2a9988bc1806618778798de271e0137a3820a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:10 +0000

    Add package kubernetes-py/1.10.13/kubernetes_py-1.10.13-py3-none-any.whl

---
+++ packages/kubernetes-py/kubernetes_py-1.10.13-py3-none-any.whl/kubernetes_py/K8sExceptions.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+
+
+class InvalidObjectException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(InvalidObjectException, self).__init__(*args, **kwargs)
+
+
+class UnauthorizedException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(UnauthorizedException, self).__init__(*args, **kwargs)
+
+
+class NotFoundException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(NotFoundException, self).__init__(*args, **kwargs)
+
+
+class UnprocessableEntityException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(UnprocessableEntityException, self).__init__(*args, **kwargs)
+
+
+class BadRequestException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(BadRequestException, self).__init__(*args, **kwargs)
+
+
+class AlreadyExistsException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(AlreadyExistsException, self).__init__(*args, **kwargs)
+
+
+class TimedOutException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(TimedOutException, self).__init__(*args, **kwargs)
+
+
+class PodNotReadyException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(PodNotReadyException, self).__init__(*args, **kwargs)
+
+
+class VersionMismatchException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(VersionMismatchException, self).__init__(*args, **kwargs)
+
+
+class DrainNodeException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(DrainNodeException, self).__init__(*args, **kwargs)
+
+
+class CronJobAlreadyRunningException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(CronJobAlreadyRunningException, self).__init__(*args, **kwargs)
+
+
+class CronJobRunException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(CronJobRunException, self).__init__(*args, **kwargs)
