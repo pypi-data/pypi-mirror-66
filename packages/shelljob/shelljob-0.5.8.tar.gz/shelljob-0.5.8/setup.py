ed537f5d7a8956a8f0e9402a3f8d0e6e58182b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:02 +0000

    Add package shelljob/0.5.8/shelljob-0.5.8.tar.gz

---
+++ packages/shelljob/shelljob-0.5.8.tar.gz/shelljob-0.5.8/setup.py
@@ -0,0 +1,53 @@
+# shelljob setup.py
+import os,sys
+from setuptools import setup
+
+if sys.version_info[0] == 2:
+    base_dir = 'python2'
+elif sys.version_info[0] == 3:
+    base_dir = 'python3'
+
+# https://bugs.launchpad.net/mortoray.com/+bug/1274176
+try:
+	import pypandoc
+	desc = pypandoc.convert( 'README.md', 'rst' )
+except Exception as e:
+	input( 'Generating Description failed, press enter to process anyway, or kill to cancel' )
+	desc = ''
+
+def list_files(path):
+	m = []
+	for root, dirnames, filenames in os.walk(path):
+		for filename in filenames:
+			m.append(os.path.join(root, filename))
+	return m
+			
+setup(
+	name = 'shelljob',
+	packages = [ 'shelljob' ],
+	package_dir = {
+		'': base_dir,
+	},
+	version = '0.5.8',
+	description = 'Run multiple subprocesses asynchronous/in parallel with streamed output/non-blocking reading. Also various tools to replace shell scripts.',
+	author = 'edA-qa mort-ora-y',
+	author_email = 'eda-qa@disemia.com',
+	url = 'https://pypi.python.org/pypi/shelljob',
+	classifiers = [
+		'Development Status :: 6 - Mature',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Intended Audience :: Developers',
+		'Environment :: Console',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Operating System :: OS Independent',
+		'Topic :: Terminals',
+		'Topic :: System',
+		'Topic :: Software Development :: Build Tools',
+		],
+	long_description = desc,
+	package_data = { 
+		'shelljob': [ base_dir + '/doc/*' ] 
+	},
+	license = 'GPLv3',
+)
