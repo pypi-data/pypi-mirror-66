e28d7aa7a671165fcd72647650e5c57146f4d8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:23 +0000

    Add package normal-distributions/0.2/normal%20distributions-0.2.tar.gz

---
+++ packages/normal-distributions/normal%20distributions-0.2.tar.gz/normal distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='normal distributions',
+      version='0.2',
+      description='normal distributions',
+      packages=['norm_dist_generator'],
+      zip_safe=False)
