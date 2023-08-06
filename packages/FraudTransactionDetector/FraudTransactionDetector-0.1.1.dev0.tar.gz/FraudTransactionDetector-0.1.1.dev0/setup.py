6d7e13b3069cbd1a44588a3ffebc955deb97c17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:03 +0000

    Add package FraudTransactionDetector/0.1.1.dev0/FraudTransactionDetector-0.1.1.dev0.tar.gz

---
+++ packages/FraudTransactionDetector/FraudTransactionDetector-0.1.1.dev0.tar.gz/FraudTransactionDetector-0.1.1.dev0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='FraudTransactionDetector',
+    version='0.1.1dev',
+    author='Venkata Siva Rama Sastry Kavuri',
+    author_email='sivaram.kavuri@gmail.com',
+    packages=['fraudtransactiondetector',],
+    scripts=['bin/sample-fraud-identifier.py',],
+    url='http://pypi.python.org/pypi/FraudTransactionDetector/',
+    license='License.txt',
+    description='Scalable Fraud Transaction Identifier using Clustering, Anamoly Detection and Classification ML Algorithms',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "h2o >= 3.30.0.1",
+        "pandas >= 0.25.1",
+        "numpy >= 1.16.5",
+        "matplotlib >= 3.1.3",
+        "sklearn >= 0.21.3",
+    ],
+)
