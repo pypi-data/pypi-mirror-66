390d3ce52354e860fcee46a6896d9d13feb4026e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:57 +0000

    Add package python-juicenet/1.0.0/python-juicenet-1.0.0.tar.gz

---
+++ packages/python-juicenet/python-juicenet-1.0.0.tar.gz/python-juicenet-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(name='python-juicenet',
+      version='1.0.0',
+      description='Read and control Juicenet/Juicepoint/Juicebox based EVSE devices',
+      url='http://github.com/jesserockz/python-juicenet',
+      author='@jesserockz',
+      license='MIT',
+      install_requires=[
+            'aiohttp',
+            'asyncio'
+      ],
+      packages=find_packages(exclude=["dist"]),
+      zip_safe=True)
