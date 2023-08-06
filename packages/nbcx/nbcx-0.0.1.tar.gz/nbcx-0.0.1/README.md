04919a55f3a2bc48330f485bbdcddcef0d87a226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:21 +0000

    Add package nbcx/0.0.1/nbcx-0.0.1.tar.gz

---
+++ packages/nbcx/nbcx-0.0.1.tar.gz/nbcx-0.0.1/README.md
@@ -0,0 +1,36 @@
+# nbconvertX
+A framework for customizing NBConvert templates and building reports
+
+[![Build Status](https://dev.azure.com/tpaine154/jupyter/_apis/build/status/timkpaine.nbcx?branchName=master)](https://dev.azure.com/tpaine154/jupyter/_build/latest?definitionId=25&branchName=master)
+[![Coverage](https://img.shields.io/azure-devops/coverage/tpaine154/jupyter/25)](https://dev.azure.com/tpaine154/jupyter/_build?definitionId=25&_a=summary)
+
+## Templates
+NBConvert's default templates are largely designed with academic styling. This repo is a collection of templates with industrial/business reports in mind.
+
+## Gallery
+
+|||
+|:--|:--|
+|Sample - PDF||
+|[![sample.pdf](examples/sample.png)](examples/sample.pdf)|[Template](nbcx/templates/reports/abc.tex.j2)|
+
+
+## Getting started
+You can install via:
+
+`pip install nbcx`
+
+or from source:
+
+`python setup.py install`
+
+
+## Usage
+This library exposes a variety of templates (See the gallery above), as well as a few exporters to handle customizations not possible with `nbconvert` exporters. Thus, we expose a number of targets (you can see these as `entrypoints` with `nbconvert.exporters` key in the `setup.py` file).
+
+Here is an example usage:
+
+`jupyter nbconvert --to nbcx_pdf sample.ipynb  --execute --template nbcx/templates/reports/abc.tex.j2`
+
+
+
---
+++ packages/nbcx/nbcx-0.0.1.tar.gz/nbcx-0.0.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+from jupyter_packaging import ensure_python, get_version
+
+pjoin = path.join
+
+ensure_python(('2.7', '>=3.3'))
+
+name = 'nbcx'
+here = path.abspath(path.dirname(__file__))
+version = get_version(pjoin(here, name, '_version.py'))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requires = [
+    "nbconvert>=5.6.1",
+    "IPython>=7.13.0"
+]
+
+setup(
+    name=name,
+    version=version,
+    description='NBConvert Templates',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/timkpaine/nbcx',
+    author='Tim Paine',
+    author_email='t.paine154@gmail.com',
+    license='Apache 2.0',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Jupyter',
+    ],
+
+    keywords='jupyter nbconvert',
+    packages=find_packages(exclude=['tests', ]),
+    install_requires=requires,
+    extras_require={
+        'dev': requires + ['pytest', 'pytest-cov', 'pylint', 'flake8', 'bumpversion', 'mock', 'codecov']
+    },
+    entry_points = {
+        'nbconvert.exporters': [
+            'nbcx_pdf = nbcx.exporters:NBCXPDFExporter',
+            'nbcx_latex = nbcx.exporters:NBCXLatexExporter',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+
+)
