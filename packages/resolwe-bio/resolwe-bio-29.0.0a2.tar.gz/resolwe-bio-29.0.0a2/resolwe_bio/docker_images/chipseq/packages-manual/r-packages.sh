b031534afcc2c273e8134a1cf47ac38b3ea92a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:12 +0000

    Add package resolwe-bio/29.0.0a2/resolwe-bio-29.0.0a2.tar.gz

---
+++ packages/resolwe-bio/resolwe-bio-29.0.0a2.tar.gz/resolwe-bio-29.0.0a2/resolwe_bio/docker_images/chipseq/packages-manual/r-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+. /var/cache/build/packages-manual/common.sh
+
+# XXX: This is unverifiable and thus may compromise the whole image.
+# XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
+Rscript --slave --no-save --no-restore-history -e " \
+  install.packages('spp', dependencies=TRUE) \
+"
