21b7c4b839ea9e2758ef83ddac3f9878d2a57580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package requests5/1.0.0/requests5-1.0.0.tar.gz

---
+++ packages/requests5/requests5-1.0.0.tar.gz/requests5-1.0.0/requests5/fast_requests_project.py
@@ -0,0 +1,8 @@
+'''
+@File       :   fast_requests_project.py
+@Author     :   Jiang Fubang
+@Time       :   2020/4/20 17:58
+@Version    :   1.0
+@Contact    :   luckybang@163.com
+@Dect       :   None
+'''
