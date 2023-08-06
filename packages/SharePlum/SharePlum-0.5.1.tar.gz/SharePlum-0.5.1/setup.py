cccf7e3c902466d1a2974021d9d12a1583a3ea65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:07 +0000

    Add package SharePlum/0.5.1/SharePlum-0.5.1.tar.gz

---
+++ packages/SharePlum/SharePlum-0.5.1.tar.gz/SharePlum-0.5.1/setup.py
@@ -0,0 +1,40 @@
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from codecs import open
+
+with open('shareplum/version.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='SharePlum',
+    version=version,
+    description='Python SharePoint Library',
+    long_description=open('README.rst').read(),
+    url='https://github.com/jasonrollins/shareplum',
+    author='Jason Rollins',
+    author_email='jason.c.rollins@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Office/Business',
+    ],
+    keywords=['SharePoint'],
+    packages=['shareplum'],
+    install_requires=['lxml', 'requests', 'requests-ntlm', 'requests-toolbelt'],
+)
