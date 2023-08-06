7a9deacbfc6e17388d354a66587c679c3792dced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:30 +0000

    Add package backtest/0.7.4/backtest-0.7.4-py3-none-any.whl

---
+++ packages/backtest/backtest-0.7.4-py3-none-any.whl/backtest/broker.py
@@ -0,0 +1,100 @@
+import json
+import logging
+import queue
+from datetime import datetime
+from multiprocessing import Value, Event, Queue
+from os.path import join
+from pathlib import Path
+
+import numpy as np
+
+from .data import Order 
+
+logger = logging.getLogger('broker')
+
+
+def run(rules, strategy, initial_cash, quantity_dict, signal_queue, ledger_dir, done: Event):
+    ledger = _prepare_ledger(ledger_dir)
+    print(json.dumps({'cash': initial_cash}), file=ledger)
+    cash = initial_cash
+
+    count = 0
+    while not done.is_set():
+        try:
+            signal = signal_queue.get(block=True, timeout=1)
+            count += 1
+        except queue.Empty:
+            continue
+
+        quantity = strategy.calc_quantity(signal, cash, quantity_dict)
+
+        price = _simulate_market_price(signal.price,
+                                       quantity,
+                                       rules[signal.market]['price_units'])
+
+        commission = _calc_commission(price,
+                                      quantity,
+                                      rules[signal.market]['commission'])
+
+        tax = _calc_tax(price,
+                        quantity,
+                        rules[signal.market]['tax'])
+
+        order = Order(
+            signal.symbol,
+            signal.market,
+            price,
+            quantity,
+            commission,
+            tax,
+            signal.price - price,
+            signal.timestamp,
+        )
+
+        initial_cash -= order.total_cost()
+        quantity_dict[order.symbol] = quantity_dict.get(signal.symbol, 0) \
+            + order.quantity
+
+        print(order.as_json(), file=ledger)
+        logger.debug('Wrote ' + repr(order))
+
+    ledger.close()
+    logger.info(f'Processed {count} orders and wrote to {ledger.name}')
+
+
+def _prepare_ledger(dir):
+    path = Path(dir)
+    path.mkdir(parents=True, exist_ok=True)
+    filename = f'{datetime.now():%Y%m%d%H%M%S}.jsonl'
+
+    return path.joinpath(filename).open('wt')
+
+
+def _simulate_market_price(price, quantity, price_units, slippage_stdev=0.7) -> float:
+    mean = 0.5 if quantity > 0 else -0.5,
+    offset = int(np.random.normal(mean, slippage_stdev))
+    price_unit = _get_price_unit(price, price_units)
+
+    return price + offset * price_unit
+
+
+def _get_price_unit(price, price_units) -> float:
+    for item in price_units:
+        if price < item['price']:
+            return item['unit']
+
+    return price_units[-1]['unit']
+
+
+def _calc_commission(price, quantity, rates) -> float:
+    trade = 'buy' if quantity > 0 else 'sell'
+    rate = rates[trade]
+
+    return round(price * abs(quantity) * rate)
+
+
+def _calc_tax(price, quantity, rates) -> float:
+    trade = 'buy' if quantity > 0 else 'sell'
+    rate = rates[trade]
+
+    return round(price * abs(quantity) * rate)
---
+++ packages/backtest/backtest-0.7.4-py3-none-any.whl/backtest/fetcher.py
@@ -0,0 +1,89 @@
+import logging
+import time
+from os import listdir
+from os.path import exists, isfile, isdir, join, basename
+
+from .data import Tick, RESET
+
+logger = logging.getLogger('fetcher')
+
+
+def run(tick_dir, tick_queues, done):
+    """Run fetcher
+
+    Args:
+        tick_dir (str): Directory that stores tick files.
+        tick_queues (multiprocessing.Queue): Queue that connects *Analyzer* module.
+        done (multiprocessing.Event): Will be set when there is no more tick.
+    
+    Returns:
+        None
+    """
+
+    files = [tick_dir] if isfile(tick_dir) else _list_dir(tick_dir)
+    route = _get_router(tick_queues)
+
+    count = 0
+    for file in files:
+        for t in _read_tick(file):
+            queue = route(t.symbol)
+            queue.put(t)
+            count += 1
+
+        logger.debug(f'Sending RESET messages (End of {file})')
+        [queue.put(RESET) for queue in tick_queues]
+
+    logger.info(f'Fetched {count} ticks')
+    time.sleep(2)
+    done.set()
+
+
+def _read_tick(file):
+    with open(file, 'rt') as f:
+        for i, line in enumerate(f, 1):
+            try:
+                yield _parse(line)
+            except ValueError:
+                logger.error(f'{basename(file)} line {i} [{line.strip()}]')
+                continue
+
+
+def _list_dir(path: str) -> []:
+    assert isdir(path)
+
+    result = []
+    for f in listdir(path):
+        p = join(path, f)
+        if isfile(p):
+            result.append(p)
+
+    return sorted(result)
+
+
+def _parse(line: str) -> Tick:
+    symbol, price, volume, timestamp = line.split()
+
+    return Tick(
+        symbol,
+        float(price),
+        float(volume),
+        int(timestamp)
+    )
+
+
+def _get_router(queues):
+    counter = {queue: 0 for queue in queues}
+    assigned = {}
+
+    def fn(symbol):
+        try:
+            queue = assigned[symbol]
+
+        except KeyError:
+            queue = min(counter, key=counter.get)
+            assigned[symbol] = queue
+            counter[queue] += 1
+
+        return queue
+
+    return fn
