463274aa21b48054de6157b88f251a762334142a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:15 +0000

    Add package remarkable-keyboard/3/remarkable-keyboard-3.tar.gz

---
+++ packages/remarkable-keyboard/remarkable-keyboard-3.tar.gz/remarkable-keyboard-3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from remarkable_keyboard import version
+
+setup(
+    name='remarkable-keyboard',
+    version=version.__version__,
+    packages=['remarkable_keyboard'],
+    author="Evan Widloski",
+    author_email="evan@evanw.org",
+    description="use reMarkable as a wireless mouse and keyboard",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="GPLv3",
+    keywords="remarkable tablet evdev mouse keyboard",
+    url="https://github.com/evidlo/remarkable_keyboard",
+    entry_points={
+        'console_scripts': [
+            'remarkable-keyboard = remarkable_keyboard.remarkable_keyboard:main',
+            'rekeyboard = remarkable_keyboard.remarkable_keyboard:main'
+        ]
+    },
+    install_requires=[
+        'paramiko',
+        'screeninfo',
+        'pynput',
+        'remarkable-mouse>=5',
+        'sortedcontainers'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
