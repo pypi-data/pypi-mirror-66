3f76e562cfa205f98c417ea983ea603d370f9e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:27 +0000

    Add package COVID19-ES-Py/2.0.0/COVID19-ES-Py-2.0.0.tar.gz

---
+++ packages/COVID19-ES-Py/COVID19-ES-Py-2.0.0.tar.gz/COVID19-ES-Py-2.0.0/COVID19_ES_Py/__init__.py
@@ -0,0 +1,20 @@
+"""Leitor de relatórios de casos de COVID-19 no Espírito Santo.
+    >>> import COVID19_ES_Py
+    >>> leitor = COVID19_ES_Py.LeitorRelatorio()
+    >>> relatorio = leitor.carrega_ultimo_relatorio()
+
+Veja https://github.com/AtilioA/COVID19-ES-Py ou https://covid19-es-py.readthedocs.io/ para mais informações.
+"""
+
+# flake8: noqa
+from .boletim import (ScraperBoletim, Boletim)  # Descontinuado
+from .relatorio import (LeitorRelatorio, Relatorio)
+
+__all__ = [
+    'ScraperBoletim',
+    'Boletim',
+    'LeitorRelatorio',
+    'Relatorio',
+]
+
+__version__ = "2.0.0"
