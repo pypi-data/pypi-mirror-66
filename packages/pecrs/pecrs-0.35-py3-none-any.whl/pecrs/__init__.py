5be2f7a29bd03717eec0a8c886cad9242b406e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:08 +0000

    Add package pecrs/0.35/pecrs-0.35-py3-none-any.whl

---
+++ packages/pecrs/pecrs-0.35-py3-none-any.whl/pecrs/__init__.py
@@ -0,0 +1,14 @@
+from .shape import Shape
+from .shape import Rect
+from .shape import Circle
+
+from .clock import Clock
+from .clock import SyncClock
+
+from .index import Index
+from .spatialhash import SpatialHash
+from .collider import Collider
+from .space import Space
+
+__all__ = ["Shape", "Rect", "Circle", "Clock", "SyncClock", "Index", "SpatialHash", "Collider", "Space"]
+version = 0.035
