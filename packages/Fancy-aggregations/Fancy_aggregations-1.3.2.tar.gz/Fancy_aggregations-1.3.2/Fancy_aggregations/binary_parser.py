ceb7e5df76a1b2327742997600b0a90c808e4771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:50 +0000

    Add package Fancy-aggregations/1.3.2/Fancy_aggregations-1.3.2.tar.gz

---
+++ packages/Fancy-aggregations/Fancy_aggregations-1.3.2.tar.gz/Fancy_aggregations-1.3.2/Fancy_aggregations/binary_parser.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on 04/12/2019
+
+@author: Javier Fumanal Idocin
+"""
+import numpy as np
+
+from . import integrals
+from . import moderate_deviations
+from . import tnorms
+from . import networks
+from . import owas
+from . import overlaps
+
+supported_functions = ['mean', 'median', 'min', 'max', 'sugeno', 'shamacher', 'choquet', 'cfminmin', 'cf12', 'cf', 'owa1', 'owa2', 'owa3', 'geomean', 'sinoverlap', 'hmean']
+
+def parse(agg_name, axis_f = 0, keepdims_f=True):
+    agg_minuscula = agg_name.lower()
+    if agg_minuscula == 'mean':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: np.mean(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'median':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: np.median(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'min':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: np.min(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'max':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: np.max(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'md':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: moderate_deviations.md_aggregation(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'sugeno':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: integrals.sugeno_fuzzy_integral(a, integrals.generate_cardinality_matrix(a.shape[axis], a.shape), axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'shamacher':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: integrals.sugeno_fuzzy_integral_generalized(a, integrals.generate_cardinality_matrix(a.shape[axis], a.shape), axis=axis, keepdims=keepdims, f1 = tnorms.hamacher_tnorm, f2 = np.amax)
+    elif agg_minuscula == 'fhamacher':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: integrals.sugeno_fuzzy_integral_generalized(a, integrals.generate_cardinality_matrix(a.shape[axis], a.shape), axis=axis, keepdims=keepdims, f1 = tnorms.prod, f2 = np.sum)
+    elif agg_minuscula == 'choquet':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: integrals.choquet_integral_symmetric(a, integrals.generate_cardinality(a.shape[axis]), axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'cfminmin':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: integrals.choquet_integral_symmetric_cf12(a, integrals.generate_cardinality(a.shape[axis]), axis=axis, keepdims=keepdims, f1=np.minimum, f2=np.minimum)
+    elif agg_minuscula == 'cf12':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: integrals.choquet_integral_symmetric_cf12(a, integrals.generate_cardinality(a.shape[axis]), axis=axis, keepdims=keepdims, f1=lambda a,b, axis=axis, keepdims=keepdims: np.sqrt(a* b), f2=tnorms.lukasiewicz_tnorm)
+    elif agg_minuscula == 'owa1':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: owas.OWA1(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'owa2':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: owas.OWA2(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'owa3':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: owas.OWA3(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'cf':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: integrals.choquet_integral_CF(a, integrals.generate_cardinality(a.shape[axis]), axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'geomean':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: overlaps.geo_mean(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'sinoverlap':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: overlaps.sin_overlap(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'hmean':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: overlaps.harmonic_mean(a, axis=axis, keepdims=keepdims)
+    elif agg_minuscula == 'lucrezia':
+        return lambda a, axis=axis_f, keepdims=keepdims_f: networks.lucrezia_simple_decisor(a, axis=axis, keepdims=keepdims, tnorm=np.minimum, agg_function=np.mean)
+    elif agg_minuscula == 'mode':
+        from scipy.stats import mode
+        if keepdims_f:
+            print('Warning: mode does not keep dimension')
+        return lambda a, axis=axis_f, keepdims=keepdims_f: mode(a, axis=axis)
+    else:
+        raise KeyError(agg_name)
---
+++ packages/Fancy-aggregations/Fancy_aggregations-1.3.2.tar.gz/Fancy_aggregations-1.3.2/Fancy_aggregations/penalties.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+File containing different penalty functions to aggregate data.
+
+Bustince, H., Beliakov, G., Dimuro, G. P., Bedregal, B., & Mesiar, R. (2017). 
+On the definition of penalty functions in data aggregation. Fuzzy Sets and Systems, 323, 1-18.
+
+@author: Javier Fumanal Idocin (UPNA).
+
+To suggest changes or submit new code please use the github page.
+"""
+
+import numpy as np
+
+# =============================================================================
+# ~ PENALTY
+# =============================================================================
+def penalty_aggregation(X, agg_functions, axis=0, keepdims=True):
+    '''
+
+    :param X:
+    :param agg_functions:
+    :return:
+    '''
+    agg_matrix = []
+    agg_distances_shape =  [len(agg_functions)] + list(agg_functions[0](X, axis=axis, keepdims=False).shape)
+    agg_distances = np.zeros(agg_distances_shape)
+
+
+    for ix, ag_f in enumerate(agg_functions):
+        aux = ag_f(X, axis=axis, keepdims=True)
+        distances = np.sum(np.abs(X - aux), axis=axis, keepdims=False)
+        aux = ag_f(X, axis=axis, keepdims=False)
+
+        agg_matrix.append(aux)
+        agg_distances[ix] = distances
+
+    agg_choose = np.argmin(agg_distances, axis=0)
+    res = np.zeros(agg_choose.shape)
+    for index, x in np.ndenumerate(agg_choose):
+        res[index] = agg_matrix[x][index]
+
+    if keepdims:
+        res =  res= np.expand_dims(res, axis=axis)
+
+    return res
