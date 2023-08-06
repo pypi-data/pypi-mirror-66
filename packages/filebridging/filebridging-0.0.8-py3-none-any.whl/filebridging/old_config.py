13a439c452cfff93baec9e91ae663881d509d8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package filebridging/0.0.8/filebridging-0.0.8-py3-none-any.whl

---
+++ packages/filebridging/filebridging-0.0.8-py3-none-any.whl/filebridging/old_config.py
@@ -0,0 +1,9 @@
+host = 'davte.it'
+host = 'localhost'
+port = 5000
+
+certificate = '/home/davte/Projects/filebridging/data/filesharing.crt'
+key = '/home/davte/Projects/filebridging/data/filesharing.key'
+
+password = 'pepperoni'
+token = 'aaa111ddd'
