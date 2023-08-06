784b9e977ca8743c275f315a483edb6e8226eec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:02 +0000

    Add package wrestler/0.0.0/wrestler-0.0.0.tar.gz

---
+++ packages/wrestler/wrestler-0.0.0.tar.gz/wrestler-0.0.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="wrestler",
+    version="0.0.0",
+    author="Michael Chow",
+    author_email='mc_al_github@fastmail.com',
+    url="https://pypi.python.org/pypi/wrestlr",
+    download_url="https://pypi.python.org/pypi/wrestlr",
+    description="Why debate R vs python, when you could convert R to python?",
+    long_description="""Use `wrestlr <https://pypi.python.org/pypi/wrestlr>`_ instead.""",
+    install_requires=['wrestlr'],
+)
+
