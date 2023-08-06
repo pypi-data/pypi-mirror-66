8a26ac8cad6c24af1cd8a65e3aa07aae6004a83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package pyobo/0.1.3/pyobo-0.1.3-py3-none-any.whl

---
+++ packages/pyobo/pyobo-0.1.3-py3-none-any.whl/pyobo/xrefdb/sources/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""Sources of xrefs not from OBO."""
+
+from typing import Iterable
+
+import pandas as pd
+
+from .cbms2019 import get_cbms2019_xrefs_df
+from .famplex import get_famplex_xrefs_df
+from .gilda import get_gilda_xrefs_df
+from .intact import get_intact_complex_portal_xrefs_df, get_intact_reactome_xrefs_df
+
+__all__ = [
+    'get_famplex_xrefs_df',
+    'get_gilda_xrefs_df',
+    'get_cbms2019_xrefs_df',
+    'get_intact_complex_portal_xrefs_df',
+    'get_intact_reactome_xrefs_df',
+    'iter_sourced_xref_dfs',
+]
+
+
+def iter_sourced_xref_dfs() -> Iterable[pd.DataFrame]:
+    """Iterate all sourced xref dataframes."""
+    yield get_gilda_xrefs_df()
+    yield get_cbms2019_xrefs_df()
+    yield get_famplex_xrefs_df()
+    yield get_intact_complex_portal_xrefs_df()
+    yield get_intact_reactome_xrefs_df()
