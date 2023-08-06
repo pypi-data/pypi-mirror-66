63560c3c364423592d6406882f9c68eae9cce601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:03 +0000

    Add package nester-dolee/0.0.3/nester_dolee-0.0.3.tar.gz

---
+++ packages/nester-dolee/nester_dolee-0.0.3.tar.gz/nester_dolee-0.0.3/nester_dolee/nester_dolee.py
@@ -0,0 +1,18 @@
+""" file(module) comment """
+
+def print_lol(the_list, level=0):
+	""" function comment """
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t", end="")
+			print(each_item)
+
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, 
+			["Graham Chapman",
+				["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+names = ['John', 'Eric', ['Cleese', 'Idle'], 'Michael', ['Palin']]
