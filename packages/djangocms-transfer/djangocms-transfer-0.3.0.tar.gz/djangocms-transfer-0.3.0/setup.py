ad74581be4745a85cdf357062d8b8920af05c9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:49 +0000

    Add package djangocms-transfer/0.3.0/djangocms-transfer-0.3.0.tar.gz

---
+++ packages/djangocms-transfer/djangocms-transfer-0.3.0.tar.gz/djangocms-transfer-0.3.0/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+from djangocms_transfer import __version__
+
+
+REQUIREMENTS = [
+    'django-cms>=3.4.5',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Framework :: Django',
+    'Framework :: Django :: 1.11',
+    'Framework :: Django :: 2.1',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django CMS',
+    'Framework :: Django CMS :: 3.4',
+    'Framework :: Django CMS :: 3.5',
+    'Framework :: Django CMS :: 3.6',
+    'Framework :: Django CMS :: 3.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+
+setup(
+    name='djangocms-transfer',
+    version=__version__,
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='http://github.com/divio/djangocms-transfer',
+    license='BSD',
+    description='Adds import and export of plugin data.',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
