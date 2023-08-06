4a6124d62cc69b79cbd6091eeb5b0953ea710f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:49 +0000

    Add package lyrics-in-terminal/1.2.0/lyrics-in-terminal-1.2.0.tar.gz

---
+++ packages/lyrics-in-terminal/lyrics-in-terminal-1.2.0.tar.gz/lyrics-in-terminal-1.2.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+from lyrics import __version__
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+	name='lyrics-in-terminal',
+	version=__version__,
+	description='Command Line Lyrics fetcher from mpris media player like Spotify, VLC, Audacious',
+	author='Samarth Jugran',
+	author_email='jugransamarth@gmail.com',
+	license='MIT',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/Jugran/lyrics-in-terminal',
+	packages=find_packages(),
+	entry_points={
+		'console_scripts': [
+			'lyrics = lyrics.lyrics_in_terminal:start'
+		]
+	},
+	classifiers=[ 
+		"Programming Language :: Python :: 3",
+	    "License :: OSI Approved :: MIT License",
+	    "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+    	'dbus-python'
+    ],
+    python_requires='>=3.6',
+	include_package_data=True
+)
