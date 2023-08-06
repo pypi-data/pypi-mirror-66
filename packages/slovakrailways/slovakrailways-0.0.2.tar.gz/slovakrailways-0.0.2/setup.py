533b014f66218062882a9b7c6f53070d3d6dcde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:23 +0000

    Add package slovakrailways/0.0.2/slovakrailways-0.0.2.tar.gz

---
+++ packages/slovakrailways/slovakrailways-0.0.2.tar.gz/slovakrailways-0.0.2/setup.py
@@ -0,0 +1,26 @@
+
+from distutils.core import setup
+setup(
+  name = 'slovakrailways',
+  packages = ['.'],
+  version = '0.0.2',
+  license='MIT',
+  description = 'Python envelope of Slovak Railways API',
+  author = 'Martin Beneš',
+  author_email = 'martinbenes1996@gmail.com',
+  url = 'https://github.com/martinbenes1996/slovakrailways',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/martinbenes1996/slovakrailways/archive/v0.0.2.tar.gz',    # I explain this later on
+  keywords = ['API', 'Railway', 'Train', 'Slovakia', 'Data', 'REST API'],
+  install_requires=[],
+  package_dir={'': '.'},
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
