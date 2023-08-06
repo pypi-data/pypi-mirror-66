c8673c3818d65885f11e81f8ae255c5d01814b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:12 +0000

    Add package scrapli/2020.4.19/scrapli-2020.4.19-py3-none-any.whl

---
+++ packages/scrapli/scrapli-2020.4.19-py3-none-any.whl/scrapli/driver/core/cisco_iosxe/driver.py
@@ -0,0 +1,166 @@
+"""scrapli.driver.core.cisco_iosxe.driver"""
+from typing import Any, Callable, Dict, Optional
+
+from scrapli.driver import NetworkDriver
+from scrapli.driver.network_driver import PrivilegeLevel
+
+
+def iosxe_on_open(conn: NetworkDriver) -> None:
+    """
+    IOSXEDriver default on_open callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        N/A  # noqa: DAR202
+
+    Raises:
+        N/A
+    """
+    conn.acquire_priv(desired_priv=conn.default_desired_priv)
+    conn.channel.send_input(channel_input="terminal length 0")
+    conn.channel.send_input(channel_input="terminal width 512")
+
+
+def iosxe_on_close(conn: NetworkDriver) -> None:
+    """
+    IOSXEDriver default on_close callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        N/A  # noqa: DAR202
+
+    Raises:
+        N/A
+    """
+    # write exit directly to the transport as channel would fail to find the prompt after sending
+    # the exit command!
+    conn.transport.write(channel_input="exit")
+    conn.transport.write(channel_input=conn.channel.comms_prompt_pattern)
+
+
+PRIVS = {
+    "exec": (
+        PrivilegeLevel(
+            r"^[a-z0-9.\-@()/:]{1,32}>$",
+            "exec",
+            "",
+            "",
+            "privilege_exec",
+            "enable",
+            True,
+            "Password:",
+            True,
+            0,
+        )
+    ),
+    "privilege_exec": (
+        PrivilegeLevel(
+            r"^[a-z0-9.\-@/:]{1,32}#$",
+            "privilege_exec",
+            "exec",
+            "disable",
+            "configuration",
+            "configure terminal",
+            False,
+            "",
+            True,
+            1,
+        )
+    ),
+    "configuration": (
+        PrivilegeLevel(
+            r"^[a-z0-9.\-@/:]{1,32}\(config[a-z0-9.\-@/:]{0,16}\)#$",
+            "configuration",
+            "privilege_exec",
+            "end",
+            "",
+            "",
+            False,
+            "",
+            True,
+            2,
+        )
+    ),
+    "special_configuration": (
+        PrivilegeLevel(
+            r"^[a-z0-9.\-@/:]{1,32}\(config[a-z0-9.\-@/:]{1,16}\)#$",
+            "special_configuration",
+            "privilege_exec",
+            "end",
+            "",
+            "",
+            False,
+            "",
+            False,
+            3,
+        )
+    ),
+}
+
+
+class IOSXEDriver(NetworkDriver):
+    def __init__(
+        self,
+        comms_prompt_pattern: str = r"^[a-z0-9.\-@()/:]{1,48}[#>$]$",
+        on_open: Optional[Callable[..., Any]] = None,
+        on_close: Optional[Callable[..., Any]] = None,
+        auth_secondary: str = "",
+        **kwargs: Dict[str, Any],
+    ):
+        """
+        IOSXEDriver Object
+
+        Args:
+            comms_prompt_pattern: raw string regex pattern -- preferably use `^` and `$` anchors!
+                this is the single most important attribute here! if this does not match a prompt,
+                scrapli will not work!
+                IMPORTANT: regex search uses multi-line + case insensitive flags. multi-line allows
+                for highly reliably matching for prompts however we do NOT strip trailing whitespace
+                for each line, so be sure to add '\\s*' if your device needs that. This should be
+                mostly sorted for you if using network drivers (i.e. `IOSXEDriver`). Lastly, the
+                case insensitive is just a convenience factor so i can be lazy.
+            on_open: callable that accepts the class instance as its only argument. this callable,
+                if provided, is executed immediately after authentication is completed. Common use
+                cases for this callable would be to disable paging or accept any kind of banner
+                message that prompts a user upon connection
+            on_close: callable that accepts the class instance as its only argument. this callable,
+                if provided, is executed immediately prior to closing the underlying transport.
+                Common use cases for this callable would be to save configurations prior to exiting,
+                or to logout properly to free up vtys or similar.
+            auth_secondary: password to use for secondary authentication (enable)
+            **kwargs: keyword args to pass to inherited class(es)
+
+        Returns:
+            N/A  # noqa: DAR202
+
+        Raises:
+            N/A
+        """
+        if on_open is None:
+            on_open = iosxe_on_open
+        if on_close is None:
+            on_close = iosxe_on_close
+
+        super().__init__(
+            auth_secondary=auth_secondary,
+            comms_prompt_pattern=comms_prompt_pattern,
+            on_open=on_open,
+            on_close=on_close,
+            **kwargs,
+        )
+
+        self.privs = PRIVS
+        self.default_desired_priv = "privilege_exec"
+
+        self.textfsm_platform = "cisco_ios"
+        self.genie_platform = "iosxe"
+
+        self.failed_when_contains = [
+            "% Ambiguous command",
+            "% Incomplete command",
+            "% Invalid input detected",
+        ]
---
+++ packages/scrapli/scrapli-2020.4.19-py3-none-any.whl/scrapli/driver/core/juniper_junos/driver.py
@@ -0,0 +1,159 @@
+"""scrapli.driver.core.juniper_junos.driver"""
+from typing import Any, Callable, Dict, Optional
+
+from scrapli.driver import NetworkDriver
+from scrapli.driver.network_driver import PrivilegeLevel
+
+
+def junos_on_open(conn: NetworkDriver) -> None:
+    """
+    JunosDriver default on_open callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        N/A  # noqa: DAR202
+
+    Raises:
+        N/A
+    """
+    conn.acquire_priv(desired_priv=conn.default_desired_priv)
+    conn.channel.send_input(channel_input="set cli complete-on-space off")
+    conn.channel.send_input(channel_input="set cli screen-length 0")
+    conn.channel.send_input(channel_input="set cli screen-width 511")
+
+
+def junos_on_close(conn: NetworkDriver) -> None:
+    """
+    JunosDriver default on_close callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        N/A  # noqa: DAR202
+
+    Raises:
+        N/A
+    """
+    # write exit directly to the transport as channel would fail to find the prompt after sending
+    # the exit command!
+    conn.transport.write(channel_input="exit")
+    conn.transport.write(channel_input=conn.channel.comms_prompt_pattern)
+
+
+PRIVS = {
+    "exec": (
+        PrivilegeLevel(
+            r"^[a-z0-9.\-@()/:]{1,32}>\s?$",
+            "exec",
+            "",
+            "",
+            "configuration",
+            "configure",
+            False,
+            "",
+            True,
+            0,
+        )
+    ),
+    "configuration": (
+        PrivilegeLevel(
+            r"^[a-z0-9.\-@()/:]{1,32}#\s?$",
+            "configuration",
+            "exec",
+            "exit configuration-mode",
+            "",
+            "",
+            False,
+            "",
+            True,
+            1,
+        )
+    ),
+}
+
+
+class JunosDriver(NetworkDriver):
+    def __init__(
+        self,
+        comms_prompt_pattern: str = r"^[a-z0-9.\-@()/:]{1,48}[#>$]\s?$",
+        on_open: Optional[Callable[..., Any]] = None,
+        on_close: Optional[Callable[..., Any]] = None,
+        auth_secondary: str = "",
+        transport: str = "system",
+        **kwargs: Dict[str, Any],
+    ):
+        """
+        JunosDriver Object
+
+        Args:
+            comms_prompt_pattern: raw string regex pattern -- preferably use `^` and `$` anchors!
+                this is the single most important attribute here! if this does not match a prompt,
+                scrapli will not work!
+                IMPORTANT: regex search uses multi-line + case insensitive flags. multi-line allows
+                for highly reliably matching for prompts however we do NOT strip trailing whitespace
+                for each line, so be sure to add '\\s*' if your device needs that. This should be
+                mostly sorted for you if using network drivers (i.e. `IOSXEDriver`). Lastly, the
+                case insensitive is just a convenience factor so i can be lazy.
+            on_open: callable that accepts the class instance as its only argument. this callable,
+                if provided, is executed immediately after authentication is completed. Common use
+                cases for this callable would be to disable paging or accept any kind of banner
+                message that prompts a user upon connection
+            on_close: callable that accepts the class instance as its only argument. this callable,
+                if provided, is executed immediately prior to closing the underlying transport.
+                Common use cases for this callable would be to save configurations prior to exiting,
+                or to logout properly to free up vtys or similar.
+            auth_secondary: password to use for secondary authentication (enable)
+            transport: system|telnet or a plugin -- type of transport to use for connection
+                system uses system available ssh (/usr/bin/ssh)
+                ssh2 uses ssh2-python *has been migrated to a plugin
+                paramiko uses... paramiko *has been migrated to a plugin
+                telnet uses telnetlib
+                choice of driver depends on the features you need. in general system is easiest as
+                it will just 'auto-magically' use your ssh config file ('~/.ssh/config' or
+                '/etc/ssh/config_file'). ssh2 is very very fast as it is a thin wrapper around
+                libssh2 however it is slightly feature limited. paramiko is slower than ssh2, but
+                has more features built in (though scrapli does not expose/support them all).
+                explicitly added here to allow for nicely checking if transport is telnet.
+            **kwargs: keyword args to pass to inherited class(es)
+
+        Returns:
+            N/A  # noqa: DAR202
+
+        Raises:
+            N/A
+        """
+        if on_open is None:
+            on_open = junos_on_open
+        if on_close is None:
+            on_close = junos_on_close
+
+        _telnet = False
+        if transport == "telnet":
+            _telnet = True
+
+        super().__init__(
+            auth_secondary=auth_secondary,
+            comms_prompt_pattern=comms_prompt_pattern,
+            on_open=on_open,
+            on_close=on_close,
+            transport=transport,
+            **kwargs,
+        )
+
+        if _telnet:
+            self.transport.username_prompt = "login:"
+
+        self.privs = PRIVS
+        self.default_desired_priv = "exec"
+
+        self.default_desired_priv = "exec"
+        self.textfsm_platform = "juniper_junos"
+
+        self.failed_when_contains = [
+            "is ambiguous.",
+            "No valid completions",
+            "unknown command.",
+        ]
