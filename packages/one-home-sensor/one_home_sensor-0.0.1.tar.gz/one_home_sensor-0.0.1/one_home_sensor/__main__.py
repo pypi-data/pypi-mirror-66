e88e46955d04312895426479b2ce93c81ff96f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:28 +0000

    Add package one-home-sensor/0.0.1/one_home_sensor-0.0.1.tar.gz

---
+++ packages/one-home-sensor/one_home_sensor-0.0.1.tar.gz/one_home_sensor-0.0.1/one_home_sensor/__main__.py
@@ -0,0 +1,64 @@
+import onehomesensor
+import argparse
+import configparser
+import os
+from os import path
+import platform
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="""
+    Gets temperature, humidity and pressure from BME280 sensor on a Raspberry Pi. 
+    Can push informations to a  MongpDB Atlas Storage. 
+    """)
+    
+    # Read running arguments 
+    parser.add_argument(
+        '--print-only',
+        default=False,
+        action="store_true",
+        help='Will only print the values and will not push to MongpDB Atlas Storage'
+    )
+    working_dir = os.getcwd()
+    conf_file_default = os.path.join(working_dir, "onehomesensor.ini")
+    parser.add_argument(
+        '-c', '--config-file',
+        default=conf_file_default,
+        help='Provide custom path to configuration file'
+    )
+    # TODO : use os logging possibilitis / syslog ... 
+    log_file_default = os.path.join("var","log" ,"onehomesensor.log")
+    parser.add_argument(
+        '-l', '--log-file',
+        default=log_file_default,
+        help='Provide custom path to logging file'
+    )    
+    parser.add_argument(
+        '-s', '--sensor-name',
+        default=platform.node(),
+        help='Name of this sensor (default : hostname)'
+    )   
+    running_args = parser.parse_args()
+    
+
+    # Read configuration file if existe 
+    mongodb_con = ""
+    Config = configparser.ConfigParser()
+    config_file = running_args.config_file
+    if path.exists(config_file):
+        Config.read(config_file)
+        # get mandatory options 
+        try:
+            if not running_args.print_only:
+                mdb_name = Config.get('MongoDBAtlasConnection', 'username')
+                mdb_pswd = Config.get('MongoDBAtlasConnection', 'password')
+                mdb_clus = Config.get('MongoDBAtlasConnection', 'clusterfqdn')
+                mongodb_con = "mongodb+srv://{}:{}@{}/test?retryWrites=true&w=majority".format(mdb_name,mdb_pswd,mdb_clus)
+                print(mongodb_con)
+        except Exception as e:
+            raise Exception("Configuration file invalide : {}".format(e)) 
+
+
+    # Run the app     
+    MyApp = onehomesensor.OneHomeSensor(running_args,mongodb_con)
+    MyApp.run()
