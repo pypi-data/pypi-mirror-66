a2ec0ed192c4da8b4e77949bbdb42a1aa832d94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:34 +0000

    Add package lidazhisheng/1.3.4/lidazhisheng-1.3.4.tar.gz

---
+++ packages/lidazhisheng/lidazhisheng-1.3.4.tar.gz/lidazhisheng-1.3.4/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/22 下午4:58
+# @Author  : zyg
+# @File    : setup.py.py
+# @Desc
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lidazhisheng',
+    version='1.3.4',
+    author='zyg',
+    author_email='zyg8929@163.com',
+    url='https://zhuanlan.zhihu.com',
+    description='',
+    packages=find_packages(),
+    install_requires=[],
+
+)
