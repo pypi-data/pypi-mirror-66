4ddcaf77f73d2ba196ab8ea56405a7a76d3100e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:15 +0000

    Add package dist1-probability/1.1/dist1_probability-1.1.tar.gz

---
+++ packages/dist1-probability/dist1_probability-1.1.tar.gz/dist1_probability-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist1_probability',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['dist1_probability'],
+      zip_safe=False)
