fda3d188b63fc06428f5886e47e23772ec087347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:19 +0000

    Add package ansilog/1.5/ansilog-1.5.tar.gz

---
+++ packages/ansilog/ansilog-1.5.tar.gz/ansilog-1.5/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ansilog',
+    version='1.5',
+    description="Smart and colorful solution for logging, "
+                "output, and basic terminal control.",
+    long_description=long_description,
+
+    url='https://github.com/lainproliant/ansilog',
+
+    author='Lain Supe (lainproliant)',
+    author_email='lainproliant@gmail.com',
+
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    keywords='ANSI CLI terminal color colour',
+
+    packages=['ansilog'],
+    install_requires=[],
+    extras_require={},
+    package_data={'ansilog': ['*.pyi', 'py.typed', "LICENSE"]},
+    entry_points={'console_scripts': []},
+)
