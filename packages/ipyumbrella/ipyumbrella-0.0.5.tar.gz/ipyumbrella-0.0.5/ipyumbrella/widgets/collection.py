57b0f63369fdd19eee3237349361692d7b0692e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:19 +0000

    Add package ipyumbrella/0.0.5/ipyumbrella-0.0.5.tar.gz

---
+++ packages/ipyumbrella/ipyumbrella-0.0.5.tar.gz/ipyumbrella-0.0.5/ipyumbrella/widgets/collection.py
@@ -0,0 +1,44 @@
+from contextlib import contextmanager
+import ipywidgets.widgets as w
+from .output import Output
+
+
+class _CollectionMixin:
+    output_layout = None
+    def item(self, layout=None, err_stop=True, **kw):
+        return self.append(Output(err_stop=err_stop, layout=layout or self.output_layout, **kw))
+
+    def append(self, child):
+        self.children += (child,)
+        return child
+
+    def __len__(self):
+        return len(self.children)
+
+class _SelectableCollectionMixin(_CollectionMixin):
+    def item(self, title=None, selected=True, err_stop=True, **kw):
+        return self.append(Output(err_stop=err_stop, **kw), title, selected=selected)
+
+    def select(self, i):
+        self.selected_index = i
+
+    def append(self, child, title=None, selected=True):
+        self.children += (child,)
+        title and self.set_title(len(self) - 1, title)
+        self.select(selected and len(self) - 1)
+        return child
+
+
+class Carousel(w.Box, _CollectionMixin):
+    layout = w.Layout(
+        flex_flow='row nowrap',
+        overflow_x='auto',
+        max_width='100%',
+    )
+    output_layout = w.Layout()
+
+class Accordion(w.Accordion, _SelectableCollectionMixin):
+    pass
+
+class Tab(w.Tab, _SelectableCollectionMixin):
+    pass
