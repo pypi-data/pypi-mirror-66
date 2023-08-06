7fb38ec1b9131df4502a93c4813f6c2696fc6269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:34 +0000

    Add package twitter-spider/0.2/twitter-spider-0.2.tar.gz

---
+++ packages/twitter-spider/twitter-spider-0.2.tar.gz/twitter-spider-0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="twitter-spider",
+    version="0.2",
+    author="Dainius Preimantas",
+    author_email="preimantasd@gmail.com",
+    description="A twitter tweets spider",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dainius-P/twitter_spider",
+    packages=setuptools.find_packages(exclude=["venv"]),
+    install_requires=[
+        'requests==2.23.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
