68af03ed81ff67ee5c083ee86e935c234aef58d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:18 +0000

    Add package zettlekasten/0.0.2/zettlekasten-0.0.2.tar.gz

---
+++ packages/zettlekasten/zettlekasten-0.0.2.tar.gz/zettlekasten-0.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="zettlekasten",
+    version="0.0.2",
+    author="Amirouche BOUBEKKI",
+    author_email="amirouche@hyper.dev",
+    url="https://github.com/amirouche/zettlekasten",
+    description="Zettlekasten at its finest!",
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',  # This is important!
+    py_modules=['zettlekasten'],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
