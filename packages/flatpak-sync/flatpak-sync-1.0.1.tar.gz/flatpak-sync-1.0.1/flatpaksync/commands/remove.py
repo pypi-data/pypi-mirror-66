fd1ac9c968addc89746f05a27ed8f90fc5fdf2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:18 +0000

    Add package flatpak-sync/1.0.1/flatpak-sync-1.0.1.tar.gz

---
+++ packages/flatpak-sync/flatpak-sync-1.0.1.tar.gz/flatpak-sync-1.0.1/flatpaksync/commands/remove.py
@@ -0,0 +1,46 @@
+import logging
+
+from flatpaksync.commands.command import command
+from flatpaksync.configs.write import write as writeconfig
+from flatpaksync.configs.read import read as readconfig
+
+from flatpaksync.structs.app import app
+
+
+class remove(command):
+
+
+    def __init__(self):
+        super().__init__()
+
+
+    def execute(self, repo, appid):
+        
+        config=readconfig(self.conf)
+        if config.read():
+
+            settings=config.getSettings()
+            repolist=config.getRepoList()
+            applist=config.getAppList()
+
+                # Remove an App from the list
+            removeApp = app(appid, repo)
+            if applist.exists(removeApp):
+                applist.remove(removeApp)
+
+                    # Write configuration
+                wconfig = writeconfig(self.conf)
+                wconfig.setSettings(settings)
+                wconfig.setRepoList(repolist)
+                wconfig.setAppList(applist)
+
+                if wconfig.write():
+                    logging.info('Successfully wrote configuration')
+                else:
+                    logging.error('Failed to write configuration')
+            else:
+                logging.warn("Unable to find application/repo {}".format(appid))
+
+        else:
+            logging.error('Failed to read configuration')
+
