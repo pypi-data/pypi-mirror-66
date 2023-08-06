621a9c48ea705e2693ae51d68e3799eab98ad06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:40 +0000

    Add package Rostering/0.1.1/Rostering-0.1.1.tar.gz

---
+++ packages/Rostering/Rostering-0.1.1.tar.gz/Rostering-0.1.1/Rostering/eample.py
@@ -0,0 +1,13 @@
+import random
+n = 10
+m = 5
+set_I = range(1, n+1)
+set_J = range(1, m+1)
+c = {(i,j): random.normalvariate(0,1) for i in set_I for j in set_J}
+a = {(i,j): random.normalvariate(0,5) for i in set_I for j in set_J}
+l = {(i,j): random.randint(0,10) for i in set_I for j in set_J}
+u = {(i,j): random.randint(10,20) for i in set_I for j in set_J}
+b = {j: random.randint(0,30) for j in set_J}
+
+import pulp as plp
+opt_model = plp.LpProblem(name="MIP Model")
