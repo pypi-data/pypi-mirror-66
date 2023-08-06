4fad2b8f6e1aaef4cfcb51d548a7efd85a5e4aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:08 +0000

    Add package emailme/0.3.0/emailme-0.3.0.tar.gz

---
+++ packages/emailme/emailme-0.3.0.tar.gz/emailme-0.3.0/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from pip._internal.req import parse_requirements
+from setuptools import setup, find_packages
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name="emailme",
+      version="0.3.0",
+      author="Eric J. Ma",
+      author_email="ericmajinglong@gmail.com",
+      description=("Super simple self-emailing."),
+      license="MIT",
+      packages=find_packages(),
+      keywords="email",
+      url="http://github.com/ericmjl/emailme",
+      package_data={'': ['README.md']},
+      install_requires=reqs,
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      entry_points={'console_scripts': ['emailme=emailme.cli:cli']}
+      )
