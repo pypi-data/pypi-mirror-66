07992ebb4701fe28a0fd94044171455902d43d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:29 +0000

    Add package terminal-pkg-Team3SP20/0.0.5/terminal-pkg-Team3SP20-0.0.5.tar.gz

---
+++ packages/terminal-pkg-Team3SP20/terminal-pkg-Team3SP20-0.0.5.tar.gz/terminal-pkg-Team3SP20-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="terminal-pkg-Team3SP20",
+    version="0.0.5",
+    author="Team3SP20",
+    author_email="dansharp94@mail.mvnu.edu",
+    description="Files for cafeteria terminal prototype",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MVNUCS/TeamThreeSP20",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6', 
+)
