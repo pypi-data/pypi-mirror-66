1353670d33d153378d40d89d35eaa1732d40d49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:02 +0000

    Add package mytestlib-tst/1.0.0/mytestlib-tst-1.0.0.tar.gz

---
+++ packages/mytestlib-tst/mytestlib-tst-1.0.0.tar.gz/mytestlib-tst-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-tst',
+    version='1.0.0',
+    author='ikegami',
+    author_email='takanori.ikegami@gmail.com',
+    description='My Test Library.',
+    packages=find_packages()
+)
