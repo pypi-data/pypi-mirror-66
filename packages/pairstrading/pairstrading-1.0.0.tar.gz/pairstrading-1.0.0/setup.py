21eea048362db81a88978aa16f1a5654fd9ed80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:07 +0000

    Add package pairstrading/1.0.0/pairstrading-1.0.0.tar.gz

---
+++ packages/pairstrading/pairstrading-1.0.0.tar.gz/pairstrading-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name         ='pairstrading',
+	version      ='1.0.0',
+   	 py_modules   =['pairstradingtest'],
+ 	 author       ='晴天看夕阳',
+    	author_email ='673999851@qq.com',
+    	description  ='配对交易',
+	install_requires = ['numpy', 'pandas', 'statsmodels'],
+
+    )
