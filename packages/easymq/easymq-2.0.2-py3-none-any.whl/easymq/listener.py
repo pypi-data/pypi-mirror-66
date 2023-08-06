970f72999e24b90628bcc73c070336d0dc8125c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:31 +0000

    Add package easymq/2.0.2/easymq-2.0.2-py3-none-any.whl

---
+++ packages/easymq/easymq-2.0.2-py3-none-any.whl/easymq/listener.py
@@ -0,0 +1,39 @@
+import logging
+import threading
+import stomp
+
+event = threading.Event()
+
+
+class MQListener(stomp.ConnectionListener):
+
+    def on_message(self, headers, body):
+        pass
+
+    def on_connected(self, headers, body):
+        logging.info(f"on connected: {headers}, {body}")
+
+    def on_error(self, headers, body):
+        logging.info(f"on error: {headers}, {body}")
+
+    def on_disconnected(self):
+        logging.info('on disconnected')
+
+
+class ReconnectListener(MQListener):
+
+    def on_message(self, headers, body):
+        pass
+
+    def on_connected(self, headers, body):
+        pass
+
+    def on_error(self, headers, body):
+        pass
+
+    def on_disconnected(self):
+        pass
+
+
+def wait_forever():
+    event.wait()
