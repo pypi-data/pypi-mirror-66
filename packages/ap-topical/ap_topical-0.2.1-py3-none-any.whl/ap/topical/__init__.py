3da192da71d4ff6ae5dfde8c94e599c9a45a996b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:37 +0000

    Add package ap-topical/0.2.1/ap_topical-0.2.1-py3-none-any.whl

---
+++ packages/ap-topical/ap_topical-0.2.1-py3-none-any.whl/ap/topical/__init__.py
@@ -0,0 +1,6 @@
+from .topical_event_payload import TopicalEventPayload, TopicalEventPayloadEncoder
+from .topical_manager import TopicalManager
+from .topical_cache import TopicalCache
+
+topical = TopicalManager()
+cache = TopicalCache()
