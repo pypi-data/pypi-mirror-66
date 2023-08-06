dd23c8d85dc003c736655a7f2862c636f768e493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:39 +0000

    Add package tenorpy/1.0.2/tenorpy-1.0.2.tar.gz

---
+++ packages/tenorpy/tenorpy-1.0.2.tar.gz/tenorpy-1.0.2/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = '1.0.2'
+download_url = 'https://github.com/realSnosh/tenorpy/archive/1.0.1.tar.gz'
+
+try:
+    setup(
+        name='tenorpy',
+        version=version,
+        author='alpha_Snosh',
+        description='API Wrapper for the Gif library Tenor. Join my server: https://discord.gg/uFdVUMH',
+        long_description=long_description,
+        license='MIT',
+        keywords='tenor, python, python3, api, wrapper, alphasnosh, snosh',
+        url='https://github.com/realSnosh/tenorpy',
+        download_url=download_url,
+        packages=['tenorpy'],
+        install_requires=['requests'],
+        classifiers=[
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Build Tools",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8"
+
+        ]
+    )
+except:
+    setup(
+        name='tenorpy',
+        version=version,
+        author='alpha_Snosh',
+        description='API Wrapper for the Gif library Tenor. Join my server: https://discord.gg/uFdVUMH',
+        license='MIT',
+        keywords='tenor, python, python3, api, wrapper, alphasnosh, snosh',
+        url='https://github.com/realSnosh/tenorpy',
+        download_url=download_url,
+        packages=['tenorpy'],
+        install_requires=['requests'],
+        classifiers=[
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Build Tools",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8"
+
+        ]
+    )
---
+++ packages/tenorpy/tenorpy-1.0.2.tar.gz/tenorpy-1.0.2/tenorpy/__init__.py
@@ -0,0 +1,1 @@
+from tenorpy import tenorpy
