d0d7862c164b4a2b8bd1f5c9338c28fd83c3df0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:45 +0000

    Add package soroushpdf2/1.0/soroushpdf2-1.0-py3-none-any.whl

---
+++ packages/soroushpdf2/soroushpdf2-1.0-py3-none-any.whl/soroushpdf/pdf2text.py
@@ -0,0 +1,18 @@
+""" This module converts PDFs to Text. """
+
+
+class Convertor:
+    """ Description for Convertor class"""
+
+    def convert(self, path):
+        """
+        Simple Function for converting PDFs to text.
+
+        Parameters:
+        path (str): a path to the pdf file
+
+        Returns:
+        str: text equivalent of the pdf file
+        """
+
+        print("PDF2TEXT")
