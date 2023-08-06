9f312ef8041d486941366c23ff63058a246c8293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:34 +0000

    Add package epyk-flask/1.0.4/epyk_flask-1.0.4-py2.py3-none-any.whl

---
+++ packages/epyk-flask/epyk_flask-1.0.4-py2.py3-none-any.whl/epyk_flask/cli/project_structure.py
@@ -0,0 +1,10 @@
+folder_struct = {'templates': [],
+                 'config': ['config.yaml'],
+                 'server_app': ['__init__.py', 'app.py'],
+                 'endpoints': ['__init__.py', 'basic_endpoints.py', 'websocket_endpoints.py'],
+                 'UI': [{'scripts': ['__init__.py', {'root': ['__init__.py', 'index.py']}],
+                         'html': []}
+                        ]
+                 }
+
+env_struct = None
