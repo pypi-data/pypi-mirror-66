10d77b2342aa3a9209ac670741dcf531ba27b3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:27 +0000

    Add package jk-utils/0.2020.4.20/jk_utils-0.2020.4.20.tar.gz

---
+++ packages/jk-utils/jk_utils-0.2020.4.20.tar.gz/jk_utils-0.2020.4.20/jk_utils/async/__init__.py
@@ -0,0 +1,10 @@
+﻿
+
+
+__version__ = "0.2020.4.20"
+
+
+
+from .queues import *
+from .TabularWriterMediaWiki import *
+from .TabularWriterSTDOUT import *
---
+++ packages/jk-utils/jk_utils-0.2020.4.20.tar.gz/jk_utils-0.2020.4.20/jk_utils/datetime/__init__.py
@@ -0,0 +1,12 @@
+﻿
+
+
+__version__ = "0.2020.4.20"
+
+
+from .D import D
+from .T1 import T1
+from .T5 import T5
+
+from .dateiterator import dateRange, dateRangeI
+from .timeiterator5 import timeRange5
