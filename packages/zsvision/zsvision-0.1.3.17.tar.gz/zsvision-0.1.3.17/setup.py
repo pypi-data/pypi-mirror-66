5596ba9fca8c529133c3020235b7b434b33ad56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:18 +0000

    Add package zsvision/0.1.3.17/zsvision-0.1.3.17.tar.gz

---
+++ packages/zsvision/zsvision-0.1.3.17.tar.gz/zsvision-0.1.3.17/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="zsvision",
+    version="0.1.3.17",
+    author="Samuel Albanie",
+    description="Python utilities for computer vision",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albanie/zsvision",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "msgpack",
+        "msgpack_numpy",
+        "typeguard",
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
