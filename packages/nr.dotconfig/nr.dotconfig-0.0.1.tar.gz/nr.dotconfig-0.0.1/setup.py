5e6cea1e70100a324692bae4523c78beea155199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:08 +0000

    Add package nr.dotconfig/0.0.1/nr.dotconfig-0.0.1.tar.gz

---
+++ packages/nr.dotconfig/nr.dotconfig-0.0.1.tar.gz/nr.dotconfig-0.0.1/setup.py
@@ -0,0 +1,38 @@
+# automatically created by shore 0.0.8
+
+import io
+import re
+import setuptools
+import sys
+
+with io.open('src/dotconfig.py', encoding='utf8') as fp:
+  version = re.search(r"__version__\s*=\s*'(.*)'", fp.read()).group(1)
+
+with io.open('README.md', encoding='utf8') as fp:
+  long_description = fp.read()
+
+requirements = ['nr.databind.core >=0.0.8,<0.1.0', 'nr.databind.json >=0.0.8,<0.1.0', 'PyYAML >=5.3.0,<5.4.0']
+
+setuptools.setup(
+  name = 'nr.dotconfig',
+  version = version,
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  description = 'A single configuration file for .bash_profile and .gitconfig.',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  url = 'https://git.niklasrosenstein.com/NiklasRosenstein/dotconfig',
+  license = 'MIT',
+  packages = setuptools.find_packages('src', ['test', 'test.*', 'docs', 'docs.*']),
+  package_dir = {'': 'src'},
+  include_package_data = True,
+  install_requires = requirements,
+  extras_require = {},
+  tests_require = [],
+  python_requires = None, # TODO: '>=3.4,<4.0.0',
+  data_files = [],
+  entry_points = {},
+  cmdclass = {},
+  keywords = [],
+  classifiers = [],
+)
