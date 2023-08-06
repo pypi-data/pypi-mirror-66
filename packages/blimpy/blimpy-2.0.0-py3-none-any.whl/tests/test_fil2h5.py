86bb66d6ad3957d1c5b4885fc3f347dfbfc3870d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:05 +0000

    Add package blimpy/2.0.0/blimpy-2.0.0-py3-none-any.whl

---
+++ packages/blimpy/blimpy-2.0.0-py3-none-any.whl/tests/test_fil2h5.py
@@ -0,0 +1,29 @@
+"""
+# test_fil2h5
+"""
+
+import os
+
+import blimpy as bl
+from tests.data import voyager_fil
+
+
+def test_fil2h5_conversion():
+    """Tests the conversion of fil files into h5 in both light and heavy modes.
+    """
+
+    # Creating test file.
+    bl.fil2h5.make_h5_file(voyager_fil, new_filename='test.h5')
+
+    # Creating a "large" test file.
+    bl.fil2h5.make_h5_file(voyager_fil, new_filename='test_large.h5', max_load=0.001)
+
+    # Testing filename
+    bl.fil2h5.make_h5_file(voyager_fil, new_filename='test')
+
+    # Deleting test file
+    os.remove('test.h5')
+    os.remove('test_large.h5')
+
+if __name__ == "__main__":
+    test_fil2h5_conversion()
---
+++ packages/blimpy/blimpy-2.0.0-py3-none-any.whl/tests/test_h52fil.py
@@ -0,0 +1,29 @@
+"""
+# test_h52fil
+"""
+
+import os
+
+import blimpy as bl
+from tests.data import voyager_h5
+
+
+def test_h52fil_conversion():
+    """ Tests the conversion of fil files into h5 in both light and heavy modes.
+    """
+
+    # Creating test file.
+    bl.h52fil.make_fil_file(voyager_h5, new_filename='test.fil')
+
+    # Creating a "large" test file.
+    bl.h52fil.make_fil_file(voyager_h5, new_filename='test_large.fil', max_load=0.001)
+
+    # Testing filename
+    bl.h52fil.make_fil_file(voyager_h5, new_filename='test')
+
+    # Deleting test file
+    os.remove('test.fil')
+    os.remove('test_large.fil')
+
+if __name__ == "__main__":
+    test_h52fil_conversion()
