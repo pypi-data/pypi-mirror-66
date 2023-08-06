5a210b7b1e9d2d15bf31ecf0295899068e6c00c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:44 +0000

    Add package FilaliAnsary/1.0.0/FilaliAnsary-1.0.0.tar.gz

---
+++ packages/FilaliAnsary/FilaliAnsary-1.0.0.tar.gz/FilaliAnsary-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md","r") as fh :
+    ld=fh.read()
+
+
+setup(
+    name="FilaliAnsary",
+    version="1.0.0",
+    description="A Python package created to kill time.",
+    long_description=ld,
+    long_description_content_type="text/markdown",
+    author="Abdelouahab FILALI ANSARY",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["requests"],
+    package_dir={'': "2"},
+    py_modules=["Ansary"],
+    extras_require = {"dev" : ["pytest>= 3.7",],},
+
+
+)
