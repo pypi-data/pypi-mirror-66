cb658e16cf137d898aeab1946c4442a92652979b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:03 +0000

    Add package hellocdw/0.0.2/hellocdw-0.0.2.tar.gz

---
+++ packages/hellocdw/hellocdw-0.0.2.tar.gz/hellocdw-0.0.2/README.md
@@ -0,0 +1,4 @@
+# This contains some documentation.
+
+## Usage
+
---
+++ packages/hellocdw/hellocdw-0.0.2.tar.gz/hellocdw-0.0.2/src/hellocdw.py
@@ -0,0 +1,11 @@
+
+
+def say_hello():
+    return "Hello Tester!"
+
+def hello():
+    return "second function"
+
+
+def hello2():
+    return "updated function"
