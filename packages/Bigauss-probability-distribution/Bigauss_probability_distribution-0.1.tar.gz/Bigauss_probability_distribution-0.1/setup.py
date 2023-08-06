c4ba54e06340469aaa718657e886f341a2b0d394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:13 +0000

    Add package Bigauss-probability-distribution/0.1/Bigauss_probability_distribution-0.1.tar.gz

---
+++ packages/Bigauss-probability-distribution/Bigauss_probability_distribution-0.1.tar.gz/Bigauss_probability_distribution-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Bigauss_probability_distribution',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['Bigauss_probability_distribution'],
+      author = 'Abhishek Chaturvedi',
+      zip_safe=False)
