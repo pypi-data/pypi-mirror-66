16ee01663e08936928bd0e14c1d4b99cb2e99193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package twitterhal/0.4.4/twitterhal-0.4.4.tar.gz

---
+++ packages/twitterhal/twitterhal-0.4.4.tar.gz/twitterhal-0.4.4/twitterhal/conf/default_settings.py
@@ -0,0 +1,36 @@
+import datetime
+
+import megahal
+import twitter
+
+
+SCREEN_NAME = ""
+RANDOM_POST_TIMES = [datetime.time(8), datetime.time(16), datetime.time(22)]
+INCLUDE_MENTIONS = False
+DATABASE_CLASS = "twitterhal.models.Database"
+DATABASE_FILE = "twitterhal"
+DETECTLANGUAGE_API_KEY = ""
+RUNNER_SLEEP_SECONDS = 5  # TODO: Document
+POST_STATUS_LIMIT = 300  # TODO: Document
+POST_STATUS_LIMIT_RESET_FREQUENCY = 3 * 60 * 60  # TODO: Document
+
+# List of Twitter handles we will never mention (including replying to them).
+# Without "@"!
+BANNED_USERS = []
+
+TWITTER_API = {
+    "consumer_key": "",
+    "consumer_secret": "",
+    "access_token_key": "",
+    "access_token_secret": "",
+    "timeout": 40,
+    "tweet_mode": "extended",
+}
+
+MEGAHAL_API = {
+    "max_length": twitter.api.CHARACTER_LIMIT,
+    "brainfile": "twitterhal-brain",
+    "order": megahal.DEFAULT_ORDER,
+    "timeout": megahal.DEFAULT_HARD_TIMEOUT,
+    "banwords": megahal.DEFAULT_BANWORDS,
+}
