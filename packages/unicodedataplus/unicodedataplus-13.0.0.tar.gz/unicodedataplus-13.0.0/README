3672e86ca5fb531ac0a99e17e3a68c5d20fdd2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:31 +0000

    Add package unicodedataplus/13.0.0/unicodedataplus-13.0.0.tar.gz

---
+++ packages/unicodedataplus/unicodedataplus-13.0.0.tar.gz/unicodedataplus-13.0.0/README
@@ -0,0 +1,19 @@
+unicodedataplus
+============
+
+Drop-in replacement for `unicodedata` with extensions for additional Unicode properties.
+
+Currently supported additional Unicode properties:
+
+* Script `script()`
+* Script Extensions `script_extensions()`
+* Block `block()`
+* Indic Positional Category `indic_positional_category()`
+* Indic Syllabic Category `indic_syllabic_category()`
+* Total Strokes (CJK) `total_strokes()`
+
+Additionally, two dictionaries (`property_value_aliases` and `property_value_by_alias`) are provided for Property Value Alias lookup.
+
+The versions of this package match unicode versions, so unicodedataplus==13.0.0 is data from unicode 13.0.0.
+
+Forked from https://github.com/mikekap/unicodedata2
---
+++ packages/unicodedataplus/unicodedataplus-13.0.0.tar.gz/unicodedataplus-13.0.0/setup.py
@@ -0,0 +1,26 @@
+import sys
+from setuptools import setup, Extension
+
+# if sys.version_info < (3,):
+#     type = 'py2'
+# else:
+type = 'py3' # Python 2 support has been removed
+
+main_module = Extension('unicodedataplus',
+                    sources = ['./unicodedataplus/' + type + '/unicodedata.c',
+                               './unicodedataplus/unicodectype.c'],
+                    include_dirs = ['./unicodedataplus/' + type, './unicodedataplus/'],
+)
+
+setup (name = "unicodedataplus",
+       version = "13.0.0",
+       description = "Unicodedata with extensions for additional properties.",
+       ext_modules = [main_module],
+       author="Ben Yang",
+       author_email="benayang@gmail.com",
+       download_url="http://github.com/iwsfutcmd/unicodedataplus",
+       license="Apache License 2.0",
+       platforms=['any'],
+       url="http://github.com/iwsfutcmd/unicodedataplus",
+       test_suite="tests",
+)
