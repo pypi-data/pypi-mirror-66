c62664bd0ff33a3d5785106b9b2fc3657cebef4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:16 +0000

    Add package macrobot-paper/0.1.6/macrobot_paper-0.1.6.tar.gz

---
+++ packages/macrobot-paper/macrobot_paper-0.1.6.tar.gz/macrobot_paper-0.1.6/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['macrobot_paper']
+install_requires = \
+['numpy>=1.18.3,<2.0.0',
+ 'opencv-python>=4.2.0,<5.0.0',
+ 'scikit-image>=0.16.2,<0.17.0']
+
+entry_points = \
+{'console_scripts': ['mb = macrobot_paper:main']}
+
+setup_kwargs = {
+    'name': 'macrobot-paper',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': None,
+    'author': 'Stefanie Lueck',
+    'author_email': 'lueck@ipk-gatersleben.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
