83124f20e972e5a920aace2a1179142ee6dd7034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:23 +0000

    Add package pytest-pytestrail/0.10.0/pytest_pytestrail-0.10.0-py2.py3-none-any.whl

---
+++ packages/pytest-pytestrail/pytest_pytestrail-0.10.0-py2.py3-none-any.whl/pytest_pytestrail/pytestrail.py
@@ -0,0 +1,32 @@
+import warnings
+from typing import Any, Optional, Iterable, Iterator, TypeVar
+
+import pytest
+
+__all__ = ["case", "steps_case", "param", "params"]
+
+ParameterSet = TypeVar("ParameterSet")
+
+
+def case(case_id: str, *args):
+    if args:
+        warnings.warn("Case takes only one argument", DeprecationWarning)
+    return pytest.mark.pytestrail(case_id)
+
+
+class steps_case:
+    def __init__(self, case_id: str):
+        self._case_id = case_id
+
+    def step(self, step: int):
+        return pytest.mark.pytestrail_case(self._case_id, step=step)
+
+
+def param(value: Any, case_id: str, step: Optional[int] = None) -> ParameterSet:
+    return pytest.param(
+        value, marks=case(case_id) if step is None else steps_case(case_id).step(step)
+    )
+
+
+def params(case_id: str, parametrize: Iterable) -> Iterator[ParameterSet]:
+    return (param(value, case_id, step) for step, value in enumerate(parametrize, 1))
