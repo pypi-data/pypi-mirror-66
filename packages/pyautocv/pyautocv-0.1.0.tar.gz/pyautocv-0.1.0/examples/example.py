3e4639e238cff22b10c33b316a2a9f4b7699bcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:48 +0000

    Add package pyautocv/0.1.0/pyautocv-0.1.0.tar.gz

---
+++ packages/pyautocv/pyautocv-0.1.0.tar.gz/pyautocv-0.1.0/examples/example.py
@@ -0,0 +1,7 @@
+from pyautocv import *
+images_list = list(read_images("../images"))
+thresheld = threshold(images_list)
+thresheld_converted = convert_thresheld(thresheld, images_list)
+edge_detection = list(map(lambda x: detect_edges("laplace",x), thresheld_converted))
+show_images(convert_thresheld(thresheld_list, images_list),thresheld=True,ncols=2)
+show_images(edge_detection, ncols=2, thresheld=True)
---
+++ packages/pyautocv/pyautocv-0.1.0.tar.gz/pyautocv-0.1.0/pyautocv/__init__.py
@@ -0,0 +1,21 @@
+"""
+(Semi) Automated Image Processing
+
+.. author: Nelson Gonzabato
+
+"""
+
+# Credit to https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
+
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+__author__ = "Nelson Gonzabato"
+__version__ =open('pyautocv/version.py').read().rstrip()
+__all__ = ["pyautocv"]
+
+
+
+
