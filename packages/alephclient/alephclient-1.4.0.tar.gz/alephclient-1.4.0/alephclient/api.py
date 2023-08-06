492c8d7c7219dc1171ba7e631b0bf2453da3810c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:09 +0000

    Add package alephclient/1.4.0/alephclient-1.4.0.tar.gz

---
+++ packages/alephclient/alephclient-1.4.0.tar.gz/alephclient-1.4.0/alephclient/api.py
@@ -0,0 +1,331 @@
+import uuid
+import json
+import logging
+import pkg_resources
+from itertools import count
+from banal import ensure_list  # type: ignore
+from six.moves.urllib.parse import urlencode, urljoin
+from requests import Session, RequestException
+from requests_toolbelt import MultipartEncoder  # type: ignore
+from pathlib import Path
+from typing import Optional, Dict, List, Iterator, Iterable
+
+from alephclient import settings
+from alephclient.errors import AlephException
+from alephclient.util import backoff, prop_push
+
+log = logging.getLogger(__name__)
+MIME = 'application/octet-stream'
+VERSION = pkg_resources.get_distribution('alephclient').version
+
+
+class APIResultSet(object):
+
+    def __init__(self, api: 'AlephAPI', url: str):
+        self.api = api
+        self.url = url
+        self.current = 0
+        self.result = self.api._request('GET', self.url)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= self.result.get('limit'):
+            next_url = self.result.get('next')
+            if next_url is None:
+                raise StopIteration
+            self.result = self.api._request('GET', next_url)
+        try:
+            res = self.result.get('results', [])[self.index]
+        except IndexError:
+            raise StopIteration
+        self.current += 1
+        return res
+
+    next = __next__
+
+    @property
+    def index(self):
+        return self.current - self.result.get('offset')
+
+    def __len__(self):
+        return self.result.get('total')
+
+    def __repr__(self):
+        return '<APIResultSet(%r, %r)>' % (self.url, len(self))
+
+
+class AlephAPI(object):
+
+    def __init__(self,
+                 host: Optional[str] = settings.HOST,
+                 api_key: Optional[str] = settings.API_KEY,
+                 session_id: Optional[str] = None,
+                 retries: int = settings.MAX_TRIES):
+
+        if not host:
+            raise AlephException('No host environment variable found')
+        self.base_url = urljoin(host, '/api/2/')
+        self.retries = retries
+        session_id = session_id or str(uuid.uuid4())
+        self.session: Session = Session()
+        self.session.headers['X-Aleph-Session'] = session_id
+        self.session.headers['User-Agent'] = 'alephclient/%s' % VERSION
+        if api_key is not None:
+            self.session.headers['Authorization'] = 'ApiKey %s' % api_key
+
+    def _make_url(self, path: str, query: Optional[str] = None,
+                  filters: Optional[List] = None, **params):
+        """Construct the target url from given args"""
+        url = self.base_url + path
+        if query:
+            params["q"] = query
+        if filters:
+            for key, val in filters:
+                params["filter:" + key] = val
+        if len(params):
+            url = url + '?' + urlencode(params)
+        return url
+
+    def _request(self, method: str, url: str, **kwargs) -> Dict:
+        """A single point to make the http requests.
+
+        Having a single point to make all requests let's us set headers, manage
+        successful and failed responses and possibly manage session etc
+        conviniently in a single place.
+        """
+        try:
+            response = self.session.request(method=method, url=url, **kwargs)
+            response.raise_for_status()
+        except RequestException as exc:
+            raise AlephException(exc)
+
+        if len(response.text):
+            return response.json()
+        return {}
+
+    def search(self, query: str, schema: Optional[str] = None,
+               schemata: Optional[str] = None,
+               filters: Optional[List] = None) -> 'APIResultSet':
+        """Conduct a search and return the search results."""
+        filters_list: List = ensure_list(filters)
+        if schema is not None:
+            filters_list.append(('schema', schema))
+        if schemata is not None:
+            filters_list.append(('schemata', schemata))
+        if schema is None and schemata is None:
+            filters_list.append(('schemata', 'Thing'))
+        url = self._make_url('entities', query=query, filters=filters_list)
+        return APIResultSet(self, url)
+
+    def get_collection(self, collection_id: str) -> Dict:
+        """Get a single collection by ID (not foreign ID!)."""
+        url = self._make_url(f'collections/{collection_id}')
+        return self._request('GET', url)
+
+    def get_entity(self, entity_id: str) -> Dict:
+        """Get a single entity by ID."""
+        url = self._make_url(f'entities/{entity_id}')
+        return self._request('GET', url)
+
+    def get_collection_by_foreign_id(self, foreign_id: str) -> Optional[Dict]:
+        """Get a dict representing a collection based on its foreign ID."""
+        if foreign_id is None:
+            return None
+        filters = [('foreign_id', foreign_id)]
+        for coll in self.filter_collections(filters=filters):
+            return coll
+        return None
+
+    def load_collection_by_foreign_id(self, foreign_id: str,
+                                      config: Optional[Dict] = None) -> Dict:
+        """Get a collection by its foreign ID, or create one."""
+        collection = self.get_collection_by_foreign_id(foreign_id)
+        if collection is not None:
+            return collection
+
+        config = config or {}
+        collection = self.create_collection({
+            'foreign_id': foreign_id,
+            'label': config.get('label', foreign_id),
+            'casefile': config.get('casefile', False),
+            'category': config.get('category', 'other'),
+            'languages': config.get('languages', []),
+            'summary': config.get('summary', ''),
+        })
+        return collection
+
+    def filter_collections(self, query: str = None,
+                           filters: Optional[List] = None,
+                           **kwargs) -> 'APIResultSet':
+        """Filter collections for the given query and/or filters.
+
+        params
+        ------
+        query: query string
+        filters: list of key, value pairs to filter collections
+        kwargs: extra arguments for api call such as page, limit etc
+        """
+        if not query and not filters:
+            raise ValueError("One of query or filters is required")
+        url = self._make_url("collections", query=query,
+                             filters=filters, **kwargs)
+        return APIResultSet(self, url)
+
+    def create_collection(self, data: Dict) -> Dict:
+        """Create a collection from the given data.
+
+        params
+        ------
+        data: dict with foreign_id, label, category etc. See `CollectionSchema`
+        for more details.
+        """
+        url = self._make_url("collections")
+        return self._request("POST", url, json=data)
+
+    def update_collection(self, collection_id: str, data: Dict) -> Dict:
+        """Update an existing collection using the given data.
+
+        params
+        ------
+        collection_id: id of the collection to update
+        data: dict with foreign_id, label, category etc. See `CollectionSchema`
+        for more details.
+        """
+        url = self._make_url(f"collections/{collection_id}")
+        return self._request("PUT", url, json=data)
+
+    def map_collection(self, collection_id: str, mapping: Dict):
+        """Run a bulk entity data mapping on a collection.
+
+        params
+        ------
+        collection_id: id of the collection to update
+        mapping: dict with the entity bulk load mapping.
+        """
+        url = self._make_url(f"collections/{collection_id}/mapping")
+        return self._request("PUT", url, json=mapping)
+
+    def stream_entities(self, collection_id: str = None,
+                        include: Optional[List] = None,
+                        schema: Optional[str] = None) -> Iterator[Dict]:
+        """Iterate over all entities in the given collection.
+
+        params
+        ------
+        collection_id: id of the collection to stream
+        include: an array of fields from the index to include.
+        """
+        url = self._make_url('entities/_stream')
+        if collection_id is not None:
+            url = f"collections/{collection_id}/_stream"
+            url = self._make_url(url)
+        params = {'include': include, 'schema': schema}
+        try:
+            res = self.session.get(url, params=params, stream=True)
+            res.raise_for_status()
+            for entity in res.iter_lines():
+                if isinstance(entity, bytes):
+                    entity = entity.decode('utf-8')
+                entity = json.loads(entity)
+                aleph_url = 'entities/%s' % entity.get('id')
+                prop_push(entity, 'alephUrl', self._make_url(aleph_url))
+                yield entity
+        except RequestException as exc:
+            raise AlephException(exc)
+
+    def _bulk_chunk(self, collection_id: str, chunk: List, force: bool = False,
+                    unsafe: bool = False):
+        for attempt in count(1):
+            url = self._make_url(f"collections/{collection_id}/_bulk")
+            params = {'unsafe': unsafe}
+            try:
+                response = self.session.post(url, json=chunk, params=params)
+                response.raise_for_status()
+                return
+            except RequestException as exc:
+                ae = AlephException(exc)
+                if not ae.transient or attempt > self.retries:
+                    if not force:
+                        raise ae
+                    log.error(ae)
+                    return
+                backoff(ae, attempt)
+
+    def write_entities(self, collection_id: str, entities: Iterable,
+                       chunk_size: int = 1000, **kw):
+        """Create entities in bulk via the API, in the given
+        collection.
+
+        params
+        ------
+        collection_id: id of the collection to use
+        entities: an iterable of entities to upload
+        """
+        chunk = []
+        for entity in entities:
+            chunk.append(entity)
+            if len(chunk) >= chunk_size:
+                self._bulk_chunk(collection_id, chunk, **kw)
+                chunk = []
+        if len(chunk):
+            self._bulk_chunk(collection_id, chunk, **kw)
+
+    def match(self, entity: Dict, collection_ids: Optional[str] = None,
+              url: str = None) -> Iterator[List]:
+        """Find similar entities given a sample entity."""
+        params = {
+            'collection_ids': ensure_list(collection_ids)
+        }
+        if url is None:
+            url = self._make_url('match')
+        try:
+            response = self.session.post(url, json=entity, params=params)
+            response.raise_for_status()
+            for result in response.json().get('results', []):
+                yield result
+        except RequestException as exc:
+            raise AlephException(exc)
+
+    def linkages(self, context_ids: Optional[List] = None) -> 'APIResultSet':
+        """Stream all linkages within the given role contexts."""
+        filters = [('context_id', c) for c in ensure_list(context_ids)]
+        url = self._make_url('linkages', filters=filters)
+        return APIResultSet(self, url)
+
+    def ingest_upload(self, collection_id: str,
+                      file_path: Optional[Path] = None,
+                      metadata: Optional[Dict] = None) -> Dict:
+        """
+        Create an empty folder in a collection or upload a document to it
+
+        params
+        ------
+        collection_id: id of the collection to upload to
+        file_path: path of the file to upload. None while creating folders
+        metadata: dict containing metadata for the file or folders. In case of
+        files, metadata contains foreign_id of the parent. Metadata for a
+        directory contains foreign_id for itself as well as its parent and the
+        name of the directory.
+        """
+        url = self._make_url("collections/{0}/ingest".format(collection_id))
+        if not file_path or file_path.is_dir():
+            data = {"meta": json.dumps(metadata)}
+            return self._request("POST", url, data=data)
+
+        for attempt in count(1):
+            try:
+                with file_path.open('rb') as fh:
+                    # use multipart encoder to allow uploading very large files
+                    m = MultipartEncoder(fields={
+                        'meta': json.dumps(metadata),
+                        'file': (file_path.name, fh, MIME)
+                    })
+                    headers = {'Content-Type': m.content_type}
+                    return self._request("POST", url, data=m, headers=headers)
+            except AlephException as ae:
+                if not ae.transient or attempt > self.retries:
+                    raise ae
+                backoff(ae, attempt)
+        return {}
---
+++ packages/alephclient/alephclient-1.4.0.tar.gz/alephclient-1.4.0/alephclient/cli.py
@@ -0,0 +1,146 @@
+import json
+import click
+import logging
+
+from alephclient import settings
+from alephclient.api import AlephAPI
+from alephclient.util import prop_push
+from alephclient.errors import AlephException
+from alephclient.tasks.crawldir import crawl_dir
+from alephclient.tasks.bulkload import bulk_load
+
+log = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option('--host', default=settings.HOST, metavar="HOST", help="Aleph API host URL")  # noqa
+@click.option('--api-key', default=settings.API_KEY, metavar="KEY", help="Aleph API key for authentication")  # noqa
+@click.option('-r', '--retries', type=int, default=settings.MAX_TRIES, help="retries upon server failure")  # noqa
+@click.pass_context
+def cli(ctx, host, api_key, retries):
+    """API client for Aleph API"""
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
+    logging.getLogger('httpstream').setLevel(logging.WARNING)
+    if not host:
+        raise click.BadParameter('Missing Aleph host URL')
+    if ctx.obj is None:
+        ctx.obj = {}
+    ctx.obj['api'] = AlephAPI(host, api_key, retries=retries)
+
+
+@cli.command()
+@click.option('--casefile', is_flag=True, default=False, help='handle as case file')  # noqa
+@click.option('-l', '--language', multiple=True, help="language hint: 2-letter language code (ISO 639)")  # noqa
+@click.option('-f', '--foreign-id', required=True, help="foreign_id of the collection")  # noqa
+@click.argument('path', type=click.Path(exists=True))
+@click.pass_context
+def crawldir(ctx, path, foreign_id, language=None, casefile=False):
+    """Crawl a directory recursively and upload the documents in it to a
+    collection."""
+    try:
+        config = {
+            'languages': language,
+            'casefile': casefile
+        }
+        api = ctx.obj["api"]
+        crawl_dir(api, path, foreign_id, config)
+    except AlephException as exc:
+        raise click.ClickException(str(exc))
+
+
+@cli.command()
+@click.argument('mapping_file')
+@click.pass_context
+def bulkload(ctx, mapping_file):
+    """Trigger a load of structured entity data using the submitted mapping."""
+    # TODO: When can we remove this?
+    try:
+        bulk_load(ctx.obj["api"], mapping_file)
+    except AlephException as exc:
+        raise click.ClickException(str(exc))
+
+
+@cli.command('write-entities')
+@click.option('-i', '--infile', type=click.File('r'), default='-')  # noqa
+@click.option('-f', '--foreign-id', required=True, help="foreign_id of the collection")  # noqa
+@click.option('--force', is_flag=True, default=False, help="continue after server errors")  # noqa
+@click.option('--unsafe', is_flag=True, default=False, help="disable server-side validation")  # noqa
+@click.pass_context
+def write_entities(ctx, infile, foreign_id, force=False, unsafe=False):
+    """Read entities from standard input and index them."""
+    api = ctx.obj["api"]
+    try:
+        collection = api.load_collection_by_foreign_id(foreign_id, {})
+        collection_id = collection.get('id')
+
+        def read_json_stream(stream):
+            count = 0
+            while True:
+                line = stream.readline()
+                if not line:
+                    return
+                count += 1
+                if count % 1000 == 0:
+                    log.info("[%s] Bulk load entities: %s...",
+                             foreign_id, count)
+                yield json.loads(line)
+
+        api.write_entities(collection_id, read_json_stream(infile),
+                           unsafe=unsafe, force=force)
+    except AlephException as exc:
+        raise click.ClickException(exc.message)
+    except BrokenPipeError:
+        raise click.Abort()
+
+
+@cli.command('stream-entities')
+@click.option('-o', '--outfile', type=click.File('w'), default='-')  # noqa
+@click.option('-s', '--schema', multiple=True, default=[])  # noqa
+@click.option('-f', '--foreign-id', help="foreign_id of the collection")
+@click.option('-p', '--publisher', is_flag=True, default=False, help="Add publisher info from collection context")  # noqa
+@click.pass_context
+def stream_entities(ctx, outfile, schema, foreign_id, publisher):
+    """Load entities from the server and print them to stdout."""
+    api = ctx.obj["api"]
+    try:
+        include = ['id', 'schema', 'properties']
+        collection = api.get_collection_by_foreign_id(foreign_id)
+        if collection is None:
+            raise click.BadParameter("Collection %r not found!" % foreign_id)
+        publisher_label = collection.get('publisher', collection.get('label'))
+        publisher_url = collection.get('links', {}).get('ui')
+        publisher_url = collection.get('publisher_url', publisher_url)
+        for entity in api.stream_entities(collection_id=collection.get('id'),
+                                          include=include, schema=schema):
+            if publisher:
+                prop_push(entity, 'publisher', publisher_label)
+                prop_push(entity, 'publisherUrl', publisher_url)
+            outfile.write(json.dumps(entity))
+            outfile.write('\n')
+    except AlephException as exc:
+        raise click.ClickException(exc.message)
+    except BrokenPipeError:
+        raise click.Abort()
+
+
+@cli.command('linkages')
+@click.option('-o', '--outfile', type=click.File('w'), default='-')  # noqa
+@click.option('-c', '--context', multiple=True, default=[])  # noqa
+@click.pass_context
+def linkages(ctx, outfile, context):
+    """Stream all linkages within the given role contexts."""
+    api = ctx.obj["api"]
+    try:
+        for entity in api.linkages(context_ids=context):
+            outfile.write(json.dumps(entity))
+            outfile.write('\n')
+    except AlephException as exc:
+        raise click.ClickException(exc.message)
+    except BrokenPipeError:
+        raise click.Abort()
+
+
+if __name__ == "__main__":
+    cli()
