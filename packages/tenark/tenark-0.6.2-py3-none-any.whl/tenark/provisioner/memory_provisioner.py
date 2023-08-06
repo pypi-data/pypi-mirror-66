51c81eca2ea5be3bb1239fd9b90f472d0cd9bfaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:04 +0000

    Add package tenark/0.6.2/tenark-0.6.2-py3-none-any.whl

---
+++ packages/tenark/tenark-0.6.2-py3-none-any.whl/tenark/provisioner/memory_provisioner.py
@@ -0,0 +1,13 @@
+from typing import Dict
+from ..common import TenantProvisionError
+from ..models import Tenant
+from .provisioner import Provisioner
+
+
+class MemoryProvisioner(Provisioner):
+
+    def __init__(self) -> None:
+        self.pool: Dict[str, Tenant] = {}
+
+    def provision_tenant(self, tenant: Tenant) -> None:
+        self.pool[tenant.id] = tenant
---
+++ packages/tenark/tenark-0.6.2-py3-none-any.whl/tenark/provisioner/provisioner.py
@@ -0,0 +1,11 @@
+from typing import Dict
+from abc import ABC, abstractmethod
+from ..models import Tenant
+
+
+class Provisioner(ABC):
+    """Tenant Provision service."""
+
+    @abstractmethod
+    def provision_tenant(self, tenant: Tenant) -> None:
+        "Provision tenant method to be implemented."
