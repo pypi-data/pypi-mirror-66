27c720e202a05f33237a11ed7e5db5e882a04005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:32 +0000

    Add package punctuation-stylometry/0.0.9/punctuation_stylometry-0.0.9.tar.gz

---
+++ packages/punctuation-stylometry/punctuation_stylometry-0.0.9.tar.gz/punctuation_stylometry-0.0.9/punctuation/parser/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.0.1'
+__all__ = ['digilibraries_parser', 'gutenberg_cache_parser', 
+           'gutenberg_parser', 'punctuation_parser']
---
+++ packages/punctuation-stylometry/punctuation_stylometry-0.0.9.tar.gz/punctuation_stylometry-0.0.9/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  9 06:28:50 2019
+
+@author: alexandradarmon
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as rq:
+    requirements = rq.read()
+
+setuptools.setup(
+    name="punctuation_stylometry",
+    version="0.0.9",
+    author="alexdarmon",
+    author_email="alexandra.darmon@hotmail.fr",
+    description="This package represents the code used for the publication of the article  https://arxiv.org/abs/1901.00519",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alex-darmon/punctuation-stylometry",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements.split('\n'),
+    dependency_links=[
+          'https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.0.0/en_core_web_sm-2.0.0.tar.gz#egg=en_core_web_sm'
+      ]
+
+)
