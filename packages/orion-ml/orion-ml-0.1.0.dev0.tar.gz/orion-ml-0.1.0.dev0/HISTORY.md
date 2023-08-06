540f6d6be907a8809d1bbbd24dd70472c07dc8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:08 +0000

    Add package orion-ml/0.1.0.dev0/orion-ml-0.1.0.dev0.tar.gz

---
+++ packages/orion-ml/orion-ml-0.1.0.dev0.tar.gz/orion-ml-0.1.0.dev0/HISTORY.md
@@ -0,0 +1,6 @@
+History
+=======
+
+## 0.1.0
+
+* First release.
---
+++ packages/orion-ml/orion-ml-0.1.0.dev0.tar.gz/orion-ml-0.1.0.dev0/orion/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """MIT Data To AI Lab"""
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.1.0.dev0'
+
+import os
+
+_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+MLBLOCKS_PRIMITIVES = os.path.join(_BASE_PATH, 'primitives', 'jsons')
+MLBLOCKS_PIPELINES = os.path.join(_BASE_PATH, 'pipelines')
+
+
+def get_available_templates():
+    return [
+        filename[:-5]
+        for filename in os.listdir(MLBLOCKS_PIPELINES)
+    ]
