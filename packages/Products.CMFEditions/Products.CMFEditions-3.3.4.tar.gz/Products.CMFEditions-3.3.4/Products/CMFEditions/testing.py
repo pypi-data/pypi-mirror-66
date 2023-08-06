7be4aaa13cabca1f5682a776ca161823ce5d5ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:26 +0000

    Add package Products.CMFEditions/3.3.4/Products.CMFEditions-3.3.4.tar.gz

---
+++ packages/Products.CMFEditions/Products.CMFEditions-3.3.4.tar.gz/Products.CMFEditions-3.3.4/Products/CMFEditions/testing.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import Products.CMFEditions
+
+
+class ProductsCmfeditionsLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        self.loadZCML(package=Products.CMFEditions)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'Products.CMFEditions:CMFEditions')
+        # with named AND dotted behaviors we need to take care of both
+        versioning_behavior = set(
+            [
+                'plone.app.versioningbehavior.behaviors.IVersionable',
+                'plone.versioning',
+            ],
+        )
+        for name in ('Document', 'Event', 'Link', 'News Item'):
+            fti = portal.portal_types[name]
+            # write back the behaviors without the versioning behaviors
+            # using a Set to keep it simple
+            # a = set((1,2,3))
+            # b = set([2,4])
+            # res = tuple(a.difference(b)) >> (1,3)
+            fti.behaviors = tuple(
+                set(fti.behaviors).difference(versioning_behavior),
+            )
+
+
+PRODUCTS_CMFEDITIONS_FIXTURE = ProductsCmfeditionsLayer()
+
+
+PRODUCTS_CMFEDITIONS_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PRODUCTS_CMFEDITIONS_FIXTURE,),
+    name='ProductsCmfeditionsLayer:IntegrationTesting',
+)
+
+
+PRODUCTS_CMFEDITIONS_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PRODUCTS_CMFEDITIONS_FIXTURE,),
+    name='ProductsCmfeditionsLayer:FunctionalTesting',
+)
+
+
+PRODUCTS_CMFEDITIONS_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        PRODUCTS_CMFEDITIONS_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name='ProductsCmfeditionsLayer:AcceptanceTesting',
+)
---
+++ packages/Products.CMFEditions/Products.CMFEditions-3.3.4.tar.gz/Products.CMFEditions-3.3.4/setup.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '3.3.4'
+
+setup(
+    name='Products.CMFEditions',
+    version=version,
+    description="Versioning for Plone",
+    long_description=(open("README.rst").read() + '\n' +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.2',
+        'Framework :: Plone :: Core',
+        'Framework :: Zope2',
+        'Framework :: Zope :: 4',
+    ],
+    keywords='Versioning Plone',
+    author='CMFEditions contributers',
+    author_email='collective-versioning@lists.sourceforge.net',
+    url='https://pypi.org/project/Products.CMFEditions',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
+        test=[
+            'plone.app.testing',
+            'plone.app.textfield',
+            'Products.CMFPlone',
+            'Products.CMFDynamicViewFTI',
+            'zope.testing',
+        ],
+        archetypes=[
+            'Products.Archetypes',
+        ]
+    ),
+    install_requires=[
+        'setuptools',
+        'six',
+        'zope.copy',
+        'zope.dottedname',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'Products.CMFCore >=2.1',
+        'Products.CMFDiffTool',  # dependency in diff template
+        'Products.CMFUid',
+        'Products.GenericSetup >=1.4.0',
+        'Products.ZopeVersionControl',
+        'Acquisition',
+        'DateTime',
+        'transaction',
+        'Zope2',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
