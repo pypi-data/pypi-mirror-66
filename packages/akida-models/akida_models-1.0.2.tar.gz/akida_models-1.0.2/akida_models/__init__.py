f433831c74a20e1e04ba8c0d462f46910ab93b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:31 +0000

    Add package akida-models/1.0.2/akida_models-1.0.2.tar.gz

---
+++ packages/akida-models/akida_models-1.0.2.tar.gz/akida_models-1.0.2/akida_models/__init__.py
@@ -0,0 +1,10 @@
+# Imports models
+from .dvs.model_convtiny import convtiny_dvs
+from .cifar10.model_ds_cnn import ds_cnn_cifar10
+from .cifar10.model_vgg import vgg_cifar10
+from .imagenet.model_mobilenet import mobilenet_imagenet
+from .kws.model_ds_cnn import ds_cnn_kws
+from .kws.model_ds_cnn_edge import ds_cnn_edge_kws
+from .utk_face.model_vgg import vgg_utk_face
+from .cse2018.model_tse import tse_mlp_cse2018
+from .tabular_data import tabular_data
