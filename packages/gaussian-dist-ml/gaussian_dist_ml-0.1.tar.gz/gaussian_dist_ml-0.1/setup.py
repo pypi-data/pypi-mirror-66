25f0c13abca8aa1ccb8bf05803429be9a5bf89a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:03 +0000

    Add package gaussian-dist-ml/0.1/gaussian_dist_ml-0.1.tar.gz

---
+++ packages/gaussian-dist-ml/gaussian_dist_ml-0.1.tar.gz/gaussian_dist_ml-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussian_dist_ml',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaussian_dist_ml'],
+      zip_safe=False)
