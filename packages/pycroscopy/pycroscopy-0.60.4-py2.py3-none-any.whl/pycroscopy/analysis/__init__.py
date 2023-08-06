9761f25ca9ba79e868238eaebbc28d0943cb6c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:00 +0000

    Add package pycroscopy/0.60.4/pycroscopy-0.60.4-py2.py3-none-any.whl

---
+++ packages/pycroscopy/pycroscopy-0.60.4-py2.py3-none-any.whl/pycroscopy/analysis/__init__.py
@@ -0,0 +1,38 @@
+"""
+Physical or chemical model-based analysis of data
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: _autosummary
+
+    be_loop_fitter
+    be_sho_fitter
+    fit_methods
+    fitter
+    giv_bayesian
+    guess_methods
+    optimize
+
+"""
+
+from . import utils
+from .utils import *
+from . import be_sho_fitter
+from .be_sho_fitter import BESHOfitter
+from . import be_loop_fitter
+from .be_loop_fitter import BELoopFitter
+from . import guess_methods
+from .guess_methods import GuessMethods
+from . import fitter
+from .fitter import Fitter
+from .optimize import Optimize
+from . import fit_methods
+from .fit_methods import *
+from .giv_bayesian import GIVBayesian
+from .be_relax_fit import BERelaxFit
+
+__all__ = ['GuessMethods', 'Fitter', 'BESHOfitter', 'BELoopFitter', 'utils', 'Optimize', 'fit_methods', 'GIVBayesian',
+           'BERelaxFit']
+__all__ += utils.__all__
