955d29d6d7cc8ce15a766bcb7a7b4b2ed4b3da3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:48 +0000

    Add package mikula/0.17/mikula-0.17.tar.gz

---
+++ packages/mikula/mikula-0.17.tar.gz/mikula-0.17/mikula/implementation/util.py
@@ -0,0 +1,73 @@
+import os
+import shutil
+from distutils.dir_util import copy_tree
+from mikula.implementation import settings
+
+
+def input_yes_no(question):
+    reply = None
+    while reply not in ("y", "n"):
+        reply = str(input(question+' (y/n): ')).lower().strip()
+    return True if reply == "y" else False
+
+
+def extension(file):
+    _, ext = os.path.splitext(file)
+    ext = ext.upper()
+    return ext[1:]
+
+
+def create(directory, overwrite=True):
+    if os.path.isdir(directory) and overwrite:
+        shutil.rmtree(directory)
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+
+
+def create_directories(parsed, output_directory, overwrite=True):
+    create(output_directory, overwrite)
+
+    gallery_dir = os.path.join(output_directory, settings.gallery_dir)
+    create(gallery_dir, overwrite)
+
+    images_dir = os.path.join(gallery_dir, settings.images_dir)
+    create(images_dir, overwrite)
+
+    thumbnails_dir = os.path.join(images_dir, settings.thumbnails_dir)
+    create(thumbnails_dir, overwrite)
+
+    for directory in reversed(parsed.keys()):
+        absolute = os.path.join(output_directory, directory)
+        if not os.path.isdir(absolute):
+            os.mkdir(absolute)
+
+
+def copy_assets(theme, output_directory):
+    src = os.path.join(theme, "assets")
+    dst = os.path.join(output_directory, settings.assets_dir)
+    shutil.copytree(src, dst, dirs_exist_ok=True)
+
+
+def copy_user_assets(source, output):
+    src = os.path.join(os.path.abspath(source), settings.assets_source)
+    dst = os.path.join(os.path.abspath(output), settings.user_assets_dir)
+    if os.path.isdir(src):
+        copy_tree(src, dst)
+
+
+def walk(path, exclude=tuple(), topdown=False):
+    nodes = list()
+    for root, dirs, files in os.walk(path, topdown=True):
+        dirs[:] = [d for d in dirs if d not in exclude]
+        nodes.append((root, dirs, files))
+    if not topdown:
+        nodes.reverse()
+    return nodes
+
+
+def get_theme_directory(theme):
+    if os.path.isdir(theme):
+        return theme
+
+    d = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(d, os.pardir, "themes", theme))
---
+++ packages/mikula/mikula-0.17.tar.gz/mikula-0.17/setup.py
@@ -0,0 +1,61 @@
+import setuptools
+import os
+import pathlib
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+PATH = os.path.dirname(__file__)
+
+
+def include_data(root, directories, *extra):
+    content = list()
+    root_path = pathlib.Path(root)
+    for directory in directories:
+        for current, _, _ in os.walk(os.path.join(root, directory)):
+            relative = os.path.relpath(current, root_path)
+            content.append(os.path.join(relative, "*"))
+    content.extend(extra)
+    return content
+
+
+def get_version():
+    with open(os.path.join(PATH, "mikula", "VERSION")) as version_file:
+        version = version_file.read().strip()
+    return version
+
+
+INCLUDE_DIRS = ("themes",
+                os.path.join("implementation", "skeleton"))
+INCLUDED_DATA = include_data("mikula", INCLUDE_DIRS, "VERSION")
+
+setuptools.setup(
+    name="mikula",
+    version=get_version(),
+    author="Roman Kosobrodov",
+    author_email="mikula@kosobrodov.net",
+    description="Static web gallery generator",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RomanKosobrodov/mikula",
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_data={
+        'mikula': INCLUDED_DATA
+    },
+    scripts=['bin/mikula'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "boto3==1.10.26",
+        "Jinja2==2.10.3",
+        "Markdown==3.1.1",
+        "Pillow>=6.2.2",
+        "piexif>=1.1.3",
+        "PyYAML==5.1.2"
+    ]
+)
