225a287763eb3916010f0667e049457f0b0eb170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:05 +0000

    Add package buildflaskapp/0.0.1.1/buildflaskapp-0.0.1.1.tar.gz

---
+++ packages/buildflaskapp/buildflaskapp-0.0.1.1.tar.gz/buildflaskapp-0.0.1.1/buildflaskapp/buildflaskapp.py
@@ -0,0 +1,4 @@
+import buildflaskapp
+
+def main():
+    print('YOyo')
---
+++ packages/buildflaskapp/buildflaskapp-0.0.1.1.tar.gz/buildflaskapp-0.0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='buildflaskapp',  
+    version='0.0.1.1',
+    author="Hans Maulloo",
+    author_email="askyourkode@gmail.com",
+    description="Generate flask app boilerplate!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/askyourkode/buildflaskapp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Freely Distributable",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ["buildflaskapp=buildflaskapp.buildflaskapp:main"]}
+)
