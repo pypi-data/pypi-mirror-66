cca03f1d64034c3e5750ec0f6c02697f73f49eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:40 +0000

    Add package yahoo-earnings-calendar/0.6.0/yahoo_earnings_calendar-0.6.0.tar.gz

---
+++ packages/yahoo-earnings-calendar/yahoo_earnings_calendar-0.6.0.tar.gz/yahoo_earnings_calendar-0.6.0/setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='yahoo_earnings_calendar',
+    packages=['yahoo_earnings_calendar'],
+    install_requires=[
+        'requests'
+    ],
+    version='0.6.0',
+    description='Scrapes data from Yahoo! Finance earnings calendar',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Wenbo Yu',
+    author_email='wenboyu2@gmail.com',
+    url='https://github.com/wenboyu2/yahoo-earnings-calendar',
+    keywords=['stock', 'earnings', 'yahoo', 'scrape', 'finance'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
