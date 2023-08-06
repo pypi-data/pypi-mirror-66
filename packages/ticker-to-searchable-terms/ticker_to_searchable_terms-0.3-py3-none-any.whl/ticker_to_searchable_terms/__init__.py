057f26f8078bced3e757aa05af23a584459e1769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:48 +0000

    Add package ticker-to-searchable-terms/0.3/ticker_to_searchable_terms-0.3-py3-none-any.whl

---
+++ packages/ticker-to-searchable-terms/ticker_to_searchable_terms-0.3-py3-none-any.whl/ticker_to_searchable_terms/__init__.py
@@ -0,0 +1,53 @@
+import os
+import pkg_resources
+import sqlite3
+from .database_management import open_connection
+from .database_management import find_ticker
+from .generate_possible_names import generate_possible_names
+
+
+class Company:
+	def __init__(self, ticker):
+		self.ticker = ticker.upper()
+		DB_FILE = pkg_resources.resource_filename('key_people', 'DB/Stocks.db')
+		self.conn, self.c = open_connection(DB_FILE)
+		NICKNAME_FILE = pkg_resources.resource_filename('key_people', 'DB/Nicknames.db')
+		self.n_conn, self.n_c = open_connection(NICKNAME_FILE)
+		self.terms = self.pop(ticker)
+
+		
+
+	def pop(self, c):
+		ticker = self.ticker.upper()
+		results = find_ticker(ticker, self.c)
+		ticker = results[0]
+		company_name = results[1]
+		alt_company_name = results[-1]
+		people = []
+		for x in results[2:]:
+			if x != 'NULL':
+				x = generate_possible_names(x, c)
+				people.append(x)
+
+		return {'ticker': ticker, 'company_name': company_name, 'alt_company_name': alt_company_name, 'key_people_nicknames': people}
+
+def get_terms(ticker):
+	ticker = ticker.upper()
+	DB_FILE = pkg_resources.resource_filename('key_people', 'DB/Stocks.db')
+	conn, c = open_connection(DB_FILE)
+
+	ticker = ticker.upper()
+	results = find_ticker(ticker, c)
+	alt_company_name = results[-1]
+	ticker = results[0]
+	company_name = results[1]
+	people = []
+	conn.close()
+	NICKNAME_FILE = pkg_resources.resource_filename('key_people', 'DB/Nicknames.db')
+	conn, c = open_connection(NICKNAME_FILE)
+	for x in results[2:]:
+		if x != 'NULL':
+			x = generate_possible_names(x, c)
+			people.append(x)
+
+	return {'ticker': ticker, 'company_name': company_name, 'alt_company_name': alt_company_name, 'key_people_nicknames': people}
