263dda97f7c63a4b85736b8055194eaf3c5e5944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:15 +0000

    Add package photototext/0.1.2/photototext-0.1.2.tar.gz

---
+++ packages/photototext/photototext-0.1.2.tar.gz/photototext-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="photototext",
+    version="0.1.2",
+    author="peler",
+    author_email="peler_little_pig@outlook.com",
+    description="This is a module that can turn a picture into a pattern of letters and symbols.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/photototext/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
