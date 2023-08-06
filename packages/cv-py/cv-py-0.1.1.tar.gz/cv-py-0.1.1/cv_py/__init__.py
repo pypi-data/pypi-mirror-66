0e081f43b869e4425e5532dfdf49b1d1f22a5f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:20 +0000

    Add package cv-py/0.1.1/cv-py-0.1.1.tar.gz

---
+++ packages/cv-py/cv-py-0.1.1.tar.gz/cv-py-0.1.1/cv_py/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "cv-py"
+# __version__ = get_version() # TODO
+__version__ = "0.1.1"  # placeholder for dynamic version
+__compatible__ = "0.1.1"
+__release__ = False
+__download_url__ = "https://github.com/usnistgov/cord19-cdcs-nist/releases"
