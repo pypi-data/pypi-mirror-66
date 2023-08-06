f3f99dd8f8b38a17dcf8b2e30a465f15103d9ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:09 +0000

    Add package OASYS1-ALS-ShadowOui/0.0.42/OASYS1-ALS-ShadowOui-0.0.42.tar.gz

---
+++ packages/OASYS1-ALS-ShadowOui/OASYS1-ALS-ShadowOui-0.0.42.tar.gz/OASYS1-ALS-ShadowOui-0.0.42/orangecontrib/shadow/als/widgets/sources/__init__.py
@@ -0,0 +1,13 @@
+
+# Category description for the widget registry
+
+NAME = "ALS Sources"
+
+DESCRIPTION = "Widgets for Shadow Sources."
+
+BACKGROUND = "#fffeb3"
+#BACKGROUND = "#FFFF00"
+
+ICON = "icons/source.png"
+
+PRIORITY = 401
---
+++ packages/OASYS1-ALS-ShadowOui/OASYS1-ALS-ShadowOui-0.0.42.tar.gz/OASYS1-ALS-ShadowOui-0.0.42/orangecontrib/wofry/als/widgets/extensions/__init__.py
@@ -0,0 +1,12 @@
+
+# Category description for the widget registry
+
+NAME = "Extensions"
+
+DESCRIPTION = "Wofry ALS Extension"
+
+BACKGROUND = "#E6E6E6"
+
+ICON = "icons/lbl-red.png"
+
+PRIORITY = 15
