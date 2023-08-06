975ae3bf8b25a2d0570d8c891bb4a0928c384562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package altimeter/1.0.35rc3/altimeter-1.0.35rc3.tar.gz

---
+++ packages/altimeter/altimeter-1.0.35rc3.tar.gz/altimeter-1.0.35rc3/altimeter/core/artifact_io/reader.py
@@ -0,0 +1,158 @@
+"""Classes for ArtifactReaders. An ArtifactReader reads a scan artifact dict
+from something - e.g. a file, s3 key, etc."""
+import abc
+import io
+import json
+from typing import Any, Dict, Type
+
+import boto3
+
+from altimeter.core.artifact_io import is_s3_uri, parse_s3_uri
+from altimeter.core.config import Config
+from altimeter.core.graph.graph_set import GraphSet
+from altimeter.core.log import Logger
+from altimeter.core.log_events import LogEvent
+from altimeter.core.rdf import GraphPackage
+
+
+class ArtifactReader(abc.ABC):
+    """ArtifactReaders read JSON artifacts from locations - e.g. s3, filesystem, etc."""
+
+    def read_json(self, path: str) -> Dict[str, Any]:
+        """Read a json artifact
+
+        Args:
+            path: path to artifact to read
+
+        Returns:
+            artifact content
+        """
+
+    def read_graph_pkg(self, path: str) -> GraphPackage:
+        """Read a graph and return a GraphPackage
+
+        Args:
+            path: path to artifact to read
+
+        Returns:
+            GraphPackage object
+        """
+
+    @classmethod
+    def from_config(cls: Type["ArtifactReader"], config: Config) -> "ArtifactReader":
+        """Create an ArtifactReader based on a config. This either returns a FileArtifactReader
+        or an S3ArtifactReader depending on the value of Config.artifact_path"""
+        if is_s3_uri(config.artifact_path):
+            bucket, key_prefix = parse_s3_uri(config.artifact_path)
+            if key_prefix is not None:
+                raise ValueError(
+                    f"S3 artifact path should be s3://<bucket>, no key - got {config.artifact_path}"
+                )
+            return S3ArtifactReader()
+        return FileArtifactReader()
+
+
+class FileArtifactReader(ArtifactReader):
+    """ArtifactReader to read from the filesystem"""
+
+    def read_json(self, path: str) -> Dict[str, Any]:
+        """Read a json artifact
+
+        Args:
+            path: filesystem path to artifact
+
+        Returns:
+            artifact content
+        """
+        logger = Logger()
+        with logger.bind(artifact_path=path):
+            logger.info(event=LogEvent.ReadFromFSStart)
+            with open(path, "r") as artifact_fp:
+                data = json.load(artifact_fp)
+            logger.info(event=LogEvent.ReadFromFSEnd)
+            return data
+
+    def read_graph_pkg(self, path: str) -> GraphPackage:
+        """Read a graph and return a GraphPackage
+
+        Args:
+            path: path to artifact to read
+
+        Returns:
+            GraphPackage object
+        """
+        with open(path, "r") as fp:
+            graph_set_dict = json.load(fp)
+        graph_set = GraphSet.from_dict(data=graph_set_dict)
+        return GraphPackage(
+            graph=graph_set.to_rdf(),
+            name=graph_set.name,
+            version=graph_set.version,
+            start_time=graph_set.start_time,
+            end_time=graph_set.end_time,
+        )
+
+
+class S3ArtifactReader(ArtifactReader):
+    """ArtifactReader to read from S3"""
+
+    def read_json(self, path: str) -> Dict[str, Any]:
+        """Read a json artifact
+
+        Args:
+            path: s3 uri to artifact. s3://bucket/key/path
+
+        Returns:
+            artifact content
+        """
+        bucket, key = parse_s3_uri(path)
+        if key is None:
+            raise ValueError(f"Unable to read from s3 uri missing key: {path}")
+        session = boto3.Session()
+        s3_client = session.client("s3")
+        logger = Logger()
+        with logger.bind(bucket=bucket, key=key):
+            with io.BytesIO() as artifact_bytes_buf:
+                logger.info(event=LogEvent.ReadFromS3Start)
+                s3_client.download_fileobj(bucket, key, artifact_bytes_buf)
+                artifact_bytes_buf.flush()
+                artifact_bytes_buf.seek(0)
+                artifact_bytes = artifact_bytes_buf.read()
+                logger.info(event=LogEvent.ReadFromS3End)
+                artifact_str = artifact_bytes.decode("utf-8")
+                artifact_dict = json.loads(artifact_str)
+                return artifact_dict
+
+    def read_graph_pkg(self, path: str) -> GraphPackage:
+        """Read a graph and return a GraphPackage
+
+        Args:
+            path: s3 uri to artifact. s3://bucket/key/path
+
+        Returns:
+            GraphPackage object
+        """
+        bucket, key = parse_s3_uri(path)
+        if key is None:
+            raise ValueError(f"Unable to read from s3 uri missing key: {path}")
+        session = boto3.Session()
+        s3_client = session.client("s3")
+        logger = Logger()
+        with logger.bind(bucket=bucket, key=key):
+            logger.info(event=LogEvent.ReadFromS3Start)
+            with io.BytesIO() as bytes_buf:
+                s3_client.download_fileobj(bucket, key, bytes_buf)
+                bytes_buf.flush()
+                bytes_buf.seek(0)
+                graph_set_bytes = bytes_buf.read()
+                logger.info(event=LogEvent.ReadFromS3End)
+            graph_set_str = graph_set_bytes.decode("utf-8")
+            graph_set_dict = json.loads(graph_set_str)
+            graph_set = GraphSet.from_dict(graph_set_dict)
+            return GraphPackage(
+                graph=graph_set.to_rdf(),
+                name=graph_set.name,
+                version=graph_set.version,
+                start_time=graph_set.start_time,
+                end_time=graph_set.end_time,
+            )
---
+++ packages/altimeter/altimeter-1.0.35rc3.tar.gz/altimeter-1.0.35rc3/altimeter/core/artifact_io/writer.py
@@ -0,0 +1,213 @@
+"""Classes for ArtifactWriters. An ArtifactWriter writes a scan artifact dict
+to something - e.g. a file, s3 key, etc."""
+import abc
+import io
+import gzip
+import json
+import os
+from pathlib import Path
+from typing import Any, Dict, Optional, Type
+
+import boto3
+
+from altimeter.core.artifact_io import is_s3_uri, parse_s3_uri
+from altimeter.core.config import Config
+from altimeter.core.json_encoder import json_encoder
+from altimeter.core.log import Logger
+from altimeter.core.log_events import LogEvent
+from altimeter.core.rdf import GraphPackage
+
+GZIP = "gz"
+
+
+class ArtifactWriter(abc.ABC):
+    """ArtifactWriters write JSON artifacts to locations - e.g. s3, filesystem, etc."""
+
+    @abc.abstractmethod
+    def write_json(self, name: str, data: Dict[str, Any]) -> str:
+        """Write a json artifact
+
+        Args:
+            name: name
+            data: data
+
+        Returns:
+            path to written artifact
+        """
+
+    @abc.abstractmethod
+    def write_graph(
+        self, name: str, graph_pkg: GraphPackage, compression: Optional[str] = None
+    ) -> str:
+        """Write a graph artifact
+
+        Args:
+            name: name
+            graph_pkg: GraphPackage object to write
+
+        Returns:
+            path to written artifact
+        """
+
+    @classmethod
+    def from_config(cls: Type["ArtifactWriter"], config: Config, scan_id: str) -> "ArtifactWriter":
+        """Create an ArtifactWriter based on a config. This either returns a FileArtifactWriter
+        or an S3ArtifactWriter depending on the value of Config.artifact_path"""
+        if is_s3_uri(config.artifact_path):
+            bucket, key_prefix = parse_s3_uri(config.artifact_path)
+            if key_prefix is not None:
+                raise ValueError(
+                    f"S3 artifact path should be s3://<bucket>, no key - got {config.artifact_path}"
+                )
+            return S3ArtifactWriter(bucket=bucket, key_prefix=scan_id)
+        return FileArtifactWriter(scan_id=scan_id, output_dir=Path(config.artifact_path))
+
+
+class FileArtifactWriter(ArtifactWriter):
+    """ArtifactWriter which writes to a file.
+
+    Args:
+         output_dir: output filesystem dir
+    """
+
+    def __init__(self, scan_id: str, output_dir: Path):
+        self.output_dir = output_dir.joinpath(scan_id)
+
+    def write_json(self, name: str, data: Dict[str, Any]) -> str:
+        """Write artifact data to self.output_dir/name.json
+
+        Args:
+            name: filename
+            data: data
+
+        Returns:
+            Full filesystem path of artifact file
+        """
+        logger = Logger()
+        os.makedirs(self.output_dir, exist_ok=True)
+        artifact_path = os.path.join(self.output_dir, f"{name}.json")
+        with logger.bind(artifact_path=artifact_path):
+            logger.info(event=LogEvent.WriteToFSStart)
+            with open(artifact_path, "w") as artifact_fp:
+                json.dump(data, artifact_fp, default=json_encoder)
+            logger.info(event=LogEvent.WriteToFSEnd)
+        return artifact_path
+
+    def write_graph(
+        self, name: str, graph_pkg: GraphPackage, compression: Optional[str] = None
+    ) -> str:
+        """Write a graph artifact
+
+        Args:
+            name: name
+            graph_pkg: GraphPackage object to write
+
+        Returns:
+            path to written artifact
+        """
+        logger = Logger()
+        os.makedirs(self.output_dir, exist_ok=True)
+        if compression is None:
+            artifact_path = os.path.join(self.output_dir, f"{name}.rdf")
+        elif compression == GZIP:
+            artifact_path = os.path.join(self.output_dir, f"{name}.rdf.gz")
+        else:
+            raise ValueError(f"Unknown compression arg {compression}")
+        with logger.bind(artifact_path=artifact_path):
+            logger.info(event=LogEvent.WriteToFSStart)
+            with open(artifact_path, "wb") as fp:
+                if compression is None:
+                    graph_pkg.graph.serialize(fp)
+                elif compression == GZIP:
+                    with gzip.GzipFile(fileobj=fp, mode="wb") as gz:
+                        graph_pkg.graph.serialize(gz)
+                else:
+                    raise ValueError(f"Unknown compression arg {compression}")
+            logger.info(event=LogEvent.WriteToFSEnd)
+        return artifact_path
+
+
+class S3ArtifactWriter(ArtifactWriter):
+    """ArtifactWriter which writes to S3.
+
+    Args:
+        bucket: s3 bucket
+        key_prefix: s3 key prefix
+    """
+
+    def __init__(self, bucket: str, key_prefix: str):
+        self.bucket = bucket
+        self.key_prefix = key_prefix
+
+    def write_json(self, name: str, data: Dict[str, Any]) -> str:
+        """Write artifact data to s3://self.bucket/self.key_prefix/name.json
+
+        Args:
+            name: s3 key name
+            data: data
+
+        Returns:
+            S3 uri (s3://bucket/key/path) to artifact
+        """
+
+        output_key = "/".join((self.key_prefix, f"{name}.json"))
+        logger = Logger()
+        with logger.bind(bucket=self.bucket, key=output_key):
+            logger.info(event=LogEvent.WriteToS3Start)
+            s3_client = boto3.Session().client("s3")
+            results_str = json.dumps(data, default=json_encoder)
+            results_bytes = results_str.encode("utf-8")
+            with io.BytesIO(results_bytes) as results_bytes_stream:
+                s3_client.upload_fileobj(results_bytes_stream, self.bucket, output_key)
+            logger.info(event=LogEvent.WriteToS3End)
+        return f"s3://{self.bucket}/{output_key}"
+
+    def write_graph(
+        self, name: str, graph_pkg: GraphPackage, compression: Optional[str] = None
+    ) -> str:
+        """Write a graph artifact
+
+        Args:
+            name: name
+            graph_pkg: GraphPackage to write
+
+        Returns:
+            path to written artifact
+        """
+        logger = Logger()
+        if compression is None:
+            key = f"{name}.rdf"
+        elif compression == GZIP:
+            key = f"{name}.rdf.gz"
+        else:
+            raise ValueError(f"Unknown compression arg {compression}")
+        output_key = "/".join((self.key_prefix, key))
+        with logger.bind(bucket=self.bucket, key_prefix=self.key_prefix, key=key):
+            logger.info(event=LogEvent.WriteToS3Start)
+            with io.BytesIO() as rdf_bytes_buf:
+                if compression is None:
+                    graph_pkg.graph.serialize(rdf_bytes_buf)
+                elif compression == GZIP:
+                    with gzip.GzipFile(fileobj=rdf_bytes_buf, mode="wb") as gz:
+                        graph_pkg.graph.serialize(gz)
+                else:
+                    raise ValueError(f"Unknown compression arg {compression}")
+                rdf_bytes_buf.flush()
+                rdf_bytes_buf.seek(0)
+                session = boto3.Session()
+                s3_client = session.client("s3")
+                s3_client.upload_fileobj(rdf_bytes_buf, self.bucket, output_key)
+            s3_client.put_object_tagging(
+                Bucket=self.bucket,
+                Key=output_key,
+                Tagging={
+                    "TagSet": [
+                        {"Key": "name", "Value": graph_pkg.name},
+                        {"Key": "version", "Value": graph_pkg.version},
+                        {"Key": "start_time", "Value": str(graph_pkg.start_time)},
+                        {"Key": "end_time", "Value": str(graph_pkg.end_time)},
+                    ]
+                },
+            )
+            logger.info(event=LogEvent.WriteToS3End)
+        return f"s3://{self.bucket}/{output_key}"
