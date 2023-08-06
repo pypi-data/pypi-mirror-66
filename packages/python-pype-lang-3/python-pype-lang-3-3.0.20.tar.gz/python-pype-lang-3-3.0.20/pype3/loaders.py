967fb95f5929af035fa5e5040bd4d2b506f6dca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:54 +0000

    Add package python-pype-lang-3/3.0.20/python-pype-lang-3-3.0.20.tar.gz

---
+++ packages/python-pype-lang-3/python-pype-lang-3-3.0.20.tar.gz/python-pype-lang-3-3.0.20/pype3/loaders.py
@@ -0,0 +1,70 @@
+import json
+import csv
+import os
+import chardet
+import pickle
+import numpy as np
+
+def dump_json(fileName,js):
+
+    with open(fileName,'w') as f:
+
+        f.write(json.dumps(js))
+
+
+def load_list_file(fileName):
+
+    with open(fileName,'r') as f:
+
+        return [ln for ln in f.read().splitlines()]
+
+
+def load_json(fileName):
+
+    with open(fileName,'r') as f:
+
+        js=json.loads(f.read())
+
+        return js
+
+
+def load_bytes(fileName):
+
+    with open(fileName,'rb') as f:
+
+        text=f.read()
+        encoding=chardet.detect(text)['encoding']
+        text=text.decode(encoding)
+
+        return text
+
+
+def load_pickle(fileName):
+
+    with open(fileName,'rb') as f:
+
+        return pickle.load(f)
+
+
+def os_listdir(dirName):
+
+    return os.listdir(dirName)
+
+
+def os_basename(pth):
+
+    return os.path.basename(pth)
+
+
+def os_join(txt1,txt2):
+
+    return os.path.join(txt1,txt2)
+
+
+def load_numpy(fileName):
+
+    return np.load(fileName)
+
+# def load_csv(fileName,delimiter=','):
+
+    
