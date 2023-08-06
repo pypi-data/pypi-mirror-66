0f90a592b8abdcb1878793ed79423f946d27a83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:02 +0000

    Add package doc-curation/0.0.2/doc_curation-0.0.2-py3-none-any.whl

---
+++ packages/doc-curation/doc_curation-0.0.2-py3-none-any.whl/curation_projects/pdf_tasks.py
@@ -0,0 +1,4 @@
+from doc_curation import pdf
+
+if __name__ == '__main__':
+    pdf.ocr(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/purANam/rAmacharitamAnasa/tulsi_shabda_kosh_part_01.pdf")
