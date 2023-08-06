c4036c79bc18165ff5a01a053c3bcdb72820db85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:27 +0000

    Add package broccoli-server/1.2.1/broccoli_server-1.2.1.tar.gz

---
+++ packages/broccoli-server/broccoli_server-1.2.1.tar.gz/broccoli_server-1.2.1/broccoli_server/web/precache-manifest.40677f10b20c1a42e6629f45244fe2bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d961fdfabbe512d2675de09af09f598b",
+    "url": "/web/static/media/jsoneditor-icons.d961fdfa.svg"
+  },
+  {
+    "revision": "440ed357d0a8ea3f003cf063fe1ed565",
+    "url": "/web/static/media/icon.440ed357.png"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "30ff8729b29b4bb474f0",
+    "url": "/web/static/js/main.30ff8729.chunk.js"
+  },
+  {
+    "revision": "badc23afbe59c37dfa45",
+    "url": "/web/static/js/1.badc23af.chunk.js"
+  },
+  {
+    "revision": "badc23afbe59c37dfa45",
+    "url": "/web/static/css/1.fa23132f.chunk.css"
+  },
+  {
+    "revision": "8b3882d5b22647b4b7556252f79134bd",
+    "url": "/web/index.html"
+  }
+];
