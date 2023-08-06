ca2c17028fce93cbf74e57b8912927b24945b7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:29 +0000

    Add package pepy/1.1.0rc1/pepy-1.1.0rc1.tar.gz

---
+++ packages/pepy/pepy-1.1.0rc1.tar.gz/pepy-1.1.0rc1/pe-parser-library/include/parser-library/to_string.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+
+#if defined(USE_ICU4C)
+#include <unicode/unistr.h>
+typedef std::basic_string<UChar> UCharString;
+#elif defined(USE_STRINGAPISET)
+typedef std::basic_string<wchar_t> UCharString;
+#else
+typedef std::u16string UCharString;
+#endif
+
+namespace peparse {
+template <class T>
+static std::string to_string(T t, std::ios_base &(*f)(std::ios_base &) ) {
+  std::ostringstream oss;
+  oss << f << t;
+  return oss.str();
+}
+
+std::string from_utf16(const UCharString &u);
+} // namespace peparse
