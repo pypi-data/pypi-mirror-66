a94f0589d397490d65c707073c5cfea23ac1567a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:27 +0000

    Add package unextexams/0.3.1.4/unextexams-0.3.1.4.tar.gz

---
+++ packages/unextexams/unextexams-0.3.1.4.tar.gz/unextexams-0.3.1.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setuptools.setup(
+  name = 'unextexams',
+  install_requires=REQUIREMENTS,
+  version = '0.3.1.4',
+  author = 'Isaac Zainea',
+  author_email = 'isaaczainea@gmail.com',  
+  description = 'paquete de examenes - Universidad Externado',
+  url = 'https://github.com/MCG-Externado/unextexams', # use the URL to the github repo
+  keywords = ['testing', 'exams', 'Externado'],
+  packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+)
