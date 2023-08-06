55dce3aae15bcb33c2d8333ac6ef0bf6ec10f536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:42 +0000

    Add package stoyled/0.5/stoyled-0.5-py3.7.egg

---
+++ packages/stoyled/stoyled-0.5-py3.7.egg/stoyled/__init__.py
@@ -0,0 +1,15 @@
+from .nGenStyls import info, \
+                       good, \
+                       warn, \
+                       bad, \
+                       coolInput, \
+                       takenInput, \
+                       coolExit, \
+                       fetchFormatedTime
+
+
+from .nGenStyls import *
+
+
+__version__ = "0.5"
+__author__ = "naryal2580"
