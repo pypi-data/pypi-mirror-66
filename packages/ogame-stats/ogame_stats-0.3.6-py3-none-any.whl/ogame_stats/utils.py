5ac48c98609e5a9a06ce3d85bdf46650d0253a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:41 +0000

    Add package ogame-stats/0.3.6/ogame_stats-0.3.6-py3-none-any.whl

---
+++ packages/ogame-stats/ogame_stats-0.3.6-py3-none-any.whl/ogame_stats/utils.py
@@ -0,0 +1,44 @@
+from datetime import datetime as dt
+import xml.etree.ElementTree as ET
+import pandas as pd
+import requests
+import xmltodict
+
+
+class ApiBaseClass:
+    def _do_get(self, url: str) -> requests.Response:
+        return requests.get(url)
+
+    def _load_data(self, url: str) -> [{str: str}]:
+        response = self._do_get(url)
+        xml_string = response.content.decode("utf-8")
+        root = ET.fromstring(xml_string)
+        return [elm.attrib for elm in root]
+
+    def _load_kv_style_data(self, url: str) -> {str: str}:
+        response = self._do_get(url)
+        xml_string = response.content.decode("utf-8")
+        root = ET.fromstring(xml_string)
+        return {elm.tag: elm.text for elm in list(root)}
+
+    def _load_data_via_xmltodict(self, url: str) -> {}:
+        response = self._do_get(url)
+        xml_string = response.content.decode("utf-8")
+        return xmltodict.parse(xml_string, attr_prefix="", dict_constructor=dict)
+
+    def _load_nested_data(self, url: str) -> {str: {str: str}}:
+        response = self._do_get(url)
+        xml_string = response.content.decode("utf-8")
+        root = ET.fromstring(xml_string)
+        return {
+            elm.tag: {child.attrib["id"]: child.text for child in list(elm)} for elm in list(root)
+        }
+
+    def _load_data_as_df(self, url: str) -> pd.DataFrame:
+        data = self._load_data(url)
+        return pd.DataFrame(data)
+
+
+def nowstr() -> str:
+    """evaluates to valid file and folder name(-component)"""
+    return dt.now().strftime("%Y%m%d_%H%M%S_%f")
