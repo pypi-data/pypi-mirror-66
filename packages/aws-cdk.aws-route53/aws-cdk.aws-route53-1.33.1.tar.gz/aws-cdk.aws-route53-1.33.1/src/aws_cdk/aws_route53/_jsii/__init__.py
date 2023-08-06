6341c83e4421ece8759cbd6a33ceca62f5294e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:21 +0000

    Add package aws-cdk.aws-route53/1.33.1/aws-cdk.aws-route53-1.33.1.tar.gz

---
+++ packages/aws-cdk.aws-route53/aws-cdk.aws-route53-1.33.1.tar.gz/aws-cdk.aws-route53-1.33.1/src/aws_cdk/aws_route53/_jsii/__init__.py
@@ -0,0 +1,18 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_ec2
+import aws_cdk.aws_logs
+import aws_cdk.core
+import aws_cdk.cx_api
+import constructs
+__all__ = []
+
+publication.publish()
