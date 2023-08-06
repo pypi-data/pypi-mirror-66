ab8a8d31efb325dbd8e6e401519f9bb9b20b2787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:44 +0000

    Add package starlette-graphql/0.0.5/starlette-graphql-0.0.5.tar.gz

---
+++ packages/starlette-graphql/starlette-graphql-0.0.5.tar.gz/starlette-graphql-0.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['stargql']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-gql<0.1', 'starlette>=0.13.3,<0.14.0']
+
+setup_kwargs = {
+    'name': 'starlette-graphql',
+    'version': '0.0.5',
+    'description': 'Starlette graphql extension.',
+    'long_description': None,
+    'author': 'syfun',
+    'author_email': 'sunyu418@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
