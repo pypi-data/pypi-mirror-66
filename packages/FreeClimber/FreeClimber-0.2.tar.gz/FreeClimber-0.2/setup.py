3799ae8d4eb23f729d650d9d500a11bdf167b543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:00 +0000

    Add package FreeClimber/0.2/FreeClimber-0.2.tar.gz

---
+++ packages/FreeClimber/FreeClimber-0.2.tar.gz/FreeClimber-0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Get the long description from the relevant file
+with open('README.md','r') as f:
+    long_description = f.read()
+
+setup(name='FreeClimber',
+      version='0.2',
+      description='A background-adjusted, particle detector used to quantify climbing performance for groups of Drosophila in a negative geotaxis assay',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/adamspierer/FreeClimber',
+      
+      author='Adam Spierer',
+      author_email='anspierer+Github_setup_py@gmail.com',
+      license='MIT',
+
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.7'],
+
+      keywords='Drosophila climbing negative geotaxis background subtraction particle detection local linear regression',
+
+      packages=find_packages(),
+#       packages=['FreeClimber'],
+      install_requires=['ffmpeg==1.4',"argparse==1.1"
+                        'pandas','numpy','scipy','pip','matplotlib==3.1.3',
+                        'wxPython==4.0.4','trackpy==0.4.2'],
+      zip_safe=False)
