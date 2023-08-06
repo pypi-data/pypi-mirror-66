7263414854b14c6ef761446ae3c90a5b14dc7953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:54 +0000

    Add package doc-curation/0.0.9/doc_curation-0.0.9-py3-none-any.whl

---
+++ packages/doc-curation/doc_curation-0.0.9-py3-none-any.whl/curation_projects/pdf_tasks.py
@@ -0,0 +1,14 @@
+import logging
+
+from doc_curation import pdf
+
+# Remove all handlers associated with the root logger object.
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s")
+
+
+if __name__ == '__main__':
+    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/test/k.pdf", small_pdf_pages=15)
