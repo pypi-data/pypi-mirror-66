d7ea234c1ecce07e3ecdb8c195b28a21eedeca9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:30 +0000

    Add package dropkick/1.1.0/dropkick-1.1.0-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/dropkick/dropkick-1.1.0-cp37-cp37m-macosx_10_14_x86_64.whl/dropkick/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+package initialization
+
+@author: C Heiser
+"""
+from .api import recipe_dropkick, dropkick, plot_thresh_obs, coef_inventory, coef_plot, score_plot
+
+__all__ = [
+    "dropkick",
+    "recipe_dropkick",
+    "plot_thresh_obs",
+    "coef_inventory",
+    "coef_plot",
+    "score_plot",
+]
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
