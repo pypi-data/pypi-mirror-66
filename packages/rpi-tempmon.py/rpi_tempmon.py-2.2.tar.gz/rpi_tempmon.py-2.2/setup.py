0502f8ad64cc923e3ecdc6ccb5d9aa1f5fcfc5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:13 +0000

    Add package rpi-tempmon.py/2.2/rpi_tempmon.py-2.2.tar.gz

---
+++ packages/rpi-tempmon.py/rpi_tempmon.py-2.2.tar.gz/rpi_tempmon.py-2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="rpi_tempmon.py",
+    version="2.2",
+    author="gavin lyons",
+    author_email="glyons66@hotmail.com",
+    description="Display the CPU and GPU temperature of Raspberry Pi",
+    license=" GPL",
+    keywords="PI Raspberry CPU ARM GPU temperature temp rpi monitor display gavin lyons",
+    url="https://github.com/gavinlyonsrepo/raspeberrypi_tempmon",
+    download_url='https://github.com/gavinlyonsrepo/raspeberrypi_tempmon/archive/2.2.tar.gz',
+    packages=['rpiTempSrc','rpiTempMod',],
+    install_requires= ['matplotlib','pip','psutil'],
+    setup_requires = ['pip'],
+    scripts=['rpiTempSrc/rpi_tempmon.py'],
+    classifiers=[
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
