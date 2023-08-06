a066dc9483955f1598fb942720a8f93c78581ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:15 +0000

    Add package ppfun/1.0.2/ppfun-1.0.2.tar.gz

---
+++ packages/ppfun/ppfun-1.0.2.tar.gz/ppfun-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ppfun",
+    version="1.0.2",
+    author="portasynthinca3",
+    author_email="portasynthinca3@gmail.com",
+    description="Allows you to use the PixelPlanet.Fun API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/portasynthinca3/ppfun",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Freeware",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+    ],
+    python_requires='>=3.6',
+)
