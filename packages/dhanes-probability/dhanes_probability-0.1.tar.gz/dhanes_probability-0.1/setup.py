9a2319782f3ae74da40ffa57fe40488a84585b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:41 +0000

    Add package dhanes-probability/0.1/dhanes_probability-0.1.tar.gz

---
+++ packages/dhanes-probability/dhanes_probability-0.1.tar.gz/dhanes_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dhanes_probability',
+      version='0.1',
+      description='dhanes_probability',
+      packages=['dhanes_probability'],
+      author = 'Dhaneswara Mandrasa',
+      zip_safe=False)
