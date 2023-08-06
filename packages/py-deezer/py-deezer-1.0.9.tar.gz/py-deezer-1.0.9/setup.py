d3bdd9fb07f58fa83cd6f6d57bf7f8182ed23990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:06 +0000

    Add package py-deezer/1.0.9/py-deezer-1.0.9.tar.gz

---
+++ packages/py-deezer/py-deezer-1.0.9.tar.gz/py-deezer-1.0.9/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="py-deezer",
+    version="1.0.9",
+    author="Aaron Gonzales",
+    author_email="aaroncgonzales.dev@gmail.com",
+    description="A package to search and download musics on Deezer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chr1st-oo/pydeezer",
+    packages=setuptools.find_packages(),
+    py_modules=["cli"],
+    install_requires=[
+        "requests",
+        "cryptography",
+        "mutagen",
+        "tqdm",
+        "click",
+        "pyinquirer"
+    ],
+    license="GNU GPL v3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    entry_points="""
+        [console_scripts]
+        pydeezer=cli:cli
+    """
+)
