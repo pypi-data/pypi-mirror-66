b1012bf06c6702ac465bc92c077a74a1eb4c841e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:26 +0000

    Add package sanya-script-runtime/0.1.5/sanya_script_runtime-0.1.5.tar.gz

---
+++ packages/sanya-script-runtime/sanya_script_runtime-0.1.5.tar.gz/sanya_script_runtime-0.1.5/sanya_script_runtime/__init__.py
@@ -0,0 +1,9 @@
+from sanya_script_runtime.builtins import scan, put, puts, source, target, weight, value, arcs, nodes
+from sanya_script_runtime.runtime_error import RuntimeError
+from sanya_script_runtime.node import Node
+from sanya_script_runtime.arc import Arc
+from sanya_script_runtime.graph import Graph
+from sanya_script_runtime.num import Num
+from sanya_script_runtime.logic import Logic
+from sanya_script_runtime.nope import Nope
+from sanya_script_runtime.type import Type
---
+++ packages/sanya-script-runtime/sanya_script_runtime-0.1.5.tar.gz/sanya_script_runtime-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'sanya_script_runtime',
+  packages = ['sanya_script_runtime'],
+  version = '0.1.5',
+  license='MIT',
+  description = 'Runtime library for SanyaScript',
+  author = 'Alexander Teslovskiy',
+  author_email = 'artoriousso@gmail.com',
+  url = 'https://github.com/ARtoriouSs/sanya-script-runtime',
+  download_url = 'https://github.com/ARtoriouSs/sanya-script-runtime/archive/0.1.5.tar.gz',
+  keywords = ['SanyaScript', 'Runtime','ANTLR', 'Graph language'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
