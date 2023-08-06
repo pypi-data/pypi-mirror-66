3485b7bc2ef616a900a23ae901ab3c3d0765bdfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package vtypes/0.5.1/vtypes-0.5.1-py2.py3-none-any.whl

---
+++ packages/vtypes/vtypes-0.5.1-py2.py3-none-any.whl/vtypes/__init__.py
@@ -0,0 +1,10 @@
+#  Authors: Sylvain Marie <sylvain.marie@se.com>
+#
+#  Copyright (c) Schneider Electric Industries, 2020. All right reserved.
+
+from vtypes.core import vtype, is_vtype, VType
+
+__all__ = [
+    'core',
+    'vtype', 'is_vtype', 'VType'
+]
