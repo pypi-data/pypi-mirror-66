e44ad174eb1bdcf23924b80191b15da1e405d53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:51 +0000

    Add package pytest-mproc/3.0.0/pytest_mproc-3.0.0.tar.gz

---
+++ packages/pytest-mproc/pytest_mproc-3.0.0.tar.gz/pytest_mproc-3.0.0/test/test_mproc_runs.py
@@ -0,0 +1,47 @@
+import os
+import pytest_mproc
+import sys
+# to pick up dummy_src (ensure in path):
+sys.path.append(os.path.basename(__file__))
+sys.path.append(os.path.join(os.path.basename(__file__), "..", "testcode"))
+
+import pytest
+
+from testcode.dummy_src.something import to_be_run_under_test
+from testcode.testsomething import Something
+
+@pytest.fixture()
+def some_fixture():
+    return 1
+
+@pytest_mproc.group("group")
+def test_group_m2():
+    Something().group_m1()
+    assert Something.proc_id == os.getpid()
+
+
+@pytest_mproc.group("class_group")
+class TestGrouped:
+
+    proc_id = None
+
+    def test1(self):
+        TestGrouped.proc_id = os.getpid()
+
+    def test2(self):
+        assert TestGrouped.proc_id == os.getpid()
+
+
+def test_some_alg1(global_fix):
+    assert global_fix == 42
+    Something().some_alg1()
+
+
+@pytest.mark.parametrize('data', ['a%s' % i for i in range(1000)])
+def test_some_alg2(data, some_fixture):
+    Something().some_alg2(data)
+
+
+def test_some_alg3():
+    Something().some_alg3()
+
