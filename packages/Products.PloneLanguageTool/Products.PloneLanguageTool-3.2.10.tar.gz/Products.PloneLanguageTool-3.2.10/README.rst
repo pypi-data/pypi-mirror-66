908f087950d969f04f4f897acdabb487396f5a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:36 +0000

    Add package Products.PloneLanguageTool/3.2.10/Products.PloneLanguageTool-3.2.10.tar.gz

---
+++ packages/Products.PloneLanguageTool/Products.PloneLanguageTool-3.2.10.tar.gz/Products.PloneLanguageTool-3.2.10/README.rst
@@ -0,0 +1,28 @@
+Overview
+========
+
+PloneLanguageTool allows you to set the available languages in your Plone
+site, select various fallback mechanisms, and control the use of flags
+for language selection and translations.
+
+Credits
+-------
+
+PloneLanguageTool was built by:
+
+- Jodok Batlogg, "Solution2U":http://solution2u.net
+
+- Simon Eisenmann, "Struktur AG":http://www.strukturag.com
+
+- Geir Baekholt, "Jarn AS":http://www.jarn.com
+
+- Alexander Limi, "Jarn AS":http://www.jarn.com
+
+- Helge Tesdal, "Jarn AS":http://www.jarn.com
+
+- Dorneles Tremea, "Jarn AS":http://www.jarn.com
+
+- Hanno Schlichting, "Jarn AS":http://www.jarn.com
+
+Parts of development sponsored by Learning Lab Denmark, donated by
+Plone Solutions.
---
+++ packages/Products.PloneLanguageTool/Products.PloneLanguageTool-3.2.10.tar.gz/Products.PloneLanguageTool-3.2.10/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '3.2.10'
+
+tests_require = ['Products.CMFTestCase', ]
+
+setup(
+    name='Products.PloneLanguageTool',
+    version=version,
+    description="PloneLanguageTool allows you to set the available "
+                "languages in your Plone site, select various fallback "
+                "mechanisms, and control the use of flags for language "
+                "selection and translations.",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Zope CMF Plone i18n l10n flags',
+    author='Hanno Schlichting',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.org/project/Products.PloneLanguageTool',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      'setuptools',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(test=tests_require),
+)
