af19b354295c91ef9f0fb36a51caa77ddac301ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:48 +0000

    Add package legitindicators/0.0.19/legitindicators-0.0.19-py3-none-any.whl

---
+++ packages/legitindicators/legitindicators-0.0.19-py3-none-any.whl/tests/test_roofingfilter.py
@@ -0,0 +1,15 @@
+import requests
+from ..legitindicators import roofing_filter
+
+BINANCE_URL = "https://api.binance.com/api/v3/klines"
+SYMBOL = "BTCUSDT"
+INTERVAL = "1h"
+PARAMS = {"symbol":SYMBOL, "interval":INTERVAL}
+
+def test_roofingfilter():
+    response = requests.get(url=BINANCE_URL, params=PARAMS)
+    data = response.json()
+    close = [float(d[4]) for d in data]
+    r_f = roofing_filter(close, 45, 30)
+    print(r_f)
+    assert len(r_f) == len(close)
