9577665acb3ea3171e873b21832f47b59260b539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:16 +0000

    Add package pypareto/0.2.2/pypareto-0.2.2-py3-none-any.whl

---
+++ packages/pypareto/pypareto-0.2.2-py3-none-any.whl/pypareto/__init__.py
@@ -0,0 +1,39 @@
+from .pypareto import (
+    Cmp,
+    Comparison,
+    ComparisonChain,
+    Domination,
+    MaxMin,
+    MaxMinList,
+    by_none,
+    by_value,
+    cmp_to_target,
+    dominates,
+    find_dimension_maxmin_set,
+    get_dominating_set,
+    split_by_dimensions,
+    GroupNones,
+    split_by_pareto)
+
+__doc__ = """
+# pypareto
+Pypareto is a Python library for pareto front seaching.
+## Usage
+""" + ComparisonChain.split_by_pareto.__doc__
+
+__all__ = [
+    'Cmp',
+    'Comparison',
+    'GroupNones',
+    'ComparisonChain',
+    'Domination',
+    'MaxMin',
+    'MaxMinList',
+    'by_none',
+    'by_value',
+    'cmp_to_target',
+    'dominates',
+    'find_dimension_maxmin_set',
+    'get_dominating_set',
+    'split_by_dimensions',
+    'split_by_pareto']
