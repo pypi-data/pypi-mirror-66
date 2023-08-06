5a9887681408d314f468527392bed349c9df52a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:58 +0000

    Add package hg-convert-prcs-extension/2.0.0/hg-convert-prcs-extension-2.0.0.tar.gz

---
+++ packages/hg-convert-prcs-extension/hg-convert-prcs-extension-2.0.0.tar.gz/hg-convert-prcs-extension-2.0.0/README.md
@@ -0,0 +1,28 @@
+# Introduction
+
+The [`convert_prcs` extension][hg-convert-prcs-extension] for [Mercurial][]
+enables the [PRCS][] source in the Mercurial [convert extension][].
+
+[hg-convert-prcs-extension]: https://pypi.org/project/hg-convert-prcs-extension/
+[PRCS]: http://prcs.sourceforge.net/
+[Mercurial]: https://www.mercurial-scm.org/
+[convert extension]: https://www.mercurial-scm.org/wiki/ConvertExtension
+
+[![(Issues)](https://img.shields.io/bitbucket/issues/kazssym/hg-convert-prcs-extension.svg)][open issues]
+
+[open issues]: https://bitbucket.org/kazssym/hg-convert-prcs-extension/issues?status=new&status=open
+
+# License
+
+This program is provided under the terms and conditions of the
+[GNU General Public License, version 3][GPL-3.0] or any later version.
+
+[![(License)](https://img.shields.io/badge/license-GPL--3.0--or--later-blue.svg)][GPL-3.0]
+
+[GPL-3.0]: https://opensource.org/licenses/GPL-3.0
+
+# Required software
+
+This extension requires the following software:
+
+  - [prcslib](https://pypi.org/project/prcslib/) 2.0 or later.
---
+++ packages/hg-convert-prcs-extension/hg-convert-prcs-extension-2.0.0.tar.gz/hg-convert-prcs-extension-2.0.0/setup.py
@@ -0,0 +1,76 @@
+# setup.py - setup script for the 'hg-convert-prcs-extension' package
+# Copyright (C) 2019-2020 Kaz Nishimura
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
+setup script for the 'hg-convert-prcs-extension' package
+"""
+
+from __future__ import absolute_import
+
+from os import path
+from setuptools import setup
+
+# Package name.
+PACKAGE_NAME = "hg-convert-prcs-extension"
+
+# Package version.
+PACKAGE_VERSION = "2.0.0"
+
+def long_description():
+    """
+    return the long description from the 'README.md' file
+    """
+    cwd = path.abspath(path.dirname(__file__))
+    with open(path.join(cwd, "README.md")) as stream:
+        # To ignore lines until a level-1 ATX header is found.
+        while True:
+            line = stream.readline()
+            if line.startswith("# "):
+                break
+        return line + stream.read()
+
+if __name__ == "__main__":
+    setup(
+        name=PACKAGE_NAME,
+        version=PACKAGE_VERSION,
+        description="PRCS source for the Mercurial convert extension.",
+        url="https://vx68k.bitbucket.io/hg-convert-prcs/",
+        project_urls={
+            "Source": "https://github.com/vx68k/hg-convert-prcs-extension",
+        },
+        author="Kaz Nishimura",
+        author_email="kazssym@linuxfront.com",
+        long_description=long_description(),
+        long_description_content_type="text/markdown",
+        classifiers=[
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: Software Development :: Version Control",
+        ],
+        python_requires=">=2.7",
+        install_requires=[
+            "mercurial>=4.7",
+            "prcslib>=4.1"
+        ],
+        zip_safe=True,
+
+        packages=[
+            "hgext3rd.convert_prcs"
+        ],
+    )
