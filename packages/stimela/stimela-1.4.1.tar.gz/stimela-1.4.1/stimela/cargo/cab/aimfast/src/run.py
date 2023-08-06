ef229a4e0e435b5f476fa78f7e7b448ae2875595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:25 +0000

    Add package stimela/1.4.1/stimela-1.4.1.tar.gz

---
+++ packages/stimela/stimela-1.4.1.tar.gz/stimela-1.4.1/stimela/cargo/cab/aimfast/src/run.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import shlex
+import shutil
+import subprocess
+import yaml
+import glob
+
+
+CONFIG = os.environ["CONFIG"]
+INPUT = os.environ["INPUT"]
+OUTPUT = os.environ["OUTPUT"]
+MSDIR = os.environ["MSDIR"]
+
+with open(CONFIG, "r") as _std:
+    cab = yaml.safe_load(_std)
+
+junk = cab["junk"]
+args = []
+
+for param in cab['parameters']:
+    name = param['name']
+    value = param['value']
+    if value is None:
+        continue
+    elif value is False:
+        continue
+
+    if type(value) is list:
+        args += ['{0}{1} {2}'.format(cab['prefix'], name,
+                                     " ".join([val.replace(':output', '') for val in value]))]
+    else:
+        args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
+
+_runc = " ".join([cab["binary"]]+ args)
+
+try:
+    subprocess.check_call(shlex.split(_runc))
+finally:
+    for item in junk:
+        for dest in [OUTPUT, MSDIR]: # these are the only writable volumes in the container
+            items = glob.glob("{dest}/{item}".format(**locals()))
+            for f in items:
+                if os.path.isfile(f):
+                    os.remove(f)
+                elif os.path.isdir(f):
+                    shutil.rmtree(f)
