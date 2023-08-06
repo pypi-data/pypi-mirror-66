61e55af71d42325340084303a126f54d79ef685c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:25 +0000

    Add package finfun/0.0.3/finfun-0.0.3.tar.gz

---
+++ packages/finfun/finfun-0.0.3.tar.gz/finfun-0.0.3/finfun/Interest.py
@@ -0,0 +1,9 @@
+class Interest:
+
+    @staticmethod
+    def compound(value, interest, periods):
+        if interest > 1:
+            compound_factor = 1 + interest / 100
+        else:
+            compound_factor = 1 + interest
+        return value * compound_factor**periods
---
+++ packages/finfun/finfun-0.0.3.tar.gz/finfun-0.0.3/finfun/__init__.py
@@ -0,0 +1,1 @@
+from finfun.Interest import Interest
