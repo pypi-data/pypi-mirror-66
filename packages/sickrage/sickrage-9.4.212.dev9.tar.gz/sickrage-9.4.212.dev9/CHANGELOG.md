6992398be839782a379e50070b9fb724cf1aad75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:54 +0000

    Add package sickrage/9.4.212.dev9/sickrage-9.4.212.dev9.tar.gz

---
+++ packages/sickrage/sickrage-9.4.212.dev9.tar.gz/sickrage-9.4.212.dev9/CHANGELOG.md
@@ -0,0 +1,16 @@
+<img width="300px" src="https://sickrage.ca/img/logo-stacked.png" />
+
+# 
+
+##Sun Apr 26 2020 01:27:58 GMT+0000 (Coordinated Universal Time)
+
+
+## Refactor
+  - Refactored how scheduled jobs are forced to run
+  ([fbcb4b0a](https://gitlab-ci-token:m7mS5ky9jRUQrzLp6AyP@git.sickrage.ca/SiCKRAGE/sickrage/commit/fbcb4b0a8ad145d4b409a964dc240154f93fdd69))
+  - Refactored usage of IOLoop by removing const io_loop from core and using IOLoop.current() calls instead
+  ([60463343](https://gitlab-ci-token:m7mS5ky9jRUQrzLp6AyP@git.sickrage.ca/SiCKRAGE/sickrage/commit/6046334370568652a611ef7b62cbfb8820b632b3))
+
+
+
+
---
+++ packages/sickrage/sickrage-9.4.212.dev9.tar.gz/sickrage-9.4.212.dev9/sickrage/core/updaters/rsscache_updater.py
@@ -0,0 +1,35 @@
+import functools
+import threading
+
+from tornado.ioloop import IOLoop
+
+import sickrage
+
+
+class RSSCacheUpdater(object):
+    def __init__(self):
+        super(RSSCacheUpdater, self).__init__()
+        self.name = "RSSCACHE-UPDATER"
+        self.lock = threading.Lock()
+        self.amActive = False
+
+    async def task(self, force=False):
+        if self.amActive or not sickrage.app.config.enable_rss_cache and not force:
+            return
+
+        self.amActive = True
+
+        # set thread name
+        threading.currentThread().setName(self.name)
+
+        for providerID, providerObj in sickrage.app.search_providers.sort().items():
+            if providerObj.is_enabled:
+                await IOLoop.current().run_in_executor(None, functools.partial(self.worker, providerObj, force))
+
+        self.amActive = False
+
+    def worker(self, provider, force):
+        threading.currentThread().setName('{}::{}'.format(self.name, provider.name.upper()))
+        sickrage.app.log.debug("Updating RSS cache")
+        provider.cache.update(force)
+        sickrage.app.log.debug("Updated RSS cache")
