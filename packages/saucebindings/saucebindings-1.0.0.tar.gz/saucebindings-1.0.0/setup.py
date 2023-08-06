d6c5fb4c3d6221b206c31b363d1b9222c0e2b290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:37 +0000

    Add package saucebindings/1.0.0/saucebindings-1.0.0.tar.gz

---
+++ packages/saucebindings/saucebindings-1.0.0.tar.gz/saucebindings-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+with open('./README.rst', 'r') as f:
+  readme = f.read()
+
+setup(
+  name = 'saucebindings',
+  packages = ['saucebindings'],
+  version = '1.0.0',
+  license='MIT',
+  description = 'Sauce Labs Python bindings library',
+  long_description=readme,
+  long_description_content_type="text/x-rst",
+  author = 'josh.grant',
+  author_email = 'josh.grant@saucelabs.com',
+  url = 'https://github.com/saucelabs/sauce_bindings/',
+  download_url = 'https://github.com/saucelabs/sauce_bindings/archive/v_100.tar.gz', # based on GitHub releases
+  keywords = ['Sauce Labs', 'testing', 'selenium'],
+  install_requires=[
+          'selenium',
+          'pytest'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Testing',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
