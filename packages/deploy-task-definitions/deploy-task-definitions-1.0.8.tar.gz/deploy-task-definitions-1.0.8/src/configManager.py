58f011b74f4cde67e4600a08b00fff57c2edbc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:25 +0000

    Add package deploy-task-definitions/1.0.8/deploy-task-definitions-1.0.8.tar.gz

---
+++ packages/deploy-task-definitions/deploy-task-definitions-1.0.8.tar.gz/deploy-task-definitions-1.0.8/src/configManager.py
@@ -0,0 +1,17 @@
+import os,sys
+from dotenv.main import dotenv_values
+
+
+def load_configs(path):
+    configs = []
+    try:
+        if os.path.isfile(path):
+            return [dotenv_values(dotenv_path=path)]
+        configFiles = os.listdir(path)
+        for file in configFiles:
+            file_path = ('{}/{}'.format(path,file))
+            configs.append(dotenv_values(dotenv_path=file_path))
+    except FileNotFoundError:
+        sys.exit('Invalid path: {}'.format(path))
+
+    return configs
