c6e80fdc3328efb2e097daf59464b60fd2b340b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:01 +0000

    Add package zelenium/1.0.2/zelenium-1.0.2.tar.gz

---
+++ packages/zelenium/zelenium-1.0.2.tar.gz/zelenium-1.0.2/zelenium/base/page.py
@@ -0,0 +1,23 @@
+from zelenium.base import Base
+from zelenium.base import BaseElement
+
+
+class MetaPage(type):
+    def __new__(cls, name, bases, attrs):
+        new_attrs = {}
+        suffix = attrs.get("_{}__suffix".format(name), "")
+        for base in bases:
+            for name, val in base.__dict__.items():
+                if isinstance(val, BaseElement):
+                    attrs[name] = (val.by, val.value)
+        for name, value in attrs.items():
+            if isinstance(value, tuple) and len(value) == 2:
+                by, selector = value
+                value = BaseElement(by, selector)
+                value.set_suffix(suffix)
+            new_attrs[name] = value
+        return super().__new__(cls, name, bases, new_attrs)
+
+
+class BasePage(Base, metaclass=MetaPage):
+    __suffix = ""
