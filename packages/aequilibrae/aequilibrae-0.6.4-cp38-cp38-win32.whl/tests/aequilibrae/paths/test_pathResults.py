bddd1464fe405dcebc19ad35268b300563abb9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:12 +0000

    Add package aequilibrae/0.6.4/aequilibrae-0.6.4-cp38-cp38-win32.whl

---
+++ packages/aequilibrae/aequilibrae-0.6.4-cp38-cp38-win32.whl/tests/aequilibrae/paths/test_pathResults.py
@@ -0,0 +1,84 @@
+import os
+import sys
+from unittest import TestCase
+
+from aequilibrae.paths import path_computation, Graph
+from aequilibrae.paths.results import PathResults
+from ...data import test_graph
+
+# Adds the folder with the data to the path and collects the paths to the files
+lib_path = os.path.abspath(os.path.join("..", "../tests"))
+sys.path.append(lib_path)
+
+origin = 5
+dest = 27
+
+
+class TestPathResults(TestCase):
+    def test_prepare(self):
+        # graph
+        self.g = Graph()
+        self.g.load_from_disk(test_graph)
+        self.g.set_graph(cost_field="distance")
+
+        self.r = PathResults()
+        try:
+            self.r.prepare(self.g)
+        except Exception as err:
+            self.fail("Path result preparation failed - {}".format(err.__str__()))
+
+    def test_reset(self):
+        self.test_prepare()
+        try:
+            self.r.reset()
+        except Exception as err:
+            self.fail("Path result resetting failed - {}".format(err.__str__()))
+
+    def test_compute_paths(self):
+        self.test_prepare()
+        try:
+            self.r.reset()
+        except Exception as err:
+            self.fail("Path result resetting failed - {}".format(err.__str__()))
+
+        path_computation(origin, dest, self.g, self.r)
+
+        if list(self.r.path) != [53, 52, 13]:
+            self.fail("Path computation failed. Wrong sequence of links")
+
+        if list(self.r.path_nodes) != [5, 168, 166, 27]:
+            self.fail("Path computation failed. Wrong sequence of path nodes")
+
+        if list(self.r.milepost) != [0, 341, 1398, 2162]:
+            self.fail("Path computation failed. Wrong milepost results")
+
+        self.r.compute_path(origin, dest)
+
+        if list(self.r.path) != [53, 52, 13]:
+            self.fail("Path computation failed. Wrong sequence of links")
+
+        if list(self.r.path_nodes) != [5, 168, 166, 27]:
+            self.fail("Path computation failed. Wrong sequence of path nodes")
+
+        if list(self.r.milepost) != [0, 341, 1398, 2162]:
+            self.fail("Path computation failed. Wrong milepost results")
+
+    def test_update_trace(self):
+        self.test_prepare()
+        try:
+            self.r.reset()
+        except Exception as err:
+            self.fail("Path result resetting failed - {}".format(err.__str__()))
+
+        self.r.compute_path(origin, dest - 1)
+
+        self.r.update_trace(dest)
+
+        if list(self.r.path) != [53, 52, 13]:
+            self.fail("Path computation failed. Wrong sequence of links")
+
+        if list(self.r.path_nodes) != [5, 168, 166, 27]:
+            self.fail("Path computation failed. Wrong sequence of path nodes")
+
+        if list(self.r.milepost) != [0, 341, 1398, 2162]:
+            self.fail("Path computation failed. Wrong milepost results")
