dcc11c0d91d818dbb8a6ad5dbdcbea720494913c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:01 +0000

    Add package z2p/0.0.3/z2p-0.0.3.tar.gz

---
+++ packages/z2p/z2p-0.0.3.tar.gz/z2p-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='z2p',
+    version='0.0.3',
+    author='z2p',
+    author_email='z2p0721@foxmail.com',
+    url = 'https://github.com/z2p',
+    description='z2p\'s libarary ',
+    packages=['z2p'],
+    install_requires=[],
+    python_requires = '>=3'
+)
