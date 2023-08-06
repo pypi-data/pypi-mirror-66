16270822ee26632d06e8b69a3476e46665e35ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:11 +0000

    Add package chaosmagpy/0.3/chaosmagpy-0.3.tar.gz

---
+++ packages/chaosmagpy/chaosmagpy-0.3.tar.gz/chaosmagpy-0.3/chaosmagpy/__init__.py
@@ -0,0 +1,9 @@
+from chaosmagpy.chaos import CHAOS, load_CHAOS_matfile, load_CHAOS_shcfile
+from chaosmagpy.config_utils import basicConfig
+from chaosmagpy.model_utils import synth_values
+
+
+__all__ = ["CHAOS", "load_CHAOS_matfile", "load_CHAOS_shcfile",
+           "basicConfig", "synth_values"]
+
+__version__ = "0.3"
