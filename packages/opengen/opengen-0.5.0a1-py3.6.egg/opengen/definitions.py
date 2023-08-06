5703010c2367bc488d846d79ba9cc7469ffa7ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:08 +0000

    Add package opengen/0.5.0a1/opengen-0.5.0a1-py3.6.egg

---
+++ packages/opengen/opengen-0.5.0a1-py3.6.egg/opengen/definitions.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import pkg_resources
+
+
+def templates_dir():
+    """Directory where the templates are found (for internal use, mainly)"""
+    return pkg_resources.resource_filename('opengen', 'templates/')
+
+
+def templates_dir_ros():
+    """Directory where the templates are found (for internal use, mainly)"""
+    return pkg_resources.resource_filename('opengen', 'templates/ros/')
+
+
+def original_icasadi_dir():
+    """Directory where the original icasadi files are found (for internal use)"""
+    return pkg_resources.resource_filename('opengen', 'icasadi/')
