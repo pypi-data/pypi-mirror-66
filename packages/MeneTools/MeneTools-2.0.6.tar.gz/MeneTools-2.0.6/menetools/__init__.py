9c89e73f5b6013cb46490aea4aca44d86ceb8787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:31 +0000

    Add package MeneTools/2.0.6/MeneTools-2.0.6.tar.gz

---
+++ packages/MeneTools/MeneTools-2.0.6.tar.gz/MeneTools-2.0.6/menetools/__init__.py
@@ -0,0 +1,9 @@
+from menetools.menescope import run_menescope
+from menetools.menecheck import run_menecheck
+from menetools.menecof import run_menecof
+from menetools.menepath import run_menepath
+
+
+__version__="2.0.6"
+"""Define the version of the package.
+"""
---
+++ packages/MeneTools/MeneTools-2.0.6.tar.gz/MeneTools-2.0.6/menetools/__main__.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+logger = logging.getLogger('menetools')
+logger.setLevel(logging.DEBUG)
+out_hdlr = logging.StreamHandler(sys.stdout)
+out_hdlr.setFormatter(logging.Formatter('%(message)s'))
+out_hdlr.setLevel(logging.DEBUG)
+logger.addHandler(out_hdlr)
+logger.propagate = True
+
+from menetools.menescope import cmd_menescope
+from menetools.menecheck import cmd_menecheck
+from menetools.menecof import cmd_menecof
+from menetools.menepath import cmd_menepath
+
+def main_menescope(args=None):
+    cmd_menescope()
+
+def main_menecheck(args=None):
+    cmd_menecheck()
+
+def main_menecof(args=None):
+    cmd_menecof()
+
+def main_menepath(args=None):
+    cmd_menepath()
