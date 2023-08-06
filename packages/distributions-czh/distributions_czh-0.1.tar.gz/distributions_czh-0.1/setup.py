088b8f199beb93822d1ae2dddf84fbc8f3165f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:52 +0000

    Add package distributions-czh/0.1/distributions_czh-0.1.tar.gz

---
+++ packages/distributions-czh/distributions_czh-0.1.tar.gz/distributions_czh-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_czh',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_czh'],
+      zip_safe=False)
