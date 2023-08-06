121a26c62a8de0cc28a307f997d20e56b617c59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:37 +0000

    Add package stack-noodle/0.0.3/stack_noodle-0.0.3-py3-none-any.whl

---
+++ packages/stack-noodle/stack_noodle-0.0.3-py3-none-any.whl/stack_noodle/__init__.py
@@ -0,0 +1,2 @@
+from stack_noodle.eggnoodle import Egg_noodle , printhi
+from stack_noodle.friend_rice import Fried_rice
---
+++ packages/stack-noodle/stack_noodle-0.0.3-py3-none-any.whl/stack_noodle/friend_rice.py
@@ -0,0 +1,4 @@
+class Fried_rice:
+    fried_rice = "munch"
+    def __init__(self, rice = True):
+        self.rice = True
