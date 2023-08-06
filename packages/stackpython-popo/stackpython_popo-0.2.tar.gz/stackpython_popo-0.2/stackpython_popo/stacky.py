2af141300af853137fe4a77262669a70903a05f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:56 +0000

    Add package stackpython-popo/0.2/stackpython_popo-0.2.tar.gz

---
+++ packages/stackpython-popo/stackpython_popo-0.2.tar.gz/stackpython_popo-0.2/stackpython_popo/stacky.py
@@ -0,0 +1,6 @@
+class Noodle():
+    composite = ['egg_noodle', 'soup','pork']
+    def __init__(self, popo=True):
+        popo = True
+    def eat_it(self):
+        print('Soooooot Ah Delicious')
