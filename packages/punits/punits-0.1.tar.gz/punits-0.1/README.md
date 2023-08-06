d298c72aa199ff0cd0f58f499d15000a8fc6a19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:05 +0000

    Add package punits/0.1/punits-0.1.tar.gz

---
+++ packages/punits/punits-0.1.tar.gz/punits-0.1/README.md
@@ -0,0 +1,3 @@
+# punits
+
+A simple utility to convert values between different units.
---
+++ packages/punits/punits-0.1.tar.gz/punits-0.1/src/punits/consts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Makes CONVERSIONS and LABELS visible directly
+from consts
+"""
+
+from .conversions import CONVERSIONS
+from .labels import LABELS
