71b668c91f8264875e71593c6faa57321a0216c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:00 +0000

    Add package aws-cdk.aws-route53-patterns/1.33.1/aws_cdk.aws_route53_patterns-1.33.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-route53-patterns/aws_cdk.aws_route53_patterns-1.33.1-py3-none-any.whl/aws_cdk/aws_route53_patterns/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_certificatemanager
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_iam
+import aws_cdk.aws_route53
+import aws_cdk.aws_route53_targets
+import aws_cdk.aws_s3
+import aws_cdk.core
+import aws_cdk.region_info
+import constructs
+__all__ = []
+
+publication.publish()
