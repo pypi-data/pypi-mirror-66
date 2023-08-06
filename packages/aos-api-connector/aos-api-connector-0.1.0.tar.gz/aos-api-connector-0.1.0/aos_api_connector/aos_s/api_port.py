6cb13b863872efaf89146d66efc7fd39a47e66ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:01 +0000

    Add package aos-api-connector/0.1.0/aos-api-connector-0.1.0.tar.gz

---
+++ packages/aos-api-connector/aos-api-connector-0.1.0.tar.gz/aos-api-connector-0.1.0/aos_api_connector/aos_s/api_port.py
@@ -0,0 +1,14 @@
+import json
+
+
+def new_name(port, port_name, **session_dict):
+    print("Changing name of port {} to {}\n".format(port, port_name))
+    json_name = json.dumps({'id': port, 'name': port_name})
+    r = session_dict['s'].put(session_dict['url'] + f"ports/{port}", data=json_name,
+                              verify=False)
+    if r.ok:
+        print("Change successful!\n")
+        return
+    else:
+        print(f"HTTP Code: {r.status_code} \n  {r.reason} \n Message {r.text}")
+    return
