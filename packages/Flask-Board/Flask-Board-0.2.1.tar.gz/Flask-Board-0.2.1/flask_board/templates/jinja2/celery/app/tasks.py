fcd2fd56efe07e9b1a27302c8dc740d74899ec93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:16 +0000

    Add package Flask-Board/0.2.1/Flask-Board-0.2.1.tar.gz

---
+++ packages/Flask-Board/Flask-Board-0.2.1.tar.gz/Flask-Board-0.2.1/flask_board/templates/jinja2/celery/app/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+from app.application import create_app
+
+
+# define your tasks here
+# @shared_task
+# def test():
+#     app = create_app(config_log=False)
+#     with app.app_context():
+#         # do something within flask app
+#         pass
