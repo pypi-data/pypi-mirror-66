e54e9e165cdeb76598caee12e621e48c59fc6cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:00 +0000

    Add package buildflaskapp/0.2.4.1/buildflaskapp-0.2.4.1.tar.gz

---
+++ packages/buildflaskapp/buildflaskapp-0.2.4.1.tar.gz/buildflaskapp-0.2.4.1/buildflaskapp/scripts/manual.py
@@ -0,0 +1,15 @@
+def print_manual():
+    print('Usage: buildflaskapp [app_name] -[arguments]\n')
+
+    print('Options and arguments available for creating flask apps:')
+    print('  -d or --debugger \n\t Enables debugger mode on')
+    print('  -sl3 or --sqlite3 \n\t Enables sqlite3 mode on')
+    print('  -cj or --css-js \n\t Import stylesheet and script tag')
+    print('  -bs or --bootstrap \n\t Import bootstrap')
+    print('  -jq or --jQuery \n\t Import jQuery')
+    print('  -gsap or --gsap \n\tImport Gsap')
+    print('  -fa or --font-awesome \n\tImport Font awesome')
+    print('  -dc or --docker-container \n\t Generate Dockerfile and docker-compose.yml\n')
+    print('  -h or --help \n\t Print help')
+    print('  -v or --version \n\t Print version\n')
+    print('Example: build-flask-app hello-world -d -bs\n')
