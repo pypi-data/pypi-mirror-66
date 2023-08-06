d64caf30050be64a7ba1086c240c4e4924a5fba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package django-helcim/0.8.1/django-helcim-0.8.1.tar.gz

---
+++ packages/django-helcim/django-helcim-0.8.1.tar.gz/django-helcim-0.8.1/helcim/bridge_oscar.py
@@ -0,0 +1,309 @@
+"""Bridging module between Django Oscar and gateway module."""
+import logging
+
+from oscar.apps.payment import exceptions as oscar_exceptions
+
+from helcim import exceptions as helcim_exceptions, gateway
+
+LOG = logging.getLogger(__name__)
+
+def remap_oscar_billing_address(address):
+    """Remaps Oscar billing address dictionary to Helcim dictionary.
+
+        Parameters:
+            address (dict): A dictionary of the billing address details
+                provided by Django Oscar.
+
+        Returns:
+            dict: Billing address details formatted for
+                django-helcim.
+    """
+    if address.get('first_name') and address.get('last_name'):
+        contact_name = '{} {}'.format(
+            address.get('first_name'), address.get('last_name')
+        )
+    elif address.get('first_name'):
+        contact_name = address.get('first_name')
+    elif address.get('last_name'):
+        contact_name = address.get('last_name')
+    else:
+        contact_name = None
+
+    return {
+        'billing_contact_name': contact_name,
+        'billing_street_1': address.get('line1'),
+        'billing_street_2': address.get('line2'),
+        'billing_city': address.get('line4'),
+        'billing_province': address.get('state'),
+        'billing_postal_code': address.get('postcode'),
+        'billing_country': address.get('country'),
+        'billing_phone': address.get('phone_number'),
+    }
+
+def remap_oscar_credit_card(card):
+    """Remaps Oscar credit card object as Helcim dictionary.
+
+        Parameters:
+            card (obj): A credit card object provided by Django Oscar.
+
+        Returns:
+            dict: Credit card details formatted for django-helcim.
+    """
+    if card.expiry_date:
+        cc_expiry = card.expiry_date.strftime('%m%y')
+    else:
+        cc_expiry = None
+
+    return {
+        'cc_name': card.name,
+        'cc_number': card.number,
+        'cc_expiry': cc_expiry,
+        'cc_cvv': card.ccv,
+    }
+
+class BaseCardTransactionBridge():
+    """Base class to bridge Oscar and Helcim transactions.
+
+        Parameters:
+            order_number (str): Order number for the transaction.
+            amount (dec): The transaction total.
+            card (obj): Instance of the Oscar bankcard class.
+            billing_address (dict): The billing address information.
+            save_token (bool): Whether the card token should be saved
+                in the token vault or not.
+            django_user (obj): The user to associate with the saved
+                card token.
+            customer_code (str): The Helcim customer code to associate
+                with the saved card token.
+    """
+    def __init__(
+            self, amount, token_id=None, card=None,
+            billing_address=None, save_token=False, django_user=None,
+            customer_code=None
+    ):
+        transaction_details = {
+            'amount': amount,
+        }
+
+        if token_id:
+            transaction_details.update(retrieve_token_details(
+                token_id, django_user, customer_code
+            ))
+
+        if billing_address:
+            transaction_details.update(remap_oscar_billing_address(
+                billing_address
+            ))
+
+        if card:
+            transaction_details.update(remap_oscar_credit_card(card))
+
+        self.transaction_details = transaction_details
+        self.save_token = save_token
+        self.django_user = django_user
+
+class PurchaseBridge(BaseCardTransactionBridge):
+    """Class to bridge Oscar and Helcim purchase transactions."""
+    def process(self):
+        """Attempts to process Purchase with transaction details.
+
+            Returns the values of ``gateway.Purchase.process``.
+
+            Raises:
+                GatewayError: An Oscar error raised when there was an
+                    error with the payment API.
+                PaymentError: An Oscar error raised when there was an
+                    error processing the payment.
+        """
+        purchase_instance = gateway.Purchase(
+            save_token=self.save_token,
+            django_user=self.django_user,
+            **self.transaction_details
+        )
+
+        try:
+            return purchase_instance.process()
+        except helcim_exceptions.ProcessingError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+        except helcim_exceptions.PaymentError as error:
+            raise oscar_exceptions.PaymentError(str(error))
+        except helcim_exceptions.DjangoError:
+            LOG.exception(
+                'Purchase complete, but errors occured while saving '
+                'transaction to database'
+            )
+        except helcim_exceptions.HelcimError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+
+class PreauthorizeBridge(BaseCardTransactionBridge):
+    """Class to bridge Oscar and Helcim preauthorization transactions."""
+    def process(self):
+        """Attempts to process Preauthorize with transaction details.
+
+            Returns the values of ``gateway.Preauthorize.process``.
+
+            Raises:
+                GatewayError: An Oscar error raised when there was an
+                    error with the payment API.
+                PaymentError: An Oscar error raised when there was an
+                    error processing the payment.
+        """
+
+        preauth_instance = gateway.Preauthorize(
+            save_token=self.save_token,
+            django_user=self.django_user,
+            **self.transaction_details
+        )
+
+        try:
+            return preauth_instance.process()
+        except helcim_exceptions.ProcessingError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+        except helcim_exceptions.PaymentError as error:
+            raise oscar_exceptions.PaymentError(str(error))
+        except helcim_exceptions.DjangoError:
+            LOG.exception(
+                'Preauthorization complete, but errors occured while saving '
+                'transaction to database'
+            )
+        except helcim_exceptions.HelcimError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+
+class RefundBridge(BaseCardTransactionBridge):
+    """Class to bridge Oscar and Helcim refund transactions."""
+    def process(self):
+        """Attempts to process Refund with transaction details.
+
+            Returns the values of ``gateway.Refund.process``.
+
+            Raises:
+                GatewayError: An Oscar error raised when there was an
+                    error with the payment API.
+                PaymentError: An Oscar error raised when there was an
+                    error processing the payment.
+        """
+
+        refund_instance = gateway.Refund(
+            save_token=self.save_token,
+            django_user=self.django_user,
+            **self.transaction_details
+        )
+
+        try:
+            return refund_instance.process()
+        except helcim_exceptions.ProcessingError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+        except helcim_exceptions.PaymentError as error:
+            raise oscar_exceptions.PaymentError(str(error))
+        except helcim_exceptions.DjangoError:
+            LOG.exception(
+                'Refund complete, but errors occured while saving '
+                'transaction to database'
+            )
+        except helcim_exceptions.HelcimError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+
+class VerificationBridge(BaseCardTransactionBridge):
+    """Class to bridge Oscar and Helcim Verification transactions."""
+    def process(self):
+        """Attempts to process Verification with transaction details.
+
+            Returns the values of ``gateway.Verification.process``.
+
+            Raises:
+                GatewayError: An Oscar error raised when there was an
+                    error with the payment API.
+                PaymentError: An Oscar error raised when there was an
+                    error processing the payment.
+        """
+
+        verification_instance = gateway.Verification(
+            save_token=self.save_token,
+            django_user=self.django_user,
+            **self.transaction_details
+        )
+
+        try:
+            return verification_instance.process()
+        except helcim_exceptions.ProcessingError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+        except helcim_exceptions.PaymentError as error:
+            raise oscar_exceptions.PaymentError(str(error))
+        except helcim_exceptions.DjangoError:
+            LOG.exception(
+                'Verification complete, but errors occured while saving '
+                'transaction to database'
+            )
+        except helcim_exceptions.HelcimError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+
+class CaptureBridge():
+    """Class to bridge Oscar and Helcim Capture transactions.
+
+        Parameters:
+            transaction_id (int): the Helcim preauthorization
+                 transaction ID to capture.
+    """
+    def __init__(self, transaction_id):
+        transaction_details = {
+            'transaction_id': transaction_id,
+        }
+
+        self.transaction_details = transaction_details
+
+    def process(self):
+        """Attempts to process Capture with transaction details.
+
+            Returns the values of ``gateway.Capture.process``.
+
+            Raises:
+                GatewayError: An Oscar error raised when there was an
+                    error with the payment API.
+                PaymentError: An Oscar error raised when there was an
+                    error processing the payment.
+        """
+
+        capture_instance = gateway.Capture(
+            **self.transaction_details
+        )
+
+        try:
+            return capture_instance.process()
+        except helcim_exceptions.ProcessingError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+        except helcim_exceptions.PaymentError as error:
+            raise oscar_exceptions.PaymentError(str(error))
+        except helcim_exceptions.DjangoError:
+            LOG.exception(
+                'Capture complete, but errors occured while saving '
+                'transaction to database'
+            )
+            return None
+        except helcim_exceptions.HelcimError as error:
+            raise oscar_exceptions.GatewayError(str(error))
+
+
+# SHORTCUT FUNCTIONS AND DICTIONARIES
+# -----------------------------------------------------------------------------
+# These functions/dictionaries are provided as a convenience shortcut to allow
+# access to main functions via the bridge module.
+
+def retrieve_token_details(token_id, django_user=None, customer_code=None):
+    """Shortcut for retrieve_token_details from the Gateway module.
+
+        Added as a convenience to allow access to core functions via
+        the bridge module exclusively.
+    """
+    return gateway.retrieve_token_details(token_id, django_user, customer_code)
+
+def retrieve_saved_tokens(django_user=None, customer_code=None):
+    """Shortcut for retrieve_saved_tokens from the Gateway module.
+
+        Added as a convenience to allow access to core functions via
+        the bridge module exclusively.
+    """
+    return gateway.retrieve_saved_tokens(
+        django_user=django_user, customer_code=customer_code
+    )
+
+SETTINGS = gateway.SETTINGS
---
+++ packages/django-helcim/django-helcim-0.8.1.tar.gz/django-helcim-0.8.1/helcim/models.py
@@ -0,0 +1,280 @@
+"""Models for the django-helcim application."""
+from uuid import uuid4
+
+from django.contrib.auth import get_user_model
+from django.contrib.staticfiles import finders
+from django.db import models
+
+
+class HelcimTransaction(models.Model):
+    """Details of a single Helcim transaction."""
+    id = models.UUIDField(
+        default=uuid4,
+        editable=False,
+        primary_key=True,
+        verbose_name='ID',
+    )
+    raw_request = models.CharField(
+        blank=True,
+        help_text='The raw request used for this transaction',
+        max_length=1024,
+        null=True,
+    )
+    raw_response = models.CharField(
+        blank=True,
+        help_text='The raw response returned for this transaction',
+        max_length=1024,
+        null=True,
+    )
+    transaction_success = models.BooleanField(
+        help_text='Whether the transaction was successful or not',
+    )
+    response_message = models.CharField(
+        blank=True,
+        help_text='The response message with the API call',
+        max_length=256,
+        null=True,
+    )
+    notice = models.CharField(
+        blank=True,
+        help_text='Any error or warning messages from Helcim',
+        max_length=128,
+        null=True,
+    )
+    date_response = models.DateTimeField(
+        help_text='The date and time of the API response',
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Date and time this transaction was recorded in database',
+    )
+    TRANSACTION_TYPES = (
+        ('s', 'purchase (sale)'),
+        ('p', 'pre-authorization'),
+        ('c', 'capture'),
+        ('r', 'refund'),
+        ('v', 'verify'),
+    )
+    transaction_type = models.CharField(
+        choices=TRANSACTION_TYPES,
+        help_text='The type of transaction',
+        max_length=1,
+    )
+    transaction_id = models.PositiveIntegerField(
+        blank=True,
+        help_text='The Helcim Commerce transaction ID',
+        null=True,
+    )
+    amount = models.DecimalField(
+        blank=True,
+        decimal_places=2,
+        help_text='The transaction amount',
+        max_digits=12,
+        null=True,
+    )
+    currency = models.CharField(
+        blank=True,
+        help_text='The transaction currency',
+        max_length=8,
+        null=True,
+    )
+    cc_name = models.CharField(
+        blank=True,
+        help_text='The cardholder name',
+        max_length=256,
+        null=True,
+    )
+    cc_number = models.CharField(
+        blank=True,
+        help_text='The first four and last 4 digits of the credit card number',
+        max_length=16,
+        null=True,
+    )
+    cc_expiry = models.DateField(
+        blank=True,
+        help_text='The credit card expiry date',
+        null=True,
+    )
+    cc_type = models.CharField(
+        blank=True,
+        help_text='The credit card type',
+        max_length=32,
+        null=True,
+    )
+    token = models.CharField(
+        blank=True,
+        help_text='The Helcim generated and stored credit card token',
+        max_length=23,
+        null=True,
+    )
+    token_f4l4 = models.CharField(
+        blank=True,
+        help_text='The first and last 4 digits of the credit card number',
+        max_length=8,
+        null=True,
+    )
+    avs_response = models.CharField(
+        blank=True,
+        help_text='The address verification response',
+        max_length=1,
+        null=True,
+    )
+    cvv_response = models.CharField(
+        blank=True,
+        help_text='The CVV verification response',
+        max_length=1,
+        null=True,
+    )
+    approval_code = models.CharField(
+        blank=True,
+        help_text='The transaction approval code',
+        max_length=16,
+        null=True,
+    )
+    order_number = models.CharField(
+        blank=True,
+        help_text='The Helcim order number',
+        max_length=16,
+        null=True,
+    )
+    customer_code = models.CharField(
+        blank=True,
+        help_text='The Helcim customer code',
+        max_length=16,
+        null=True,
+    )
+    django_user = models.ForeignKey(
+        get_user_model(),
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='helcim_transactions',
+    )
+
+    class Meta:
+        ordering = ('-date_response',)
+        permissions = (
+            (
+                'helcim_transactions',
+                'Can view and interact with Helcim transactions.'
+            ),
+        )
+
+    def __str__(self):
+        string_parts = [
+            str(self.date_response),
+            self.transaction_type,
+        ]
+
+        return ' - '.join(string_parts)
+
+    @property
+    def can_be_captured(self):
+        """Check if this transaction can be captured."""
+        return bool(
+            self.transaction_success and self.transaction_type == 'p'
+        )
+
+    @property
+    def can_be_refunded(self):
+        """Check if this transaction can be refunded."""
+        return all([
+            self.transaction_success,
+            (self.transaction_type == 's' or self.transaction_type == 'c'),
+            (self.amount if self.amount else 0) > 0,
+        ])
+
+class HelcimToken(models.Model):
+    """A Helcim card token."""
+    id = models.UUIDField(
+        default=uuid4,
+        editable=False,
+        primary_key=True,
+        verbose_name='ID',
+    )
+    token = models.CharField(
+        help_text='The Helcim card token number',
+        max_length=23,
+    )
+    token_f4l4 = models.CharField(
+        help_text='The first & last four digits of the credit card number',
+        max_length=8
+    )
+    cc_name = models.CharField(
+        blank=True,
+        help_text='The cardholder name',
+        max_length=256,
+        null=True,
+    )
+    cc_expiry = models.DateField(
+        blank=True,
+        help_text='The credit card expiry date',
+        null=True,
+    )
+    cc_type = models.CharField(
+        blank=True,
+        help_text='The credit card type',
+        max_length=32,
+        null=True,
+    )
+    date_added = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Date and time this token was added to database',
+    )
+    customer_code = models.CharField(
+        blank=True,
+        help_text='The Helcim customer code',
+        max_length=16,
+        null=True,
+    )
+    django_user = models.ForeignKey(
+        get_user_model(),
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='helcim_tokens',
+    )
+
+    class Meta:
+        permissions = (
+            (
+                'helcim_tokens',
+                'Can view and interact with Helcim tokens.'
+            ),
+        )
+        unique_together = (
+            'token', 'token_f4l4', 'customer_code', 'django_user'
+        )
+
+    @property
+    def display_as_card_number(self):
+        """Displays token_f4l4 as a 16 character credit card number."""
+        return '{}********{}'.format(self.token_f4l4[:4], self.token_f4l4[-4:])
+
+    @property
+    def get_credit_card_png(self):
+        """Returns a path to a credit card .png for this token."""
+        # Look for an image for this credit card
+        if self.cc_type:
+            image_path = 'helcim/{}.png'.format(self.cc_type.lower())
+
+            # If found, return the path to the static file image
+            if finders.find(image_path):
+                return image_path
+
+        # Return placeholder image
+        return 'helcim/placeholder.png'
+
+    @property
+    def get_credit_card_svg(self):
+        """Returns a path to a credit card .svg for this token."""
+        # Look for an image for this credit card
+        if self.cc_type:
+            image_path = 'helcim/{}.svg'.format(self.cc_type.lower())
+
+            # If found, return the path to the static file image
+            if finders.find(image_path):
+                return image_path
+
+        # Return placeholder image
+        return 'helcim/placeholder.svg'
