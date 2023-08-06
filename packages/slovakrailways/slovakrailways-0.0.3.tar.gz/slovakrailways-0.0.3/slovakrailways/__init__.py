40152a1dae4b49930e6d32160fb817d139a606ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:22 +0000

    Add package slovakrailways/0.0.3/slovakrailways-0.0.3.tar.gz

---
+++ packages/slovakrailways/slovakrailways-0.0.3.tar.gz/slovakrailways-0.0.3/slovakrailways/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Slovak Railways API.
+
+This module contains functions to call Slovak Railways API.
+For more information check README.md.
+ 
+Reference: https://github.com/martinbenes1996/slovakrailways
+Todo:
+    * delays of multiple trains at single request
+    * caching
+"""
+
+import sys
+sys.path.append('slovakrailways')
+
+from main import *
+from meta import *
