6aa8246ee42fea7cbab0ae6d8c9eb820e9d371df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:35 +0000

    Add package spaceone-statistics/0.1.1.post2/spaceone_statistics-0.1.1.post2-py3-none-any.whl

---
+++ packages/spaceone-statistics/spaceone_statistics-0.1.1.post2-py3-none-any.whl/spaceone/statistics/error/__init__.py
@@ -0,0 +1,2 @@
+from spaceone.statistics.error.stat import *
+from spaceone.statistics.error.schedule import *
---
+++ packages/spaceone-statistics/spaceone_statistics-0.1.1.post2-py3-none-any.whl/spaceone/statistics/service/history_service.py
@@ -0,0 +1,99 @@
+import logging
+
+from spaceone.core.service import *
+from spaceone.statistics.error import *
+from spaceone.statistics.manager.stat_manager import StatManager
+from spaceone.statistics.manager.history_manager import HistoryManager
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@authentication_handler
+@authorization_handler
+@event_handler
+class HistoryService(BaseService):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stat_mgr: StatManager = self.locator.get_manager('StatManager')
+        self.history_mgr: HistoryManager = self.locator.get_manager('HistoryManager')
+
+    @transaction
+    @check_required(['topic', 'resource_type', 'query', 'domain_id'])
+    def create(self, params):
+        """Statistics query to resource
+
+        Args:
+            params (dict): {
+                'topic': 'str',
+                'data_source_id': 'str',
+                'resource_type': 'str',
+                'query': 'dict (spaceone.api.core.v1.StatisticsQuery)',
+                'join': 'list',
+                'formulas': 'list',
+                'domain_id': 'str'
+            }
+
+        Returns:
+            None
+        """
+
+        domain_id = params['domain_id']
+        resource_type = params['resource_type']
+        query = params.get('query', {})
+        join = params.get('join', [])
+        formulas = params.get('formulas', [])
+        sort = query.get('sort')
+        limit = query.get('limit')
+
+        if len(join) > 0:
+            query['sort'] = None
+            query['limit'] = None
+
+        results = self.stat_mgr.query(resource_type, query, domain_id)
+        params['results'] = self.stat_mgr.join_and_execute_formula(results, join, formulas, sort, limit, domain_id)
+
+        self.history_mgr.create_history(params)
+
+    @transaction
+    @check_required(['domain_id'])
+    @append_query_filter(['topic', 'domain_id'])
+    @append_keyword_filter(['topic'])
+    @change_timestamp_filter(['created_at'])
+    def list(self, params):
+        """ List history
+
+        Args:
+            params (dict): {
+                'topic': 'str',
+                'domain_id': 'str',
+                'query': 'dict (spaceone.api.core.v1.Query)'
+            }
+
+        Returns:
+            history_vos (object)
+            total_count
+        """
+
+        query = params.get('query', {})
+        return self.history_mgr.list_history(query)
+
+    @transaction
+    @check_required(['query', 'domain_id'])
+    @append_query_filter(['domain_id'])
+    @change_timestamp_filter(['created_at'])
+    def stat(self, params):
+        """
+        Args:
+            params (dict): {
+                'domain_id': 'str',
+                'query': 'dict (spaceone.api.core.v1.StatisticsQuery)'
+            }
+
+        Returns:
+            values (list) : 'list of statistics data'
+
+        """
+
+        query = params.get('query', {})
+        return self.history_mgr.stat_history(query)
