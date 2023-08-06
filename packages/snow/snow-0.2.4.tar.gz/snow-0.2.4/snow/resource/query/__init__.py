7e9e1dfaf7b1871d2e794916a6b4bd00197d8012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:31 +0000

    Add package snow/0.2.4/snow-0.2.4.tar.gz

---
+++ packages/snow/snow-0.2.4.tar.gz/snow-0.2.4/snow/resource/query/__init__.py
@@ -0,0 +1,4 @@
+from .builder import QueryBuilder
+from .condition import Condition
+from .operators import BaseOperator, StringOperator, ReferenceField, NumericOperator, DateTimeOperator, BooleanOperator, EmailOperator, LogicalOperator
+from .utils import select
