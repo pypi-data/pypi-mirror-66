1cd2c0d910d3122c4252d90870f5a4b998ab6b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:02 +0000

    Add package paiktj/0.0.1/paiktj-0.0.1.tar.gz

---
+++ packages/paiktj/paiktj-0.0.1.tar.gz/paiktj-0.0.1/README.md
@@ -0,0 +1,2 @@
+# my first pip for test
+hi hello I'm fine thank you, and you?
---
+++ packages/paiktj/paiktj-0.0.1.tar.gz/paiktj-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="paiktj",
+    version="0.0.1",
+    # license='MIT',
+    author="paiktj",
+    author_email="paiktj@snu.ac.kr",
+    description="testing",
+    long_description=open('README.md').read(),
+    # url="github url 등",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
