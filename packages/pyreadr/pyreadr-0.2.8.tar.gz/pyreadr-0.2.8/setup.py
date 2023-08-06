c138eaff48b1d111f2c5ed342cdc73bbd24a1723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:10 +0000

    Add package pyreadr/0.2.8/pyreadr-0.2.8.tar.gz

---
+++ packages/pyreadr/pyreadr-0.2.8.tar.gz/pyreadr-0.2.8/setup.py
@@ -0,0 +1,97 @@
+"""
+    @author: Otto Fajardo
+
+"""
+
+import setuptools
+
+import platform
+import glob
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+librdata_source_files = []
+librdata_source_files += glob.glob('pyreadr/libs/librdata/src/*.c')
+librdata_source_files += ['pyreadr/librdata.pyx']
+
+library_dirs = []
+libraries = []
+include_dirs = []
+extra_link_args = []
+extra_compile_args = ['-DHAVE_ZLIB', '-DHAVE_BZIP2']
+data_files = []
+data_folder = ""
+
+if platform.system() == 'Darwin':
+    libraries.append("iconv")
+elif platform.system() == 'Windows':
+    include_dirs.append(".")
+    include_dirs.append('pyreadr')
+    include_dirs.append('pyreadr/libs/zlib')
+    include_dirs.append('pyreadr/libs/bzip2')
+    include_dirs.append('pyreadr/libs/librdata')
+    include_dirs.append('pyreadr/libs/iconv')
+    library_dirs.append('pyreadr/libs/librdata')
+    
+    data_folder = "win_libs/64bit/"
+    data_files = [("Lib/site-packages/pyreadr", [data_folder + "zlib.dll", data_folder + "iconv.dll",
+                        data_folder + "charset.dll", data_folder + "iconv.lib",
+                        data_folder + "libbz2-1.dll"])]
+                        
+    library_dirs.append(data_folder)
+    libraries.append('z')
+    libraries.append('iconv')
+    libraries.append('bz2')
+    
+elif platform.system() == 'Linux':
+    libraries.append('z')
+    libraries.append('bz2')
+else:
+    raise RuntimeError('Unsupported OS')
+
+
+librdata = Extension(
+    'pyreadr.librdata',
+    sources=librdata_source_files,
+    library_dirs=library_dirs,
+    libraries=libraries,
+    include_dirs=include_dirs,
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args)
+
+long_description = """ A Python package to read and write R RData and Rds files
+into/from pandas data frames. It does not need to have R or other external
+dependencies installed.
+It is based on the C library librdata and
+a modified version of the cython wrapper jamovi-readstat.<br>
+Please visit out project home page for more information:<br>
+https://github.com/ofajardo/pyreadr
+"""
+
+short_description = "Reads/writes R RData and Rds files into/from pandas data frames."
+
+setup(
+    name='pyreadr',
+    version='0.2.8',
+    ext_modules=cythonize([librdata], force=True),
+    packages=["pyreadr"],
+    include_package_data=True,
+    data_files=data_files,
+    install_requires=['pandas>0.24.0'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Cython",
+        "Programming Language :: C",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Environment :: Console",
+    ],
+    description=short_description,
+    author="Otto Fajardo",
+    author_email="pleasecontactviagithub@notvalid.com",
+    url="https://github.com/ofajardo/pyreadr",
+    download_url="https://pypi.org/project/pyreadr/#files",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
