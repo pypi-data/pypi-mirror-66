d9210e71b658db53dbdd191852b516d4308b356f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:56 +0000

    Add package cengine/0.0.11/cengine-0.0.11.tar.gz

---
+++ packages/cengine/cengine-0.0.11.tar.gz/cengine-0.0.11/ce_cli/utils.py
@@ -0,0 +1,197 @@
+import json
+import os
+import shutil
+import urllib.request
+
+import click
+import py
+import yaml
+from dateutil import tz
+
+import ce_api
+from ce_api.rest import ApiException
+from ce_cli import constants
+import logging
+
+
+class Info(dict):
+    def __init__(self, *args, **kwargs):
+        self.path = py.path.local(
+            click.get_app_dir(constants.APP_NAME)).join('info.json')
+        super(Info, self).__init__(*args, **kwargs)
+
+    def load(self):
+        try:
+            self.update(json.loads(self.path.read()))
+        except py.error.ENOENT:
+            pass
+
+    def save(self):
+        self.path.ensure()
+        with self.path.open('w') as f:
+            f.write(json.dumps(self))
+
+
+pass_info = click.make_pass_decorator(Info, ensure=True)
+
+
+def title(text):
+    click.echo(click.style(text.upper(), fg='cyan', bold=True, underline=True))
+
+
+def confirmation(text, *args, **kwargs):
+    return click.confirm(click.style(text, fg='yellow'), *args,
+                         **kwargs)
+
+
+def question(text, *args, **kwargs):
+    return click.prompt(text=text, *args, **kwargs)
+
+
+def declare(text):
+    click.echo(click.style(text, fg='green'))
+
+
+def notice(text):
+    click.echo(click.style(text, fg='cyan'))
+
+
+def error(text):
+    raise click.ClickException(message=click.style(text, fg='red', bold=True))
+
+
+def warning(text):
+    click.echo(click.style(text, fg='yellow', bold=True))
+
+
+def search_column(message, schema, cancel_token=constants.CANCEL_TOKEN):
+    column = question(message, type=str)
+
+    error_message = 'ERROR: There is no such column as {c}. Possible ' \
+                    'selections include {l}. If you want to cancel the ' \
+                    'last query, type: {ct}'.format(c=column,
+                                                    l=list(schema),
+                                                    ct=cancel_token)
+
+    while column not in schema:
+        column = question(error_message, type=str)
+        if column == cancel_token:
+            break
+    if column == cancel_token:
+        return None
+    else:
+        return column
+
+
+def check_login_status(info):
+    # TODO: APi call to check the login status?
+    if constants.ACTIVE_USER not in info or \
+            info[constants.ACTIVE_USER] is None:
+        raise click.ClickException(
+            "You need to login first.\n"
+            "In order to login please use: 'cengine auth login'")
+
+
+def load_config(path):
+    if path is None:
+        config = {}
+    else:
+        with open(path, 'rt', encoding='utf8') as f:
+            config = yaml.load(f)
+
+    return config
+
+
+def save_config(config, path):
+    # Save the pulled pipeline locally
+    if os.path.isdir(path):
+        raise Exception('{} is a directory. Please provide a path to a '
+                        'file.'.format(path))
+    elif os.path.isfile(path):
+        if not path.endswith('.yaml'):
+            path += '.yaml'
+        if confirmation('A file with the same name exists already.'
+                        ' Do you want to overwrite?'):
+            with open(path, 'w') as outfile:
+                yaml.dump(config, outfile)
+            declare('Config file saved to: {}'.format(path))
+    else:
+        if not path.endswith('.yaml'):
+            path += '.yaml'
+        with open(path, 'w') as outfile:
+            yaml.dump(config, outfile)
+        declare('Config file saved to: {}'.format(path))
+
+
+def assert_exists(item, iterable, error_message='Something failed!'):
+    if item not in iterable:
+        exception = click.ClickException(click.style(
+            error_message, fg='red', bold=True))
+        raise exception
+
+
+def api_client(info):
+    active_user = info[constants.ACTIVE_USER]
+    config = ce_api.Configuration()
+    config.host = constants.API_HOST
+    config.access_token = info[active_user][constants.TOKEN]
+
+    return ce_api.ApiClient(config)
+
+
+def api_call(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except ApiException as e:
+        error('{}: {}'.format(e.reason, e.body))
+    except Exception as e:
+        logging.error(str(e))
+        error('There is something wrong going on. Please contact '
+              'core@maiot.io to get further information.')
+
+
+def download_artifact(artifact_json, path='/'):
+    """
+    This will replace the folder with the files if they already exist
+    """
+    if artifact_json['name'] == '/':
+        full_path = path
+    else:
+        full_path = os.path.join(path, artifact_json['name'])
+
+    if artifact_json['is_dir']:
+        # TODO: [LOW] Short term fix for empty files being labelled as dirs
+        if len(artifact_json['children']) == 0:
+            # turn it into a file
+            artifact_json['is_dir'] = False
+            with open(full_path, 'wb') as f:
+                f.write(b'')
+        else:
+            os.makedirs(full_path, exist_ok=True)
+            for child in artifact_json['children']:
+                download_artifact(child, path=full_path)
+    else:
+        # Download the file from `url` and save it locally under `file_name`:
+        url = artifact_json['signed_url']
+        with urllib.request.urlopen(url) as response, open(full_path,
+                                                           'wb') as out_file:
+
+            shutil.copyfileobj(response, out_file)
+
+
+def format_date_for_display(dt, format='%Y-%m-%d %H:%M:%S'):
+    if dt is None:
+        return ''
+    local_zone = tz.tzlocal()
+    # make sure this is UTC
+    dt = dt.replace(tzinfo=tz.tzutc())
+    local_time = dt.astimezone(local_zone)
+    return local_time.strftime(format)
+
+
+# TODO: [LOW] Hack for now, should be done in API
+def get_workers_cpus_from_env_config(env_config):
+    workers = int(env_config['execution']['num_workers'])
+    cpus_per_worker = \
+        int(env_config['execution']['worker_machine_type'].split('-')[-1])
+    return workers, cpus_per_worker
---
+++ packages/cengine/cengine-0.0.11.tar.gz/cengine-0.0.11/ce_cli/workspace.py
@@ -0,0 +1,90 @@
+import click
+from tabulate import tabulate
+
+import ce_api
+from ce_api.models import WorkspaceCreate
+from ce_cli import constants
+from ce_cli.cli import cli, pass_info
+from ce_cli.utils import check_login_status, api_client, api_call, declare
+
+
+@cli.group()
+@pass_info
+def workspace(info):
+    """Interaction with the available workspaces"""
+    check_login_status(info)
+
+
+@workspace.command('list')
+@pass_info
+def list_workspaces(info):
+    """List of all workspaces available to the user"""
+    user = info[constants.ACTIVE_USER]
+
+    api = ce_api.WorkspacesApi(api_client(info))
+    ws_list = api_call(api.get_loggedin_workspaces_api_v1_workspaces_get)
+
+    if constants.ACTIVE_WORKSPACE in info[user]:
+        active_workspace = info[user][constants.ACTIVE_WORKSPACE]
+    else:
+        active_workspace = None
+
+    declare('You have created {count} different '
+            'workspace(s). \n'.format(count=len(ws_list)))
+
+    if ws_list:
+        table = []
+        for w in ws_list:
+            table.append({'Selection': '*' if w.id == active_workspace else '',
+                          'ID': w.id,
+                          'Name': w.name})
+        click.echo(tabulate(table, headers='keys', tablefmt='presto'))
+        click.echo()
+
+
+@workspace.command('set')
+@click.argument("workspace_id", default=None, type=int)
+@pass_info
+def set_workspace(info, workspace_id):
+    """Set workspace to be active"""
+    user = info[constants.ACTIVE_USER]
+
+    api = ce_api.WorkspacesApi(api_client(info))
+    ws = api_call(api.get_workspace_api_v1_workspaces_workspace_id_get,
+                  workspace_id)
+
+    info[user][constants.ACTIVE_WORKSPACE] = ws.id
+    info.save()
+    declare('Active workspace set to id: {id}'.format(id=workspace_id))
+
+
+# @workspace.command('delete')
+# @click.argument("workspace_id", type=int)
+# @pass_info
+# def delete_workspace(info, workspace_id):
+#     api = ce_api.WorkspacesApi(api_client(info))
+#     api_call(api.delete_workspace_api_v1_workspaces_workspace_id_delete,
+#              workspace_id)
+#
+#     users = [u for u in info.keys() if u != constants.ACTIVE_USER]
+#     for user in users:
+#         if constants.ACTIVE_WORKSPACE in info[user] and \
+#                 info[user][constants.ACTIVE_WORKSPACE] == workspace_id:
+#             info[user].pop(constants.ACTIVE_WORKSPACE)
+#
+#     info.save()
+
+
+@workspace.command('create')
+@click.argument("name", type=str)
+@pass_info
+@click.pass_context
+def create_workspace(ctx, info, name):
+    """Create a workspace and set it to be active."""
+    click.echo('Registering the workspace "{}"...'.format(name))
+
+    api = ce_api.WorkspacesApi(api_client(info))
+    ws = api_call(api.create_workspace_api_v1_workspaces_post,
+                  WorkspaceCreate(name=name))
+    declare('Workspace registered.')
+    ctx.invoke(set_workspace, workspace_id=ws.id)
