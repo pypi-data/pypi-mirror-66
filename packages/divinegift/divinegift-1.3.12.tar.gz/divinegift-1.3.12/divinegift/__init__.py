5e67c942f162bb1b8939c097691da8a1ec08e56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:42 +0000

    Add package divinegift/1.3.12/divinegift-1.3.12.tar.gz

---
+++ packages/divinegift/divinegift-1.3.12.tar.gz/divinegift-1.3.12/divinegift/__init__.py
@@ -0,0 +1,3 @@
+name = "DivineGift"
+
+__all__ = ["main", "logger", "banner", "config", "sender", "db", "translit", "excel", "templator", "kafka_client", "cipher", "zabbix_agent"]
---
+++ packages/divinegift/divinegift-1.3.12.tar.gz/divinegift-1.3.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='divinegift',
+      version='1.3.12',
+      description='Modified send_telegram. It will send raw message which you provide.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  # This is important!
+      classifiers=[
+                   'Development Status :: 5 - Production/Stable',
+                   #'Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: MIT License',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 3',
+                   "Operating System :: OS Independent",
+                   ],
+      keywords='s7_it',
+      url='https://gitlab.com/gng-group/divinegift.git',
+      author='Malanris',
+      author_email='admin@malanris.ru',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['requests', 'mailer', 'deprecation', 'cryptography', 'pyyaml>=5.1', 'python-dateutil'],
+      include_package_data=True,
+      zip_safe=False)
