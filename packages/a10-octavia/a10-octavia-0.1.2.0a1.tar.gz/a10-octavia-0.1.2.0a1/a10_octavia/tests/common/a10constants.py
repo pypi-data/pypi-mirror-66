32d5ebd959a0d2feaf1b418690acca2b3d0b012d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:47 +0000

    Add package a10-octavia/0.1.2.0a1/a10-octavia-0.1.2.0a1.tar.gz

---
+++ packages/a10-octavia/a10-octavia-0.1.2.0a1.tar.gz/a10-octavia-0.1.2.0a1/a10_octavia/tests/common/a10constants.py
@@ -0,0 +1,28 @@
+#    Copyright 2020, A10 Networks
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+MOCK_POOL_ID = 'mock-pool-1'
+MOCK_MEMBER_ID = 'mock-member-1'
+MOCK_LOAD_BALANCER_ID = 'mock-lb-1'
+MOCK_COOKIE_NAME = 'mock-cookie-1'
+MOCK_LISTENER_ID = 'mock-listener-1'
+MOCK_CERT_FILENAME = 'mock-cert-filename-1'
+MOCK_CERT_CONTENT = 'mock-cert-content-1'
+MOCK_KEY_FILENAME = 'mock-key-filename-1'
+MOCK_KEY_CONTENT = 'mock-key-content-1'
+MOCK_KEY_PASS = 'mock-key-pass-1'
+MOCK_TEMPLATE_NAME = 'mock-template-name-1'
+MOCK_CERT_ACTION = 'import'
+MOCK_CERT_TYPE = 'pem'
