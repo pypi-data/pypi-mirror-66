0546af35cb305ac3fdf00fbcdc09e921a57153d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:45 +0000

    Add package ndstextgen/1.3.0/ndstextgen-1.3.0-py3-none-any.whl

---
+++ packages/ndstextgen/ndstextgen-1.3.0-py3-none-any.whl/ndstextgen/__init__.py
@@ -0,0 +1,6 @@
+def gen(font, text, out="text.png", vert=2, fw=0, color="black", bg="transparent", width=256, height=256, no_crop=False):
+    from . import cli
+    args = [font, text, "--out", out, "--vert", vert, "--fw", fw, "--color", color, "--bg", bg, "--width", width, "--height", height]
+    if no_crop:
+        args.append("--no-crop")
+    cli.gen(args, standalone_mode=False)
