a30c33f71b4ee66416ff5fd34f42c8b18c14a571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:35 +0000

    Add package pyface/7.0.0/pyface-7.0.0.tar.gz

---
+++ packages/pyface/pyface-7.0.0.tar.gz/pyface-7.0.0/pyface/_version.py
@@ -0,0 +1,18 @@
+# (C) Copyright 2005-2020 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+# THIS FILE IS GENERATED FROM PYFACE SETUP.PY
+version = '7.0.0'
+full_version = '7.0.0'
+git_revision = '97e1f255'
+is_released = True
+
+if not is_released:
+    version = full_version
