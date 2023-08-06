0feb8ffcb6c8bfc907869f9fbf335a4b3990567e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:43 +0000

    Add package Chronix2Grid/0.1.0rc4/Chronix2Grid-0.1.0rc4.tar.gz

---
+++ packages/Chronix2Grid/Chronix2Grid-0.1.0rc4.tar.gz/Chronix2Grid-0.1.0rc4/chronix2grid/generation/consumption/consumption_utils.py
@@ -0,0 +1,125 @@
+from scipy.interpolate import interp1d
+import numpy as np
+import pandas as pd
+import os
+from .. import generation_utils as utils
+
+def compute_loads(loads_charac, temperature_noise, params, load_weekly_pattern):
+    # Compute active part of loads
+    weekly_pattern = load_weekly_pattern['test'].values
+    loads_series = {}
+    for i, name in enumerate(loads_charac['name']):
+        mask = (loads_charac['name'] == name)
+        if loads_charac[mask]['type'].values == 'residential':
+            locations = [loads_charac[mask]['x'].values[0], loads_charac[mask]['y'].values[0]]
+            Pmax = loads_charac[mask]['Pmax'].values[0]
+            loads_series[name] = compute_residential(locations, Pmax, temperature_noise, params, weekly_pattern, index=i)
+
+        if loads_charac[mask]['type'].values == 'industrial':
+            raise NotImplementedError("Impossible to generate industrial loads for now.")
+            Pmax = loads_charac[mask]['Pmax'].values[0]
+            loads_series[name] = compute_industrial(Pmax, params)
+    return loads_series
+
+def compute_residential(locations, Pmax, temperature_noise, params, weekly_pattern, index):
+
+
+    # Compute refined signals
+    temperature_signal = utils.interpolate_noise(
+        temperature_noise,
+        params,
+        locations,
+        time_scale=params['temperature_corr'])
+
+    # Compute seasonal pattern
+    Nt_inter = int(params['T'] // params['dt'] + 1)
+    t = np.linspace(0, params['T'], Nt_inter, endpoint=True)
+    start_year = pd.to_datetime(str(params['start_date'].year) + '/01/01', format='%Y-%m-%d')
+    # start_min = int(pd.Timedelta(params['start_date'] - pd.to_datetime('2018/01/01', format='%Y-%m-%d')).total_seconds() // 60)
+    start_min = int(pd.Timedelta(params['start_date'] - start_year).total_seconds() // 60)
+    seasonal_pattern = 5.5/7 + 1.5/7*np.cos((2*np.pi/(365*24*60))*(t-30*24*60 - start_min))
+
+    # Get weekly pattern
+    weekly_pattern = compute_load_pattern(params, weekly_pattern, index)
+    std_temperature_noise = params['std_temperature_noise']
+    residential_series = Pmax * weekly_pattern * (std_temperature_noise*temperature_signal + seasonal_pattern)
+
+    return residential_series
+
+def compute_load_pattern(params, weekly_pattern, index):
+    """
+    Loads a typical hourly pattern, and interpolates it to generate
+    a smooth solar generation pattern between 0 and 1
+
+    Input:
+        computation_params: (dict) Defines the mesh dimensions and
+            precision. Also define the correlation scales
+            interpolation_params: (dict) params of the interpolation
+
+    Output:
+        (np.array) A smooth solar pattern
+    """
+    # solar_pattern resolution : 1H, 8761
+
+    # Keep only one week of pattern
+    index_weekly_perweek = 12 * 24 * 7
+    index %= int(weekly_pattern.shape[0] / index_weekly_perweek - 1)
+    # print(index)
+    weekly_pattern = weekly_pattern[(index * index_weekly_perweek):((index + 1) * index_weekly_perweek)]
+    weekly_pattern /= np.mean(weekly_pattern)
+
+    start_year = pd.to_datetime(str(params['start_date'].year) + '/01/01', format='%Y-%m-%d')
+    T_bis = int(pd.Timedelta(params['end_date'] - start_year).total_seconds() // (60))
+
+    Nt_inter_hr = int(T_bis // 5 + 1)
+    N_repet = int((Nt_inter_hr - 1) // len(weekly_pattern) + 1)
+    stacked_weekly_pattern = weekly_pattern
+    for i in range(N_repet - 1):
+        stacked_weekly_pattern = np.append(stacked_weekly_pattern, weekly_pattern)
+
+    # The time is in minutes
+    t_pattern = np.linspace(0, 60 * 7 * 24 * N_repet, 12 * 7 * 24 * N_repet, endpoint=False)
+    f2 = interp1d(t_pattern, stacked_weekly_pattern, kind='cubic')
+
+    Nt_inter = int(params['T'] // params['dt'] + 1)
+    start_year = pd.to_datetime(str(params['start_date'].year) + '/01/01', format='%Y-%m-%d')
+    start_min = int(pd.Timedelta(params['start_date'] - start_year).total_seconds() // 60)
+    end_min = int(pd.Timedelta(params['end_date'] - start_year).total_seconds() // 60)
+    t_inter = np.linspace(start_min, end_min, Nt_inter, endpoint=True)
+    output = f2(t_inter)
+    output = output * (output > 0)
+
+    return output
+
+def create_csv(dict_, path, forecasted=False, reordering=True, noise=None, shift=False,
+               write_results=True, index=True):
+    df = pd.DataFrame.from_dict(dict_)
+    df.set_index('datetime', inplace = True)
+    df = df.sort_index(ascending=True)
+    df = df.head(len(df) - 1) # Last value is lonely for another day
+    if reordering:
+        value = []
+        for name in list(df):
+            value.append(utils.natural_keys(name))
+        new_ordering = [x for _,x in sorted(zip(value,list(df)))]
+        df = df[new_ordering]
+    if shift:
+        df = df.shift(-1)
+        df = df.fillna(0)
+
+    df_reactive_power = 0.7 * df
+    if noise is not None:
+        df *= (1+noise*np.random.normal(0, 1, df.shape))
+        df_reactive_power *= (1+noise*np.random.normal(0, 1, df.shape))
+
+    if write_results:
+        file_extension = '_forecasted' if forecasted else ''
+        df.to_csv(
+            os.path.join(path, f'load_p{file_extension}.csv.bz2'),
+            index=index, sep=';', float_format='%.1f')
+        df_reactive_power.to_csv(
+            os.path.join(path, f'load_q{file_extension}.csv.bz2'),
+            index=False, sep=';', float_format='%.1f')
+
+    return df
+
---
+++ packages/Chronix2Grid/Chronix2Grid-0.1.0rc4.tar.gz/Chronix2Grid-0.1.0rc4/tests/test_EconomicDispatch.py
@@ -0,0 +1,103 @@
+import datetime as dt
+import json
+import os
+import tempfile
+import unittest
+
+import pandas as pd
+
+from chronix2grid.generation.dispatch.EconomicDispatch import (
+    ChroniXScenario, init_dispatcher_from_config, Dispatcher)
+import grid2op
+from grid2op.Chronics import ChangeNothing
+
+
+class TestDispatch(unittest.TestCase):
+    def setUp(self):
+        self.grid_path = ''
+        self.input_folder = os.path.abspath('chronix2grid/generation/input')
+        self.CASE = 'case118_l2rpn'
+        self.year = 2012
+        self.scenario_name = 'Scenario_0'
+        with open(os.path.join(self.input_folder, self.CASE, 'params_opf.json'), 'r') as params_opf_jons:
+            self.params_opf = json.load(params_opf_jons)
+        self.dispatcher = init_dispatcher_from_config(self.params_opf["grid_path"], self.input_folder)
+        self.hydro_file_path = os.path.join(self.input_folder, 'patterns',
+                                            'hydro_french.csv')
+
+    def test_from_grid2op_env(self):
+        grid2op_env = grid2op.make("blank",
+                                   grid_path=self.params_opf["grid_path"],
+                                   chronics_class=ChangeNothing)
+        dispatcher = Dispatcher.from_gri2op_env(grid2op_env)
+        self.assertTrue(isinstance(dispatcher, Dispatcher))
+
+    def test_read_chronix_scenario(self):
+        base_path = os.path.join(self.input_folder, self.CASE, 'dispatch',
+                                 str(self.year), self.scenario_name)
+        load_path = os.path.join(base_path, 'load_p.csv.bz2')
+        prod_path = os.path.join(base_path, 'prod_p.csv.bz2')
+
+        self.dispatcher.read_load_and_res_scenario(load_path, prod_path,
+                                                   self.scenario_name)
+        self.assertEqual(self.dispatcher.wind_p.iloc[0, 0], 2.0)
+        print(self.dispatcher.solar_p.iloc[0, 0], 0.0)
+
+    def test_read_hydro_guide_curves(self):
+        self.dispatcher.read_hydro_guide_curves(self.hydro_file_path)
+        self.assertAlmostEqual(self.dispatcher._max_hydro_pu.iloc[0, 0],
+                               0.249654, places=5)
+
+
+class TestChronixScenario(unittest.TestCase):
+    def setUp(self):
+        self.input_folder = 'chronix2grid/generation/input'
+        self.case = 'case118_l2rpn'
+        self.grid2op_env = grid2op.make(
+            "blank",
+            grid_path=os.path.join(self.input_folder, self.case,
+                                   "L2RPN_2020_case118_redesigned.json"),
+            chronics_class=ChangeNothing,
+        )
+
+        self.chronics_path_gen = tempfile.mkdtemp()
+        self.loads = pd.DataFrame(
+            index=[dt.datetime(2019, 9, 1, 0, 0), dt.datetime(2019, 9, 1, 0, 5)],
+            columns=['load_1', 'load_2'],
+            data=[[9, 10], [11, 12]]
+        )
+        self.prods = pd.DataFrame(
+            index=[dt.datetime(2019, 9, 1, 0, 0), dt.datetime(2019, 9, 1, 0, 5)],
+            columns=['gen_1', 'gen_2', 'gen_3', 'gen_4'],
+            data=[[1, 2, 3, 4], [5, 6, 7, 8]]
+        )
+        self.loads.to_csv(
+            os.path.join(self.chronics_path_gen, 'load_p.csv.bz2'), sep=';', index=True)
+        self.prods.to_csv(
+            os.path.join(self.chronics_path_gen, 'prod_p.csv.bz2'), sep=';', index=True)
+        self.res_names = dict(wind=['gen_1'], solar=['gen_2', 'gen_4'])
+        self.chronix_scenario = ChroniXScenario(self.loads, self.prods, self.res_names, 'scen')
+
+    def test_instanciation(self):
+        self.assertEqual(self.chronix_scenario.total_res.iloc[0], 7)
+        self.assertEqual(self.chronix_scenario.wind_p.columns, ['gen_1'])
+
+    def test_from_disk(self):
+        chronix_scenario = ChroniXScenario.from_disk(
+            os.path.join(self.chronics_path_gen, 'load_p.csv.bz2'),
+            os.path.join(self.chronics_path_gen, 'prod_p.csv.bz2'),
+            self.res_names, 'scen')
+        self.assertEqual(chronix_scenario.total_res.iloc[0], 7)
+        self.assertEqual(chronix_scenario.wind_p.columns, ['gen_1'])
+
+    def test_net_load(self):
+        net_load = self.chronix_scenario.net_load(50., 'net_load')
+        self.assertEqual(float(net_load.iloc[0]), 21.5)
+
+    def test_simplify_chronix(self):
+        simplified_chronix = self.chronix_scenario.simplify_chronix()
+        self.assertEqual(float(simplified_chronix.solar_p.iloc[0]), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
