dc61d4683777326310b137d151054b660f1312e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:56 +0000

    Add package klib/0.0.70/klib-0.0.70.tar.gz

---
+++ packages/klib/klib-0.0.70.tar.gz/klib-0.0.70/klib/__init__.py
@@ -0,0 +1,22 @@
+"""
+Data Science Module for Python
+==================================
+klib is an easy to use Python library of customized functions for cleaning and analyzing data.
+"""
+
+__author__ = """Andreas Kanz"""
+
+from ._version import __version__
+from .describe import cat_plot, corr_mat, corr_plot, dist_plot, missingval_plot
+from .clean import convert_datatypes, data_cleaning, drop_missing
+
+__version__ = __version__
+
+__all__ = ['cat_plot',
+           'convert_datatypes',
+           'corr_mat',
+           'corr_plot',
+           'data_cleaning',
+           'dist_plot',
+           'drop_missing',
+           'missingval_plot']
