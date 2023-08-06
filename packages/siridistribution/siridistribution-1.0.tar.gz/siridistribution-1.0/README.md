9968ffd84a4cf7809bd7f6bbf9f1eedeca41be99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:42 +0000

    Add package siridistribution/1.0/siridistribution-1.0.tar.gz

---
+++ packages/siridistribution/siridistribution-1.0.tar.gz/siridistribution-1.0/README.md
@@ -0,0 +1,3 @@
+This package is implementation of various distribution functions like Guassian and Binomial
+You can install this package using following command
+pip install sirish@distribution
---
+++ packages/siridistribution/siridistribution-1.0.tar.gz/siridistribution-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name="siridistribution",
+    version="1.0",
+    description="A Python package for Gaussian and Normal distributions",
+    author="Sirish Bhudolia",
+    author_email="sirishbhudolia88@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["siridistribution"]
+    
+)
