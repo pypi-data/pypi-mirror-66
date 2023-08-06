6aee84fbadba2a0a913aca59629464afac29b6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:38 +0000

    Add package spodgi/0.0.4/spodgi-0.0.4.tar.gz

---
+++ packages/spodgi/spodgi-0.0.4.tar.gz/spodgi-0.0.4/setup.py
@@ -0,0 +1,52 @@
+import sys
+
+from setuptools import setup
+
+project = "spodgi"
+version = "0.0.4"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name=project,
+    version=version,
+    description="rdflib extension adding genome graph ODGI back-end store",
+    author="Jerven Bolleman",
+    author_email="me@jerven.eu",
+    url="http://github.com/JervenBolleman/spdogi",
+    packages=["spodgi"],
+    download_url="https://github.com/JervenBolleman/spodgi/master",
+    license="MIT",
+    platforms=["any"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "Click>=7.0.0",
+        "rdflib>=5.0.0"
+    ],
+    setup_requires=[
+    
+    ],
+    tests_require=[
+        "pytest"
+    ],
+    entry_points={
+        'rdf.plugins.store': [
+            'OdgiStore = spodgi.OdgiStore'
+        ]
+    }
+)
---
+++ packages/spodgi/spodgi-0.0.4.tar.gz/spodgi-0.0.4/test/test_select.py
@@ -0,0 +1,57 @@
+import pytest
+import io
+from spodgi import OdgiStore
+
+from rdflib.namespace import RDF
+from rdflib.store import Store
+from rdflib import Graph
+from rdflib import plugin
+from rdflib.collection import Collection
+
+def test_open_odgi():
+    plugin.register('OdgiStore', Store,'spodgi.OdgiStore', 'OdgiStore')
+    s = plugin.get('OdgiStore', Store)(base="http://example.org/test/")
+    spodgi = Graph(store=s)
+    spodgi.open('./test/t.odgi', create=False)
+    count=0;
+    for t in spodgi.triples((None, None, None)):
+        (s, p, o) = t
+        assert s != None
+        assert p != None
+        assert o != None
+        count=count+1
+    assert count > 40
+    spodgi.close()
+
+def test_count_all():
+    plugin.register('OdgiStore', Store,'spodgi.OdgiStore', 'OdgiStore')
+    s = plugin.get('OdgiStore', Store)(base="http://example.org/test/")
+    spodgi = Graph(store=s)
+    spodgi.open('./test/t.odgi', create=False)
+    for r in spodgi.query('SELECT (count(*) as ?count) WHERE {?s ?p ?o}'):
+        assert r[0].value == 241
+        
+    spodgi.close()
+    assert True
+
+def test_select_specific_step():
+    plugin.register('OdgiStore', Store,'spodgi.OdgiStore', 'OdgiStore')
+    s = plugin.get('OdgiStore', Store)(base="http://example.org/test/")
+    spodgi = Graph(store=s)
+    spodgi.open('./test/t.odgi', create=False)
+    for r in spodgi.query('SELECT ?rank WHERE {<path/x/step/2> <http://biohackathon.org/resource/VG#rank> ?rank}'):
+        assert r[0].value == 2
+        
+    spodgi.close()
+    assert True
+
+def test_select_specific_sequence():
+    plugin.register('OdgiStore', Store,'spodgi.OdgiStore', 'OdgiStore')
+    s = plugin.get('OdgiStore', Store)(base="http://example.org/test/")
+    spodgi = Graph(store=s)
+    spodgi.open('./test/t.odgi', create=False)
+    for r in spodgi.query('SELECT ?sequence WHERE {<node/2> rdf:value ?sequence}'):
+        assert r[0].value == 'A'
+        
+    spodgi.close()
+    assert True
