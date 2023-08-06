be1430b40dff56a2301b1b8bc496335ba3df8c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:43 +0000

    Add package maggie_nester/1.0.0/maggie_nester-1.0.0.tar.gz

---
+++ packages/maggie_nester/maggie_nester-1.0.0.tar.gz/maggie_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+        name = 'maggie_nester',
+        version = '1.0.0',
+        py_modules = ['nested_list'],
+        author = 'Meghna',
+        author_email = 'meghna.jain9187@gmail.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+     )
+     
