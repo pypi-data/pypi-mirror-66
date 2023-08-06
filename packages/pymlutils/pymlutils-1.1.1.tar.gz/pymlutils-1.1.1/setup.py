af6c20b835582c8a334e8be4ae7e4b952fd2e904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package pymlutils/1.1.1/pymlutils-1.1.1.tar.gz

---
+++ packages/pymlutils/pymlutils-1.1.1.tar.gz/pymlutils-1.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+  readme = readme_file.read()
+
+requirements = []
+
+setup(
+  name="pymlutils",
+  version="1.1.1",
+  author="Grant Backes",
+  author_email="gsbackes@gmail.com",
+  description="Various functions useful for python and machine learning. Keep your code clean.",
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  url="https://gitlab.com/baka-san/pymlutils",
+  packages=find_packages(),
+  install_requires=requirements,
+  classifiers=[
+      "Programming Language :: Python :: 3.6",
+      "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+  ],
+)
