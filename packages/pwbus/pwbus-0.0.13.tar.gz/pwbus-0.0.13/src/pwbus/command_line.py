02056a497cfdd643b14ab32c1ed3b0a63b2c6dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:13 +0000

    Add package pwbus/0.0.13/pwbus-0.0.13.tar.gz

---
+++ packages/pwbus/pwbus-0.0.13.tar.gz/pwbus-0.0.13/src/pwbus/command_line.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+### PWBus - Main
+#:
+#:  maintainer: fabio.szostak@perfweb.com.br | Tue Nov 19 11:17:54 -03 2019
+
+__author__ = 'fszostak@gmail.com'
+
+from os import environ
+import sys
+import getopt
+import traceback
+
+from pwbus.commons.logging import log_exit
+from pwbus.engines.engine_manager import EngineManager
+
+
+def main():
+    argv = sys.argv[1:]
+
+    channel = None
+    engine = None
+    command = None
+
+    help_message = '\npwbus -f <registry-config-file> [-e <engine>] [-c <channel> start\n\n      -f  <registry-config-file>\n      --registry=<registry-config-file>\n      -e <redis|socket>\n      ----engine=<redis|socket>\n\n      -c <channel_id>\n      --channel=channel_id\n\n      start the pwbus server\n'
+
+    try:
+        opts, args = getopt.getopt(argv, "he:c:", ["start|channel=|engine="])
+    except getopt.GetoptError:
+        print(help_message)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(help_message)
+            sys.exit()
+        elif opt == 'start':
+            command = arg
+        elif opt in ("-e", "--engine"):
+            engine = arg
+        elif opt in ("-c", "--channel"):
+            channel = arg
+        elif opt in ("-f", "--registry-file"):
+            environ['PWBUS_REGISTRY'] = arg
+
+    for arg in args:
+        if arg == "start":
+            command = arg
+
+    try:
+        if command:
+            engine_manager = EngineManager()
+            engine_manager.action(command, engine, channel)
+            sys.exit(0)
+
+    except:
+        sys.exit(1)
+
+    print(help_message)
+
+
+# __main__
+#
+if __name__ == "__main__":
+    main()
