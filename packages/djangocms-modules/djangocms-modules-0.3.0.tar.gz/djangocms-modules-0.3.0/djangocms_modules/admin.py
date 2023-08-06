ab8acdd6ace6b9e0808d3033f482675dbefa5b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:49 +0000

    Add package djangocms-modules/0.3.0/djangocms-modules-0.3.0.tar.gz

---
+++ packages/djangocms-modules/djangocms-modules-0.3.0.tar.gz/djangocms-modules-0.3.0/djangocms_modules/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from cms.admin.placeholderadmin import PlaceholderAdminMixin
+
+from .models import Category
+
+
+class CategoryAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Category, CategoryAdmin)
