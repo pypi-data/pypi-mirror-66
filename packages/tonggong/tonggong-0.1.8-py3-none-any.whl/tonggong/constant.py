fce4449e2b76b2a23c42252ded7572ca4df1c1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:01 +0000

    Add package tonggong/0.1.8/tonggong-0.1.8-py3-none-any.whl

---
+++ packages/tonggong/tonggong-0.1.8-py3-none-any.whl/tonggong/constant.py
@@ -0,0 +1,40 @@
+from .enum import IntEnum, enum
+
+
+@enum.unique
+class DatabaseAdapter(IntEnum):
+    POSTGRESQL = 1  # 默认的数据库类型
+    CLICKHOUSE = 2
+    MYSQL = 3
+    SPARK = 4
+
+    @classmethod
+    def choices(cls):
+        return [
+            (cls.POSTGRESQL.value, "PostgreSQL"),
+            (cls.CLICKHOUSE.value, "ClickHouse"),
+            (cls.MYSQL.value, "MySQL"),
+            (cls.SPARK.value, "Spark"),
+        ]
+
+
+@enum.unique
+class TimeUnit(IntEnum):
+    MINUTES = 1
+    HOURS = 2
+    DAYS = 3
+    WEEKS = 4
+    MONTHS = 5
+    QUARTERS = 6
+    YEARS = 7
+
+    def get_abbreviation(self) -> str:
+        return {
+            self.MINUTES.value: "i",
+            self.HOURS.value: "h",
+            self.DAYS.value: "d",
+            self.WEEKS.value: "w",
+            self.MONTHS.value: "m",
+            self.QUARTERS.value: "q",
+            self.YEARS.value: "y",
+        }.get(self.value)
