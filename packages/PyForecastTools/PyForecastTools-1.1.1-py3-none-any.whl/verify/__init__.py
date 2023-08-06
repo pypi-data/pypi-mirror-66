a822819a368ce54f7317c3cd4d51b1e4db19c554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:46 +0000

    Add package PyForecastTools/1.1.1/PyForecastTools-1.1.1-py3-none-any.whl

---
+++ packages/PyForecastTools/PyForecastTools-1.1.1-py3-none-any.whl/verify/__init__.py
@@ -0,0 +1,23 @@
+"""Module containing verification and performance metrics
+
+With the exception of the ContingencyNxN and Contingency2x2 classes,
+the inputs for all metrics are assumed to be array-like and 1D. Bad
+values are assumed to be stored as NaN and these are excluded in
+metric calculations.
+
+With the exception of the ContingencyNxN and Contingency2x2 classes,
+the inputs for all metrics are assumed to be array-like and 1D. Bad
+values are assumed to be stored as NaN and these are excluded in 
+metric calculations.
+
+Author: Steve Morley
+Institution: Los Alamos National Laboratory
+Contact: smorley@lanl.gov
+Los Alamos National Laboratory
+
+Copyright (c) 2017, Los Alamos National Security, LLC
+All rights reserved.
+"""
+from .metrics import *
+from .categorical import *
+from . import plot
