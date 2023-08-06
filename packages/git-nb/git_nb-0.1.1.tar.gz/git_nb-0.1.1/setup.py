f4c110f144a50e8e25ade2c8f889c91a9877b40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:26 +0000

    Add package git-nb/0.1.1/git_nb-0.1.1.tar.gz

---
+++ packages/git-nb/git_nb-0.1.1.tar.gz/git_nb-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='git_nb',
+    version='0.1.1',
+    description='Python package to execute git command in notebook',
+    long_description=readme(),
+    long_description_content_type="text/x-rst",
+    url='http://github.com/yohann84L/git_nb',
+    keywords="git python repo repository pull clone notebook nb",
+    author='Yohann Lereclus',
+    author_email='lereclus84L@gmail.com',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=['git_nb'],
+    zip_safe=False
+)
