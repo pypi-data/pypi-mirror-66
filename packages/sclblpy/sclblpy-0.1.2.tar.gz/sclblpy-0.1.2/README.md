867f270acd9a8ab6279779f2d53a270d5d9dbf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:50 +0000

    Add package sclblpy/0.1.2/sclblpy-0.1.2.tar.gz

---
+++ packages/sclblpy/sclblpy-0.1.2.tar.gz/sclblpy-0.1.2/README.md
@@ -0,0 +1,185 @@
+# sclblpy
+
+Now automatically publishes to PyPI on committing to master when the commit is tagged with "release".
+
+sclblpy is the core python package provided by Scailable to convert models fit in python to WebAssembly and
+open them up as a REST endpoint. 
+
+sclblpy is only functional in combination with a valid Scailable user account.
+
+- **Website:** [https://www.scailable.net](https://www.scailable.net)
+- **Docs:** [https://docs.sclbl.net/sclblpy](https://docs.sclbl.net/sclblpy)
+- **Get an account:** [https://admin.sclbl.net](https://admin.sclbl.net/signup.html)
+- **Source:**[https://github.com/scailable/sclblpy/](https://github.com/scailable/sclblpy/)
+
+## Background
+The sclblpy package allows users with a valid scailable account (see [https://admin.sclbl.net](https://admin.sclbl.net))
+to upload fitted ML / AI models to the Scailable toolchain server. This will result in:
+
+1. The model being tested on the client side.
+2. The model being uploaded to Scailable, tested again, and if all test pass it will be converted to [WebAssembly](https://webassembly.org).
+3. The model being made available as an easy to access REST endpoint.
+
+## Getting started
+After installing the package using `pip install sclblpy` you can easily fit a ML / AI model using your preferred tools and
+upload it to our toolchains. The following code block provides a simple example:
+
+````
+# Neccesary imports:
+import sclblpy as sp
+
+from sklearn import svm
+from sklearn import datasets
+
+# Start fitting a simple model:
+clf = svm.SVC()
+X, y = datasets.load_iris(return_X_y=True)
+clf.fit(X, y)
+
+# Create an example feature vector (required):
+row = X[130, :]
+
+# Create documentation (optional, but useful):
+docs = {}
+docs['name'] = "My first fitted model"
+docs['documentation'] = "Any documentation you would like to provide."
+
+# Upload the model:
+sp.upload(clf, row, docs=docs)
+````
+
+The call to `sp.upload()` will upload the fitted model, after running a number of local tests, to the 
+Scailable toolchain server and create an associated REST endpoint. Limited user feedback will be printed to show progress,
+and you will receive an email at the email address associated with your account when the conversion is fully completed (which might take a few minutes).
+This email also contains further details regarding the usage of your created endpoint.
+
+Note that upon first upload you will be prompted to provide your Scailable username and password; you can choose to
+store the provided credentials locally to enable easy login on subsequently uploads. (users can signup for an account at
+ [https://admin.sclbl.net](https://admin.sclbl.net/signup.html)).
+ 
+## Examples
+> These examples are merely intended to show the desired syntax for the various packages; we do not intend to fit models
+> that actually have a good predictive performance in these examples.
+
+Currently we support uploading `sklearn`, `statsmodels`, and `xgboost` models (run `sp.list_models()` to print an overview of all supported models). 
+Here we provide an example for each of these.
+
+### sklearn: the elastic net
+
+The [elastic net](https://web.stanford.edu/~hastie/Papers/elasticnet.pdf) provides a flexible regularized model that is 
+useful for many supervised learning tasks.
+
+````
+import sclblpy as sp
+from sklearn import linear_model  # Import linear_model
+from sklearn import datasets  # Import sklearn datasets
+
+iris_data = datasets.load_iris(return_X_y=True)
+X, y = iris_data
+
+mod = linear_model.ElasticNet()  # Instantiate the model
+
+mod.fit(X, y)  # Fit the model
+
+fv = X[0, :]  # An example feature vector
+docs = {'name': "ElasticNet example model", 'documentation' : "Documentation for this model."}
+
+sp.upload(mod, fv, docs)
+
+````
+
+### statsmodels: OLS regression
+
+The statsmodels package provides a number of regression models with flexible error functions. We provide a simple OLS
+example:
+
+```` 
+import sclblpy as sp
+
+import statsmodels.api as sm  # Import statsmodels
+from sklearn import datasets  # Import sklearn datasets
+
+iris_data = datasets.load_iris(return_X_y=True)
+X, y = iris_data
+
+est = sm.OLS(y, X)  # Specify the model
+mod = est.fit()  # Fit the model; note that we send the fitted model to scailable
+
+fv = X[0, :]  # An example feature vector
+docs = {'name': "OLS example model"}
+
+sp.upload(mod, fv, docs=docs)
+````
+
+### xgboost: tree boosting
+
+The xgboost package provides flexibly tree boosting models (see [xgboost](https://dl.acm.org/doi/abs/10.1145/2939672.2939785)).
+This model often performs very well "off the shelf" for many supervised learning tasks.
+
+```` 
+import sclblpy as sp
+
+from xgboost import XGBClassifier  # Import xgboost classifier
+from sklearn import datasets  # Import sklearn datasets
+
+iris_data = datasets.load_iris(return_X_y=True)
+X, y = iris_data
+
+mod = XGBClassifier()  # Instantiate the model
+mod.fit(X, y)  # Fit the model
+
+fv = X[0, :]  # An example feature vector
+docs = {'name': "XGBoost example model"}
+
+sp.upload(mod, fv, docs=docs)
+````
+
+## Additional functionality
+Next to the main ``upload()`` function, the package also exposes the following functions to administer endpoints:
+
+````
+# List all endpoints owned by the current user
+sp.endpoints()
+
+# Remove an endpoint
+sp.delete_endpoint("cfid-cfid-cfid")
+````
+
+Additionally, the following methods are available:
+
+````
+# List all models currently supported by our toolchains:
+sp.list_models()  
+
+# Prevent any user feedback from being printed:
+sp.stop_print()  
+
+# Turn user feedback back on:
+sp.start_print()  
+
+# Remove locally stored user credentials:
+sp.remove_credentials()
+
+````
+
+
+## Dependencies
+
+sclblpy needs python 3, and has been tested on python `> 3.7`. Furthermore, dependent on usage, sclblpy will import
+the following packages:
+
+* `numpy`
+* `requests`
+* `uuid`
+* `sklearn`
+
+The `statsmodels` and `xgboost` packages are imported when used.
+
+## Notes:
+
+* We try to stick to the naming conventions in [http://google.github.io/styleguide/pyguide.html](http://google.github.io/styleguide/pyguide.html).
+* The methods `_set_toolchain_URL(string)` and `_set_usermanager_URL(string)` can be used to change the default location of
+the toolchain and user-management function. These are useful when running the Scailable stack locally. Also the method `_toggle_debug_mode()` can
+be used for troubleshooting (this will raise exceptions and provide a trace upon errors).
+
+For more information please contact us at [go@scailable.net](mailto:go@scailable.net).
---
+++ packages/sclblpy/sclblpy-0.1.2.tar.gz/sclblpy-0.1.2/test/test_all_models.py
@@ -0,0 +1,581 @@
+# Testing the upload function for all supported models.
+# This might take a while..
+import time
+import statsmodels.api as sm
+
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn import tree
+from sklearn import ensemble
+from sklearn import svm
+
+from xgboost import XGBClassifier
+from xgboost import XGBRegressor
+from xgboost import XGBRFClassifier
+from xgboost import XGBRFRegressor
+
+from sclblpy import upload, _set_toolchain_URL, _set_usermanager_URL
+from sclblpy import endpoints
+from sclblpy import delete_endpoint
+
+# Script settings:
+from sclblpy.main import _toggle_debug_mode, stop_print
+
+RUN_TESTS = False  # Prevent unintended testing
+DEBUG = False  # Set to debug mode; if true it will raise exceptions
+PRINTING = True  # Toggle printing on and off.
+TEAR_DOWN = False  # Remove all endpoints after running tests (you will be prompted)?
+ADMIN_URL = "http://localhost:8008"  # Location of admin for this test
+TOOLCHAIN_URL = "http://localhost:8010"  # Location of toolchain for this test
+SLEEPTIME = 2  # Time in between blocks of models.
+
+
+iris_data = datasets.load_iris(return_X_y=True)
+
+
+# StatsModels
+def test_sm_OLS():
+    X, y = iris_data
+    est = sm.OLS(y, X)
+    mod = est.fit()
+    docs = {'name': "OLS test"}
+    fv = X[0, :]
+    upload(mod, fv, docs=docs)
+    print("Tested SM, OLS...")
+
+
+def test_sm_GLS():
+    data = sm.datasets.longley.load(as_pandas=False)
+    X = sm.add_constant(data.exog)
+    est = sm.GLS(data.endog, X, sigma=1)
+    mod = est.fit()
+    docs = {'name': "GLS test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested SM, GLS...")
+
+
+def test_sm_WLS():
+    Y = [1, 3, 4, 5, 2, 3, 4]
+    X = range(1, 8)
+    X = sm.add_constant(X)
+    est = sm.WLS(Y, X, weights=list(range(1, 8)))
+    mod = est.fit()
+    docs = {'name': "WLS test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested SM, WLS...")
+
+
+# Sklearn
+def test_sk_ARDRegression():
+    mod = linear_model.ARDRegression()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "ARDRegression test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, ARDRegression...")
+
+
+def test_sk_DecisionTreeClassifier():
+    mod = tree.DecisionTreeClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "DecisionTreeClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, DecisionTreeClassifier...")
+
+
+def test_sk_DecisionTreeRegressor():
+    mod = tree.DecisionTreeRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "DecisionTreeClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, DecisionTreeRegressor...")
+
+
+def test_sk_ElasticNet():
+    mod = linear_model.ElasticNet()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "ElasticNet test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, ElasticNet...")
+
+
+def test_sk_ElasticNetCV():
+    mod = linear_model.ElasticNetCV()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "ElasticNetCV test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, ElasticNetCV...")
+
+
+def test_sk_ExtraTreeClassifier():
+    mod = tree.ExtraTreeClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "ExtraTreeClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, ExtraTreeClassifier...")
+
+
+def test_sk_ExtraTreeRegressor():
+    mod = tree.ExtraTreeRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "ExtraTreeRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, ExtraTreeRegressor...")
+
+
+def test_sk_ExtraTreesClassifier():
+    mod = ensemble.ExtraTreesClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "ExtraTreeClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, Ensemble ExtraTreeClassifier...")
+
+
+def test_sk_ExtraTreesRegressor():
+    mod = ensemble.ExtraTreesRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "ExtraTreeRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, Ensemble ExtraTreeRegressor...")
+
+
+def test_sk_HuberRegressor():
+    mod = linear_model.HuberRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "HuberRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, HuberRegressor...")
+
+
+def test_sk_Lars():
+    mod = linear_model.Lars()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "Lars test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, Lars...")
+
+
+def test_sk_LarsCV():
+    mod = linear_model.LarsCV()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LarsCV test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LarsCV...")
+
+
+def test_sk_Lasso():
+    mod = linear_model.Lasso()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "Lasso test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, Lasso...")
+
+
+def test_sk_LassoCV():
+    mod = linear_model.LassoCV()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LassoCV test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LassoCV...")
+
+
+def test_sk_LassoLars():
+    mod = linear_model.LassoLars()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LassoLars test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LassoLars...")
+
+
+def test_sk_LassoLarsCV():
+    mod = linear_model.LassoLarsCV()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LassoLarsCV test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LassoLarsCV...")
+
+
+def test_sk_LassoLarsIC():
+    mod = linear_model.LassoLarsIC()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LassoLarsIC test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LassoLarsIC...")
+
+
+def test_sk_LinearRegression():
+    mod = linear_model.LinearRegression()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LinearRegression test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LinearRegression...")
+
+
+def test_sk_LinearSVC():
+    mod = svm.LinearSVC(max_iter=10000)  # Needs more iterations to converge
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LinearSVC test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LinearSVC...")
+
+
+def test_sk_LinearSVR():
+    mod = svm.LinearSVR(max_iter=100000)  # Needs way more iterations to converge
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "LinearSVR test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, LinearSVR...")
+
+
+def test_sk_NuSCV():
+    mod = svm.NuSVC(max_iter=10000)  # Needs more iterations to converge
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "NuSCV test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, NuSCV...")
+
+
+def test_sk_NuSVR():
+    mod = svm.NuSVR(max_iter=10000)  # Needs more iterations to converge
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "NuSVR test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, NuSVR...")
+
+
+def test_sk_OrthogonalMatchingPursuit():
+    mod = linear_model.OrthogonalMatchingPursuit()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "OrthogonalMatchingPursuit test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, OrthogonalMatchingPursuit...")
+
+
+def test_sk_OrthogonalMatchingPursuitCV():
+    mod = linear_model.OrthogonalMatchingPursuitCV()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "OrthogonalMatchingPursuitCV test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, OrthogonalMatchingPursuitCV...")
+
+
+def test_sk_PassiveAggressiveClassifier():
+    mod = linear_model.PassiveAggressiveClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "PassiveAggressiveClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, PassiveAggressiveClassifier...")
+
+
+def test_sk_PassiveAggressiveRegressor():
+    mod = linear_model.PassiveAggressiveRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "PassiveAggressiveRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, PassiveAggressiveRegressor...")
+
+
+def test_sk_RandomForestClassifier():
+    mod = ensemble.RandomForestClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "RandomForestClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, RandomForestClassifier...")
+
+
+def test_sk_RandomForestRegressor():
+    mod = ensemble.RandomForestRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "RandomForestRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, RandomForestRegressor...")
+
+
+def test_sk_RANSACRegressor():
+    mod = linear_model.RANSACRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "RANSACRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, RANSACRegressor...")
+
+
+def test_sk_Ridge():
+    mod = linear_model.Ridge()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "Ridge test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, Ridge...")
+
+
+def test_sk_RidgeCV():
+    mod = linear_model.RidgeCV()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "RidgeCV test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, RidgeCV...")
+
+
+def test_sk_SGDClassifier():
+    mod = linear_model.SGDClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "SGDClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, SGDClassifier...")
+
+
+def test_sk_SGDRegressor():
+    mod = linear_model.SGDRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "SGDRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, SGDRegressor...")
+
+
+def test_sk_SVC():
+    mod = svm.SVC()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "SVC test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, SVC...")
+
+
+def test_sk_SVR():
+    mod = svm.SVR()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "SVR test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, SVR...")
+
+
+def test_sk_TheilSenRegressor():
+    mod = linear_model.TheilSenRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "TheilSenRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested sklearn, TheilSenRegressor...")
+
+
+# XGBoost package
+def test_xg_XGBClassifier():
+    mod = XGBClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "XGBClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested xgboost, XGBClassifier...")
+
+
+def test_xg_XGBRegressor():
+    mod = XGBRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "XGBRegressor test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested xgboost, XGBRegressor...")
+
+
+def test_xg_XGBRFClassifier():
+    mod = XGBRFClassifier()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "XGBRFClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested xgboost, XGBRFClassifier...")
+
+
+def test_xg_XGBRFRegressor():
+    mod = XGBRFRegressor()
+    X, y = iris_data
+    mod.fit(X, y)
+    docs = {'name': "XGBRFXGBRFRegressorClassifier test"}
+    fv = X[0, :]
+    upload(mod, fv, docs)
+    print("Tested xgboost, XGBRFRegressor...")
+
+
+if __name__ == '__main__':
+
+    if not RUN_TESTS:
+        print("Not running tests.")
+        exit()
+
+    if not PRINTING:
+        stop_print()
+
+    if DEBUG:
+        _toggle_debug_mode()
+
+    # Remove:
+    # remove_credentials()
+
+    # Set correct endpoints
+    _set_toolchain_URL(TOOLCHAIN_URL)
+    _set_usermanager_URL(ADMIN_URL)
+
+    print("Running tests off all supported models. This might take some time")
+    print("===============================")
+
+    print("# Statsmodels:")
+    test_sm_OLS()
+    test_sm_GLS()
+    test_sm_WLS()
+
+    time.sleep(SLEEPTIME)
+    print("# SciKit Learn:")
+    test_sk_ARDRegression()
+    test_sk_DecisionTreeClassifier()
+    test_sk_DecisionTreeRegressor()
+    test_sk_ElasticNet()
+
+    time.sleep(SLEEPTIME)
+    test_sk_ElasticNetCV()
+    test_sk_ExtraTreeClassifier()
+    test_sk_ExtraTreeRegressor()
+    test_sk_ExtraTreesClassifier()
+    test_sk_ExtraTreesRegressor()
+
+    time.sleep(SLEEPTIME)
+    test_sk_HuberRegressor()
+    test_sk_Lars()
+    test_sk_LarsCV()
+    test_sk_Lasso()
+    test_sk_LassoCV()
+
+    time.sleep(SLEEPTIME)
+    test_sk_LassoLars()
+    test_sk_LassoLarsCV()
+    test_sk_LassoLarsIC()
+    test_sk_LinearRegression()
+    test_sk_LinearSVC()
+
+    time.sleep(SLEEPTIME)
+    test_sk_LinearSVR()
+    test_sk_NuSCV()
+    test_sk_NuSVR()
+    test_sk_OrthogonalMatchingPursuit()
+    test_sk_OrthogonalMatchingPursuitCV()
+
+    time.sleep(SLEEPTIME)
+    test_sk_PassiveAggressiveClassifier()
+    test_sk_PassiveAggressiveRegressor()
+    test_sk_RandomForestClassifier()
+    test_sk_RandomForestClassifier()
+    test_sk_RANSACRegressor()
+
+    time.sleep(SLEEPTIME)
+    test_sk_Ridge()
+    test_sk_RidgeCV()
+    test_sk_SGDClassifier()
+    test_sk_SGDRegressor()
+    test_sk_SVC()
+
+    time.sleep(SLEEPTIME)
+    test_sk_SVR()
+    test_sk_TheilSenRegressor()
+
+    time.sleep(SLEEPTIME)
+    print("# XGBoost Learn:")
+    test_xg_XGBRegressor()
+    test_xg_XGBClassifier()
+    test_xg_XGBRFClassifier()
+    test_xg_XGBRFRegressor()
+
+    print("===============================")
+    print("All tests passed.")
+
+    if TEAR_DOWN:
+
+        while True:
+            query = input('Are you sure you would like to remove all endpoints? (y/n) ')
+            answ = query[0].lower()
+            if query == '' or not answ in ['y', 'n']:
+                print('Please answer with yes or no')
+            else:
+                break
+
+        if answ == 'y':
+            endpoints = endpoints()
+            # Loop with pause and remove...
+            for key in endpoints:
+                print('Delete: {}, cfid: {}.'.format(key['name'], key['cfid']))
+                delete_endpoint(key['cfid'])
+                time.sleep(.5)
+
+    print("===============================")
+    print("Done.")
