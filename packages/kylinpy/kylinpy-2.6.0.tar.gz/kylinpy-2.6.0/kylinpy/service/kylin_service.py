01a872daf578ca13723dca51511e6fb1e8f6f1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:58 +0000

    Add package kylinpy/2.6.0/kylinpy-2.6.0.tar.gz

---
+++ packages/kylinpy/kylinpy-2.6.0.tar.gz/kylinpy-2.6.0/kylinpy/service/kylin_service.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from kylinpy.client import InternalServerError, UnauthorizedError
+from kylinpy.exceptions import KylinQueryError
+from ._service_interface import ServiceInterface
+
+
+class _Api(object):
+    """
+    For testing convenience
+    """
+    @staticmethod
+    def query(client, endpoint, **kwargs):
+        return client.post(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def tables_and_columns(client, endpoint, **kwargs):
+        return client.get(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def projects(client, endpoint, **kwargs):
+        return client.get(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def jobs(client, endpoint, **kwargs):
+        return client.get(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def resume_job(client, endpoint, **kwargs):
+        return client.put(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def tables(client, endpoint, **kwargs):
+        return client.get(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def cube_desc(client, endpoint, **kwargs):
+        return client.get(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def models(client, endpoint, **kwargs):
+        return client.get(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def cubes(client, endpoint, **kwargs):
+        return client.get(endpoint=endpoint, **kwargs).json()
+
+    @staticmethod
+    def authentication(client, endpoint, **kwargs):
+        rv = client.get(endpoint=endpoint, **kwargs).json()
+        if rv == {}:
+            raise UnauthorizedError
+        return rv
+
+
+class KylinService(ServiceInterface):
+    api = _Api
+
+    def __init__(self, client, project=None):
+        self.client = client
+        self.project = project
+
+    def query(self, sql, limit=50000, offset=0, acceptPartial=False, **kwargs):
+        json_data = {
+            'acceptPartial': acceptPartial,
+            'limit': limit,
+            'offset': offset,
+            'project': self.project,
+            'sql': sql,
+        }
+        kwargs.setdefault('json', json_data)
+        try:
+            response = self.api.query(self.client, '/query', **kwargs)
+        except InternalServerError as err:
+            raise KylinQueryError(err)
+
+        err_message = response.get('exceptionMessage')
+        if err_message:
+            raise KylinQueryError(err_message)
+
+        return response
+
+    def projects(self, **kwargs):
+        params = {
+            'pageOffset': 0,
+            'pageSize': 1000,
+        }
+        kwargs.setdefault('params', params)
+        _projects = self.api.projects(self.client, '/projects', **kwargs)
+        return _projects
+
+    def jobs(self, **kwargs):
+        params = {
+            'offset': 0,
+        }
+        kwargs.setdefault('params', params)
+        _jobs = self.api.jobs(self.client, '/jobs', **kwargs)
+        return _jobs
+
+    def resume_job(self, job_id, **kwargs):
+        params = {
+            'jobId': job_id,
+        }
+        kwargs.setdefault('params', params)
+        res = self.api.resume_job(self.client, '/jobs/{0}/resume'.format(job_id), **kwargs)
+        return res
+
+    def tables_and_columns(self, **kwargs):
+        params = {
+            'project': self.project,
+        }
+        kwargs.setdefault('params', params)
+        resp = self.api.tables_and_columns(self.client, '/tables_and_columns', **kwargs)
+        tbl_pair = tuple(('{}.{}'.format(tbl.get('table_SCHEM'), tbl.get('table_NAME')), tbl) for tbl in resp)
+        for tbl in tbl_pair:
+            tbl[1]['columns'] = [(col['column_NAME'], col) for col in tbl[1]['columns']]
+        return dict(tbl_pair)
+
+    def tables_in_hive(self, **kwargs):
+        params = {
+            'project': self.project,
+            'ext': True,
+        }
+        kwargs.setdefault('params', params)
+        tables = self.api.tables(self.client, '/tables', **kwargs)
+
+        __tables_in_hive = {}
+        for tbl in tables:
+            db = tbl['database']
+            name = tbl['name']
+            fullname = '{}.{}'.format(db, name)
+            __tables_in_hive[fullname] = tbl
+
+        return __tables_in_hive
+
+    def cube_desc(self, name, **kwargs):
+        return self.api.cube_desc(self.client, '/cube_desc/{}/desc'.format(name), **kwargs)
+
+    def model_desc(self, name, **kwargs):
+        return [_ for _ in self.models(**kwargs) if _.get('name') == name][0]
+
+    def models(self, **kwargs):
+        params = {
+            'projectName': self.project,
+            'pageOffset': 0,
+            'pageSize': 1000,
+        }
+        kwargs.setdefault('params', params)
+        return self.api.models(self.client, '/models', **kwargs)
+
+    def cubes(self, **kwargs):
+        params = {
+            'pageOffset': 0,
+            'offset': 0,
+            'limit': 50000,
+            'pageSize': 1000,
+            'projectName': self.project,
+        }
+        kwargs.setdefault('params', params)
+        return self.api.cubes(self.client, '/cubes', **kwargs)
+
+    def get_authentication(self, **kwargs):
+        rv = self.api.authentication(self.client, '/user/authentication', **kwargs)
+        return rv.get('userDetails')
---
+++ packages/kylinpy/kylinpy-2.6.0.tar.gz/kylinpy-2.6.0/kylinpy/utils/compat.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+import time
+
+try:
+    # Python 3
+    import urllib.request as urllib  # noqa
+    from urllib.request import urlparse  # noqa
+    from urllib.request import HTTPSHandler  # noqa
+    from urllib.parse import urlencode  # noqa
+    from urllib.parse import parse_qsl  # noqa
+    from urllib.error import HTTPError  # noqa
+except ImportError:
+    # Python 2
+    import urllib2 as urllib  # noqa
+    from urlparse import urlparse  # noqa
+    from urlparse import parse_qsl  # noqa
+    from urllib2 import HTTPError  # noqa
+    from urllib2 import HTTPSHandler  # noqa
+    from urllib import urlencode  # noqa
+
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    string_types = str
+    integer_types = int
+    class_types = type
+    text_type = str
+    binary_type = bytes
+
+    def as_unicode(s):
+        if isinstance(s, bytes):
+            return s.decode('utf-8')
+        return str(s)
+
+else:
+    import types
+    string_types = basestring  # noqa
+    integer_types = (int, long)  # noqa
+    class_types = (type, types.ClassType)  # noqa
+    text_type = unicode  # noqa
+    binary_type = str
+
+    def as_unicode(s):
+        if isinstance(s, str):
+            return s.decode('utf-8')
+        return unicode(s)  # noqa
+
+
+def to_seconds(date):
+    """ return timestamp for date object compatible py2 """
+    return time.mktime(date.timetuple())
