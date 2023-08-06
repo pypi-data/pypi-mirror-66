0d5ee47ba9da7276b9a650e5e94124ce641da376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:25 +0000

    Add package transcov/1.0.5/transcov-1.0.5-py3-none-any.whl

---
+++ packages/transcov/transcov-1.0.5-py3-none-any.whl/transcov/__init__.py
@@ -0,0 +1,18 @@
+from .generator import (
+    generate_coverage_matrix,
+    generate_read_ends_matrix,
+    generate_length_matrix,
+)
+from .preprocessor import preprocess
+from .manipulations import collapse, pick_subset
+
+__version__ = "1.0.5"
+
+__all__ = (
+    "generate_coverage_matrix",
+    "generate_read_ends_matrix",
+    "generate_length_matrix",
+    "preprocess",
+    "collapse",
+    "pick_subset",
+)
