335b9573d86686012e2dfc093a546132e385e9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:00 +0000

    Add package comicom/1.2.2/comicom-1.2.2.tar.gz

---
+++ packages/comicom/comicom-1.2.2.tar.gz/comicom-1.2.2/tests/min-page-height/test_min_height.py
@@ -0,0 +1,30 @@
+import unittest
+import tests
+
+from comiccompiler import compiler
+
+
+class MinHeightTests(tests.ComicomTestCase):
+    def test_default_args(self):
+        self.setup_test_vars("min-page-height", "Compiled-default")
+        self.args.min_height_per_page = 300
+        compiler.run(self.args)
+        self.compare_output()
+
+    def test_breakpoint_unset(self):
+        self.setup_test_vars("min-page-height", "Compiled-default")
+        self.args.min_height_per_page = 300
+        self.args.breakpoint_detection_mode = -1
+        compiler.run(self.args)
+        self.compare_output()
+
+    def test_breakpoint_dynamic(self):
+        self.setup_test_vars("min-page-height", "Compiled-b1")
+        self.args.min_height_per_page = 300
+        self.args.breakpoint_detection_mode = 1
+        compiler.run(self.args)
+        self.compare_output()
+
+
+if __name__ == '__main__':
+    unittest.main()
