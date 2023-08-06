f5374b2edc23488f8257989e218b9216c3720a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:42 +0000

    Add package algorithmx/1.1.3/algorithmx-1.1.3-py3-none-any.whl

---
+++ packages/algorithmx/algorithmx-1.1.3-py3-none-any.whl/algorithmx/nbextension/__init__.py
@@ -0,0 +1,7 @@
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'nbextension/static',
+        'dest': 'algorithmx-jupyter',
+        'require': 'algorithmx-jupyter/extension'
+    }]
