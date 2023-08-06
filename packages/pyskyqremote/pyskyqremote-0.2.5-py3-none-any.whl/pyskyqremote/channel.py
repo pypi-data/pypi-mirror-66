dd2af45eca440d1df48ed6d226a08ebca75713da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:41 +0000

    Add package pyskyqremote/0.2.5/pyskyqremote-0.2.5-py3-none-any.whl

---
+++ packages/pyskyqremote/pyskyqremote-0.2.5-py3-none-any.whl/pyskyqremote/channel.py
@@ -0,0 +1,12 @@
+
+class Programme:
+    def __init__(
+        self, programmeuuid, starttime, endtime, title, season, episode, imageUrl
+    ):
+        self.progammeuuid = programmeuuid
+        self.starttime = starttime
+        self.endtime = endtime
+        self.title = title
+        self.season = season
+        self.episode = episode
+        self.imageUrl = imageUrl
