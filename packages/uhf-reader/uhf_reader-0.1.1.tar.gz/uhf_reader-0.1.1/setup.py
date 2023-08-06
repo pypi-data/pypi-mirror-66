f1bcdb1a32e5fa33928b782aa802b6ca811d67f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:23 +0000

    Add package uhf-reader/0.1.1/uhf_reader-0.1.1.tar.gz

---
+++ packages/uhf-reader/uhf_reader-0.1.1.tar.gz/uhf_reader-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='uhf_reader',
+    version='0.1.1',
+    packages=['uhf_reader'],
+    url='https://github.com/madprogrammer/mr6100-python',
+    license='MIT',
+    author='Sergey Anufrienko',
+    author_email='serg@anufrienko.net',
+    description='Library to interface with Marktrace MR6100 series UHF RFID reader',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
