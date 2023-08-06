08721980418f4fa54118082ee483873778520141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:14 +0000

    Add package dsnd-probability-ir-2020/0.1/dsnd_probability_ir_2020-0.1.tar.gz

---
+++ packages/dsnd-probability-ir-2020/dsnd_probability_ir_2020-0.1.tar.gz/dsnd_probability_ir_2020-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_ir_2020',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_ir_2020'],
+      author='Ibad Rather',
+      author_email='ibad.rather.ir@gmail.com', 
+      zip_safe=False)
