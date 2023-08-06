731544a95b2eef361c95021b78e595f24ff3f170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:05 +0000

    Add package multi-file/0.0.1/multi-file-0.0.1.tar.gz

---
+++ packages/multi-file/multi-file-0.0.1.tar.gz/multi-file-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="multi-file",
+    version="0.0.1",
+    author="Casey Marks",
+    author_email="casey@carveacre.com",
+    description="put multiple fileobjs inside a fileobj",
+    packages=['multi_file'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
