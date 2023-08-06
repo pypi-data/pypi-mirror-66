0f5cab72843d7eb0358c270d08e885909fe5d78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:27 +0000

    Add package flake8-django/1.0.0/flake8-django-1.0.0.tar.gz

---
+++ packages/flake8-django/flake8-django-1.0.0.tar.gz/flake8-django-1.0.0/flake8_django/checkers/__init__.py
@@ -0,0 +1,12 @@
+from .model_content_order import ModelContentOrderChecker
+from .model_dunder_str import ModelDunderStrMissingChecker
+from .model_fields import ModelFieldChecker
+from .model_form import ModelFormChecker
+from .render import RenderChecker
+from .urls import URLChecker
+from .model_meta import ModelMetaChecker
+
+__all__ = [
+    'ModelDunderStrMissingChecker', 'ModelFieldChecker', 'ModelFormChecker',
+    'RenderChecker', 'URLChecker', 'ModelMetaChecker', 'ModelContentOrderChecker'
+]
