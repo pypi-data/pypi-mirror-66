2ecba93ba19050f6fc8e49c9c9deb2690664c2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:37 +0000

    Add package python-schema-registry-client/1.2.10/python-schema-registry-client-1.2.10.tar.gz

---
+++ packages/python-schema-registry-client/python-schema-registry-client-1.2.10.tar.gz/python-schema-registry-client-1.2.10/tests/client/test_schema.py
@@ -0,0 +1,21 @@
+import fastavro
+import pytest
+
+from schema_registry.client import schema
+from tests import data_gen
+
+
+def test_schema_from_string():
+    parsed = schema.AvroSchema(data_gen.BASIC_SCHEMA)
+
+    assert isinstance(parsed, schema.AvroSchema)
+
+
+def test_schema_from_file():
+    parsed = schema.load(data_gen.get_schema_path("adv_schema.avsc"))
+    assert isinstance(parsed, schema.AvroSchema)
+
+
+def test_schema_load_parse_error():
+    with pytest.raises(fastavro.schema.UnknownType):
+        schema.load(data_gen.get_schema_path("invalid_schema.avsc"))
