0df0b673b1712d59fe573817ea4520feaed9b2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:43 +0000

    Add package gauss-bin-prob-distrib/0.1/gauss_bin_prob_distrib-0.1.tar.gz

---
+++ packages/gauss-bin-prob-distrib/gauss_bin_prob_distrib-0.1.tar.gz/gauss_bin_prob_distrib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_bin_prob_distrib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_bin_prob_distrib'],
+      author= 'Boukary Ouedraogo',
+      author_email='abhou16@gmail.com',
+      zip_safe=False)
