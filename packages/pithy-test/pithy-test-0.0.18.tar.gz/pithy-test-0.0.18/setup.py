7ee6da1caf56284995d137d46120cda4bc541fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:51 +0000

    Add package pithy-test/0.0.18/pithy-test-0.0.18.tar.gz

---
+++ packages/pithy-test/pithy-test-0.0.18.tar.gz/pithy-test-0.0.18/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="pithy-test",
+    version="0.0.18",
+    keywords=("interface", "automation", "testing", "pithy"),
+    description=u"简化接口测试",
+    long_description=u"简化接口测试",
+    url="https://github.com/yuyu1987/pithy-test",
+    author="coolfish",
+    author_email="hgbaczt@gmail.com",
+    packages=['pithy'],
+    include_package_data=True,
+    platforms="any",
+    install_requires=['requests', 'records', 'objectpath', 'click',
+                      'jinja2', 'pyyaml', 'configobj'],
+    entry_points='''
+    [console_scripts]
+    pithy-cli=pithy.cli:cli
+    '''
+)
