fd82ad6a529ed7bae1932e163d3b0933207e839f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:04 +0000

    Add package lisflood-utilities/0.12.2/lisflood-utilities-0.12.2.tar.gz

---
+++ packages/lisflood-utilities/lisflood-utilities-0.12.2.tar.gz/lisflood-utilities-0.12.2/src/lisfloodutilities/cutmaps/cutlib.py
@@ -0,0 +1,136 @@
+"""
+
+Copyright 2019 European Union
+
+Licensed under the EUPL, Version 1.2 or as soon they will be approved by the European Commission  subsequent versions of the EUPL (the "Licence");
+
+You may not use this work except in compliance with the Licence.
+You may obtain a copy of the Licence at:
+
+https://joinup.ec.europa.eu/sites/default/files/inline-files/EUPL%20v1_2%20EN(1).txt
+
+Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the Licence for the specific language governing permissions and limitations under the Licence.
+
+"""
+
+import os
+import sys
+import logging
+from pathlib import Path
+
+import xarray as xr
+import numpy as np
+
+from lisfloodutilities.readers import PCRasterMap
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def cutmap(f, fileout, x_min, x_max, y_min, y_max):
+    try:
+        nc = xr.open_dataset(f, chunks={'time': 100}, decode_cf=False)
+        num_dims = 3
+    except Exception:  # file has no time component
+        num_dims = 2
+        nc = xr.open_dataset(f)
+    var = [v for v in nc.variables if len(nc.variables[v].dims) == num_dims][0]
+    logger.info('Variable: %s', var)
+
+    if 'lat' in nc.variables:
+        nc.variables[var].attrs['esri_pe_string'] = 'GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137,298.257223563]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.0174532925199433]]"'
+    else:
+        nc.variables[var].attrs['esri_pe_string'] = 'PROJCS["ETRS_1989_LAEA",GEOGCS["GCS_ETRS_1989",DATUM["D_ETRS_1989",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Lambert_Azimuthal_Equal_Area"],PARAMETER["False_Easting",4321000.0],PARAMETER["False_Northing",3210000.0],PARAMETER["Central_Meridian",10.0],PARAMETER["Latitude_Of_Origin",52.0],UNIT["Meter",1.0]]'
+        nc.variables[var].attrs['proj4_params'] = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"
+    if isinstance(x_min, float):
+        # bounding box input from user
+        sliced_var = _cut_from_coords(nc, var, x_min, x_max, y_min, y_max)
+    else:
+        # user provides with indices directly (not coordinates)
+        sliced_var = _cut_from_indices(nc, var, x_min, x_max, y_min, y_max)
+
+    if sliced_var is not None:
+        logger.info('Creating: %s', fileout)
+        sliced_var.to_netcdf(fileout)
+    nc.close()
+
+
+def _cut_from_indices(nc, var, x_min, x_max, y_min, y_max):
+    # note: netcdf has lats on first dimension e.g. y_min:y_max are Y/lat dimension indices
+    # that in nc file are stored on first dimension: ta(time, lat, lon)
+    # you can always adjust indices in input in order to match your nc files structure
+    if 'time' in nc.variables:
+        sliced_var = nc[var][:, y_min:y_max + 1, x_min:x_max + 1]
+    else:
+        sliced_var = nc[var][y_min:y_max + 1, x_min:x_max + 1]
+    return sliced_var
+
+
+def _cut_from_coords(nc, var, x_min, x_max, y_min, y_max):
+    # we have coordinates bounds and not indices yet
+    lats = nc.variables['lat'][:]
+    lons = nc.variables['lon'][:]
+    # find indices
+    ys = np.where((lats >= y_min) & (lats <= y_max))[0]
+    xs = np.where((lons >= x_min) & (lons <= x_max))[0]
+    if 'time' in nc.variables:
+        sliced_var = nc[var][:, ys, xs]
+    else:
+        try:
+            sliced_var = nc[var][ys, xs]
+        except IndexError:
+            # it happens when cutting lat or lon netcdf files (not 2D array)
+            if var not in ('lats', 'lat', 'latitude', 'latitudes', 'x', 'y', 'lons', 'longitude', 'longitudes', 'lon'):
+                raise ValueError('Cannot find main variable to cut')
+
+            sliced_var = nc[var][ys] if var in ('lats', 'lat', 'latitude', 'latitudes', 'y') else nc[var][xs]
+    return sliced_var
+
+
+def get_filelist(filelist=None, input_folder=None, glofas_folder=None):
+    list_to_cut = []
+    if filelist:
+        list_to_cut = open(filelist).readlines()
+        list_to_cut = [Path(l.strip()) for l in list_to_cut]
+    elif input_folder:
+        list_to_cut = [f for f in Path(input_folder).glob('**/*.nc')]
+    elif glofas_folder:
+        list_to_cut = [f for f in Path(glofas_folder).glob('**/*')]
+    logger.info('==================> Going to cut %d files', len(list_to_cut))
+    return list_to_cut
+
+
+def get_cuts(cuts=None, mask=None):
+    if mask:
+        if not os.path.isfile(mask):
+            raise FileNotFoundError('Wrong input mask: %s not a file' % mask)
+        maskname, ext = os.path.splitext(mask)
+        if ext == '.map':
+            mask = PCRasterMap(mask)
+            lats = mask.lats
+            lons = mask.lons
+            mask.close()
+            x_min, x_max = float(np.min(lons)), float(np.max(lons))
+            y_min, y_max = float(np.min(lats)), float(np.max(lats))
+        elif ext == '.nc':
+            maskmap = xr.open_dataset(mask)
+            latitudes = maskmap['lat'][:]
+            longitudes = maskmap['lon'][:]
+            y_min, y_max = float(np.min(latitudes)), float(np.max(latitudes))
+            x_min, x_max = float(np.min(longitudes)), float(np.max(longitudes))
+        else:
+            logger.error('Mask map format not recognized. Must be either .map or .nc. Found %s', ext)
+            sys.exit(1)
+    elif cuts:
+        # user provided coordinates bounds
+        x, y = cuts.split(':')
+        apply = float if '.' in x or '.' in y else int  # user can provide coords (float) or matrix indices bbox (int)
+        x_min, x_max = list(map(apply, x.split('_')))
+        y_min, y_max = list(map(apply, y.split('_')))
+    else:
+        logger.error('You must provide either cuts (in the format minlon_maxlon:minlat_maxlat) or a mask map')
+        sys.exit(1)
+    logger.info('CUTS: \nmin x: %s \nmax x: %s \nmin y: %s \nmax y: %s', x_min, x_max, y_min, y_max)
+    return x_min, x_max, y_min, y_max
---
+++ packages/lisflood-utilities/lisflood-utilities-0.12.2.tar.gz/lisflood-utilities-0.12.2/src/lisfloodutilities/cutmaps/main.py
@@ -0,0 +1,105 @@
+"""
+Copyright 2019 European Union
+
+Licensed under the EUPL, Version 1.2 or as soon they will be approved by the European Commission  subsequent versions of the EUPL (the "Licence");
+
+You may not use this work except in compliance with the Licence.
+You may obtain a copy of the Licence at:
+
+https://joinup.ec.europa.eu/sites/default/files/inline-files/EUPL%20v1_2%20EN(1).txt
+
+Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the Licence for the specific language governing permissions and limitations under the Licence.
+
+cutmaps
+A tool to cut netcdf files
+"""
+
+import argparse
+import os
+import shutil
+import sys
+
+from .cutlib import get_filelist, get_cuts, cutmap, logger
+
+
+class ParserHelpOnError(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('Error: %s\n' % message)
+        self.print_help()
+        sys.exit(1)
+
+    def add_args(self):
+        group_mask = self.add_mutually_exclusive_group()
+        group_filelist = self.add_mutually_exclusive_group()
+
+        group_mask.add_argument("-m", "--mask", help='mask file cookie-cutter, .map if pcraster, .nc if netcdf')
+        group_mask.add_argument("-c", "--cuts", help='Cut coordinates in the form lonmin_lonmax:latmin_latmax')
+
+        group_filelist.add_argument("-l", "--list", help='list of files to be cut, in a text file, one file per line, '
+                                                         'main variable i.e. pr/e0/tx/tavg must be '
+                                                         'last variable in the input file')
+        group_filelist.add_argument("-f", "--folder", help='Directory with netCDF files to be cut')
+        group_filelist.add_argument("-g", "--global-setup", help='Directory with GloFAS static maps. '
+                                                                 'Output files will have same directories structure')
+
+        self.add_argument("-o", "--outpath", help='path where to save cut files', default='./cutmaps_out', required=True)
+
+
+def main(cliargs):
+    parser = ParserHelpOnError(description='Convert PCRaster maps to a NetCDF map stack')
+    parser.add_args()
+    args = parser.parse_args(cliargs)
+
+    mask = args.mask
+    cuts = args.cuts
+
+    filelist = args.list
+    input_folder = args.folder
+    glofas_folder = args.global_setup
+
+    pathout = args.outpath
+
+    logger.info('\n\nCutting using: %s\n Files to cut from: %s\n Output: %s\n\n ',
+                mask or cuts,
+                filelist or input_folder or glofas_folder,
+                pathout)
+
+    list_to_cut = get_filelist(filelist, input_folder, glofas_folder)
+    x_min, x_max, y_min, y_max = get_cuts(cuts=cuts, mask=mask)
+
+    # walk through list_to_cut
+    for file_to_cut in list_to_cut:
+
+        filename, ext = os.path.splitext(file_to_cut)
+
+        # localdir used only with glofas_folder.
+        # It will track folder structures in a GloFAS like setup and replicate it on output folder
+        localdir = os.path.dirname(file_to_cut).replace(os.path.dirname(glofas_folder), '').lstrip('/') if glofas_folder else ''
+
+        fileout = os.path.join(pathout, localdir, os.path.basename(file_to_cut))
+        if os.path.isdir(file_to_cut) and glofas_folder:
+            # just create folder
+            os.makedirs(fileout, exist_ok=True)
+            continue
+        if ext != '.nc':
+            if glofas_folder:
+                logger.warning('%s is not in netcdf format, just copying to ouput folder', file_to_cut)
+                shutil.copy(file_to_cut, fileout)
+            else:
+                logger.warning('%s is not in netcdf format, skipping...', file_to_cut)
+            continue
+        elif os.path.isfile(fileout) and os.path.exists(fileout):
+            logger.warning('%s already existing. This file will not be overwritten', fileout)
+            continue
+
+        cutmap(file_to_cut, fileout, x_min, x_max, y_min, y_max)
+
+
+def main_script():
+    sys.exit(main(sys.argv[1:]))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
