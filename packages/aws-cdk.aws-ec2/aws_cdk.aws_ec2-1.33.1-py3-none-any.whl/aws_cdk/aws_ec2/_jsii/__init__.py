d8e1cf9a60a5332fea00730efd6ed8501a282606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package aws-cdk.aws-ec2/1.33.1/aws_cdk.aws_ec2-1.33.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-ec2/aws_cdk.aws_ec2-1.33.1-py3-none-any.whl/aws_cdk/aws_ec2/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_iam
+import aws_cdk.aws_logs
+import aws_cdk.aws_s3
+import aws_cdk.aws_ssm
+import aws_cdk.core
+import aws_cdk.cx_api
+import aws_cdk.region_info
+import constructs
+__all__ = []
+
+publication.publish()
