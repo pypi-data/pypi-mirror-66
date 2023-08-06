89887c9557d32f8e20a044ab90466c5df1e4dc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:55 +0000

    Add package trakt-downloader/0.5/trakt_downloader-0.5.tar.gz

---
+++ packages/trakt-downloader/trakt_downloader-0.5.tar.gz/trakt_downloader-0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='trakt_downloader',
+      version='0.5',
+      description="Ever remember a film and want to watch it right away? Well that used to be difficult. Until now.",
+      url='https://github.com/TheSelectiveOppidan/trakt-downloader',
+      author='The Selective Oppidan',
+      author_email='theselectiveoppidan@gmail.com',
+      license='MIT',
+      packages=['trakt_downloader'],
+      entry_points={
+            'console_scripts': ['trakt-pull=trakt_downloader.trakt_pull:go']
+      },
+      install_requires=[
+          'sqlalchemy',
+          'deluge-client'
+      ],
+      zip_safe=False,
+      python_requires='>=3.6')
