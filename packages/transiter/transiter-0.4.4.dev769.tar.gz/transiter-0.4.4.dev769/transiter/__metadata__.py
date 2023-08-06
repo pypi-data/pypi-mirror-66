9067cb5d408d17bea0df9f044806126c66987789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:37 +0000

    Add package transiter/0.4.4.dev769/transiter-0.4.4.dev769.tar.gz

---
+++ packages/transiter/transiter-0.4.4.dev769.tar.gz/transiter-0.4.4.dev769/transiter/__metadata__.py
@@ -0,0 +1,14 @@
+__version__ = "0.4.4"
+
+# The following are set by Travis CI at build time.
+__build_number__ = None
+__build_timestamp__ = None
+__build_href__ = None
+__git_commit_hash__ = None
+
+# NOTE: the following were set by Travis CI
+__version__ = "0.4.4.dev769"
+__build_number__ = 769
+__build_timestamp__ = 1587679879
+__build_href__ = "https://travis-ci.org/jamespfennell/transiter/jobs/678801955"
+__git_commit_hash__ = "ebd222e4658b0b2a1afb651159bac321f36a325d"
