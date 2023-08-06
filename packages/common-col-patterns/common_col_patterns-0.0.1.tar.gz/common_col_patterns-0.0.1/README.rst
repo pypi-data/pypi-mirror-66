9b51802149e59a402ce4cafa87b333cd4f7b341f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:35 +0000

    Add package common-col-patterns/0.0.1/common_col_patterns-0.0.1.tar.gz

---
+++ packages/common-col-patterns/common_col_patterns-0.0.1.tar.gz/common_col_patterns-0.0.1/README.rst
@@ -0,0 +1,29 @@
+.. role:: bash(code)
+   :language: bash
+
+.. role:: py(code)
+   :language: python
+
+Common Collection Patterns
+################################
+Got the inspiration from funcy_ and being tired of typing the same things over and over.
+
+.. _funcy: https://github.com/Suor/funcy
+
+How to install
+------------------
+* :bash:`pip install common_col_patterns`
+
+
+Usage
+--------
+* Currently you will have to browse the sources
+
+Missing functionality
+------------------------
+* Submit a PR or ping me if you have anything you would like to have - or found a bug
+
+Disclosure
+-------------
+Runtimes of all the functions have not been benchmarked - nor it has been optimized,
+if using any of attached function in a computation heavy manner - verify the performance first
---
+++ packages/common-col-patterns/common_col_patterns-0.0.1.tar.gz/common_col_patterns-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name='common_col_patterns',
+    packages=['common_col_patterns'],
+    version='0.0.1',
+    license='MIT',
+    description='Some functional and collection patterns',
+    author='Lev Gourevitch',
+    author_email='lev.gourevitch@gmail.com',
+    url='https://github.com/levgou/common_col_patters',
+    download_url='https://github.com/levgou/common_col_patters/archive/head.tar.gz',
+    keywords=['functional', 'collection', 'common'],
+    install_requires=[
+        'funcy',
+    ],
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
