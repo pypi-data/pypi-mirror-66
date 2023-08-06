7874a6d24d64e09d3c14c7d901c0b52442441174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:48 +0000

    Add package tmj/1.0.0/tmj-1.0.0.tar.gz

---
+++ packages/tmj/tmj-1.0.0.tar.gz/tmj-1.0.0/README.md
@@ -0,0 +1,19 @@
+# tmj
+Small CLI utility script to help me with TMJ disorder isometric jaw exercises.
+
+The exercises used are as in [this video](https://www.youtube.com/watch?v=Vrz25x3qnTY&feature=youtu.be&t=208).
+
+In the program they are labelled as 'push up', 'pull down', 'push left', and 'push right'.
+
+## Prerequisites
+- Python 3.7
+
+## Installation
+```bash
+pip install tmj
+```
+
+## Usage
+```bash
+$ tmj
+```
---
+++ packages/tmj/tmj-1.0.0.tar.gz/tmj-1.0.0/tmj.py
@@ -0,0 +1,61 @@
+import dataclasses
+import queue
+import time
+from typing import List, Callable
+
+import colorama
+from colorama import Fore, Style
+
+
+@dataclasses.dataclass
+class Exercise:
+    name: str
+    reps: int
+    hold_time: float
+    rep_delay: float = 2
+
+    def _perform(self) -> None:
+        for i in range(self.reps):
+            print(
+                f"\tNow doing rep: {i + 1}, hold for {self.hold_time} seconds")
+            time.sleep(self.hold_time)
+            print("\tGet ready to do the next rep!")
+            time.sleep(self.rep_delay)
+            print(
+                f"\tExercise done! Press {Fore.GREEN}ENTER{Style.RESET_ALL} to continue."
+            )
+
+    def exec_buf(self) -> List[Callable[[], None]]:
+        return [
+            lambda: print(
+                f"Press {Fore.GREEN}ENTER{Style.RESET_ALL} when ready to "
+                f"do exercise {Fore.YELLOW}'{self.name}'{Style.RESET_ALL}"),
+            self._perform
+        ]
+
+
+def welcome_msg():
+    print(f"Press {Fore.GREEN}ENTER{Style.RESET_ALL} to begin")
+
+
+def main():
+    colorama.init()
+
+    exercises = map(lambda name: Exercise(name, 1, 0.1, 0.1),
+                    ["Push up", "Pull down", "Push left", "Push right"])
+    exercise_funcs = [
+        func for exercise in exercises for func in exercise.exec_buf()
+    ]
+
+    exec_queue = queue.SimpleQueue()
+    exec_queue.put(welcome_msg)
+    for exercise_func in exercise_funcs:
+        exec_queue.put(exercise_func)
+
+    while not exec_queue.empty():
+        exec_queue.get()()
+        input()
+
+
+if __name__ == "__main__":
+    main()
