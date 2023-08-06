3a7ece1362add81d227eb32e2fba9dceb371ac00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:06 +0000

    Add package nester-dvrj/1.3.0/nester-dvrj-1.3.0.tar.gz

---
+++ packages/nester-dvrj/nester-dvrj-1.3.0.tar.gz/nester-dvrj-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester-dvrj',
+        version         = '1.3.0',
+        py_modules      = ['nester'],
+        author          = 'Daniel',
+        author_email    = 'valimreis@gmail.com',
+        url             = ' ',
+        description     = 'A simple printer of nested lists',
+        )
