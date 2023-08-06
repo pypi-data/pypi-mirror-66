22777a0161e96db7472be6fc0c79da79e1f64e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:26 +0000

    Add package spawner/0.1.21/spawner-0.1.21-py3-none-any.whl

---
+++ packages/spawner/spawner-0.1.21-py3-none-any.whl/spawner/nlp.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import pandas as pd
+
+# return answer as dataframe 
+def answer(question): 
+    try: 
+        url = "https://spawnerapi.com/answer"
+        trade_list = {"text": question}
+        headers={'Content-type':'application/json'}
+        r = requests.post(url, headers=headers, data=json.dumps(trade_list))
+        content = r.text
+        df = pd.read_json(content, orient='records')
+        df = df.drop(columns=['chart_type', 'condensed_data'])
+        return df
+    except: 
+        return 'I am not smart enough to answer that question. Try again.'
