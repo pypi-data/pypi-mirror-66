8695f0b857d9bca53d7ba1454d0ed2b3a1fd888f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:49 +0000

    Add package lino-xl/20.4.2/lino-xl-20.4.2.tar.gz

---
+++ packages/lino-xl/lino-xl-20.4.2.tar.gz/lino-xl-20.4.2/lino_xl/lib/cal/actions.py
@@ -0,0 +1,92 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2011-2020 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+
+from django.conf import settings
+from lino.api import dd, rt, _
+from lino.core.gfks import gfk2lookup
+from .choicelists import EntryStates, GuestStates
+
+
+class ShowEntriesByDay(dd.Action):
+    label = _("Today")
+    show_in_bbar = True
+    sort_index = 60
+    icon_name = 'calendar'
+
+    def __init__(self, date_field, **kw):
+        self.date_field = date_field
+        super(ShowEntriesByDay, self).__init__(**kw)
+
+    def run_from_ui(self, ar, **kw):
+        obj = ar.selected_rows[0]
+        today = getattr(obj, self.date_field)
+        pv = dict(start_date=today)
+        pv.update(end_date=today)
+        sar = ar.spawn(rt.models.cal.EntriesByDay, param_values=pv)
+        js = ar.renderer.request_handler(sar)
+        ar.set_response(eval_js=js)
+
+
+class UpdateGuests(dd.Action):
+
+    label = _('Update presences')
+    # icon_name = 'lightning'
+    button_text = ' ☷ '  # 2637
+    custom_handler = True
+
+    def run_from_ui(self, ar, **kw):
+        if settings.SITE.loading_from_dump:
+            return 0
+        for obj in ar.selected_rows:
+            self.run_on_event(ar, obj)
+
+    def run_on_event(self, ar, obj):
+
+        # existing = set([g.partner.pk for g in obj.guest_set.all()])
+        existing = {g.partner.pk : g for g in obj.guest_set.all()}
+
+        if len(existing) and obj.can_edit_guests_manually():
+            return
+
+        c = u = d = 0
+        #print("20190328 existing: {}".format(existing))
+        # create suggested guest that don't exist
+        for sg in obj.suggest_guests():
+            #print("20190328 suggested {}".format(sg))
+            eg = existing.pop(sg.partner.pk, None)
+            if eg is None:
+                sg.save()
+                c += 1
+            else:
+                u += 1
+
+        # remove unwanted participants
+        for pk, g in existing.items():
+            if g.state == GuestStates.invited:
+                g.delete()
+                d += 1
+        msg = _("Update presences for {} : "
+                "{} created, {} unchanged, {} deleted.").format(
+                    obj, c, u, d)
+        ar.info(msg)
+
+
+class UpdateAllGuests(UpdateGuests):
+
+    def run_from_ui(self, ar, **kw):
+        Event = rt.models.cal.Event
+        gfk = Event._meta.get_field('owner')
+        states = EntryStates.filter(fixed=False)
+
+        for obj in ar.selected_rows:
+            qs = Event.objects.filter(
+                **gfk2lookup(gfk, obj, state__in=states))
+            def ok(ar2):
+                for e in qs:
+                    self.run_on_event(ar, e)
+
+            fmt = obj.get_date_formatter()
+            txt = ', '.join([fmt(e.start_date) for e in qs])
+            ar.confirm(ok, _("Update presences for {} events: {}").format(
+                qs.count(), txt))
---
+++ packages/lino-xl/lino-xl-20.4.2.tar.gz/lino-xl-20.4.2/lino_xl/lib/cal/workflows/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2011-2020 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+
+"""Some standard workflow definition modules for
+:mod:`lino_xl.lib.cal`.
+
+.. autosummary::
+   :toctree:
+
+    feedback
+
+"""
+
+
+from django.conf import settings
+
+from django.utils.translation import ugettext_lazy as _
+
+
+from lino.api import dd
+
+from lino_xl.lib.cal.choicelists import TaskStates
+
+
+def f(name):
+    if settings.SITE.use_silk_icons:
+        return name
+
+TaskStates.todo.add_transition(
+    _("Reopen"), required_states='done cancelled important')
+TaskStates.done.add_transition(
+    required_states='todo started important', icon_name=f('accept'))
+TaskStates.cancelled.add_transition(
+    required_states='todo started important', icon_name=f('cancel'))
+TaskStates.important.add_transition(
+    required_states='todo started', icon_name=f('lightning'))
