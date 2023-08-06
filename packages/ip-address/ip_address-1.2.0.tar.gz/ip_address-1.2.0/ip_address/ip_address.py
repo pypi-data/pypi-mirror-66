2d7d36bd7e5ae77a09ef91061e1d1d4d19629880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:23 +0000

    Add package ip-address/1.2.0/ip_address-1.2.0.tar.gz

---
+++ packages/ip-address/ip_address-1.2.0.tar.gz/ip_address-1.2.0/ip_address/ip_address.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get():
+    # Requests data from page
+    page = requests.get("https://iplocation.com/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    ip = soup.find("b", {"class": "ip"}).text
+    
+    return ip
+
+print(get())
+
+
