c12fe9b758b01d16f5131f774eda4b8544b41b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:13 +0000

    Add package webtul/1.2/webtul-1.2.tar.gz

---
+++ packages/webtul/webtul-1.2.tar.gz/webtul-1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" webtul's setup file
+"""
+__author__ = 'Zagfai'
+__license__ = 'MIT@2014-01'
+
+from setuptools import setup
+from webtul import __version__
+
+
+setup(
+  name="webtul",
+  version=__version__,
+  keywords=('web', 'lib', 'library', 'toolkit'),
+  description="A set of web/task developing tools.",
+
+  author="Zagfai",
+  author_email='zagfai@gmail.com',
+  url="http://github.com/zagfai/webtul",
+  license="MIT License",
+  packages=["webtul"],
+  # packages = find_packages(),
+  install_requires=[],
+  python_requires='>=3',
+  platforms=["any"],
+)
---
+++ packages/webtul/webtul-1.2.tar.gz/webtul-1.2/webtul/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" Webtul is short for WEB toolkit
+See modules of this package for more details.
+"""
+__author__ = 'Zagfai'
+__license__ = 'MIT@2014-01'
+__all__ = ['struct']
+__version__ = '1.2'
+
+
+from . import aescbc
+from . import dal
+from . import geo
+from . import img
+from . import jsonx
+from . import path
+from . import rsa
+from . import stringx
+from . import struct
+
+
+if __name__ == "__main__":
+    struct, geo, img, jsonx, aescbc, path, rsa, stringx, dal
