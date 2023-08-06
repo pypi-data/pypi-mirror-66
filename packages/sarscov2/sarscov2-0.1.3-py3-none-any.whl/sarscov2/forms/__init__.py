57a82644328e204d25da925584a3ac8b2a410bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:37 +0000

    Add package sarscov2/0.1.3/sarscov2-0.1.3-py3-none-any.whl

---
+++ packages/sarscov2/sarscov2-0.1.3-py3-none-any.whl/sarscov2/forms/__init__.py
@@ -0,0 +1,1 @@
+from .corona_kap_form_validator import CoronaKapFormValidator
---
+++ packages/sarscov2/sarscov2-0.1.3-py3-none-any.whl/sarscov2/forms/corona_kap_form_validator.py
@@ -0,0 +1,21 @@
+from edc_constants.constants import OTHER, YES
+from edc_form_validators.form_validator import FormValidator
+
+
+class CoronaKapFormValidator(FormValidator):
+    def clean(self):
+        self.required_if(YES, field="hiv_pos", field_required="hiv_pos_year")
+        self.required_if(YES, field="hiv_pos", field_required="hiv_year_started_art")
+        self.required_if(YES, field="diabetic", field_required="diabetic_dx_year")
+        self.applicable_if(YES, field="diabetic", field_applicable="diabetes_on_meds")
+        self.required_if(
+            YES, field="hypertensive", field_required="hypertensive_dx_year"
+        )
+        self.applicable_if(
+            YES, field="hypertensive", field_applicable="hypertensive_on_meds"
+        )
+        self.validate_other_specify(field="employment")
+        self.validate_other_specify(field="health_insurance")
+        self.required_if(
+            YES, field="know_other_symptoms", field_required="symptoms_other"
+        )
