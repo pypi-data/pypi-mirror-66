e4e8fe6ff71b6deb1f792e5c1e0a72b731813dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:52 +0000

    Add package ulid-py/0.0.14/ulid_py-0.0.14-py2.py3-none-any.whl

---
+++ packages/ulid-py/ulid_py-0.0.14-py2.py3-none-any.whl/ulid/__init__.py
@@ -0,0 +1,16 @@
+"""
+    ulid
+    ~~~~
+
+    Universally Unique Lexicographically Sortable Identifier
+
+    :copyright: (c) 2017 Andrew Hawker.
+    :license: Apache 2.0, see LICENSE for more details.
+"""
+from . import api, ulid
+from .api import *  # pylint: disable=wildcard-import
+from .ulid import *  # pylint: disable=wildcard-import
+
+__all__ = api.__all__ + ulid.__all__
+
+__version__ = '0.0.14'
