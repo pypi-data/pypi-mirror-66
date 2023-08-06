536560f9c24d720fdc0360b963a1d31d389991db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package cumulocitypython/0.1.4/cumulocitypython-0.1.4.zip

---
+++ packages/cumulocitypython/cumulocitypython-0.1.4.zip/cumulocitypython-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'cumulocitypython',
+  packages = ['cumulocitypython'],
+  version = '0.1.4',
+  license='MIT',
+  description = 'Purpose of this package is to easily request historical data from your cumulocity tenant and return it as a pandas dataframe.',
+  author = 'Silver Laius',
+  author_email = 'silver.laius@gmail.com',
+  url = 'https://github.com/SilverLaius/cumulocitypython',
+  download_url = 'https://github.com/SilverLaius/cumulocitypython/archive/1.1.4.tar.gz',
+  keywords = ['Python', 'Cumulocity', 'Pandas'],
+  install_requires=[
+          'python-dateutil',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
