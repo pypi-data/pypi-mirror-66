43043eee08434cbbc543e130697de8528c207c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:01 +0000

    Add package sempler/0.1.0/sempler-0.1.0.tar.gz

---
+++ packages/sempler/sempler-0.1.0.tar.gz/sempler-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='sempler',
+    version='0.1.0',
+    author='Juan L Gamella',
+    author_email='juangamella@gmail.com',
+    packages=['sempler', 'sempler.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/sempler/',
+    license='LICENSE.txt',
+    description='Sample from general structural causal models (SCMs)',
+    long_description=open('README.txt').read()
+)
