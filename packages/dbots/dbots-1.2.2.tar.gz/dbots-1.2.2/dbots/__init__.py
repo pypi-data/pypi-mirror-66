f3dc3b2649a3f98b6d677d871ff9feb530693b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:39 +0000

    Add package dbots/1.2.2/dbots-1.2.2.tar.gz

---
+++ packages/dbots/dbots-1.2.2.tar.gz/dbots-1.2.2/dbots/__init__.py
@@ -0,0 +1,29 @@
+__title__ = 'dbots'
+__author__ = 'Snazzah'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Snazzah'
+__version__ = '1.2.2'
+
+from collections import namedtuple
+import logging
+
+from .http import HTTPClient, HTTPResponse
+from .client_filler import *
+from .errors import *
+from .service import *
+from .poster import Poster, ClientPoster, AsyncLoop
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+version_info = VersionInfo(
+    major = 1, minor = 2, micro = 2,
+    releaselevel = 'final', serial = 0
+)
+
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
