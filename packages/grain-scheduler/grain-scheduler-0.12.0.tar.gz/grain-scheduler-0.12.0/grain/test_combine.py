2c45335202f26dba8f0f2da370bc4e2ebc006a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:31 +0000

    Add package grain-scheduler/0.12.0/grain-scheduler-0.12.0.tar.gz

---
+++ packages/grain-scheduler/grain-scheduler-0.12.0.tar.gz/grain-scheduler-0.12.0/grain/test_combine.py
@@ -0,0 +1,25 @@
+from .combine import *
+from .resource import Memory
+
+import pytest
+import trio
+
+from functools import partial
+
+run_combine = partial(run_combine, config_file="grain.pytest.toml")
+
+async def _anop():
+    await trio.sleep(0)
+
+class Critical(Exception):
+    pass
+
+async def _main_subtask(i):
+    async with open_waitgroup() as wg:
+        wg.submit(Memory(0), _anop)
+    if i == 1:
+        raise Critical
+
+def test_main_subtask_exception():
+    with pytest.raises(Critical):
+        run_combine([partial(_main_subtask, i) for i in range(10)], [], Memory(0))
