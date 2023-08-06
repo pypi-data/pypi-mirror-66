b085b045845ec865c6d4a672c1e7f824ddc0413f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:39 +0000

    Add package nwavclauncher/0.1.0/nwavclauncher-0.1.0-py3-none-any.whl

---
+++ packages/nwavclauncher/nwavclauncher-0.1.0-py3-none-any.whl/nwavclauncher/__init__.py
@@ -0,0 +1,6 @@
+"""-"""
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+NW_DEBUG_AVRELAY_PATH: str = SCRIPT_DIR + \
+    "/../../../nwavrelay/nwavrelay/bin/Debug/netcoreapp3.1"
---
+++ packages/nwavclauncher/nwavclauncher-0.1.0-py3-none-any.whl/nwavclauncher/__main__.py
@@ -0,0 +1,12 @@
+"""-"""
+
+import argparse
+import os
+
+
+def main() -> None:
+    """-"""
+
+
+if __name__ == "__main__":
+    main()
