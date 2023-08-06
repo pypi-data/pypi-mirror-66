3bc364b3a7dbee55294ffe006647a904072a2074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:19 +0000

    Add package dalex/0.1.5/dalex-0.1.5-py3-none-any.whl

---
+++ packages/dalex/dalex-0.1.5-py3-none-any.whl/dalex/instance_level/__init__.py
@@ -0,0 +1,9 @@
+from ._break_down.object import BreakDown
+from ._shap.object import Shap
+from ._ceteris_paribus.object import CeterisParibus
+
+__all__ = [
+    "BreakDown",
+    "CeterisParibus",
+    "Shap"
+]
