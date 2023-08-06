d95c156f39638af288ecce8702a190f07672279f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:37 +0000

    Add package preimutils/1.0.4/preimutils-1.0.4.tar.gz

---
+++ packages/preimutils/preimutils-1.0.4.tar.gz/preimutils-1.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup,find_packages
+import os
+
+
+setup(
+    name="preimutils",
+    packages=find_packages(),
+    version='1.0.4',
+    description="all you need to prepare and preprocess your annotated images",
+    url="https://github.com/mrl-amrl/preimutils",
+    download_url='https://github.com/mrl-amrl/preimutils/archive/1.0.5.tar.gz',
+    author="Amir Sharifi",
+    author_email="ami_rsh@outlook.com",
+    license='MIT',
+    keywords=['computer vision', 'image processing', 'opencv',
+              'matplotlib', 'preprocess image', 'image dataset', 'voc'],
+    entry_points={
+        'console_scripts': ['preimutils=preimutils.object_detection.run:main'],
+    },
+    install_requires=[
+        'pascal_voc_writer',
+        'tqdm',
+        'pandas',
+        'albumentations',
+        'imutils',
+        'imageio',
+        'pycocotools'
+    ],
+    zip_safe=False,
+)
