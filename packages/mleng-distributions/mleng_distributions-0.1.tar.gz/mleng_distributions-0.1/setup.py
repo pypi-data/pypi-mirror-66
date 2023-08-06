4b9ebd603f3d4b643ec02445ed7ffa3b73450b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:47 +0000

    Add package mleng-distributions/0.1/mleng_distributions-0.1.tar.gz

---
+++ packages/mleng-distributions/mleng_distributions-0.1.tar.gz/mleng_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mleng_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['mleng_distributions'],
+      author='Adrian R',
+      zip_safe=False)
