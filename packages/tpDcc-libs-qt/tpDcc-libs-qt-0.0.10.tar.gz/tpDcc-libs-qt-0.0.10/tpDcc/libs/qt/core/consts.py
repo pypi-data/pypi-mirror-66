5fddff205bc4138c8034577860afa8e891321ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:19 +0000

    Add package tpDcc-libs-qt/0.0.10/tpDcc-libs-qt-0.0.10.tar.gz

---
+++ packages/tpDcc-libs-qt/tpDcc-libs-qt-0.0.10.tar.gz/tpDcc-libs-qt-0.0.10/tpDcc/libs/qt/core/consts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains constants for tpQtLib
+"""
+
+from __future__ import print_function, division, absolute_import
+
+__author__ = "Tomas Poveda"
+__license__ = "MIT"
+__maintainer__ = "Tomas Poveda"
+__email__ = "tpovedatd@gmail.com"
+
+# DPI
+DEFAULT_DPI = 96
+
+# SPACINGS
+SPACING = 2
+DEFAULT_SUB_WIDGET_SPACING = 6
+
+# COLORS
+DARK_BG_COLOR = tuple([93, 93, 93])
+MEDIUM_DARK_BG_COLOR = tuple([73, 73, 73])
