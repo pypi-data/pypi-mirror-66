9736b01437895a4a065996361791fb5b9134068d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:38 +0000

    Add package libhamming/0.3/libhamming-0.3-cp36-cp36m-win32.whl

---
+++ packages/libhamming/libhamming-0.3-cp36-cp36m-win32.whl/libhamming/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+""" Library for hamming code """
+# !/usr/bin/python
+# Python:   3.6.5
+# Platform: Windows/Linux
+# Author:   Heyn (heyunhuan@gmail.com)
+# Program:  Library for hamming code.
+# History:  2019-03-21 Wheel Ver:0.2 [Heyn] Initialize
+
+
+from .hamming  import *
