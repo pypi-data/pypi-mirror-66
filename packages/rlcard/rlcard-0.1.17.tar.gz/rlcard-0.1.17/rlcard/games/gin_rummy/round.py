2aac0da786c3f433329c287b1f6c13fbee4ffb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:48 +0000

    Add package rlcard/0.1.17/rlcard-0.1.17.tar.gz

---
+++ packages/rlcard/rlcard-0.1.17.tar.gz/rlcard-0.1.17/rlcard/games/gin_rummy/round.py
@@ -0,0 +1,169 @@
+'''
+    File name: gin_rummy/round.py
+    Author: William Hale
+    Date created: 2/12/2020
+'''
+
+from typing import List
+
+from rlcard.games.gin_rummy.dealer import GinRummyDealer
+
+from .utils.action_event import DrawCardAction, PickUpDiscardAction, DeclareDeadHandAction
+from .utils.action_event import DiscardAction, KnockAction, GinAction
+from .utils.action_event import ScoreNorthPlayerAction, ScoreSouthPlayerAction
+
+from .utils.move import GinRummyMove
+from .utils.move import DealHandMove
+from .utils.move import DrawCardMove, PickupDiscardMove, DeclareDeadHandMove
+from .utils.move import DiscardMove, KnockMove, GinMove
+from .utils.move import ScoreNorthMove, ScoreSouthMove
+
+from .player import GinRummyPlayer
+from . import judge
+
+import rlcard.games.gin_rummy.utils.melding as melding
+import rlcard.games.gin_rummy.utils.utils as utils
+
+
+class GinRummyRound(object):
+
+    def __init__(self, dealer_id: int):
+        ''' Initialize the round class
+
+            The round class maintains the following instances:
+                1) dealer: the dealer of the round; dealer has stock_pile and discard_pile
+                2) players: the players in the round; each player has his own hand_pile
+                3) current_player_id: the id of the current player who has the move
+                4) is_over: true if the round is over
+                5) going_out_action: knock or gin or None
+                6) going_out_player_id: id of player who went out or None
+                7) move_sheet: history of the moves of the player (including the deal_hand_move)
+
+            The round class maintains a list of moves made by the players in self.move_sheet.
+            move_sheet is similar to a chess score sheet.
+            I didn't want to call it a score_sheet since it is not keeping score.
+            I could have called move_sheet just moves, but that might conflict with the name moves used elsewhere.
+            I settled on the longer name "move_sheet" to indicate that it is the official list of moves being made.
+
+        Args:
+            dealer_id: int
+        '''
+        self.dealer_id = dealer_id
+        self.dealer = GinRummyDealer()
+        self.players = [GinRummyPlayer(player_id=0), GinRummyPlayer(player_id=1)]
+        self.current_player_id = (dealer_id + 1) % 2
+        self.is_over = False
+        self.going_out_action = None  # going_out_action: int or None
+        self.going_out_player_id = None  # going_out_player_id: int or None
+        self.move_sheet = []  # type: List[GinRummyMove]
+        player_dealing = GinRummyPlayer(player_id=dealer_id)
+        shuffled_deck = self.dealer.shuffled_deck
+        self.move_sheet.append(DealHandMove(player_dealing=player_dealing, shuffled_deck=shuffled_deck))
+
+    def get_current_player(self) -> GinRummyPlayer or None:
+        current_player_id = self.current_player_id
+        return None if current_player_id is None else self.players[current_player_id]
+
+    def draw_card(self, action: DrawCardAction):
+        # when current_player takes DrawCardAction step, the move is recorded and executed
+        # current_player keeps turn
+        current_player = self.players[self.current_player_id]
+        assert len(current_player.hand) == 10
+        card = self.dealer.stock_pile.pop()
+        self.move_sheet.append(DrawCardMove(current_player, action=action, card=card))
+        current_player.add_card_to_hand(card=card)
+
+    def pick_up_discard(self, action: PickUpDiscardAction):
+        # when current_player takes PickUpDiscardAction step, the move is recorded and executed
+        # opponent knows that the card is in current_player hand
+        # current_player keeps turn
+        current_player = self.players[self.current_player_id]
+        assert len(current_player.hand) == 10
+        card = self.dealer.discard_pile.pop()
+        self.move_sheet.append(PickupDiscardMove(current_player, action, card=card))
+        current_player.add_card_to_hand(card=card)
+        current_player.known_cards.append(card)
+
+    def declare_dead_hand(self, action: DeclareDeadHandAction):
+        # when current_player takes DeclareDeadHandAction step, the move is recorded and executed
+        # north becomes current_player to score his hand
+        current_player = self.players[self.current_player_id]
+        self.move_sheet.append(DeclareDeadHandMove(current_player, action))
+        self.going_out_action = action
+        self.going_out_player_id = self.current_player_id
+        assert len(current_player.hand) == 10
+        self.current_player_id = 0
+
+    def discard(self, action: DiscardAction):
+        # when current_player takes DiscardAction step, the move is recorded and executed
+        # opponent knows that the card is no longer in current_player hand
+        # current_player loses his turn and the opponent becomes the current player
+        current_player = self.players[self.current_player_id]
+        assert len(current_player.hand) == 11
+        self.move_sheet.append(DiscardMove(current_player, action))
+        card = action.card
+        current_player.remove_card_from_hand(card=card)
+        if card in current_player.known_cards:
+            current_player.known_cards.remove(card)
+        self.dealer.discard_pile.append(card)
+        self.current_player_id = (self.current_player_id + 1) % 2
+
+    def knock(self, action: KnockAction):
+        # when current_player takes KnockAction step, the move is recorded and executed
+        # opponent knows that the card is no longer in current_player hand
+        # north becomes current_player to score his hand
+        current_player = self.players[self.current_player_id]
+        self.move_sheet.append(KnockMove(current_player, action))
+        self.going_out_action = action
+        self.going_out_player_id = self.current_player_id
+        assert len(current_player.hand) == 11
+        card = action.card
+        current_player.remove_card_from_hand(card=card)
+        if card in current_player.known_cards:
+            current_player.known_cards.remove(card)
+        self.current_player_id = 0
+
+    def gin(self, action: GinAction, going_out_deadwood_count: int):
+        # when current_player takes GinAction step, the move is recorded and executed
+        # opponent knows that the card is no longer in current_player hand
+        # north becomes current_player to score his hand
+        current_player = self.players[self.current_player_id]
+        self.move_sheet.append(GinMove(current_player, action))
+        self.going_out_action = action
+        self.going_out_player_id = self.current_player_id
+        assert len(current_player.hand) == 11
+        _, gin_cards = judge.get_going_out_cards(current_player.hand, going_out_deadwood_count)
+        card = gin_cards[0]
+        current_player.remove_card_from_hand(card=card)
+        if card in current_player.known_cards:
+            current_player.known_cards.remove(card)
+        self.current_player_id = 0
+
+    def score_player_0(self, action: ScoreNorthPlayerAction):
+        # when current_player takes ScoreNorthPlayerAction step, the move is recorded and executed
+        # south becomes current player
+        assert self.current_player_id == 0
+        current_player = self.get_current_player()
+        best_meld_clusters = melding.get_best_meld_clusters(hand=current_player.hand)
+        best_meld_cluster = [] if not best_meld_clusters else best_meld_clusters[0]
+        deadwood_count = utils.get_deadwood_count(hand=current_player.hand, meld_cluster=best_meld_cluster)
+        self.move_sheet.append(ScoreNorthMove(player=current_player,
+                                              action=action,
+                                              best_meld_cluster=best_meld_cluster,
+                                              deadwood_count=deadwood_count))
+        self.current_player_id = 1
+
+    def score_player_1(self, action: ScoreSouthPlayerAction):
+        # when current_player takes ScoreSouthPlayerAction step, the move is recorded and executed
+        # south remains current player
+        # the round is over
+        assert self.current_player_id == 1
+        current_player = self.get_current_player()
+        best_meld_clusters = melding.get_best_meld_clusters(hand=current_player.hand)
+        best_meld_cluster = [] if not best_meld_clusters else best_meld_clusters[0]
+        deadwood_count = utils.get_deadwood_count(hand=current_player.hand, meld_cluster=best_meld_cluster)
+        self.move_sheet.append(ScoreSouthMove(player=current_player,
+                                              action=action,
+                                              best_meld_cluster=best_meld_cluster,
+                                              deadwood_count=deadwood_count))
+        self.is_over = True
---
+++ packages/rlcard/rlcard-0.1.17.tar.gz/rlcard-0.1.17/rlcard/games/nolimitholdem/game.py
@@ -0,0 +1,202 @@
+import numpy as np
+from copy import deepcopy
+from rlcard.games.limitholdem.game import LimitholdemGame
+
+from rlcard.games.nolimitholdem.dealer import NolimitholdemDealer as Dealer
+from rlcard.games.nolimitholdem.player import NolimitholdemPlayer as Player
+from rlcard.games.nolimitholdem.judger import NolimitholdemJudger as Judger
+from rlcard.games.nolimitholdem.round import NolimitholdemRound as Round
+
+class NolimitholdemGame(LimitholdemGame):
+
+    def __init__(self, allow_step_back=False, num_players=2):
+        ''' Initialize the class nolimitholdem Game
+        '''
+        self.allow_step_back = allow_step_back
+
+        # small blind and big blind
+        self.small_blind = 1
+        self.big_blind = 2 * self.small_blind
+
+        # config players
+        self.num_players = num_players
+        self.init_chips = 100
+
+    def init_game(self):
+        ''' Initialilze the game of Limit Texas Hold'em
+
+        This version supports two-player limit texas hold'em
+
+        Returns:
+            (tuple): Tuple containing:
+
+                (dict): The first state of the game
+                (int): Current player's id
+        '''
+        # Initilize a dealer that can deal cards
+        self.dealer = Dealer()
+
+        # Initilize two players to play the game
+        self.players = [Player(i, self.init_chips) for i in range(self.num_players)]
+
+        # Initialize a judger class which will decide who wins in the end
+        self.judger = Judger()
+
+        # Deal cards to each  player to prepare for the first round
+        for i in range(2 * self.num_players):
+            self.players[i % self.num_players].hand.append(self.dealer.deal_card())
+
+        # Initilize public cards
+        self.public_cards = []
+
+        # Randomly choose a big blind and a small blind
+        s = np.random.randint(0, self.num_players)
+        b = (s + 1) % self.num_players
+        self.players[b].bet(chips=self.big_blind)
+        self.players[s].bet(chips=self.small_blind)
+
+        # The player next to the small blind plays the first
+        self.game_pointer = (b + 1) % self.num_players
+
+        # Initilize a bidding round, in the first round, the big blind and the small blind needs to
+        # be passed to the round for processing.
+        self.round = Round(self.num_players, self.big_blind)
+
+        self.round.start_new_round(game_pointer=self.game_pointer, raised=[p.in_chips for p in self.players])
+
+        # Count the round. There are 4 rounds in each game.
+        self.round_counter = 0
+
+        # Save the hisory for stepping back to the last state.
+        self.history = []
+
+        state = self.get_state(self.game_pointer)
+
+        return state, self.game_pointer
+
+    def get_legal_actions(self):
+        ''' Return the legal actions for current player
+
+        Returns:
+            (list): A list of legal actions
+        '''
+        return self.round.get_nolimit_legal_actions(self.players)
+
+    def step(self, action):
+        ''' Get the next state
+
+        Args:
+            action (str): a specific action. (call, raise, fold, or check)
+
+        Returns:
+            (tuple): Tuple containing:
+
+                (dict): next player's state
+                (int): next plater's id
+        '''
+        if self.allow_step_back:
+            # First snapshot the current state
+            r = deepcopy(self.round)
+            b = self.game_pointer
+            r_c = self.round_counter
+            d = deepcopy(self.dealer)
+            p = deepcopy(self.public_cards)
+            ps = deepcopy(self.players)
+            self.history.append((r, b, r_c, d, p, ps))
+
+        # Then we proceed to the next round
+        self.game_pointer = self.round.proceed_round(self.players, action)
+        self.dealer.pot = np.sum([player.in_chips for player in self.players])
+
+        # If a round is over, we deal more public cards
+        if self.round.is_over():
+            # For the first round, we deal 3 cards
+            if self.round_counter == 0:
+                self.public_cards.append(self.dealer.deal_card())
+                self.public_cards.append(self.dealer.deal_card())
+                self.public_cards.append(self.dealer.deal_card())
+            # For the following rounds, we deal only 1 card
+            elif self.round_counter <= 2:
+                self.public_cards.append(self.dealer.deal_card())
+
+            self.round_counter += 1
+            self.round.start_new_round(self.game_pointer)
+
+        state = self.get_state(self.game_pointer)
+
+        return state, self.game_pointer
+
+    def get_state(self, player):
+        ''' Return player's state
+
+        Args:
+            player_id (int): player id
+
+        Returns:
+            (dict): The state of the player
+        '''
+        chips = [self.players[i].in_chips for i in range(self.num_players)]
+        legal_actions = self.get_legal_actions()
+        state = self.players[player].get_state(self.public_cards, chips, legal_actions)
+        state['stakes'] = [self.players[i].remained_chips for i in range(self.num_players)]
+        state['current_player'] = self.game_pointer
+        state['pot'] = self.dealer.pot
+        return state
+
+    def step_back(self):
+        ''' Return to the previous state of the game
+
+        Returns:
+            (bool): True if the game steps back successfully
+        '''
+        if len(self.history) > 0:
+            self.round, self.game_pointer, self.round_counter, self.dealer, self.public_cards, self.players = self.history.pop()
+            return True
+        return False
+
+    def get_action_num(self):
+        ''' Return the number of applicable actions
+
+        Returns:
+            (int): The number of actions. There are 4 kinds actions (call, raise, check and fold). For 'raise' action, we provide all possible raise amount.
+        '''
+        return self.init_chips + 3
+
+    def get_payoffs(self):
+        ''' Return the payoffs of the game
+
+        Returns:
+            (list): Each entry corresponds to the payoff of one player
+        '''
+        hands = [p.hand + self.public_cards if p.status == 'alive' else None for p in self.players]
+        chips_payoffs = self.judger.judge_game(self.players, hands)
+        return chips_payoffs
+
+
+#if __name__ == "__main__":
+#    game = NolimitholdemGame()
+#
+#    while True:
+#        print('New Game')
+#        state, game_pointer = game.init_game()
+#        print(game_pointer, state)
+#        i = 1
+#        while not game.is_over():
+#            i += 1
+#            legal_actions = game.get_legal_actions()
+#            # if i == 3:
+#            #     print('Step back')
+#            #     print(game.step_back())
+#            #     game_pointer = game.get_player_id()
+#            #     print(game_pointer)
+#            #     legal_actions = game.get_legal_actions()
+#
+#            action = np.random.choice(legal_actions)
+#            # action = input()
+#            # if action != 'call' and action != 'fold' and action != 'check':
+#            #     action = int(action)
+#            print(game_pointer, action, legal_actions)
+#            state, game_pointer = game.step(action)
+#            print(game_pointer, state)
+#
+#        print(game.get_payoffs())
