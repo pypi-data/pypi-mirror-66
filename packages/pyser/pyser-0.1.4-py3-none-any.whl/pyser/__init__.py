98ac4c2bfe30930ef0dfd76b7ef455a0411512da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package pyser/0.1.4/pyser-0.1.4-py3-none-any.whl

---
+++ packages/pyser/pyser-0.1.4-py3-none-any.whl/pyser/__init__.py
@@ -0,0 +1,16 @@
+from .json import (
+    BaseJSON,
+    SchemaJSON,
+    SerField,
+    DeserField,
+    DeserObjectField,
+    SerObjectField,
+)
+from .configparser import (
+    ConfigSectionBase,
+    ConfigBase,
+    SerConfigOption,
+    DeserConfigOption,
+    SerConfigSection,
+    DeserConfigSection,
+)
