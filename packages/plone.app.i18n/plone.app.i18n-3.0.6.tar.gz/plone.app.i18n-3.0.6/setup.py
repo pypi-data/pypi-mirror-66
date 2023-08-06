0858b8a9195aadd466e19075f769effde7f5bd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package plone.app.i18n/3.0.6/plone.app.i18n-3.0.6.tar.gz

---
+++ packages/plone.app.i18n/plone.app.i18n-3.0.6.tar.gz/plone.app.i18n-3.0.6/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '3.0.6'
+
+setup(name='plone.app.i18n',
+      version=version,
+      description="Plone specific i18n extensions.",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 5.0",
+          "Framework :: Plone :: 5.1",
+          "Framework :: Plone :: 5.2",
+          "Framework :: Plone :: Core",
+          "Framework :: Zope2",
+          "Framework :: Zope :: 4",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+      ],
+      keywords='plone i18n',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='https://pypi.org/project/plone.app.i18n',
+      license='GPL version 2',
+      packages=find_packages(),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'six',
+          ]
+      },
+      )
