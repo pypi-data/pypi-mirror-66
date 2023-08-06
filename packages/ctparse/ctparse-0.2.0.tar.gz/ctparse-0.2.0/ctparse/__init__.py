8c40935041fb05fec44d93aa9349f6fa464e4137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:37 +0000

    Add package ctparse/0.2.0/ctparse-0.2.0.tar.gz

---
+++ packages/ctparse/ctparse-0.2.0.tar.gz/ctparse-0.2.0/ctparse/__init__.py
@@ -0,0 +1,10 @@
+"""ctparse - parse time expressions in strings
+
+.. moduleauthor:: Comtravo
+
+"""
+__author__ = """Sebastian Mika"""
+__email__ = "sebastian.mika@comtravo.com"
+__version__ = "__version__ = '0.2.0'"
+
+from .ctparse import ctparse, ctparse_gen  # noqa
