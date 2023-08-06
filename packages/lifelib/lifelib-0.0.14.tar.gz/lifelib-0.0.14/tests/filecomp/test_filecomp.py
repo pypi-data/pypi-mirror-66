d6f9f81b307353c3386038edb170c72c96222bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:47 +0000

    Add package lifelib/0.0.14/lifelib-0.0.14.tar.gz

---
+++ packages/lifelib/lifelib-0.0.14.tar.gz/lifelib-0.0.14/tests/filecomp/test_filecomp.py
@@ -0,0 +1,37 @@
+import os.path
+import pathlib
+import filecmp
+
+import pytest
+
+import lifelib
+import lifelib.projects.simplelife as simplelife
+import lifelib.projects.nestedlife as nestedlife
+import lifelib.projects.ifrs17sim as ifrs17sim
+import lifelib.projects.solvency2 as solvency2
+
+simplepath = simplelife.__path__[0]
+nestedpath = nestedlife.__path__[0]
+ifrs17simpath = ifrs17sim.__path__[0]
+solvency2path = solvency2.__path__[0]
+
+all_projects = [simplepath, nestedpath, ifrs17simpath, solvency2path]
+
+common_files = [('build_input.py', all_projects),
+                ('lifetable.py', all_projects),
+                ('policy.py', all_projects),
+                ('assumption.py', all_projects),
+                ('economic.py', all_projects),
+                ('projection.py', all_projects),
+                ('present_value.py', all_projects),
+                ('draw_charts.py', [nestedpath, ifrs17simpath]),
+                ('input.xlsx', all_projects)]
+
+
+@pytest.mark.parametrize('filename, filepaths', common_files)
+def test_filecomp(filename, filepaths):
+    """Check equality of common files between simplelife and nestedlife"""
+
+    for path1, path2 in zip(filepaths, filepaths[1:]):
+        assert filecmp.cmp(os.path.join(path1, filename),
+                           os.path.join(path2, filename))
