cea531b4cd8b9dbe1210af513c07368e265d4a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:39 +0000

    Add package pytest-check-links/0.3.4/pytest_check_links-0.3.4.tar.gz

---
+++ packages/pytest-check-links/pytest_check_links-0.3.4.tar.gz/pytest_check_links-0.3.4/README.md
@@ -0,0 +1,27 @@
+# pytest-check-links
+
+pytest plugin that checks URLs for HTML-containing files.
+
+Supported files:
+
+- .html
+- .rst
+- .md (TODO: select renderer)
+- .ipynb (requires nbconvert)
+
+Install:
+
+    pip install pytest-check-links
+
+Use:
+
+    pytest --check-links mynotebook.ipynb
+
+
+TODO:
+
+- pick a markdown renderer (probably commonmark) or make the markdown renderer pluggable
+- options for validating links (allow absolute links, only remote or local, etc.)
+- check internal links (`#anchors`)
+- find URLs in Python docstrings
+- test myself, obvs!
---
+++ packages/pytest-check-links/pytest_check_links-0.3.4.tar.gz/pytest_check_links-0.3.4/test/test_check_links.py
@@ -0,0 +1,38 @@
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+examples = os.path.join(os.path.dirname(here), 'examples')
+
+
+def test_ipynb(testdir):
+    testdir.copy_example('linkcheck.ipynb')
+    result = testdir.runpytest("-v", "--check-links")
+    result.assert_outcomes(passed=3, failed=3)
+
+def test_markdown(testdir):
+    testdir.copy_example('markdown.md')
+    result = testdir.runpytest("-v", "--check-links")
+    result.assert_outcomes(passed=8, failed=4)
+
+def test_rst(testdir):
+    testdir.copy_example('rst.rst')
+    result = testdir.runpytest("-v", "--check-links")
+    result.assert_outcomes(passed=7, failed=2)
+
+def test_link_ext(testdir):
+    testdir.copy_example('linkcheck.ipynb')
+    testdir.copy_example('rst.rst')
+    testdir.copy_example('markdown.md')
+    result = testdir.runpytest("-v", "--check-links", "--links-ext=.md,rst")
+    result.assert_outcomes(passed=15, failed=6)
+
+def test_anchors_self(testdir):
+    testdir.copy_example('anchors_self.html')
+    result = testdir.runpytest("-v", "--check-links", "--check-anchors")
+    result.assert_outcomes(passed=1, failed=2)
+
+def test_anchors_other(testdir):
+    testdir.copy_example('anchors_self.html')
+    testdir.copy_example('anchors_other.html')
+    result = testdir.runpytest("-v", "--check-links", "--check-anchors", "anchors_other.html")
+    result.assert_outcomes(passed=1, failed=2)
