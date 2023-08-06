2ba13c4d20f3e73f2a66bf479a69abac90bebe14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:03 +0000

    Add package python-xrectsel/1.3/python_xrectsel-1.3-py3-none-any.whl

---
+++ packages/python-xrectsel/python_xrectsel-1.3-py3-none-any.whl/xrectsel/console.py
@@ -0,0 +1,62 @@
+import click
+
+from xrectsel import XRectSel
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+CURSOR_ICONS = ["crosshair", "cross", "pencil", "dotbox"]
+
+FORMAT_HELP = """Format output string with fallowing options:
+
+        %x - start x-coordinate
+
+        %y - start y-coordinate
+
+        %X - start
+
+        %Y - end
+
+        %w - width
+
+        %h - height
+
+        Note: default output is in "%wx%h+%x+%y" format.
+        """
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option("-f", "--format", default="%wx%h+%x+%y", help=FORMAT_HELP)
+@click.option(
+    "-ci",
+    "--cursor-icon",
+    default="crosshair",
+    type=click.Choice(CURSOR_ICONS),
+    help="Select cursor icon",
+)
+@click.option(
+    "-cf",
+    "--cursor-foreground",
+    default=(65535, 65535, 65535),
+    help="Select cursor foreground color",
+)
+@click.option(
+    "-cb", "--cursor-background", default=(0, 0, 0), help="Select cursor background color"
+)
+def cli(format, cursor_icon, cursor_foreground, cursor_background):
+    xrect = XRectSel(
+        cursor_icon=cursor_icon,
+        cursor_foreground=cursor_foreground,
+        cursor_background=cursor_background,
+    )
+    geometry = xrect.select()
+
+    if geometry and (geometry["width"] >= 1 or geometry["height"] >= 1):
+        click.echo(
+            format.replace("%x", str(geometry["start"]["x"]))
+            .replace("%y", str(geometry["start"]["y"]))
+            .replace("%X", str(geometry["start"]))
+            .replace("%Y", str(geometry["end"]))
+            .replace("%w", str(geometry["width"]))
+            .replace("%h", str(geometry["height"]))
+        )
+    else:
+        click.echo("Please select area!")
