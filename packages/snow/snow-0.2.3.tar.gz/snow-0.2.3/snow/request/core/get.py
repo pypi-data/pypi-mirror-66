e5e619e2843e20245f4c53e1e4483ac51dff21af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:17 +0000

    Add package snow/0.2.3/snow-0.2.3.tar.gz

---
+++ packages/snow/snow-0.2.3.tar.gz/snow-0.2.3/snow/request/core/get.py
@@ -0,0 +1,34 @@
+from urllib.parse import urlencode
+
+from .base import Request
+
+
+class GetRequest(Request):
+    __verb__ = "GET"
+
+    def __init__(self, resource, limit=30, offset=0, query=None):
+        super(GetRequest, self).__init__(resource)
+        self._limit = limit
+        self._offset = offset
+        self.query = query
+
+    async def send(self, **kwargs):
+        return await self.send_resolve(**kwargs)
+
+    @property
+    def _page_size(self):
+        if not self._limit:
+            return False
+
+        return self._limit - self._offset
+
+    @property
+    def url(self):
+        params = dict(sysparm_offset=self._offset)
+
+        if self._page_size:
+            params["sysparm_limit"] = self._page_size
+        if self.query:
+            params["sysparm_query"] = self.query
+
+        return f"{self.base_url}&{urlencode(params)}"
---
+++ packages/snow/snow-0.2.3.tar.gz/snow-0.2.3/snow/resource/query/__init__.py
@@ -0,0 +1,4 @@
+from .builder import QueryBuilder
+from .condition import Condition
+from .operators import *
+from .utils import select
