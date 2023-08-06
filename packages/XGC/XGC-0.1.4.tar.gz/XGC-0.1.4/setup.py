082384a5994f8593452c20f454c74793906333c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:34 +0000

    Add package XGC/0.1.4/XGC-0.1.4.tar.gz

---
+++ packages/XGC/XGC-0.1.4.tar.gz/XGC-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="XGC",
+    version="0.1.4",
+    description="Xodus Python Enginering modules",
+    author="Daniel Serna",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Topic :: Office/Business",
+    ],
+    author_email="daniel.serna@xodusgroup.com",
+    url="https://github.com/danielsernaxodus/PiPy",
+    packages=["xodus"],
+    install_requires=[""],
+    include_package_data=False,
+)
