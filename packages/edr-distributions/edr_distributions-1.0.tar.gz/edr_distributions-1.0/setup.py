7eee89942b256c39e7e1e032eb949daee315d8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:45 +0000

    Add package edr-distributions/1.0/edr_distributions-1.0.tar.gz

---
+++ packages/edr-distributions/edr_distributions-1.0.tar.gz/edr_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='edr_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['edr_distributions'],
+      author = 'Edenilson Ruiz',
+	  author_email = 'ruiz.edenilson@gmail.com',
+      zip_safe=False)
