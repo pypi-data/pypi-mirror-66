cf080f613a0d805ef81e580ab06ec9d11a74b722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:10 +0000

    Add package virtualbus/v0.2.1/virtualbus-v0.2.1.tar.gz

---
+++ packages/virtualbus/virtualbus-v0.2.1.tar.gz/virtualbus-v0.2.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+import subprocess
+
+################################################################################
+# Git version
+rv = subprocess.check_output(["git", "describe", "--always", "--dirty", "--long", "--tags"]).strip().decode()
+
+if "dirty" in rv:
+	print("Repository is dirty... Try to clean it!")
+	#exit(0)
+
+label = rv.split("-")
+majorDotMinor = label[0]
+build = label[1]
+version = majorDotMinor + "." + build
+
+################################################################################
+# Long description
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+################################################################################
+# Setup config
+setup(
+	name = 'virtualbus',
+	packages = ['virtualbus'],
+	version = version,
+	license='MIT',
+	description = 'Virtual bus',
+	long_description = long_description,
+	author = 'Kanelis Elias',
+	author_email = 'hkanelhs@yahoo.gr',
+	url = 'https://github.com/tedicreations/virtualbus',
+	download_url = 'https://github.com/TediCreations/virtualbus/archive/' + str(majorDotMinor) + '.tar.gz',
+	keywords = ['virtual', 'bus', 'socket', 'networking'],
+	#install_requires=[],
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Build Tools',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+)
