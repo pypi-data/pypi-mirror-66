60746fd0ef3e5fe73b6c7f6485186b192072aa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:34 +0000

    Add package pypads/0.1.2/pypads-0.1.2-py3-none-any.whl

---
+++ packages/pypads/pypads-0.1.2-py3-none-any.whl/pypads/__init__.py
@@ -0,0 +1,21 @@
+import sys
+
+from loguru import logger as log
+
+from pypads.parallel import joblib
+from pypads.parallel import parallel
+
+logger = log
+
+
+# import logging
+# logger = logging
+
+
+def set_logger():
+    logger.remove()
+    logger.add(sys.stdout, filter="pypads", level="INFO")
+    logger.add(sys.stderr, filter="pypads", level="INFO")
+
+
+set_logger()
