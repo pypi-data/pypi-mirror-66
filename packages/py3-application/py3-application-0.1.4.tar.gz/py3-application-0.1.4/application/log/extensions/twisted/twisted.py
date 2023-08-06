ea47a78bccfa2cc31d3ddc6006c3baf5eee385a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:17 +0000

    Add package py3-application/0.1.4/py3-application-0.1.4.tar.gz

---
+++ packages/py3-application/py3-application-0.1.4.tar.gz/py3-application-0.1.4/application/log/extensions/twisted/twisted.py
@@ -0,0 +1,17 @@
+
+
+import os
+import sys
+
+from application.log.extensions import twisted
+
+try:
+    sys.path.remove(os.path.realpath(twisted.__path__[0]))
+except ValueError:
+    pass
+else:
+    sys.modules.pop('twisted', None)
+    # noinspection PyPackageRequirements
+    import twisted
+    from application.log.extensions.twisted import divert_logger
+    divert_logger()
