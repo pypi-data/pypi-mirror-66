ea1c21a9527a6e00e2710c07f6be045ec742142b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:56 +0000

    Add package cli2/1.1.9.dev2/cli2-1.1.9.dev2.tar.gz

---
+++ packages/cli2/cli2-1.1.9.dev2.tar.gz/cli2-1.1.9.dev2/cli2/command.py
@@ -0,0 +1,32 @@
+from .introspection import Callable
+
+
+def command(**config):
+    def wrap(cb):
+        if 'cli2' not in cb.__dict__:
+            name = config.pop('name', cb.__name__)
+            cb.cli2 = Callable(name, cb, **config)
+        else:
+            cb.cli2.__dict__.update(config)
+        return cb
+    return wrap
+
+
+class Option:
+    def __init__(self, name, help=None, color=None, alias=None,
+                 immediate=False, default=None):
+        self.name = name
+        self.help = help or 'Undocumented option'
+        self.color = color or ''
+        self.alias = alias
+        self.immediate = immediate
+        self.default = default
+
+
+def option(name, **cfg):
+    def wrap(cb):
+        if 'cli2' not in cb.__dict__:
+            cb = command()(cb)
+        cb.cli2.options[name] = Option(name, **cfg)
+        return cb
+    return wrap
