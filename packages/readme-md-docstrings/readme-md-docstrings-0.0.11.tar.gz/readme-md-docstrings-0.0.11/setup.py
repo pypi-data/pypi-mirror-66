fc103e2db501df85f19c7bce8cc4a120f56692b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:32 +0000

    Add package readme-md-docstrings/0.0.11/readme-md-docstrings-0.0.11.tar.gz

---
+++ packages/readme-md-docstrings/readme-md-docstrings-0.0.11.tar.gz/readme-md-docstrings-0.0.11/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='readme-md-docstrings',
+    version="0.0.11",
+    description=(
+        'Update README.md from Docstrings'
+    ),
+    author='David Belais',
+    author_email='david@belais.me',
+    python_requires='~=3.6',
+    py_modules=['readme_md_docstrings'],
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "wheel>=0.34.2",
+            "setuptools>=40.8.0",
+            "setuptools-setup-versions>=0.0.30",
+            "pytest>=5.3.5",
+            "twine>=3.1.1",
+            "tox>=3.14.3"
+        ],
+        "test": [
+            "pytest>=5.3.5",
+            "tox>=3.14.3"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
