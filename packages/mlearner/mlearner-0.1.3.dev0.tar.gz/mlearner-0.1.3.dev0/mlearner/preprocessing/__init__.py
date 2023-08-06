5698317dbe5023fb21521ee1ccbb53afacb8eee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:57 +0000

    Add package mlearner/0.1.3.dev0/mlearner-0.1.3.dev0.tar.gz

---
+++ packages/mlearner/mlearner-0.1.3.dev0.tar.gz/mlearner-0.1.3.dev0/mlearner/preprocessing/__init__.py
@@ -0,0 +1,25 @@
+
+"""Jaime Sendra Berenguer-2020.
+MLearner Machine Learning Library Extensions
+Author:Jaime Sendra Berenguer <www.linkedin.com/in/jaisenbe>
+License: MIT
+"""
+
+from .scaling import minmax_scaling
+from .mean_centering import MeanCenterer
+from .droper import FeatureDropper, DropOutliers
+from .replace_na import FillNaTransformer_forward, FillNaTransformer_backward, FillNaTransformer_value, FillNaTransformer_all, FillNaTransformer_any, FillNaTransformer_median, FillNaTransformer_mean, FillNaTransformer_idmax
+from .log_skewed import FixSkewness
+from .one_hot_encoder import OneHotEncoder
+from .replace_categorical import ReplaceTransformer, ReplaceMulticlass
+from .extract_target import ExtractCategories
+from .base_preprocess import DataCleaner, DataAnalyst
+
+__all__ = ["minmax_scaling", "MeanCenterer", "FeatureDropper",
+            "FillNaTransformer_median", "FillNaTransformer_mean",
+            "FillNaTransformer_idmax", "FillNaTransformer_any",
+            "FillNaTransformer_all", "FillNaTransformer_value",
+            "FillNaTransformer_backward", "FillNaTransformer_forward",
+            "FixSkewness", "OneHotEncoder", "DropOutliers", "ReplaceTransformer",
+            "ReplaceMulticlass", "ExtractCategories", "DataCleaner",
+            "DataAnalyst"]
