ab02f7f06396ea6c41a45293281ddc276d1fc382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:32 +0000

    Add package aioprint/0.0.4/aioprint-0.0.4.tar.gz

---
+++ packages/aioprint/aioprint-0.0.4.tar.gz/aioprint-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+import setuptools  # noqa
+from Cython.Build import cythonize
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="aioprint",
+    version="0.0.4",
+    description="Provides an asynchronous interface for print() using aiofiles.",
+    url="https://github.com/crrapi/aioprint",
+    author="crrapi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    ext_modules=cythonize("aioprint/*.py", compiler_directives={"language_level": "3"}),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
