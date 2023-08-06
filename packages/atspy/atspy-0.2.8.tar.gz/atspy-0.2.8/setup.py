c826d720da8f5a25e396c27b0d3c302510c938c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:34 +0000

    Add package atspy/0.2.8/atspy-0.2.8.tar.gz

---
+++ packages/atspy/atspy-0.2.8.tar.gz/atspy-0.2.8/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, Command
+import os
+import sys
+
+
+
+setup(name='atspy',
+      version='0.2.8',
+      description='Automated Time Series in Python',
+      url='https://github.com/firmai/atspy',
+      author='snowde',
+      author_email='d.snow@firmai.org',
+      license='MIT',
+      packages=['atspy'],
+      install_requires=[
+
+            "cachetools==4.0.0",
+            "Cython==0.29.14",
+            "dataclasses",
+            "DateTime==4.3",
+            "decorator==4.4.1",
+            "easydict==1.9",
+            "fbprophet==0.5",
+            "gluonts==0.4.2",
+            "gunicorn==20.0.4",
+            "Keras==2.2.5",
+            "lightgbm==2.2.3",
+            "matplotlib==3.1.2",
+            "mxnet-cu100==1.4.1",
+            "numba==0.47.0",
+            "numexpr==2.7.1",
+            "pandas==0.25.3",
+            "pip-tools==4.2.0",
+            "pmdarima==1.5.2",
+            "psutil==5.4.8",
+            "psycopg2==2.7.6.1",
+            "scikit-learn==0.22.1",
+            "scipy==1.4.1",
+            "seaborn==0.9.1",
+            "sklearn==0.0",
+            "statsmodels==0.10.2",
+            "sympy==1.1.1",
+            "tbats==1.0.9",
+            "tblib==1.6.0",
+            "tensorflow==1.15.0",
+            "tflearn==0.3.2",
+            "torch==1.4.0",
+            "tqdm==4.28.1",
+            "tsfresh==0.14.1",
+            "tweepy==3.6.0",
+            "typing==3.6.6",
+            "typing-extensions==3.6.6",
+            "xlrd==1.1.0",
+            "seasonal==0.3.1",
+            "nbeats-pytorch==1.3.0",
+            "gluonts==0.4.2",
+            "numpy==1.17.4",
+            "pydantic==1.4",
+            #"dataclasses==0.7",
+            
+            ],
+            
+      zip_safe=False)
