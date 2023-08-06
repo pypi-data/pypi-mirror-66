7ec56cc25513d90db7544e2746b06c302497e7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:34 +0000

    Add package key-people/0.92/key-people-0.92.tar.gz

---
+++ packages/key-people/key-people-0.92.tar.gz/key-people-0.92/key_people/__init__.py
@@ -0,0 +1,58 @@
+import os
+import pkg_resources
+import sqlite3
+from .database_management import open_connection
+from .database_management import find_ticker
+from .generate_possible_names import generate_possible_names
+
+
+class Company:
+	def __init__(self, ticker):
+		DB_FILE = pkg_resources.resource_filename('key_people', 'DB/Stocks.db')
+		self.conn, self.c = open_connection(DB_FILE)
+		NICKNAME_FILE = pkg_resources.resource_filename('key_people', 'DB/Nicknames.db')
+		self.n_conn, self.n_c = open_connection(DB_FILE)
+		self.terms = self.pop(ticker)
+
+		
+
+	def pop(self, c):
+		ticker = ticker.upper()
+		ticker = (ticker, )
+		results = find_ticker(ticker, self.c)[0]
+		ticker = results[0]
+		company_name = results[1]
+		people = []
+		for x in results[2:]:
+			if x != 'NULL':
+				x = generate_possible_names(x, c)
+				people.append(x)
+
+		return {'ticker': ticker, 'company_name': company_name, 'key_people_nicknames': people}
+
+def get_terms(ticker):
+	NICKNAME_FILE = pkg_resources.resource_filename('key_people', 'DB/Nicknames.db')
+	conn, c = open_connection(NICKNAME_FILE)
+
+	ticker = ticker.upper()
+	ticker = (ticker, )
+	results = find_ticker(ticker, c)[0]
+	ticker = results[0]
+	company_name = results[1]
+	people = []
+	for x in results[2:]:
+		if x != 'NULL':
+			x = generate_possible_names(x, c)
+			people.append(x)
+
+	return {'ticker': ticker, 'company_name': company_name, 'key_people_nicknames': people}
+	
+def search_company_name(ticker):
+	ticker = ticker.upper()
+	DB_FILE = pkg_resources.resource_filename('key_people', 'DB/Stocks.db')
+	conn, c = open_connection(DB_FILE)
+	ticker = (ticker, )
+	results = find_ticker(ticker, c)[0]
+	company_name = results[1]
+	conn.close()
+	return company_name
