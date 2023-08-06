9306b83ec0805b31b771a74c8c52c2982f15fc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:50 +0000

    Add package formio-data/0.1.0.dev5/formio-data-0.1.0.dev5.tar.gz

---
+++ packages/formio-data/formio-data-0.1.0.dev5.tar.gz/formio-data-0.1.0.dev5/formiodata/components.py
@@ -0,0 +1,138 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+class Component:
+
+    def __init__(self, raw):
+        self.raw = raw
+        self.form = {}
+
+    @property
+    def key(self):
+        return self.raw.get('key')
+
+    @property
+    def type(self):
+        return self.raw.get('type')
+
+    @property
+    def label(self):
+        return self.raw.get('label')
+
+    @label.setter
+    def label(self, value):
+        if self.raw.get('label'):
+            self.raw['label'] = value
+
+    @property
+    def value(self):
+        return self.form['value']
+
+    @value.setter
+    def value(self, value):
+        self.form['value'] = value
+
+
+# Basic
+
+class textfieldComponent(Component):
+    pass
+
+
+class textareaComponent(Component):
+    pass
+
+
+class numberComponent(Component):
+    pass
+
+
+class passwordComponent(Component):
+    pass
+
+
+class checkboxComponent(Component):
+    pass
+
+
+class selectboxesComponent(Component):
+    pass
+
+
+class selectComponent(Component):
+    pass
+
+
+class radioComponent(Component):
+    pass
+
+
+class buttonComponent(Component):
+    pass
+
+
+# Advanced
+
+class emailComponent(Component):
+    pass
+
+
+class urlComponent(Component):
+    pass
+
+
+class phoneNumberComponent(Component):
+    pass
+
+
+# TODO: tags, address
+
+
+class datetimeComponent(Component):
+    pass
+
+
+class dateComponent(Component):
+    pass
+
+
+class timeComponent(Component):
+    pass
+
+
+class currencyComponent(Component):
+    pass
+
+
+class surveyComponent(Component):
+    pass
+
+
+class signatureComponent(Component):
+    pass
+
+
+# Layout components
+
+class htmlelementComponent(Component):
+    pass
+
+
+class contentComponent(Component):
+    pass
+
+
+class columnsComponent(Component):
+    pass
+
+
+class fieldsetComponent(Component):
+    pass
+
+
+class panelComponent(Component):
+    pass
+
+
+class tableComponent(Component):
+    pass
