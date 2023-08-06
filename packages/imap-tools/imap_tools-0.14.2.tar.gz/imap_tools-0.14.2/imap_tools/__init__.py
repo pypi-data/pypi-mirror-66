3d15afdd71e76fcb649e05fe3dcba78057913f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:47 +0000

    Add package imap-tools/0.14.2/imap_tools-0.14.2.tar.gz

---
+++ packages/imap-tools/imap_tools-0.14.2.tar.gz/imap_tools-0.14.2/imap_tools/__init__.py
@@ -0,0 +1,5 @@
+from .query import Q, AND, OR, NOT, H
+from .mailbox import *
+from .message import *
+from .folder import *
+from .utils import *
---
+++ packages/imap-tools/imap_tools-0.14.2.tar.gz/imap_tools-0.14.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='imap_tools',
+    version='0.14.2',
+    packages=setuptools.find_packages(),
+    url='https://github.com/ikvk/imap_tools',
+    license='Apache-2.0',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author='v.kaukin',
+    author_email='KaukinVK@ya.com',
+    description='Working with email and mailbox using IMAP protocol.',
+    keywords=['imap', 'imap-client', 'python3', 'email'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    # install_requires=['typing>=3.6.2'],
+)
