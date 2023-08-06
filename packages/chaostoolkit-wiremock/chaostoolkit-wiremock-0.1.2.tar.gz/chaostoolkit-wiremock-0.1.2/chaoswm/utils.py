16d1538e28ca7939a37af8d42cdc98bd22a7f489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:32 +0000

    Add package chaostoolkit-wiremock/0.1.2/chaostoolkit-wiremock-0.1.2.tar.gz

---
+++ packages/chaostoolkit-wiremock/chaostoolkit-wiremock-0.1.2.tar.gz/chaostoolkit-wiremock-0.1.2/chaoswm/utils.py
@@ -0,0 +1,42 @@
+from contextlib import closing
+import socket
+from typing import Any, Dict, Optional
+
+from logzero import logger
+
+__all__ = ["can_connect_to", "get_wm_params", "check_configuration"]
+
+
+def can_connect_to(host: str, port: int) -> bool:
+    """ Test a connection to a host/port """
+
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        return bool(sock.connect_ex((host, port)) == 0)
+
+
+def get_wm_params(c: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+    wm_conf = c.get("wiremock", {})
+
+    host = wm_conf.get("host", None)
+    port = wm_conf.get("port", None)
+    context_path = wm_conf.get("contextPath", "")
+    timeout = wm_conf.get("timeout", 1)
+
+    url = ""
+
+    if host and port:
+        url = "http://{}:{}{}".format(host, port, context_path)
+
+    if not url:
+        logger.error("No configuration params to set WM server url")
+        return None
+
+    return {"url": url, "timeout": timeout}
+
+
+def check_configuration(c: Dict[str, Any] = None) -> bool:
+    c = c or {}
+    if "wiremock" not in c:
+        logger.error("Error: wiremock key not found in configuration section")
+        return False
+    return True
