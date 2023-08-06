e4ed27ab09608719472d056ffb390b8592874660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:36 +0000

    Add package mr-sim/0.2.0/mr_sim-0.2.0.tar.gz

---
+++ packages/mr-sim/mr_sim-0.2.0.tar.gz/mr_sim-0.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="mr_sim",
+    version="0.2.0",
+    author="Cameron Devine",
+    author_email="camdev@uw.edu",
+    description="A package for the simulation of abrasive material removal processes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.4",
+    install_requires=["numpy"],
+    url="https://github.com/CameronDevine/mr_sim",
+)
