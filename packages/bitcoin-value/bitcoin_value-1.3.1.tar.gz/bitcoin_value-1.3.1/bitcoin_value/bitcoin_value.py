22c5c1c8dfbdf687ad8703faa94c07db4274aca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:56 +0000

    Add package bitcoin-value/1.3.1/bitcoin_value-1.3.1.tar.gz

---
+++ packages/bitcoin-value/bitcoin_value-1.3.1.tar.gz/bitcoin_value-1.3.1/bitcoin_value/bitcoin_value.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+res = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+json_data = json.loads(res.text)
+
+def EUR():
+    return "€" + str(json_data['bpi']['EUR']['rate_float'])
+
+def USD():
+    return "$" + str(json_data['bpi']['USD']['rate_float'])
+
+def GBP():
+    return "£" + str(json_data['bpi']['GBP']['rate_float'])
