3b37b4ee5c218434feddb2eaace80370cd177c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:12 +0000

    Add package orco/0.5.0/orco-0.5.0-py3-none-any.whl

---
+++ packages/orco/orco-0.5.0-py3-none-any.whl/orco/jobsetup.py
@@ -0,0 +1,18 @@
+class JobSetup:
+    """
+    Structure for configuring Job computation.
+
+    Attributes:
+
+    - timeout (int|None): Time limit (in seconds) for computation. If the computation is not finished
+               before the limit, an exception is thrown. Default: No time limit.
+    - relay (bool): If true, stdout/stderr, redirect output also into the executor's console.
+    """
+
+    __slots__ = ("runner_name", "timeout", "setup", "relay")
+
+    def __init__(self, runner_name="local", timeout=None, relay=False, setup=None):
+        self.runner_name = runner_name
+        self.timeout = timeout
+        self.setup = setup
+        self.relay = relay
