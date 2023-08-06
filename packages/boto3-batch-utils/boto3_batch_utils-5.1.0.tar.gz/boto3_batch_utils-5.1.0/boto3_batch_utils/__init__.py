0a2fbe40461dafce3127055a7a9e9e48a3939547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:20 +0000

    Add package boto3-batch-utils/5.1.0/boto3_batch_utils-5.1.0.tar.gz

---
+++ packages/boto3-batch-utils/boto3_batch_utils-5.1.0.tar.gz/boto3_batch_utils-5.1.0/boto3_batch_utils/__init__.py
@@ -0,0 +1,15 @@
+from boto3_batch_utils.Cloudwatch import CloudwatchBatchDispatcher, cloudwatch_dimension
+from boto3_batch_utils.Dynamodb import DynamoBatchDispatcher
+from boto3_batch_utils.Kinesis import KinesisBatchDispatcher
+from boto3_batch_utils.SQS import SQSBatchDispatcher, SQSFifoBatchDispatcher
+
+__all__ = [
+    'CloudwatchBatchDispatcher',
+    'cloudwatch_dimension',
+    'DynamoBatchDispatcher',
+    'KinesisBatchDispatcher',
+    'SQSBatchDispatcher',
+    'SQSFifoBatchDispatcher'
+]
+
+__version__ = '5.1.0'
