0f5c0efbd8b987411c43a94a0507b09c60d1897c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:07 +0000

    Add package sendbee-api/1.2.0/sendbee_api-1.2.0.tar.gz

---
+++ packages/sendbee-api/sendbee_api-1.2.0.tar.gz/sendbee_api-1.2.0/sendbee_api/automation/query_params.py
@@ -0,0 +1,8 @@
+from sendbee_api.query_params import QueryParams
+
+
+class ChatbotActivity(QueryParams):
+    """Parameters for turning the chatbot on or off for a conversation"""
+
+    conversation_id = 'conversation_id', 'Conversation UUID'
+    active = 'active', 'Chatbot activity'
