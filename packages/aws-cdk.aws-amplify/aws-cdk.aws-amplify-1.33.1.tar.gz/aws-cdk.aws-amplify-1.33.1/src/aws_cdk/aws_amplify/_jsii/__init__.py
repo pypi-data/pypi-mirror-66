91ff7a836deb392ae4524482022784160081b82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:06 +0000

    Add package aws-cdk.aws-amplify/1.33.1/aws-cdk.aws-amplify-1.33.1.tar.gz

---
+++ packages/aws-cdk.aws-amplify/aws-cdk.aws-amplify-1.33.1.tar.gz/aws-cdk.aws-amplify-1.33.1/src/aws_cdk/aws_amplify/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_codebuild
+import aws_cdk.aws_codecommit
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_secretsmanager
+import aws_cdk.core
+import constructs
+__all__ = []
+
+publication.publish()
