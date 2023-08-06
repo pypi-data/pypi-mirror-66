cf6c15d9a6d0c8014913c1018964dfba918e78f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:28 +0000

    Add package Comickaze/2.0.0b1/Comickaze-2.0.0b1.tar.gz

---
+++ packages/Comickaze/Comickaze-2.0.0b1.tar.gz/Comickaze-2.0.0b1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="Comickaze",
+    version="2.0.0b1",
+    author="Aaron Gonzales",
+    author_email="aaroncgonzales.dev@gmail.com",
+    description="A package to search and download comics on ReadComicsOnline.ru",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chr1st-oo/comickaze",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "bs4",
+        "coloredlogs",
+        "colorama",
+        "progress",
+        "img2pdf"
+    ],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
