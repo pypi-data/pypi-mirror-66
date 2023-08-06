c71d4238738a78c6acb6461e3d45659492c8fe94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:20 +0000

    Add package FaBo9Axis-MPU9250-Python3/1.0.1/FaBo9Axis_MPU9250_Python3-1.0.1.tar.gz

---
+++ packages/FaBo9Axis-MPU9250-Python3/FaBo9Axis_MPU9250_Python3-1.0.1.tar.gz/FaBo9Axis_MPU9250_Python3-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = ''
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: Apache Software License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 3.8',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(
+    name         = 'FaBo9Axis_MPU9250_Python3',
+    version      = '1.0.1',
+    author       = 'RandomUser1',
+    description  = "This is a library for the FaBo 9AXIS I2C Brick.",
+    long_description=long_description,
+    url          = 'https://github.com/piotrek-k/FaBo9AXIS-MPU9250-Python3',
+    license      = 'Apache License 2.0',
+    classifiers  = classifiers,
+    packages     = find_packages()
+)
