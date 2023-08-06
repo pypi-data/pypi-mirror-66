9d70af973637d338d68e460277bb55a40e888450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:37 +0000

    Add package v2t/1.1.4/v2t-1.1.4.tar.gz

---
+++ packages/v2t/v2t-1.1.4.tar.gz/v2t-1.1.4/v2t/__version__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#                            _                _____
+#      /\                   | |       /\     |_   _|
+#     /  \     ____   __ _  | |_     /  \      | |
+#    / /\ \   |_  /  / _` | | __|   / /\ \     | |
+#   / ____ \   / /  | (_| | | |_   / ____ \   _| |_
+#  /_/    \_\ /___|  \__,_|  \__| /_/    \_\ |_____|
+#
+#
+
+__title__ = "v2t"
+__description__ = "AzatAI Video To Text Converter. "
+__url__ = "https://github.com/AzatAI/v2t"
+__version__ = "1.1.4"
+__author__ = "Yaakov Azat"
+__author_email__ = "yaakovazat@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 Azat Artificial Intelligence, LLP (AzatAI)"
