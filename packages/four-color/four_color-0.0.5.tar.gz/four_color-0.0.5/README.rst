04f7c43affae3022ae6ae031afe83e4aee4023c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package four-color/0.0.5/four_color-0.0.5.tar.gz

---
+++ packages/four-color/four_color-0.0.5.tar.gz/four_color-0.0.5/README.rst
@@ -0,0 +1,31 @@
+`four_color` is a package for Four Color Problem.
+::
+
+* Download picture from https://www.dropbox.com/s/twiscp9h15so8no/sample.png?dl=1
+* `python -m four_color`
+* Open http://localhost:8000/
+* Set "sample.png" and push "send" button.
+* Download "fig.png".
+
+![](https://raw.githubusercontent.com/SaitoTsutomu/four_color/master/fig.gif
+   :scale: 200%
+
+Requirements
+------------
+* Python 3, Pillow, Flask, NetworkX, PuLP
+
+Features
+--------
+* nothing
+
+Setup
+-----
+::
+
+   $ pip install four_color
+
+History
+-------
+0.0.1 (2016-6-13)
+~~~~~~~~~~~~~~~~~~
+* first release
---
+++ packages/four-color/four_color-0.0.5.tar.gz/four_color-0.0.5/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['four_color']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=7.1.1,<8.0.0',
+ 'flask>=1.1.2,<2.0.0',
+ 'networkx>=2.4,<3.0',
+ 'pulp>=2.1,<3.0']
+
+setup_kwargs = {
+    'name': 'four-color',
+    'version': '0.0.5',
+    'description': '`four_color` is a package for Four Color Problem.',
+    'long_description': '`four_color` is a package for Four Color Problem.\n::\n\n* Download picture from https://www.dropbox.com/s/twiscp9h15so8no/sample.png?dl=1\n* `python -m four_color`\n* Open http://localhost:8000/\n* Set "sample.png" and push "send" button.\n* Download "fig.png".\n\n![](https://raw.githubusercontent.com/SaitoTsutomu/four_color/master/fig.gif\n   :scale: 200%\n\nRequirements\n------------\n* Python 3, Pillow, Flask, NetworkX, PuLP\n\nFeatures\n--------\n* nothing\n\nSetup\n-----\n::\n\n   $ pip install four_color\n\nHistory\n-------\n0.0.1 (2016-6-13)\n~~~~~~~~~~~~~~~~~~\n* first release\n',
+    'author': 'SaitoTsutomu',
+    'author_email': 'tsutomu7@hotmail.co.jp',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/SaitoTsutomu/four_color',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
