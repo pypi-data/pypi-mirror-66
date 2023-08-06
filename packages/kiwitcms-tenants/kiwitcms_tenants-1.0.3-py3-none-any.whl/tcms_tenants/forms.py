ca0f278adff36f6b75f03ef85bad53cb73343597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:32 +0000

    Add package kiwitcms-tenants/1.0.3/kiwitcms_tenants-1.0.3-py3-none-any.whl

---
+++ packages/kiwitcms-tenants/kiwitcms_tenants-1.0.3-py3-none-any.whl/tcms_tenants/forms.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
+
+# Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+
+from django import forms
+from django_tenants.postgresql_backend import base as validators
+
+from tcms_tenants import utils
+
+
+class NewTenantForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    schema_name = forms.CharField(max_length=63,
+                                  # pylint: disable=protected-access
+                                  validators=[validators._check_schema_name,
+                                              utils.schema_name_not_used])
+    paid_until = forms.DateTimeField(required=False, widget=forms.HiddenInput)
+    on_trial = forms.BooleanField(initial=True, required=False,
+                                  widget=forms.HiddenInput)
+    organization = forms.CharField(max_length=64, required=False,
+                                   widget=forms.HiddenInput)
