2c7c26b1ffc874115ce4c89f174af665f37e1f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:56 +0000

    Add package huginn/0.1.1/huginn-0.1.1-py3-none-any.whl

---
+++ packages/huginn/huginn-0.1.1-py3-none-any.whl/tests/test_huginn.py
@@ -0,0 +1,32 @@
+import time
+
+import pytest
+import pandas as pd
+
+from huginn.huginn import Huginn
+
+@pytest.fixture
+def point72():
+    key_word = "Point72"
+
+    cl = Huginn(key_word)
+    cl.get_anomalies()
+
+    return cl
+
+
+def test_get_text(point72):
+    time.sleep(45)
+    point72.get_text()
+
+    text = point72.text
+
+    assert type(text) == dict
+
+    assert len(text) <= 10
+
+    for ts, text in text.items():
+        assert type(ts) == pd.Timestamp
+        assert type(text) == list
+
+
