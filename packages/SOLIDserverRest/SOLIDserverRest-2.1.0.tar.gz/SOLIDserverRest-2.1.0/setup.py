7f95a05b3696af2dd990938bed5696f3ede0119f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:10 +0000

    Add package SOLIDserverRest/2.1.0/SOLIDserverRest-2.1.0.tar.gz

---
+++ packages/SOLIDserverRest/SOLIDserverRest-2.1.0.tar.gz/SOLIDserverRest-2.1.0/setup.py
@@ -0,0 +1,34 @@
+import io
+import os
+
+from setuptools import setup
+
+here = os.path.dirname(__file__)
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='SOLIDserverRest',
+    version='2.1.0',
+    author='Gregory CUGAT',
+    url='https://gitlab.com/efficientip/solidserverrest',
+    description='The SOLIDserverRest is a library to drive EfficientIP API',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author_email='gregory.cugat@efficientip.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    license='BSD 2',
+    packages=['SOLIDserverRest', 'SOLIDserverRest.adv'],
+    zip_safe=False,
+    python_requires=">=3.0",
+    py_modules=['check_python_versions'],
+    entry_points={
+        'console_scripts': [
+            'check-python-versions = check_python_versions:main',
+        ],}
+    )
