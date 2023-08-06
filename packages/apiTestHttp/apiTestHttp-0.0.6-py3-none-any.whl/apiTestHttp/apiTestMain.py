1b6a2d457f0a703c3113f0214203cbbb8f73dcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package apiTestHttp/0.0.6/apiTestHttp-0.0.6-py3-none-any.whl

---
+++ packages/apiTestHttp/apiTestHttp-0.0.6-py3-none-any.whl/apiTestHttp/apiTestMain.py
@@ -0,0 +1,50 @@
+import getopt,sys
+
+ENV_NAME = ''
+FILE_PATH = ''
+TEST_CASES = []
+from apitestbasiclib.filetools import get_testcases
+print('sys.path=%s'% sys.path)
+from apiTestHttp.testCasesRun import runApiCases
+
+def help():
+    msg = "*"*100+"\n" \
+          +"您正在使用接口测试服务，请仔细查看使用说明\n" \
+          +"--help or -h :显示帮助信息\n" \
+          +"--file or -f : 指定命令执行的文件路径\n" \
+          +"--env or -e : 指定运行的被测环境\n" \
+          +"*"*100
+    print(msg)
+
+def main():
+    try:
+        opts,args = getopt.getopt(sys.argv[1:],"-hf:e:",["help","file=","env="])
+        print(opts)
+    except Exception:
+        print('参数输入错误，请查看帮助文档')
+        help()
+        sys.exit(-1)
+    options = []
+    for opt,value in opts:
+        options.append(opt)
+    if ('-h' in options) or ('--help' in options):
+        help()
+        sys.exit(0)
+    elif ('-f' in options) or ('--file' in options):
+        if ('-e' not in options) and ('--env' not in options):
+            print('\n必须指定被测环境,请查看帮助\n')
+            help()
+            sys.exit(-1)
+        else:
+            for opt,value in opts:
+                if opt == '-f' or opt == '--file':
+                    global FILE_PATH
+                    FILE_PATH = value
+                if opt == '-e' or opt == '--env':
+                    global ENV_NAME
+                    ENV_NAME = value
+                global TEST_CASES
+                TEST_CASES = get_testcases(FILE_PATH)
+            runApiCases()
+
+main()
---
+++ packages/apiTestHttp/apiTestHttp-0.0.6-py3-none-any.whl/apiTestHttp/testCasesRun.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+import subprocess
+import pytest
+
+from apiTestHttp.apiTestMain import FILE_PATH, ENV_NAME, TEST_CASES
+from apitestbasiclib.apitestlogic import apitest
+def runApiCases():
+    print('runCases run ......')
+    subprocess.call('pytest testCasesRun.py',shell=False)
+print('_FILE_PATH=%s,_ENV_NAME=%s'% (FILE_PATH,ENV_NAME))
+print('_TEST_CASES=%s' % TEST_CASES)
+@pytest.mark.parametrize('test_cases',TEST_CASES)
+def test_http_request(test_cases):
+    print('test_yamlapi run ......')
+    apitest(test_cases,ENV_NAME)
