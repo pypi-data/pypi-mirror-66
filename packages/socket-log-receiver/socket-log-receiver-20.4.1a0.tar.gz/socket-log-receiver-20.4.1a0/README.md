36797b6d83145cf0a84e3455c11c4a321e54bdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:13 +0000

    Add package socket-log-receiver/20.4.1a0/socket-log-receiver-20.4.1a0.tar.gz

---
+++ packages/socket-log-receiver/socket-log-receiver-20.4.1a0.tar.gz/socket-log-receiver-20.4.1a0/README.md
@@ -0,0 +1,58 @@
+[![pyversion Status](https://img.shields.io/pypi/pyversions/socket-log-receiver.svg)](https://img.shields.io/pypi/pyversions/socket-log-receiver.svg)
+[![CircleCI](https://circleci.com/gh/okomestudio/socket-log-receiver.svg?style=shield)](https://circleci.com/gh/okomestudio/socket-log-receiver)
+[![Coverage Status](https://coveralls.io/repos/github/okomestudio/socket-log-receiver/badge.svg?branch=development)](https://coveralls.io/github/okomestudio/socket-log-receiver?branch=development&kill_cache=1)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+
+# Socket Log Receiver
+
+`socket_log_receiver` is a light-weight socket log receiver. It runs
+as a server and aggregates messages from multi-process application via
+socket and logs to a single file. Python's `logging` package does not
+support logging to a single file from multiple processes. By pointing
+`SocketHandler` to `socket_log_receiver`, the multi-process
+application can log to a single file.
+
+
+## Installation
+
+``` bash
+$ pip install socket-log-receiver
+```
+
+
+## Basic Usage
+
+The receiver service should be run as a service.
+
+``` bash
+$ python -m socket_log_receiver  # as a module
+$ log_receiver                   # as a command-line program
+```
+
+In the application, use `SocketHandler` to send logs to the receiver
+service.
+
+``` python
+from logging.handlers import SocketHandler
+
+handler = SocketHandler('localhost', 9020)  # handler to send logs to localhost:9020
+logging.root.addHandler(handler)            # add the socket handler to the root logger
+```
+
+This way, the root logger sends logging messages to the receiver service.
+
+
+## Development
+
+```bash
+$ pip install -e .[dev]
+$ pre-commit install
+```
+
+
+### Running Tests
+
+``` bash
+$ python setup.py tests
+```
---
+++ packages/socket-log-receiver/socket-log-receiver-20.4.1a0.tar.gz/socket-log-receiver-20.4.1a0/src/socket_log_receiver/receivers.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import logging
+import pickle
+from select import select
+from struct import unpack
+
+try:
+    from socketserver import StreamRequestHandler
+    from socketserver import ThreadingTCPServer
+except ImportError:
+    # For Python 2.7
+    from SocketServer import StreamRequestHandler
+    from SocketServer import ThreadingTCPServer
+
+
+class _Handler(StreamRequestHandler):
+    """Handler for log record pickled by logging.handlers.SocketHandler.
+
+    The SocketHandler.makePickle(record) streams the data according to
+
+    >>> data = pickle.dumps(record_attr_dict, 1)
+    >>> datalen = struct.pack('>L', len(data))
+    >>> datalen + data  # this will be streamed to socket
+
+    Basically the first four bytes contains the data length of the pickled log record.
+    """
+
+    def handle(self):
+        while 1:
+            chunk = self.connection.recv(4)
+            if len(chunk) < 4:
+                break
+            slen = unpack(">L", chunk)[0]  # big-endian (>), unsigned long (L)
+            chunk = self.connection.recv(slen)
+            while len(chunk) < slen:
+                chunk = chunk + self.connection.recv(slen - len(chunk))
+            obj = self._unpickle(chunk)
+            record = logging.makeLogRecord(obj)
+            self._handle_log_record(record)
+
+    def _unpickle(self, data):
+        return pickle.loads(data)
+
+    def _handle_log_record(self, record):
+        if self.server.logname is not None:
+            name = self.server.logname
+        else:
+            name = record.name
+        logger = logging.getLogger(name)
+        logger.handle(record)
+
+
+class Receiver(ThreadingTCPServer):
+
+    allow_reuse_address = True
+
+    def __init__(
+        self,
+        host,
+        port,
+        handler=_Handler,
+        bind_and_activate=True,
+        abort=0,
+        timeout=1,
+        logname=None,
+    ):
+        ThreadingTCPServer.__init__(
+            self, (host, port), handler, bind_and_activate=bind_and_activate
+        )
+        self.abort = abort
+        self.timeout = timeout
+        self.logname = logname
+
+    def serve(self):
+        abort = 0
+        while not abort:
+            rd, wr, ex = select([self.socket.fileno()], [], [], self.timeout)
+            if rd:
+                self.handle_request()
+            abort = self.abort
+
+
+def serve(action, _, con):
+    receiver = Receiver(con["host"], con["port"])
+    logging.info("%r starting", receiver)
+    receiver.serve()
+
+
+def configure_logging(action, _, con):
+    logging.root.setLevel(con["level"])
+
+    handlers = []
+
+    filename = con["filename"]
+    if filename:
+        mode = con["filemode"]
+        handlers = [logging.handlers.WatchedFileHandler(filename, mode=mode)]
+
+    if not any(type(h) == logging.StreamHandler for h in logging.root.handlers):
+        handlers.append(logging.StreamHandler())
+
+    format = con["format"]
+    datefmt = con["datefmt"]
+    formatter = logging.Formatter(format, datefmt)
+
+    for handler in handlers:
+        handler.setLevel(con["level"])
+        handler.setFormatter(formatter)
+        logging.root.addHandler(handler)
