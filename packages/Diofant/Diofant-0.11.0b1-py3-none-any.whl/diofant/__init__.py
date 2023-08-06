02cf9f2ca7114560c5889cadfe4e08dee2c51fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:07 +0000

    Add package Diofant/0.11.0b1/Diofant-0.11.0b1-py3-none-any.whl

---
+++ packages/Diofant/Diofant-0.11.0b1-py3-none-any.whl/diofant/__init__.py
@@ -0,0 +1,31 @@
+"""Diofant is a Python library for symbolic mathematics."""
+
+import os
+DIOFANT_DEBUG = os.getenv('DIOFANT_DEBUG', 'False') != 'False'
+del os
+
+import pkg_resources
+__version__ = pkg_resources.get_distribution(__name__).version
+del pkg_resources
+
+from .core import *
+from .logic import *
+from .polys import *
+from .domains import *
+from .series import *
+from .functions import *
+from .ntheory import *
+from .concrete import *
+from .simplify import *
+from .sets import *
+from .solvers import *
+from .matrices import *
+from .geometry import *
+from .utilities import *
+from .integrals import *
+from .tensor import *
+from .calculus import *
+from .combinatorics import *
+from .plotting import *
+from .printing import *
+from .interactive import *
---
+++ packages/Diofant/Diofant-0.11.0b1-py3-none-any.whl/diofant/utilities/memoization.py
@@ -0,0 +1,22 @@
+from functools import wraps
+
+
+def recurrence_memo(initial):
+    """
+    Memo decorator for sequences defined by recurrence
+
+    See usage examples e.g. in the specfun/combinatorial module
+    """
+    cache = initial
+
+    def decorator(f):
+        @wraps(f)
+        def g(n):
+            L = len(cache)
+            if n <= L - 1:
+                return cache[n]
+            for i in range(L, n + 1):
+                cache.append(f(i, cache))
+            return cache[-1]
+        return g
+    return decorator
