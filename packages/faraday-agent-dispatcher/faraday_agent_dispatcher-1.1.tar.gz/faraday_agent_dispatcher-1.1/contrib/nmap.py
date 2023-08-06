be2b18a9149b302051fdfb5987af9c8679e8821d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:14 +0000

    Add package faraday-agent-dispatcher/1.1/faraday_agent_dispatcher-1.1.tar.gz

---
+++ packages/faraday-agent-dispatcher/faraday_agent_dispatcher-1.1.tar.gz/faraday_agent_dispatcher-1.1/contrib/nmap.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+
+"""You need to clone and install faraday plugins"""
+from faraday_plugins.plugins.repo.nmap.plugin import NmapPlugin
+
+cmd = [
+    "nmap",
+    "-p{}".format(os.environ.get('EXECUTOR_CONFIG_PORT_LIST')),
+    os.environ.get('EXECUTOR_CONFIG_TARGET'),
+    "-oX",
+    "-",
+]
+
+
+results = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+nmap = NmapPlugin()
+nmap.parseOutputString(results.stdout)
+print(nmap.get_json())
