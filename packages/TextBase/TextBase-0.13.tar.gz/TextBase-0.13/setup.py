c89c5d57f398049db90c0940bdb0d94364577fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:20 +0000

    Add package TextBase/0.13/TextBase-0.13.tar.gz

---
+++ packages/TextBase/TextBase-0.13.tar.gz/TextBase-0.13/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="TextBase",
+    version="0.13",
+    description="TextBase library to manipulate DBText style data files.",
+    author="Etienne Posthumus",
+    author_email="ep@epoz.org",
+    url="https://github.com/epoz/textbase/",
+    py_modules=["textbase"],
+)
