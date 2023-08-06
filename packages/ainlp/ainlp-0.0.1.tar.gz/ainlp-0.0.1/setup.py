45f28a30edc81d1451ea758cab195f9800dc280e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:27 +0000

    Add package ainlp/0.0.1/ainlp-0.0.1.tar.gz

---
+++ packages/ainlp/ainlp-0.0.1.tar.gz/ainlp-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ainlp",
+    version="0.0.1",
+    author="ymai",
+    author_email="zhangjinjie@yimian.com.cn",
+    description="nlp library which support text processing methods, text match methods, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
