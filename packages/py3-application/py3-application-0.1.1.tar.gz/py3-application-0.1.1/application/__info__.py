7dfbd9b8067adae8a7f737d43d5243c777ded746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:08 +0000

    Add package py3-application/0.1.1/py3-application-0.1.1.tar.gz

---
+++ packages/py3-application/py3-application-0.1.1.tar.gz/py3-application-0.1.1/application/__info__.py
@@ -0,0 +1,14 @@
+
+"""Package information"""
+
+__project__ = 'py3-application'
+__summary__ = 'Basic building blocks for python applications'
+__webpage__ = 'https://github.com/AGProjects/python-application'
+
+__version__ = '0.1.1'
+
+__author__ = 'Dan Pascu'
+__email__ = 'dan@ag-projects.com'
+
+__license__ = 'LGPL'
+__copyright__ = 'Copyright 2006-2019 {}'.format(__author__)
