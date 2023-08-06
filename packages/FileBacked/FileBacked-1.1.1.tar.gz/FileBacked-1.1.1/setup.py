ebedb402d87588e47b054eea2ab0e823b9eb3500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:08 +0000

    Add package FileBacked/1.1.1/FileBacked-1.1.1.tar.gz

---
+++ packages/FileBacked/FileBacked-1.1.1.tar.gz/FileBacked-1.1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='FileBacked',
+    version='1.1.1',
+    description='Simple file-backed HDF5 storage for Python objects',
+    long_description=readme,
+    maintainer='Eivind Fonn',
+    maintainer_email='evfonn@gmail.com',
+    url='https://github.com/TheBB/FileBacked',
+    py_modules=['filebacked'],
+    install_requires=[
+        'dill',
+        'numpy',
+        'typing_inspect',
+    ],
+)
