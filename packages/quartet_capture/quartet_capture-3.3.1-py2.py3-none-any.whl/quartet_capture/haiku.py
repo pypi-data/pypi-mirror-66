931c0d66440e599c9c4d9cb7bce7537db98c1d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:30 +0000

    Add package quartet_capture/3.3.1/quartet_capture-3.3.1-py2.py3-none-any.whl

---
+++ packages/quartet_capture/quartet_capture-3.3.1-py2.py3-none-any.whl/quartet_capture/haiku.py
@@ -0,0 +1,72 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2018 SerialLab Corp.  All rights reserved.
+adjectives = [
+    'active', 'alveolar', 'autumn', 'bonded', 'comparative', 'conchoidal',
+    'convective',
+    'constant', 'cartesian', 'close', 'crystalline',
+    'conductive', 'current', 'crimson',
+    'eutropic', 'inorganic',
+    'diffracted', 'dimensional', 'dissolved', 'diatomic',
+    'deuterated', 'erosive', 'endemic', 'exothermic',
+    'entropic',
+    'isomorphic', 'prospective', 'grey', 'green',
+    'hybrid', 'heliocentric', 'insoluble', 'inert', 'ionized', 'iterative',
+    'igneous', 'intrepid', 'inferential',
+    'kinetic', 'lunar', 'pythagorean', 'linear', 'opaque',
+    'median', 'magnetic', 'malleable', 'metric', 'mechanical', 'macroscopic',
+    'neural', 'nuclear', 'nucleic', 'metamorphic', 'micorbial',
+    'oblique',
+    'optical', 'polar',
+    'organic', 'interstitial', 'periodic', 'photosynthetic', 'polymorphic',
+    'seismic',
+    'random',
+    'riparian', 'radioactive', 'variable', 'rectangular', 'reflective',
+    'relativistic',
+    'solvent', 'statistical',
+    'square', 'second', 'singular', 'spectral',
+    'scalar', 'soluble',
+    'theoretical',
+    'thermal', 'terrestrial', 'ultraviolet', 'viscous',
+    'whole',
+    'quantum','xerophilous','xenodochial'
+]
+
+nouns = [
+    'allotropy', 'amplitude', 'atom', 'anode', 'base', 'beam', 'binomial',
+    'biome', 'buffer',
+    'alkaloid', 'blueshift', 'compound', 'cathode', 'spectrograph', 'catalyst',
+    'cell', 'conference',
+    'chromosome', 'diffusion', 'density', 'data', 'disk', 'delta',
+    'chloroplast',
+    'effusion', 'electrolyte', 'element', 'emulsion', 'enzyme',
+    'formula', 'force', 'gamete', 'geosphere',
+    'frequency', 'genome', 'gravity', 'hall', 'hypothesis',
+    'hominid', 'lattice', 'lab', 'line', 'light', 'limit',
+    'lithosphere', 'math', 'neuron',
+    'mode', 'moon', 'mesosphere', 'magnitude', 'matter', 'mineral', 'molecule',
+    'meosis', 'nitrogen', 'network',
+    'nucleus', 'neutron',
+    'patent', 'particle', 'parameter', 'polymer',
+    'quartz', 'resonance',
+    'relativity', 'regression',
+    'serum', 'trigonometry', 'serum', 'radian', 'scale', 'scale',
+    'spectrograph',
+    'solution', 'solute', 'volume', 'star', 'RNA',
+    'term', 'trachyte', 'theorum', 'universe', 'travel', 'time',
+    'telemetry',
+    'union',
+    'volume', 'velocity', 'wave', 'wavelength',
+    'water', 'vector'
+]
