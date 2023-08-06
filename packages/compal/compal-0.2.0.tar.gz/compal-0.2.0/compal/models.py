6604c0ea6fb4809da267993c7f88ee709d8d5bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:01 +0000

    Add package compal/0.2.0/compal-0.2.0.tar.gz

---
+++ packages/compal/compal-0.2.0.tar.gz/compal-0.2.0/compal/models.py
@@ -0,0 +1,84 @@
+"""Objects used by CH7465LG"""
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+
+@dataclass
+class BandSetting:
+    radio: Optional[int] = None
+    bss_enable: Optional[int] = None
+    ssid: Optional[str] = None
+    hidden: Optional[str] = None
+    bandwidth: Optional[int] = None
+    tx_rate: Optional[int] = None
+    tx_mode: Optional[int] = None
+    security: Optional[int] = None
+    multicast_rate: Optional[int] = None
+    channel: Optional[int] = None
+    pre_shared_key: Optional[str] = None
+    re_key: Optional[str] = None
+    wpa_algorithm: Optional[int] = None
+
+
+@dataclass
+class RadioSettings:
+    nv_country: Optional[int] = None
+    band_mode: Optional[int] = None
+    channel_range: Optional[int] = None
+    bss_coexistence: Optional[int] = None
+    son_admin_status: Optional[int] = None
+    smart_wifi: Optional[int] = None
+    radio_2g: Optional[BandSetting] = None
+    radio_5g: Optional[BandSetting] = None
+
+
+class FilterAction(Enum):
+    """
+    Filter action, used by internet access filters
+    """
+
+    add = 1
+    delete = 2
+    enable = 3
+
+
+class NatMode(Enum):
+    """
+    Values for NAT-Mode
+    """
+
+    enabled = 1
+    disabled = 2
+
+
+@dataclass
+class PortForward:
+    local_ip: Optional[str] = None
+    ext_port: Optional[int] = None
+    int_port: Optional[int] = None
+    proto: Optional[str] = None
+    enabled: Optional[bool] = None
+    delete: Optional[bool] = None
+    idd: Optional[str] = None
+    id: Optional[str] = None
+    lan_ip: Optional[str] = None
+
+
+class Proto(Enum):
+    """
+    protocol (from form): 1 = tcp, 2 = udp, 3 = both
+    """
+
+    tcp = 1
+    udp = 2
+    both = 3
+
+
+class TimerMode(Enum):
+    """
+    Timermodes used for internet access filtering
+    """
+
+    generaltime = 1
+    dailytime = 2
