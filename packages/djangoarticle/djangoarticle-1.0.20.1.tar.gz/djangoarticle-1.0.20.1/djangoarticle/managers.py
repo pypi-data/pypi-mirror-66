56fe42cfd3a449b6deb65865b270a455a9ccb331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:06 +0000

    Add package djangoarticle/1.0.20.1/djangoarticle-1.0.20.1.tar.gz

---
+++ packages/djangoarticle/djangoarticle-1.0.20.1.tar.gz/djangoarticle-1.0.20.1/djangoarticle/managers.py
@@ -0,0 +1,57 @@
+from django.db import models 
+
+
+""" Category model managers. """
+# Model Manager for ApptwoCategoryModel.
+class CategoryModelSchemeQuerySet(models.QuerySet):
+
+    def published(self):
+        return self.filter(status='publish')
+
+class CategoryModelSchemeManager(models.Manager):
+
+    def get_queryset(self):
+        return CategoryModelSchemeQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+
+""" Model manager for ApptwoArticleModel. """
+# model manager start here.
+class ArticleModelSchemeQuerySet(models.QuerySet):
+
+    def published(self):
+        return self.filter(status='publish')
+
+    def promoted(self):
+        return self.published().filter(is_promote=True)
+
+    def trending(self):
+        return self.published().filter(is_trend=True)
+
+    def author(self, username):
+        return self.published().filter(author__username=username)
+
+    def promotional(self):
+        return self.published().filter(is_promotional=True, verification=True)
+
+class ArticleModelSchemeManager(models.Manager):
+
+    def get_queryset(self):
+        return ArticleModelSchemeQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+    def promoted(self):
+        return self.get_queryset().promoted()
+
+    def trending(self):
+        return self.get_queryset().trending()
+
+    def author(self, username):
+        return self.get_queryset().author(username)
+
+    def promotional(self):
+        return self.get_queryset().promotional()
