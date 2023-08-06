24b75d315d3fa32bef771a6227840954a19c6636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:23 +0000

    Add package crawl-photos/0.1.dev0/crawl_photos-0.1.dev0.tar.gz

---
+++ packages/crawl-photos/crawl_photos-0.1.dev0.tar.gz/crawl_photos-0.1.dev0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import crawl_photos
+
+setup(
+    name='crawl_photos',
+    version=crawl_photos.__version__,
+    author=crawl_photos.__author__,
+    author_email='1830mm@sina.com',
+    description='a test',
+    license='MIT',
+    url='https://github.com/mcwt/crawl_photos',
+    py_modules=['crawl_photos'],
+    scripts=['crawl_photos.py'],
+    entry_points={
+        'console_scripts': [
+            'crawl_photos = crawl_photos.run:main'
+        ]
+    }
+
+)
