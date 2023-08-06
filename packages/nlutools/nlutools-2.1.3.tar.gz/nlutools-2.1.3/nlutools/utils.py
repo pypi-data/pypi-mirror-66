c0833341874ee0ea06b180c623f1bcd35225b853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:50 +0000

    Add package nlutools/2.1.3/nlutools-2.1.3.tar.gz

---
+++ packages/nlutools/nlutools-2.1.3.tar.gz/nlutools-2.1.3/nlutools/utils.py
@@ -0,0 +1,15 @@
+
+def printWarning(msg):
+    print("WARNING:", msg)
+
+def printFatal(msg):
+    print("FATAL:", msg)
+
+def printException(msg):
+    print("EXCEPTION:", msg)
+
+def printInfo(msg):
+    print("INFO:", msg)
+
+def raiseException(msg):
+    raise Exception(msg)
