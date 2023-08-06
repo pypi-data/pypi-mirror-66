2b93af5c4ba7cf59ae5c98fe2423e1ca4f820c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:09 +0000

    Add package metanetx-sdk/0.5.0/metanetx_sdk-0.5.0-py2.py3-none-any.whl

---
+++ packages/metanetx-sdk/metanetx_sdk-0.5.0-py2.py3-none-any.whl/metanetx_sdk/transform/compartment.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2019, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide compartment data transformation functions."""
+
+
+import logging
+from typing import Mapping
+
+import pandas as pd
+
+
+logger = logging.getLogger(__name__)
+
+
+def transform_cell_cycle_ontology_prefix(table: pd.DataFrame):
+    """Transform all CCO terms."""
+    logger.debug("Transforming Cell Cycle Ontology identifiers.")
+    mask = table["prefix"] == "cco"
+    table.loc[mask, "identifier"] = "CCO:" + table.loc[mask, "identifier"]
+
+
+def transform_go_prefix(table: pd.DataFrame):
+    """Transform all GO terms."""
+    logger.debug("Transforming Gene Ontology identifiers.")
+    mask = table["prefix"] == "go"
+    table.loc[mask, "identifier"] = "GO:" + table.loc[mask, "identifier"]
+
+
+def transform_metanetx_prefix(table: pd.DataFrame):
+    """Transform all MetaNetX identifiers."""
+    logger.debug("Transforming MetaNetX identifiers.")
+    # MetaNetX identifiers themselves have no prefix. So we add it.
+    mnx_mask = table["identifier"].isnull()
+    table.loc[mnx_mask, "identifier"] = table.loc[mnx_mask, "prefix"]
+    table.loc[mnx_mask, "prefix"] = "metanetx.compartment"
+
+
+def transform_compartment_properties(
+    compartments: pd.DataFrame, prefix_mapping: Mapping
+) -> pd.DataFrame:
+    """Transform the MetaNetX compartment properties."""
+    df = compartments.copy()
+    # Cross references have a prefix.
+    # We split the prefixes so that we know the actual data sources.
+    df[["prefix", "identifier"]] = df["source"].str.split(":", n=1, expand=True)
+    namespaces = set(df.loc[df["identifier"].notnull(), "prefix"].unique())
+    # Remove those namespaces that we handle specially.
+    if "cco" in namespaces:
+        transform_cell_cycle_ontology_prefix(df)
+        namespaces.remove("cco")
+    if "go" in namespaces:
+        transform_go_prefix(df)
+        namespaces.remove("go")
+    # Map all source databases to MIRIAM compliant versions.
+    for prefix in namespaces:
+        if prefix in prefix_mapping:
+            df.loc[df["prefix"] == prefix, "prefix"] = prefix_mapping[prefix]
+        else:
+            logger.error("The resource prefix '%s' is unhandled.", prefix)
+    transform_metanetx_prefix(df)
+    del df["source"]
+    logger.debug(df.head())
+    return df
+
+
+def transform_compartment_cross_references(
+    references: pd.DataFrame, prefix_mapping: Mapping
+) -> pd.DataFrame:
+    """Transform the MetaNetX compartment cross-references."""
+    df = references.copy()
+    # Cross references have a prefix.
+    # We split the prefixes so that we know the actual data sources.
+    df[["prefix", "identifier"]] = df["xref"].str.split(":", n=1, expand=True)
+    namespaces = set(df.loc[df["identifier"].notnull(), "prefix"].unique())
+    # Remove those namespaces that we handle specially.
+    if "cco" in namespaces:
+        transform_cell_cycle_ontology_prefix(df)
+        namespaces.remove("cco")
+    if "go" in namespaces:
+        transform_go_prefix(df)
+        namespaces.remove("go")
+    # Map all xref databases to MIRIAM compliant versions.
+    for prefix in namespaces:
+        if prefix in prefix_mapping:
+            df.loc[df["prefix"] == prefix, "prefix"] = prefix_mapping[prefix]
+        else:
+            logger.error("The resource prefix '%s' is unhandled.", prefix)
+    transform_metanetx_prefix(df)
+    del df["xref"]
+    logger.debug(df.head())
+    return df
