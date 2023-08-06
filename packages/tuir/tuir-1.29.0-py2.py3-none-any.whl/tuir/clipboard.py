03e197c146c0ac073ddc2944fc41e9f0f745def8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:19 +0000

    Add package tuir/1.29.0/tuir-1.29.0-py2.py3-none-any.whl

---
+++ packages/tuir/tuir-1.29.0-py2.py3-none-any.whl/tuir/clipboard.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import sys
+import subprocess
+
+def _subprocess_copy(text, args_list):
+    p = subprocess.Popen(args_list, stdin=subprocess.PIPE, close_fds=True)
+    p.communicate(input=text.encode('utf-8'))
+
+
+def copy(text, cmd=None):
+    """
+    Copy text to OS clipboard.
+    """
+
+    # If no command is specified (i.e. the config option is empty) try
+    # to find a reasonable default based on the operating system
+    if cmd is None:
+        if sys.platform == 'darwin':
+            cmd = 'pbcopy w'
+        else: # For Linux, BSD, cygwin, etc.
+            cmd = 'xclip -selection clipboard'
+
+    _subprocess_copy(text, cmd.split())
