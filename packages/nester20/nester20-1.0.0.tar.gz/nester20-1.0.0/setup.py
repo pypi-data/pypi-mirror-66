af77436c437fc910d7c4fe3659738d67ce37cd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:07 +0000

    Add package nester20/1.0.0/nester20-1.0.0.tar.gz

---
+++ packages/nester20/nester20-1.0.0.tar.gz/nester20-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'nester20',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'Dacwi',
+    author_email = 'yusufdacwi@gmail.com',
+    url = 'http://www.Dacwi.com',
+    description = 'A simple printer of nested lists',
+)
