440f1f5a45b3a2f299bd45e00819b7416a9608f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:01 +0000

    Add package djangocms-history/1.2.0/djangocms-history-1.2.0.tar.gz

---
+++ packages/djangocms-history/djangocms-history-1.2.0.tar.gz/djangocms-history-1.2.0/djangocms_history/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.conf import settings
+
+
+class UndoRedoForm(forms.Form):
+    language = forms.ChoiceField(
+        choices=settings.LANGUAGES,
+        required=True,
+    )
+    cms_path = forms.CharField(required=True)
