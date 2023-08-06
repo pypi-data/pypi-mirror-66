5fe2b32af939c1172a361b841f9cfb9c2701647b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:43 +0000

    Add package ddop/0.2.1/ddop-0.2.1.tar.gz

---
+++ packages/ddop/ddop-0.2.1.tar.gz/ddop-0.2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    README = f.read()
+
+setup(
+    name='ddop',
+    version='v0.2.1',
+    url='',
+    license='MIT',
+    author='Andreas Philippi',
+    author_email='',
+    description='Package for data-driven operations management',
+    long_description=README,
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=['sklearn>=0.0','pandas','PuLP==2.0',
+                      'tensorflow==2.1.0', 'Keras==2.3.1',
+                      'numpy==1.18.2', 'scipy==1.4.1']
+)
