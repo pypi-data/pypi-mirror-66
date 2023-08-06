8994f5b52af87f9c10efeccf9f33c261612c2bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:54 +0000

    Add package doc-curation/0.0.3/doc_curation-0.0.3-py3-none-any.whl

---
+++ packages/doc-curation/doc_curation-0.0.3-py3-none-any.whl/curation_projects/pdf_tasks.py
@@ -0,0 +1,5 @@
+from doc_curation import pdf
+
+if __name__ == '__main__':
+    pdf.ocr(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/mImAmsA-nyAya-manjarI-p1.pdf")
+    pdf.ocr(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/mImAmsA-nyAya-manjarI-p2.pdf")
