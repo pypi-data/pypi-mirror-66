aa4b6d204782caf3d775e320fba20c6a903d67bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:14 +0000

    Add package joblib-windower/0.3.0/joblib_windower-0.3.0-py3-none-any.whl

---
+++ packages/joblib-windower/joblib_windower-0.3.0-py3-none-any.whl/joblib_windower/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from joblib_windower.ndarray_windower import ndarray_windower
+from joblib_windower.ndframe_windower import ndframe_windower
+
+
+__all__ = [
+    "ndarray_windower",
+    "ndframe_windower",
+]
+__version__ = "0.3.0"
