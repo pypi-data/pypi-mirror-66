06469f67e5490dc529c081b652856d1988f00e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:10 +0000

    Add package kubernetes-py/1.10.12/kubernetes-py-1.10.12.tar.gz

---
+++ packages/kubernetes-py/kubernetes-py-1.10.12.tar.gz/kubernetes-py-1.10.12/kubernetes_py/utils/ConvertData.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+
+import collections
+from six import string_types
+
+
+def convert(data):
+    if isinstance(data, string_types):
+        return str(data)
+    elif isinstance(data, collections.Mapping):
+        return dict(map(convert, data.items()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
