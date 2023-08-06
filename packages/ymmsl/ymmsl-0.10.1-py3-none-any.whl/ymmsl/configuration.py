1b829c9597fed7f5facd04db1c9b42204028bc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:22 +0000

    Add package ymmsl/0.10.1/ymmsl-0.10.1-py3-none-any.whl

---
+++ packages/ymmsl/ymmsl-0.10.1-py3-none-any.whl/ymmsl/configuration.py
@@ -0,0 +1,35 @@
+"""This module contains all the definitions for yMMSL."""
+from typing import Optional
+
+import yatiml
+
+from ymmsl.document import Document
+from ymmsl.settings import Settings
+from ymmsl.model import ModelReference
+
+
+class Configuration(Document):
+    """Top-level class for all information in a yMMSL file.
+
+    Args:
+        model: A description of the model to run.
+        settings: Settings to run the model with.
+
+    Attributes:
+        model: A model to run.
+        settings: Settings to run the model with.
+    """
+
+    def __init__(self,
+                 model: Optional[ModelReference] = None,
+                 settings: Optional[Settings] = None
+                 ) -> None:
+        self.model = model
+        self.settings = settings
+
+    @classmethod
+    def _yatiml_sweeten(cls, node: yatiml.Node) -> None:
+        if node.get_attribute('settings').is_scalar(type(None)):
+            node.remove_attribute('settings')
+        if node.get_attribute('model').is_scalar(type(None)):
+            node.remove_attribute('model')
---
+++ packages/ymmsl/ymmsl-0.10.1-py3-none-any.whl/ymmsl/io.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+from typing import Any, IO, Union
+from typing_extensions import Type
+
+from ruamel import yaml
+import yatiml
+
+from ymmsl.configuration import Configuration
+from ymmsl.document import Document
+from ymmsl.settings import Settings
+from ymmsl.identity import Identifier, Reference
+from ymmsl.model import ComputeElement, Conduit, Model, ModelReference
+
+
+def _loader() -> Type:
+    class YmmslLoader(yatiml.Loader):
+        pass
+    yatiml.add_to_loader(YmmslLoader,
+                         [ComputeElement, Conduit, Configuration, Document,
+                          Identifier, Model, ModelReference, Reference,
+                          Settings])
+    yatiml.set_document_type(YmmslLoader, Configuration)
+    return YmmslLoader
+
+
+def _dumper() -> Type:
+    class YmmslDumper(yatiml.Dumper):
+        pass
+    yatiml.add_to_dumper(YmmslDumper,
+                         [ComputeElement, Conduit, Configuration, Document,
+                          Identifier, Model, ModelReference, Reference,
+                          Settings])
+    return YmmslDumper
+
+
+loader = _loader()
+"""A loader class for yMMSL, for use with yaml.load.
+
+Usage:
+    config = yaml.load(text, Loader=ymmsl.loader)
+"""
+dumper = _dumper()
+"""A dumper class for yMMSL, for use with yaml.dump.
+
+Usage:
+    text = yaml.dump(config, Dumper=ymmsl.dumper)
+"""
+
+def load(source: Union[str, Path, IO[Any]]) -> Configuration:
+    """Loads a yMMSL document from a string or a file.
+
+    Args:
+        source: A string containing yMMSL data, a pathlib Path to a
+                file containing yMMSL data, or an open file-like
+                object containing from which yMMSL data can be read.
+
+    Returns:
+        A Configuration object corresponding to the input data.
+    """
+    if isinstance(source, Path):
+        with source.open('r') as f:
+            return yaml.load(f, Loader=loader)
+    return yaml.load(source, Loader=loader)
+
+
+def dump(config: Configuration) -> str:
+    """Converts a Ymmsl to a string containing YAML.
+
+    Args:
+        config: The configuration to be saved to yMMSL.
+
+    Returns:
+        A yMMSL YAML description of the given document.
+    """
+    return yaml.dump(config, Dumper=dumper)
+
+
+def save(config: Configuration, target: Union[str, Path, IO[Any]]) -> None:
+    """Saves a yMMSL document to a file.
+
+    Args:
+        config: The configuration to save to yMMSL.
+        target: The file to save to, either as a string containing a
+            path, as a pathlib Path object, or as an open file-like
+            object.
+    """
+    if isinstance(target, str):
+        with open(target, 'w') as f:    # type: IO[Any]
+            f.write(dump(config))
+    elif isinstance(target, Path):
+        with target.open('w') as f:
+            f.write(dump(config))
+    else:
+        target.write(dump(config))
