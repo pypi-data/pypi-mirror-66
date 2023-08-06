d19a7a5d5abcc917d29ccbd8a218d8452af80ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:42 +0000

    Add package pybytom/0.1.0/pybytom-0.1.0.tar.gz

---
+++ packages/pybytom/pybytom-0.1.0.tar.gz/pybytom-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open("requirements.txt", "r") as _requirements:
+    requirements = list(map(str.strip, _requirements.read().split("\n")))[:-1]
+
+setup(
+    name="pybytom",
+    version="0.1.0",
+    description="Python library with tools for Bytom blockchain.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Meheret Tesfaye",
+    author_email="meherett@zoho.com",
+    url="https://github.com/meherett/pybytom",
+    packages=find_packages(),
+    python_requires=">=3.6,<4",
+    install_requires=requirements,
+    extras_require={
+        "tests": [
+            "pytest>=5.4.1,<6",
+            "pytest-cov>=2.8.1,<3"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
