9fe5ca2879f7cb6a0be4269ebea8b29a8e80c55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:32 +0000

    Add package git-heimdall/0.0.2.dev0/git-heimdall-0.0.2.dev0.tar.gz

---
+++ packages/git-heimdall/git-heimdall-0.0.2.dev0.tar.gz/git-heimdall-0.0.2.dev0/heimdall/codescan/plugins/manager.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python
+
+
+"""Manager of codescan plugins.
+
+This module is for scanning the code via different plugins available to this
+module.
+"""
+
+from heimdall.codescan.plugins import secrets_checker
+
+
+class CodescanPluginManager:
+
+    def __init__(self):
+        self.plugins = []
+        self.plugins.append(secrets_checker.SecretsChecker())
+
+    def scan(self, files):
+        # for plugin in self.plugins:
+        _val = self.plugins[0].scan(files)
+        return _val
