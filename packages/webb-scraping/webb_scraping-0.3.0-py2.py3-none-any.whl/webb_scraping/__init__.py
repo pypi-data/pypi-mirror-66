09654a89bd3a5e7d5f75f7ce8adfedddb5ef3f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:46 +0000

    Add package webb-scraping/0.3.0/webb_scraping-0.3.0-py2.py3-none-any.whl

---
+++ packages/webb-scraping/webb_scraping-0.3.0-py2.py3-none-any.whl/webb_scraping/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__uri__ = "https://webb-scraping.readthedocs.io"
+__author__ = "Arjun Baliga Savel"
+__email__ = "asavel@gmail.com"
+__license__ = "MIT"
+__version__ = "0.3.0"
+__description__ = "Web scraping for JWST"
+__tags__ = "-beta"
