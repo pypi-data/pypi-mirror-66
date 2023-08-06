3c8151919ab58055d6c3a071c60a2ff2c162d1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:07 +0000

    Add package lcapy/0.50.3/lcapy-0.50.3-py3-none-any.whl

---
+++ packages/lcapy/lcapy-0.50.3-py3-none-any.whl/lcapy/discretetime.py
@@ -0,0 +1,73 @@
+"""This module provides discrete-time support.
+
+It introduces three special variables:
+   n for discrete-time sequences
+   k for discrete-frequency sequences
+   z for z-transforms.
+
+Copyright 2020 Michael Hayes, UCECE
+
+"""
+
+import sympy as sym
+from .sym import sympify
+from .nexpr import nexpr, nExpr, n
+from .kexpr import kexpr, kExpr, k
+from .zexpr import zexpr, zExpr, z
+from .dsym import nsym, ksym, zsym, dt, df
+
+from .expr import expr as expr1
+from .transform import transform as transform1
+from .transform import call as call1
+from .functions import Function
+from .ztransform import *
+from .seq import seq
+
+
+def expr(arg, **assumptions):
+
+    expr = sympify(arg, **assumptions)
+
+    symbols = expr.free_symbols    
+
+    if nsym in symbols:
+        return nexpr(expr, **assumptions)
+    elif ksym in symbols:
+        return kexpr(expr, **assumptions)    
+    elif zsym in symbols:
+        return zexpr(expr, **assumptions)    
+
+    return expr1(arg, **assumptions)
+
+
+def transform(expr, arg, **assumptions):
+
+    # Is this wise?   It makes sense for Voltage and Impedance objects
+    # but may cause too much confusion for other expressions
+    if arg is n and isinstance(expr, zExpr):
+        return expr.IZT(**assumptions)
+    elif arg is n and isinstance(expr, kExpr):
+        return expr.IDFT(**assumptions)        
+    elif arg is z and isinstance(expr, nExpr):
+        return expr.ZT(**assumptions)
+    elif arg is z and isinstance(expr, kExpr):
+        return expr.IDFT(**assumptions).ZT(**assumptions)
+    elif arg is k and isinstance(expr, nExpr):
+        return expr.DFT(**assumptions)
+    elif arg is k and isinstance(expr, zExpr):
+        return expr.IZT(**assumptions).DFT(**assumptions)
+
+    # Perhaps if arg is f, use DTFT?
+    
+    return transform1(expr, arg, **assumptions)    
+
+
+def call(expr, arg, **assumptions):
+
+    if id(arg) in (id(n), id(z), id(k)):
+        return transform(expr, arg, **assumptions)
+
+    if arg in (n, k, z):
+        return transform(expr, arg, **assumptions)    
+    
+    return call1(expr, arg, **assumptions)
---
+++ packages/lcapy/lcapy-0.50.3-py3-none-any.whl/lcapy/transform.py
@@ -0,0 +1,97 @@
+"""This module performs transformations between domains.
+
+Copyright 2018--2019 Michael Hayes, UCECE
+
+"""
+
+from .sym import sympify, pi
+from .symbols import f, s, t, omega, jomega
+
+def transform(expr, arg, **assumptions):
+    """If arg is f, s, t, omega, jomega perform domain substitution,
+    otherwise perform substitution.
+
+    Note (5 * s)(omega) will fail since 5 * s is assumed not to be
+    causal and so Fourier transform is unknown.  However, Zs(5 *
+    s)(omega) will work since Zs is assumed to be causal.
+
+    """
+
+    if isinstance(expr, arg.__class__) and not isinstance(expr, Super):
+        return expr.subs(arg)
+
+    # Handle expr(t), expr(s), expr(f)
+    if arg is t:
+        return expr.time(**assumptions)
+    elif arg is s:
+        return expr.laplace(**assumptions)
+    elif arg is f:
+        if isinstance(expr, omegaExpr):
+            return expr.subs(2 * pi * f)
+        else:
+            return expr.fourier(**assumptions)    
+
+    # Handle expr(texpr), expr(sexpr), expr(fexpr).  For example,
+    # expr(2 * f).
+    result = None 
+    if isinstance(arg, tExpr):
+        result = expr.time(**assumptions)
+    elif isinstance(arg, sExpr):
+        result = expr.laplace(**assumptions)
+    elif isinstance(arg, fExpr):
+        if isinstance(expr, omegaExpr):
+            result = expr.subs(2 * pi * f)
+        else:
+            result = expr.fourier(**assumptions)
+
+    if result is not None:
+        return result.__class__(result.subs(arg))
+
+    sarg = arg
+    try:
+        sarg.has(t)
+    except:
+        sarg = sympify(arg)            
+
+    if sarg.has(jomega):
+        if isinstance(expr, sExpr):
+            result = expr.subs(arg)
+            return result
+        else:
+            result = expr.laplace(**assumptions)
+    elif isinstance(arg, omegaExpr):
+        if isinstance(expr, fExpr):
+            result = expr.subs(omega / (2 * pi))
+        elif isinstance(expr, cExpr):
+            result = expr
+        else:
+            result = expr.fourier(**assumptions).subs(omega / (2 * pi))
+    elif sarg.is_constant():
+        if not isinstance(expr, Super):
+            result = expr.time(**assumptions)
+        else:
+            result = expr
+    else:
+        raise ValueError('Can only return t, f, s, or omega domains')
+    
+    return result.subs(arg)
+
+
+def call(expr, arg, **assumptions):
+
+    if id(arg) in (id(f), id(s), id(t), id(omega), id(jomega)):
+        return transform(expr, arg, **assumptions)
+
+    if arg in (f, s, t, omega, jomega):
+        return transform(expr, arg, **assumptions)    
+    
+    return expr.subs(arg)
+
+
+from .cexpr import cExpr    
+from .fexpr import fExpr    
+from .sexpr import sExpr
+from .texpr import tExpr
+from .omegaexpr import omegaExpr
+from .super import Super
+
