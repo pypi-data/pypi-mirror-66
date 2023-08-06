36a3365bf65b147945baa2bfb55a0257c94c4653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:52 +0000

    Add package webpdf/1.0/webpdf-1.0.tar.gz

---
+++ packages/webpdf/webpdf-1.0.tar.gz/webpdf-1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(
+name="webpdf",
+version=1.0,
+long_discription=Path("README.md").read_text(),
+packages=setuptools.find_packages(exclude=["tests","data"])
+
+
+
+)
