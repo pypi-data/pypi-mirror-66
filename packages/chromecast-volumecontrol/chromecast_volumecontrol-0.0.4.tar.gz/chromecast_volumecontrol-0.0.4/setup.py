5c04a8d74169a58ba4adca10847bad8437f5814e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:26 +0000

    Add package chromecast-volumecontrol/0.0.4/chromecast_volumecontrol-0.0.4.tar.gz

---
+++ packages/chromecast-volumecontrol/chromecast_volumecontrol-0.0.4.tar.gz/chromecast_volumecontrol-0.0.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chromecast_volumecontrol", # Replace with your own username
+    version="0.0.4",
+    author="Lukas Merkle",
+    author_email="lukas.merkle@live.de",
+    description="Control Chromecast Volume",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/merklel/chromecast-frontend",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    package_data={'': ['ico/*.png']},
+    python_requires='>=3.6',
+    install_requires=[
+        "pychromecast",
+            ]
+)
