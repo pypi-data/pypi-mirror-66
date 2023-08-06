21b23162bcba7b17f724f20b789b7600faa70a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:24 +0000

    Add package nsmpy/1.0.3/nsmpy-1.0.3.tar.gz

---
+++ packages/nsmpy/nsmpy-1.0.3.tar.gz/nsmpy-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+README = open("pypi_readme.md","r").read()
+setup(
+	name="nsmpy",
+	version="1.0.3",
+	long_description=README,
+	long_description_content_type="text/markdown",
+	author="SciencePi",
+	license="MIT",
+	scripts=['nsmpy.py'])
