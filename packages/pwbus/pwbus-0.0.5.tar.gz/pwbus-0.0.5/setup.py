0e9e39e7f099d47610029e7347786a2bf659dca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:57 +0000

    Add package pwbus/0.0.5/pwbus-0.0.5.tar.gz

---
+++ packages/pwbus/pwbus-0.0.5.tar.gz/pwbus-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="pwbus",
+    version="0.0.5",
+    author="Fabio Szostak",
+    author_email="fszostak@gmail.com",
+    description="A simple task execution manager with integration bus cababities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fszostak/pwbus",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)
