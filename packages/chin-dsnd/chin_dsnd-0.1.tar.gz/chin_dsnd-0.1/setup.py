2c5af5ed0db6d305ecbed11b3e1d6deff24e742d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:58 +0000

    Add package chin-dsnd/0.1/chin_dsnd-0.1.tar.gz

---
+++ packages/chin-dsnd/chin_dsnd-0.1.tar.gz/chin_dsnd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chin_dsnd',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['chin_dsnd'],
+      author = 'Chinmay kumar Das',
+      author_email = 'chinmaydasbat@gmail.com',
+      zip_safe=False)
