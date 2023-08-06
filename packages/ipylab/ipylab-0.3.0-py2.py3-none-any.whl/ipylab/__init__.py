95a0576644e7dd2f9e1ba3ba8b7830b2c757931f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:56 +0000

    Add package ipylab/0.3.0/ipylab-0.3.0-py2.py3-none-any.whl

---
+++ packages/ipylab/ipylab-0.3.0-py2.py3-none-any.whl/ipylab/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jeremy Tuloup.
+# Distributed under the terms of the Modified BSD License.
+
+from .jupyterfrontend import JupyterFrontEnd
+from .widgets import Panel, SplitPanel
+
+from ._version import __version__, version_info
