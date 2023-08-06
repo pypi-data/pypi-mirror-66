a9a7036bd4037c5ca5013998b1508c65c78330bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:27 +0000

    Add package python-layer/1.1.2/python_layer-1.1.2-py3-none-any.whl

---
+++ packages/python-layer/python_layer-1.1.2-py3-none-any.whl/aws_layers/layer_utils.py
@@ -0,0 +1,29 @@
+"""
+Helper functions for python-layer
+"""
+import boto3
+
+
+def read_layer(path, loader=None, binary_file=False):
+    open_mode = "rb" if binary_file else "r"
+    with open(path, mode=open_mode) as fh:
+        if not loader:
+            return fh.read()
+        return loader(fh.read())
+
+
+def get_client(
+    profile_name=None, region=None,
+):
+    """Shortcut for getting an initialized instance of the boto3 client."""
+    boto3.setup_default_session(profile_name=profile_name, region_name=region)
+    return boto3.client("lambda")
+
+
+def get_layer_arn(layer: dict) -> str:
+    """
+
+    :param layer:
+    :return:
+    """
+    return layer["Layer_arn"] + ":" + str(layer["Layer_version"])
