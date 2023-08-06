8cc796c9f37166329f8cca1731dc6986e4998c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package g2p/0.5.20200419/g2p-0.5.20200419.tar.gz

---
+++ packages/g2p/g2p-0.5.20200419.tar.gz/g2p-0.5.20200419/g2p/log.py
@@ -0,0 +1,33 @@
+"""
+    Error Log
+
+"""
+
+import logging
+import coloredlogs
+import sys
+
+FIELD_STYLES = dict(
+    levelname=dict(color='green'),
+)
+
+def setup_logger(name):
+    """ Create logger and configure with cool colors!
+    """
+
+    logging.basicConfig(
+        level=logging.INFO
+        # filename="logger.log"
+    )
+    logger = logging.getLogger(name)
+    coloredlogs.install(level='INFO', fmt='%(levelname)s - %(message)s',
+                        logger=logger, field_styles=FIELD_STYLES)
+    return logger
+
+LOGGER = setup_logger('root')
+
+TEST_LOGGER = setup_logger('test')
+
+FH = logging.FileHandler('test.log')
+
+TEST_LOGGER.addHandler(FH)
