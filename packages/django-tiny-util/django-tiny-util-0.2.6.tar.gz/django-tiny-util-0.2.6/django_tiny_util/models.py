c3f07809a43ceebb6a5c771ab9bd549dfef2f36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:18 +0000

    Add package django-tiny-util/0.2.6/django-tiny-util-0.2.6.tar.gz

---
+++ packages/django-tiny-util/django-tiny-util-0.2.6.tar.gz/django-tiny-util-0.2.6/django_tiny_util/models.py
@@ -0,0 +1,26 @@
+"""
+Copyright (c) 2020 Oleg Bogumirski <reg@olegb.ru>
+"""
+from typing import Optional
+
+from django.db.models import Model
+
+
+def get_or_none(cls: type(Model), **kwargs) -> Optional[Model]:
+    try:
+        return cls.objects.get(**kwargs)
+    except cls.DoesNotExist:
+        return None
+
+
+def update_fields(model: Model, **kwargs) -> Model:
+    if kwargs.get('filter_fields'):
+        fields = [x for x, y in kwargs.items() if x in [z.name for z in model._meta.get_fields(include_hidden=True)]]
+    else:
+        fields = list(kwargs.keys())
+
+    for key in fields:
+        setattr(model, key, kwargs[fields])
+
+    model.save(update_fields=fields)
+    return model
