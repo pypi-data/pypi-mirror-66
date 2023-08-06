9db77678180679b89dcc9b0a8d20837b584ce184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:12 +0000

    Add package aws-cdk.aws-cloudwatch-actions/1.33.1/aws-cdk.aws-cloudwatch-actions-1.33.1.tar.gz

---
+++ packages/aws-cdk.aws-cloudwatch-actions/aws-cdk.aws-cloudwatch-actions-1.33.1.tar.gz/aws-cdk.aws-cloudwatch-actions-1.33.1/src/aws_cdk/aws_cloudwatch_actions/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_applicationautoscaling
+import aws_cdk.aws_autoscaling
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_iam
+import aws_cdk.aws_sns
+import aws_cdk.core
+import constructs
+__all__ = []
+
+publication.publish()
