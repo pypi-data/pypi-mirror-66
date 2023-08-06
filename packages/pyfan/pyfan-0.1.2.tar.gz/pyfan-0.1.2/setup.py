057085632354f0f1f97964524575ad5609f4bc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:08 +0000

    Add package pyfan/0.1.2/pyfan-0.1.2.tar.gz

---
+++ packages/pyfan/pyfan-0.1.2.tar.gz/pyfan-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyfan', 'pyfan.util', 'pyfan.util.rmd']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyfan',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Fan Wang',
+    'author_email': 'wangfanbsg75@live.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
