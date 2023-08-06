f86fa4be726fb7a562b4dfe3f6f2afb8b23cfa3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:29 +0000

    Add package pyrt/0.5.7/pyrt-0.5.7-py3-none-any.whl

---
+++ packages/pyrt/pyrt-0.5.7-py3-none-any.whl/pyrt/material/__init__.py
@@ -0,0 +1,8 @@
+"""
+Material Definitions
+
+This contains different Material definitions. The base class is defined in material.py
+"""
+
+from .material import Material
+from .phongmaterial import PhongMaterial
---
+++ packages/pyrt/pyrt-0.5.7-py3-none-any.whl/pyrt/math/constants.py
@@ -0,0 +1,7 @@
+""""
+In this file all mathematical constants are defined
+
+So far, onle the Global epsilon "G_EPSILON" is defined.
+"""
+
+G_EPSILON = 1E-11
