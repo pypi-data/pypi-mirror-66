01d17681a0cff31caa6c48841878840ea93f9ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package pyatag/0.2.19/pyatag-0.2.19.tar.gz

---
+++ packages/pyatag/pyatag-0.2.19.tar.gz/pyatag-0.2.19/pyatag/__init__.py
@@ -0,0 +1,5 @@
+"""Provides connection to ATAG One Thermostat REST API."""
+from .gateway import AtagDataStore
+from .errors import AtagException
+from .const import *
+from .discovery import discover_atag
