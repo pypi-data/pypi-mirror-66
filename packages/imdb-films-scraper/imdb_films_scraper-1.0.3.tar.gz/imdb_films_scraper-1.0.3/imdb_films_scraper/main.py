3df21aef5642ae54d368a6f4c86e45b0e21b1ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:46 +0000

    Add package imdb-films-scraper/1.0.3/imdb_films_scraper-1.0.3.tar.gz

---
+++ packages/imdb-films-scraper/imdb_films_scraper-1.0.3.tar.gz/imdb_films_scraper-1.0.3/imdb_films_scraper/main.py
@@ -0,0 +1,21 @@
+from . import films as f
+from . import related_films as r
+from . import dataframing as df
+
+films = [""] #You Can place a single ID or a list of them
+related = False
+
+def main(films, related):
+
+	movies_collection = []
+
+	movies = f.scrape(films)
+	movies_collection.extend(movies)
+
+	related_movies = r.controller(movies, related)
+	movies_collection.extend(related_movies)
+
+	data = df.export(movies_collection)
+
+if __name__ == "__main__":
+	main(films, related)
