1f368ec38405c6622a453702d6bc59b77aa51215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:25 +0000

    Add package imagenet-voice/0.30.0/imagenet_voice-0.30.0.tar.gz

---
+++ packages/imagenet-voice/imagenet_voice-0.30.0.tar.gz/imagenet_voice-0.30.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='imagenet_voice',
+    version='0.30.0',
+    scripts=['./imagenet_voice/__init__.py'],
+    author="Charles Averill",
+    author_email="charlesaverill20@gmail.com",
+    description="A collection of synthetic audio files containing spoken labels from ImageNet",
+    long_description=long_description,
+    install_requires=['progressbar2', 'wavio', 'numpy'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/CharlesAverill/ImageNet-Voice/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
