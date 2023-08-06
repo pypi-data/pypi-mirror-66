f22a7e39986db41aa5d6d61d77bfd7740a3d2b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:46 +0000

    Add package osm2geojson/0.1.21/osm2geojson-0.1.21.tar.gz

---
+++ packages/osm2geojson/osm2geojson-0.1.21.tar.gz/osm2geojson-0.1.21/setup.py
@@ -0,0 +1,22 @@
+from os import path
+
+from setuptools import setup
+
+dirname = path.abspath(path.dirname(__file__))
+with open(path.join(dirname, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='osm2geojson',
+    version='0.1.21',
+    license='MIT',
+    description='Parse OSM and Overpass JSON',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='geometry gis osm parsing',
+    author='Parfeniuk Mykola',
+    author_email='mikola.parfenyuck@gmail.com',
+    packages=['osm2geojson'],
+    include_package_data=True,
+    install_requires=['shapely', 'requests']
+)
