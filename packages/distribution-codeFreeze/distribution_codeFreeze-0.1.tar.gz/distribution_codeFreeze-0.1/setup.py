4410083d6709f344f2572dafcbb0556337ed9dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:07 +0000

    Add package distribution-codeFreeze/0.1/distribution_codeFreeze-0.1.tar.gz

---
+++ packages/distribution-codeFreeze/distribution_codeFreeze-0.1.tar.gz/distribution_codeFreeze-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_codeFreeze',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distribution_codeFreeze'],
+      zip_safe=False)
