e301016c83a66031d1209040cc6f28b192dea85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:02 +0000

    Add package distest-check/0.2/distest_check-0.2.tar.gz

---
+++ packages/distest-check/distest_check-0.2.tar.gz/distest_check-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distest_check',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distest_check'],
+      zip_safe=False)
