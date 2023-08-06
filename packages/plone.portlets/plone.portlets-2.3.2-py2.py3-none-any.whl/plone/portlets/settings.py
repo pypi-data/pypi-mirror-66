41858fe9e052bcc2a8248a96459719b4a9cbb65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:16 +0000

    Add package plone.portlets/2.3.2/plone.portlets-2.3.2-py2.py3-none-any.whl

---
+++ packages/plone.portlets/plone.portlets-2.3.2-py2.py3-none-any.whl/plone/portlets/settings.py
@@ -0,0 +1,41 @@
+from persistent.dict import PersistentDict
+from plone.portlets.constants import ASSIGNMENT_SETTINGS_KEY
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletAssignmentSettings
+from zope.annotation import IAnnotations
+from zope.component import adapter
+from zope.component import queryAdapter
+from zope.container.contained import Contained
+from zope.interface import implementer
+
+
+@implementer(IPortletAssignmentSettings)
+class PortletAssignmentSettings(Contained):
+    def __init__(self):
+        self.data = PersistentDict()
+
+    def __setitem__(self, name, value):
+        self.data[name] = value
+
+    def __delitem__(self, name):
+        del self.data[name]
+
+    def __getitem__(self, name):
+        return self.data.__getitem__(name)
+
+    def get(self, name, default=None):
+        return self.data.get(name, default)
+
+
+@adapter(IPortletAssignment)
+@implementer(IPortletAssignmentSettings)
+def portletAssignmentSettingsFactory(context):
+    annotations = queryAdapter(context, IAnnotations)
+    settings = annotations.get(ASSIGNMENT_SETTINGS_KEY, None)
+
+    if settings is None:
+        settings = annotations[
+            ASSIGNMENT_SETTINGS_KEY
+        ] = PortletAssignmentSettings()
+
+    return settings
