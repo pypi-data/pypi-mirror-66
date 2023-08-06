f0c7dceb2bba1fb1e793134a1f97e81e9646f7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package exceliser/0.0.3/exceliser-0.0.3-py3-none-any.whl

---
+++ packages/exceliser/exceliser-0.0.3-py3-none-any.whl/exceliser/workbook.py
@@ -0,0 +1,15 @@
+from . import WorkbookSerializer
+from . import WorbookDeserializer
+
+"""
+    Main API for serializing and deserializing 
+    Excel documents
+"""
+
+
+def serialize(path: str):
+    return WorkbookSerializer(path).serialize()
+
+
+def deserialize(path: str, json_decoder=None):
+    return WorbookDeserializer(path, json_decoder).deserialize()
