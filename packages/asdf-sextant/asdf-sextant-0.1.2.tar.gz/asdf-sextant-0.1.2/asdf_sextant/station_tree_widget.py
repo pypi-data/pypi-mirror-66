1f09409e3900996bf557d0678d43abb431ce0cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:29 +0000

    Add package asdf-sextant/0.1.2/asdf-sextant-0.1.2.tar.gz

---
+++ packages/asdf-sextant/asdf-sextant-0.1.2.tar.gz/asdf-sextant-0.1.2/asdf_sextant/station_tree_widget.py
@@ -0,0 +1,29 @@
+from PySide2 import QtGui, QtCore
+
+
+class StationTreeWidget(QtGui.QTreeWidget):
+    cellExited = QtCore.Signal(int, int)
+    itemExited = QtCore.Signal(QtGui.QTreeWidgetItem)
+
+    def __init__(self, *args, **kwargs):
+        QtGui.QTreeWidget.__init__(self, *args, **kwargs)
+        self._last_index = QtCore.QPersistentModelIndex()
+        self.viewport().installEventFilter(self)
+        self.setMouseTracking(True)
+
+    def eventFilter(self, widget, event):
+        if widget is self.viewport():
+            index = self._last_index
+            if event.type() == QtCore.QEvent.MouseMove:
+                index = self.indexAt(event.pos())
+            elif event.type() == QtCore.QEvent.Leave:
+                index = QtCore.QModelIndex()
+            if index != self._last_index:
+                row = self._last_index.row()
+                column = self._last_index.column()
+                item = self.itemAt(row, column)
+                if item is not None:
+                    self.itemExited.emit(item)
+                self.cellExited.emit(row, column)
+                self._last_index = QtCore.QPersistentModelIndex(index)
+        return QtGui.QTreeWidget.eventFilter(self, widget, event)
