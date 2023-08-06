798357c1f080f519cfb7881b5dc5cbfa0c51f980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:15 +0000

    Add package pytest-bug/0.8.0/pytest_bug-0.8.0-py2.py3-none-any.whl

---
+++ packages/pytest-bug/pytest_bug-0.8.0-py2.py3-none-any.whl/pytest_bug/hooks.py
@@ -0,0 +1,25 @@
+"""Plugin hooks"""
+
+
+def pytest_bug_set_mark(mark_bug, config):  # noqa
+    """
+    Called set mark
+    :param mark_bug: MarkBug
+    :param config: Base pytest config
+    """
+
+
+def pytest_bug_item_mark(item, config):
+    """
+    Called after set mark
+    :param item: pytest item
+    :param config: Base pytest config
+    """
+
+
+def pytest_bug_report_teststatus(report, report_bug):  # noqa
+    """
+    Called before output
+    :param report: Base pytest report
+    :param report_bug: SkipBug or FailBug or PassBug
+    """
