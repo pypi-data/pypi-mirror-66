5a90fcb8673949ec411f55195510ac685d210097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:22 +0000

    Add package python-mbedtls/1.2.1/python_mbedtls-1.2.1-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/python-mbedtls/python_mbedtls-1.2.1-cp27-cp27m-manylinux1_x86_64.whl/mbedtls/cipher/ARIA.py
@@ -0,0 +1,40 @@
+"""The ARIA algorithm is a symmetric block cipher that can encrypt and
+decrypt information. It is defined by the Korean Agency for Technology
+and Standards (KATS) in *KS X 1213:2004* (in Korean, but see
+http://210.104.33.10/ARIA/index-e.html in English) and also described by
+the IETF in *RFC 5794*.
+
+"""
+
+__author__ = "Mathias Laurin"
+__copyright__ = "Copyright 2019, Mathias Laurin"
+__license__ = "MIT License"
+
+
+from . import _cipher
+from mbedtls.exceptions import *
+
+__all__ = ["block_size", "key_size", "new"]
+
+
+block_size = 16
+key_size = None
+
+
+def new(key, mode, iv=None):
+    mode = _cipher.Mode(mode)
+    if mode in {
+        _cipher.Mode.ECB,
+        _cipher.Mode.CBC,
+        # _cipher.Mode.CFB128,
+        _cipher.Mode.CTR,
+        _cipher.Mode.GCM,
+    }:
+        if len(key) * 8 not in {128, 192, 256}:
+            raise TLSError(
+                msg="key size must 16, 24, or 32 bytes, got %i" % len(key)
+            )
+    else:
+        raise TLSError(msg="unsupported mode %r" % mode)
+    name = ("ARIA-%i-%s" % (len(key) * 8, mode.name)).encode("ascii")
+    return _cipher.Cipher(name, key, mode, iv)
