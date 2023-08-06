37b9008701870bf0c6985b910c893f1c03e8afbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:36 +0000

    Add package distributions-vlad/1.0/distributions-vlad-1.0.tar.gz

---
+++ packages/distributions-vlad/distributions-vlad-1.0.tar.gz/distributions-vlad-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-vlad',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-vlad'],
+      author='Vlad Ionescu',
+      zip_safe=False)
