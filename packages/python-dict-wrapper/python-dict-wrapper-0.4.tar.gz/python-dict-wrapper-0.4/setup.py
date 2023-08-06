64f793ed21a074b0cfac74190f325f066d7156a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:27 +0000

    Add package python-dict-wrapper/0.4/python-dict-wrapper-0.4.tar.gz

---
+++ packages/python-dict-wrapper/python-dict-wrapper-0.4.tar.gz/python-dict-wrapper-0.4/setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'python-dict-wrapper',
+    py_modules = ['python_dict_wrapper'],
+    version = '0.4',
+    description = 'Wraps python dictionary keys are attributes',
+    long_description=long_description,
+    author = 'Steve Brettschneider',
+    author_email = 'steve@bluehousefamily.com',
+    url = 'https://github.com/brettschneider/python_dict_wrapper',
+    download_url = 'https://github.com/brettschneider/python_lazy_streams/archive/0.4.tar.gz',
+    keywords = ['dictionary', 'wrapper', 'attributes', 'enforce'],
+    classifiers = []
+)
