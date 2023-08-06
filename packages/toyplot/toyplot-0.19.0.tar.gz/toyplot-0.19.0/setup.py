ee331cc3d74dc5dba6efa8c20f8344fee9854dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:17 +0000

    Add package toyplot/0.19.0/toyplot-0.19.0.tar.gz

---
+++ packages/toyplot/toyplot-0.19.0.tar.gz/toyplot-0.19.0/setup.py
@@ -0,0 +1,48 @@
+# Copyright 2014, Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+# rights in this software.
+
+from setuptools import setup, find_packages
+import re
+
+setup(
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Environment :: Web Environment",
+        "Framework :: IPython",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    description="A modern plotting toolkit supporting electronic publishing and reproducibility.",
+    install_requires=[
+        "arrow",
+        "custom_inherit",
+        "multipledispatch",
+        "numpy>=1.8.0",
+        "pypng",
+        "reportlab",
+    ],
+    long_description="""Toyplot is the kid-sized plotting toolkit for Python with grownup-sized goals:
+  * Develop beautiful plots that embrace the unique capabilities of electronic publishing and support repoducibility.
+  * Create the best possible data graphics "out-of-the-box", maximizing data ink and minimizing chartjunk.
+  * Provide a clean, minimalist interface that scientists and engineers will love.
+
+  See the Toyplot documentation at http://toyplot.readthedocs.io, and the Toyplot sources at http://github.com/sandialabs/toyplot""",
+    maintainer="Timothy M. Shead",
+    maintainer_email="tshead@sandia.gov",
+    name="toyplot",
+    packages=find_packages(),
+    package_data = {"": ["*.csv"]},
+    url="http://toyplot.readthedocs.org",
+    version=re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        open(
+            "toyplot/__init__.py",
+            "r").read(),
+        re.M).group(1),
+)
