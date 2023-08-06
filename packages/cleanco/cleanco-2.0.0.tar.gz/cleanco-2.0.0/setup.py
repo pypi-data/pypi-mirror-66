f2c9166078ff5fab8ee9640c1cd629d8e439fee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:57 +0000

    Add package cleanco/2.0.0/cleanco-2.0.0.tar.gz

---
+++ packages/cleanco/cleanco-2.0.0.tar.gz/cleanco-2.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+from setuptools import setup
+
+setup(name='cleanco',
+      description='Python library to process company names',
+      version='2.0.0',
+      license="MIT",
+      classifiers = [
+         "Topic :: Office/Business",
+         "Development Status :: 4 - Beta",
+         "Intended Audience :: Developers",
+         "License :: OSI Approved :: MIT License",
+         "Programming Language :: Python :: 3"
+      ],
+      url='https://github.com/psolin/cleanco',
+      author='Paul Solin',
+      author_email='paul@paulsolin.com',
+      packages=["cleanco"],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'tox'],
+)
