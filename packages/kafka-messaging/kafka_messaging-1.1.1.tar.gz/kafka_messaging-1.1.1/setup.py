fdbca817dfc7b1c7ba039e7ce6ea192b87726b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:10 +0000

    Add package kafka-messaging/1.1.1/kafka_messaging-1.1.1.tar.gz

---
+++ packages/kafka-messaging/kafka_messaging-1.1.1.tar.gz/kafka_messaging-1.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kafka_messaging",
+    version="1.1.1",
+    author="Andre Ferrari Moukarzel",
+    author_email="andremoukarzel@gmail.com",
+    description="Simple interface for sending messages through Kafka",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/zaitt_computer_vision/kafka_api",
+    packages=setuptools.find_packages(),
+    install_requires=["confluent_kafka"],
+    license="MIT",
+    python_requires=">=3.6"
+)
