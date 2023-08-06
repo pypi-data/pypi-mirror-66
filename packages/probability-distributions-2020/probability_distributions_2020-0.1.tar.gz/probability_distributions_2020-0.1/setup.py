ba2057b73d500bd9b2a1b9578a5d8acc42b7d1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:27 +0000

    Add package probability-distributions-2020/0.1/probability_distributions_2020-0.1.tar.gz

---
+++ packages/probability-distributions-2020/probability_distributions_2020-0.1.tar.gz/probability_distributions_2020-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_distributions_2020',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probability_distributions_2020'],
+      zip_safe=False)
