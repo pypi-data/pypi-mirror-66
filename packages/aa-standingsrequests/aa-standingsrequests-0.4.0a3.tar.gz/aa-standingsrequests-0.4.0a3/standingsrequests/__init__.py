1c965b30cf0d13ec017d7a6130c0586dd2d34017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:52 +0000

    Add package aa-standingsrequests/0.4.0a3/aa-standingsrequests-0.4.0a3.tar.gz

---
+++ packages/aa-standingsrequests/aa-standingsrequests-0.4.0a3.tar.gz/aa-standingsrequests-0.4.0a3/standingsrequests/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.4.0a3'
+__title__ = 'Standings Requests'
---
+++ packages/aa-standingsrequests/aa-standingsrequests-0.4.0a3.tar.gz/aa-standingsrequests-0.4.0a3/standingsrequests/apps.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class StandingsRequestsConfig(AppConfig):
+    name = 'standingsrequests'
+
+    def ready(self):
+        pass
