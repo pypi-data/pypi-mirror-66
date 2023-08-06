cd7677e3519243f7705fb3825d00dc955e387710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:02 +0000

    Add package temmeta/0.0.1/temmeta-0.0.1.tar.gz

---
+++ packages/temmeta/temmeta-0.0.1.tar.gz/temmeta-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="temmeta",
+    version="0.0.1",
+    description=("TEMMETA is a library for transmission electron microscopy "
+                 "(TEM) (meta)data manipulation"),
+    url='https://github.com/din14970/TEMMETA',
+    author='Niels Cautaerts',
+    author_email='nielscautaerts@hotmail.com',
+    license='GPL-3.0',
+
+    classifiers=['Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8'],
+    keywords='TEM',
+    packages=find_packages(exclude=["*tests*", "*examples*"]),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'jupyter',
+        'hyperspy',
+        'matplotlib_scalebar',
+        'tqdm',
+        'Pillow',
+        'opencv-python',
+        'PyQt5'
+    ],
+)
