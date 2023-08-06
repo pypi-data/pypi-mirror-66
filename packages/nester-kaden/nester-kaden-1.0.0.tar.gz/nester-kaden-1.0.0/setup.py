da8a5a1ca25989ccc78834d28e046bcab5c1f8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:59 +0000

    Add package nester-kaden/1.0.0/nester-kaden-1.0.0.tar.gz

---
+++ packages/nester-kaden/nester-kaden-1.0.0.tar.gz/nester-kaden-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester-kaden',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='kaden-kykim',
+    author_email='kaden.kykim@gmail.com',
+    url='https://github.com/kaden-kykim',
+    description='A simple printer of nested lists'
+)
