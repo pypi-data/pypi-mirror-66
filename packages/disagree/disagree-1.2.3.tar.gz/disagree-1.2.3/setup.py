81327278212d48798895199ca5ae109ae56ce732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:01 +0000

    Add package disagree/1.2.3/disagree-1.2.3.tar.gz

---
+++ packages/disagree/disagree-1.2.3.tar.gz/disagree-1.2.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='disagree',
+      version='1.2.3',
+      author='Oliver Price',
+      author_email='op.oliverprice@gmail.com',
+      download_url='https://github.com/o-P-o/disagree/',
+      packages=['disagree', 'disagree.test'],
+      include_package_data=True,
+      license='LICENSE',
+      description='Visual and statistical assessment of annotator agreements',
+      #long_description=open('README.md').read(),
+      install_requires=[
+        'scipy >= 1.1.0',
+        'tqdm >= 4.26.0',
+        'pandas >= 0.23.0']
+     )
