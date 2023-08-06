13a7248d03ca40b47147c6840eae74d3eb369158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:02 +0000

    Add package hellocdw/0.0.1/hellocdw-0.0.1.tar.gz

---
+++ packages/hellocdw/hellocdw-0.0.1.tar.gz/hellocdw-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'hellocdw',
+    version = '0.0.1',
+    description = 'Some description here!',
+    py_modules = ["hellocdw"],
+    package_dir = {'':'src'},
+    classifiers = ["Programming Language :: Python :: 3.6"], # Need to add license and operating systems
+    long_description = long_description,
+    long_description_content_type = "text/markdown"
+    # you can now add also url, author, author_email
+)
