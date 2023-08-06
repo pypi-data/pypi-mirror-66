738cbdc6b64d8ddf545453292dfca73f8360fc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:34 +0000

    Add package labstep/2.3.1/labstep-2.3.1.tar.gz

---
+++ packages/labstep/labstep-2.3.1.tar.gz/labstep-2.3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(name='labstep',
+      version='2.3.1',
+      description='Python SDK for working with the Labstep API',
+      long_description=long_description,
+      url='http://github.com/Labstep/labstepPy',
+      author='Barney Walker',
+      author_email='barney@labstep.com',
+      license='MIT',
+      packages=['labstep'],
+      install_requires=['requests', 'python-dotenv'],
+      tests_require=["pytest"],
+      zip_safe=False)
