0030e8efe2f488ccd0995a9a89f661835f57d739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:31 +0000

    Add package automated-stock-rl/2.0/automated_stock_rl-2.0-py3-none-any.whl

---
+++ packages/automated-stock-rl/automated_stock_rl-2.0-py3-none-any.whl/automated_stock_rl/package.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from .preprocessing.data_scraping import load_stock_data
+from .RL_algorithms.DQN import run_dqn_algorithm, run_ddqn_algorithm, run_dddqn_algorithm
+
+def trade(stock,algorithm):
+	data=load_stock_data(stock)
+	returns=run_dqn_algorithm(data,algorithm)
+	
+	plt.plot(range(70), returns, marker='o', linestyle='--', color='g', label='Square') 
+	plt.xlabel('Training epochs')
+	plt.ylabel('Reward') 
+	plt.title('Profit')
+	plt.show()
+	
