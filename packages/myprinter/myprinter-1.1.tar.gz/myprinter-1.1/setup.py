a62a392b0da4033589fbf5118189651d00b6f1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:40 +0000

    Add package myprinter/1.1/myprinter-1.1.tar.gz

---
+++ packages/myprinter/myprinter-1.1.tar.gz/myprinter-1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="myprinter",
+    version='1.1',
+    description="Test setuptools",
+    url="http://pypi.python.org/pypi/myprinter/",
+    author="fshenx",
+    author_email="22@qq.com",
+    license="LICENSE",
+    packages=find_packages(),
+    data_files=[('testsetup/config3', ['testsetup/config/t.json']),]
+    #include_package_data = True,
+    #package_data = {'': ['.json'], '': ['.conf']}
+)
