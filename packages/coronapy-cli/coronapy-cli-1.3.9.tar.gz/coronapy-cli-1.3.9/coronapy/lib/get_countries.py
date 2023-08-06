4d350d70a9083502dd0f9d8d9a733cf08f1df7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:51 +0000

    Add package coronapy-cli/1.3.9/coronapy-cli-1.3.9.tar.gz

---
+++ packages/coronapy-cli/coronapy-cli-1.3.9.tar.gz/coronapy-cli-1.3.9/coronapy/lib/get_countries.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def all_countries(sorted_by):
+    url = 'https://corona.lmao.ninja/v2/countries?sort=' + sorted_by
+    response = requests.get(url)
+    allCountries = []
+    i = 1
+    for country in response.json():
+        allCountries.append([
+            i,
+            country['country'],
+            '{:,}'.format(country['cases']),
+            '{:,}'.format(country['todayCases']),
+            '{:,}'.format(country['deaths']),
+            '{:,}'.format(country['todayDeaths']),
+            '{:,}'.format(country['recovered']),
+            '{:,}'.format(country['active']),
+            '{:,}'.format(country['critical'])
+        ])
+        i = i + 1
+    return allCountries
