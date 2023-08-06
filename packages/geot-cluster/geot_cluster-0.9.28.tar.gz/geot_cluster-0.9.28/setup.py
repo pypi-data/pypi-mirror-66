7de3269009aeeb7781b55fed725f012012a80eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:15 +0000

    Add package geot-cluster/0.9.28/geot_cluster-0.9.28.tar.gz

---
+++ packages/geot-cluster/geot_cluster-0.9.28.tar.gz/geot_cluster-0.9.28/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+with open("README.md","r") as fh:
+    ld = fh.read()
+
+setup(
+    name='geot_cluster',
+    version='0.9.28',
+    description='Reasearch project on clustering clients from transaction history',
+    author='Korsakov Aleksandr',
+    author_email='corsakov.alex@yandex.ru',
+    url='https://github.com/LarsWallenstein/Geotrans_cluster',
+    py_modules=["geot_cluster"],
+    package_dir={'':'src'},
+    install_requires=[
+    'pytest>=3.7',
+    'numpy',
+    'pandas',
+    'markov-clustering',
+    'matplotlib',
+    'pytz',
+    'folium>=0.10.1',
+    'networkx',
+    'haversine',
+    'timezonefinder'],
+    long_description = ld,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
