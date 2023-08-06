bf38caefef6b338b1edf907fb5d6edb00ac07510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:03 +0000

    Add package aiojaeger/0.1.1/aiojaeger-0.1.1.tar.gz

---
+++ packages/aiojaeger/aiojaeger-0.1.1.tar.gz/aiojaeger-0.1.1/aiojaeger/mypy_types.py
@@ -0,0 +1,9 @@
+import asyncio  # flake8: noqa
+from typing import Mapping, Optional
+
+Headers = Mapping[str, str]
+OptStr = Optional[str]
+OptTs = Optional[float]
+OptInt = Optional[int]
+OptBool = Optional[bool]
+OptLoop = Optional[asyncio.AbstractEventLoop]
