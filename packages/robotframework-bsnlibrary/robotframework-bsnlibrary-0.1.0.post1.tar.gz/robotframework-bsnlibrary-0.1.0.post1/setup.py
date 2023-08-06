f05dbeb57c0a32fbf84aade42c12a5aa14fa0cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:04 +0000

    Add package robotframework-bsnlibrary/0.1.0.post1/robotframework-bsnlibrary-0.1.0.post1.tar.gz

---
+++ packages/robotframework-bsnlibrary/robotframework-bsnlibrary-0.1.0.post1.tar.gz/robotframework-bsnlibrary-0.1.0.post1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='robotframework-bsnlibrary',
+    version='0.1.0.post1',
+    packages=['BSNLibrary'],
+    url='https://github.com/HaaiHenkie/bsnlibrary',
+    license='GNU General Public License version 3',
+    author='Henk van den Akker',
+    description='Robot Framework library for generating BSNs (Dutch citizen service number)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Framework :: Robot Framework :: Library",
+    ],
+    install_requires=['robotframework']
+)
