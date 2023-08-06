f2492300f304d7cf1de3525da36dab66c25821a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:08 +0000

    Add package kcleaner/0.3.2/kcleaner-0.3.2.tar.gz

---
+++ packages/kcleaner/kcleaner-0.3.2.tar.gz/kcleaner-0.3.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kcleaner',
+    version='0.3.2',
+    author='Gui Martins',
+    url='https://fancywhale.ca/',
+    author_email='gui.martins.94@outlook.com',
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['kcleaner'],
+    install_requires=[
+        'Click',
+        'iterfzf',
+        'pyyaml'
+    ],
+    entry_points='''
+        [console_scripts]
+        kcleaner=kcleaner:cli
+    ''',
+)
