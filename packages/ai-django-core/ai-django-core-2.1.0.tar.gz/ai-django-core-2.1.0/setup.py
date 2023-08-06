a3db16aa483a006f1fd106de9e83426d39c9ba2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:12 +0000

    Add package ai-django-core/2.1.0/ai-django-core-2.1.0.tar.gz

---
+++ packages/ai-django-core/ai-django-core-2.1.0.tar.gz/ai-django-core-2.1.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import ast
+import re
+
+from setuptools import setup, find_packages
+
+# Dependencies
+requirements = [
+    'django-crispy-forms>=1.4.0',
+    'Django>=1.7.0',
+    'bleach>=1.4.2',
+    'python-dateutil>=2.5.3',
+    'future>=0.16',
+    'freezegun',
+    'pytest-cov',
+    'pytest-django',
+]
+
+# Parse version
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+with open("ai/__init__.py", "rb") as f:
+    version = str(
+        ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1))
+    )
+
+setup(
+    name='ai-django-core',
+    version=version,
+    author='Ambient Innovation: GmbH',
+    author_email='hello@ambient-innovation.com',
+    packages=find_packages(),
+    include_package_data=True,
+    license="The MIT License (MIT)",
+    description='Lots of helper functions and useful widgets.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    zip_safe=False,
+    install_requires=requirements
+)
