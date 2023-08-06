03aaa1bcee40da1410b4181f9822c547fb381f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:23 +0000

    Add package dippykit/3.0.0/dippykit-3.0.0.tar.gz

---
+++ packages/dippykit/dippykit-3.0.0.tar.gz/dippykit-3.0.0/setup.py
@@ -0,0 +1,39 @@
+"""Setuptools based setup module for dippykit.
+
+"""
+
+from setuptools import setup, find_packages
+import os
+
+cur_path = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(cur_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dippykit',
+    version='3.0.0',
+    author='Brighton Ancelin',
+    author_email='bancelin3@gatech.edu',
+    description='A Python Package for Digital Image Processing Education',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/dippykit/dippykit",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English'
+    ],
+    packages=find_packages(exclude=['docs', 'doc']),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'opencv-python',
+        'Pillow',
+        'scipy',
+        'scikit-image',
+    ],
+    python_requires='>=3.5.2',
+)
