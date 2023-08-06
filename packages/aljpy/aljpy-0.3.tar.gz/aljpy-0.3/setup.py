008dfae40d61e0d4c6c3141203684bef292e08a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:35 +0000

    Add package aljpy/0.3/aljpy-0.3.tar.gz

---
+++ packages/aljpy/aljpy-0.3.tar.gz/aljpy-0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='aljpy',
+      version='0.3',
+      description='Andy\'s common tools',
+      author='Andy Jones',
+      author_email='andyjones.ed@gmail.com',
+      url='https://github.com/andyljones/aljpy',
+      packages=['aljpy'],
+      package_data={'aljpy': ['*.txt']},
+      include_package_data=True)
