0cbccc060fd8ba3b9d978725a3b6cecb9f0a4739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:36 +0000

    Add package junit2html/023/junit2html-023.tar.gz

---
+++ packages/junit2html/junit2html-023.tar.gz/junit2html-023/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+files = ["*.css"]
+
+
+setup(
+    name="junit2html",
+    version="023",
+    description="Generate HTML reports from Junit results",
+    author="Ian Norton",
+    author_email="inorton@gmail.com",
+    url="https://gitlab.com/inorton/junit2html",
+    packages=["junit2htmlreport"],
+    package_data={"junit2htmlreport": files},
+    scripts=["junit2html"],
+    platforms=["any"],
+    license="License :: OSI Approved :: MIT License",
+    long_description="Genearate a single file HTML report from a Junit XML file"
+)
