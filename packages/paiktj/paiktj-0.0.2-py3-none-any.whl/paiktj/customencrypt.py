6c8a3bce0ce2bb391516df58eadca63d3b4953ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:56 +0000

    Add package paiktj/0.0.2/paiktj-0.0.2-py3-none-any.whl

---
+++ packages/paiktj/paiktj-0.0.2-py3-none-any.whl/paiktj/customencrypt.py
@@ -0,0 +1,34 @@
+import pickle
+from getpass import getpass
+from cryptography.fernet import Fernet
+
+
+def my_encrypt(object_input, key_save=False, file_name=None):
+    if key_save and file_name is None:
+        raise Exception("file name needed")
+    while True:
+        dialog = input("file is small (y or n)")
+        if dialog == "n":
+            return
+        elif dialog == "y":
+            break
+        else:
+            print("wrong answer")
+    string_object = pickle.dumps(object_input)
+    key = Fernet.generate_key()
+    if key_save:
+        with open(file_name, 'wb') as f:
+            f.write(key)
+    else:
+        print("'''", end="")
+        print(key)
+        print("'''", end="")
+    f = Fernet(key)
+    return f.encrypt(string_object)
+
+
+def my_decrypt(string_encrypted):
+    key = getpass("key : ")
+    f = Fernet(key)
+    string_decrypted = f.decrypt(string_encrypted)
+    return pickle.loads(string_decrypted)
