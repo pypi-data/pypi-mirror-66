1fe9395e380d02e572e9697f713c84a59903d253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:21 +0000

    Add package twilioSMS/0.2/twilioSMS-0.2.tar.gz

---
+++ packages/twilioSMS/twilioSMS-0.2.tar.gz/twilioSMS-0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='twilioSMS',
+    version='0.2',
+    description='a fun texting app',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["sms"],
+    package_dir={'': 'src'},
+    setup_requires=['wheel'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
