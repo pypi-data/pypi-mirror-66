8e4071416330e80e6a6f537d669c59ffc0cb83cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:06 +0000

    Add package synchronize/0.0.1/synchronize-0.0.1.tar.gz

---
+++ packages/synchronize/synchronize-0.0.1.tar.gz/synchronize-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="synchronize",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Oleksii Petrenko",
+    author_email="oleksiiypetrenko@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
