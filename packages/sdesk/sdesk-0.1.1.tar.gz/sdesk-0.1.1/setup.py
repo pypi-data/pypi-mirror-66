a49b2f07666c1196c8deb699c898818147820886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:49 +0000

    Add package sdesk/0.1.1/sdesk-0.1.1.tar.gz

---
+++ packages/sdesk/sdesk-0.1.1.tar.gz/sdesk-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sdesk', 'sdesk.api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'sdesk',
+    'version': '0.1.1',
+    'description': 'ScienceDesk helper library',
+    'long_description': '# ScienceDesk Python helpers\n\nThis module provides Python code to help you interact with and extend the\nScienceDesk platform.\n\n## Modules\n\n- api: helpers to interact with the ScienceDesk API\n- proc: helpers to write ScienceDesk algorithms\n',
+    'author': 'ScienceDesk GmbH',
+    'author_email': 'github@sciencedesk.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://sciencedesk.net',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
