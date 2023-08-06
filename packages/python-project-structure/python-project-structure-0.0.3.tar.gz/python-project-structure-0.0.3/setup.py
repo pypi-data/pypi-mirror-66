1d13de8c57ec76ac0f75eb45896af112e32f7657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:59 +0000

    Add package python-project-structure/0.0.3/python-project-structure-0.0.3.tar.gz

---
+++ packages/python-project-structure/python-project-structure-0.0.3.tar.gz/python-project-structure-0.0.3/setup.py
@@ -0,0 +1,42 @@
+"""
+Python project structure foundation or template, distribution/package metadata.
+"""
+
+import setuptools
+
+with open("README.rst", "r") as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setuptools.setup(
+    name="python-project-structure",
+    author="Ross Patterson",
+    author_email="me@rpatterson.net",
+    description="Python project structure foundation or template",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/rpatterson/python-project-structure",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    use_scm_version=dict(
+        write_to="src/pythonprojectstructure/version.py",
+        local_scheme="no-local-version",
+    ),
+    setup_requires=["setuptools_scm"],
+    extras_require=dict(
+        dev=[
+            "pytest",
+            "pre-commit",
+            "coverage",
+            "flake8",
+            "autoflake",
+            "autopep8",
+            "flake8-black",
+        ]
+    ),
+)
---
+++ packages/python-project-structure/python-project-structure-0.0.3.tar.gz/python-project-structure-0.0.3/src/pythonprojectstructure/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python project structure foundation or template, top-level package.
+"""
+
+# Manage version through the VCS CI/CD process
+try:
+    from . import version
+except ImportError:  # pragma: no cover
+    version = None
+if version is not None:  # pragma: no cover
+    __version__ = version.version
