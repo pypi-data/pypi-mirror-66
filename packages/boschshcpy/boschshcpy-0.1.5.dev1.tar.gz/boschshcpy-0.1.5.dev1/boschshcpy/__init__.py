93a354ee8b8e78b0ed241085c98ed515562238df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:36 +0000

    Add package boschshcpy/0.1.5.dev1/boschshcpy-0.1.5.dev1.tar.gz

---
+++ packages/boschshcpy/boschshcpy-0.1.5.dev1.tar.gz/boschshcpy-0.1.5.dev1/boschshcpy/__init__.py
@@ -0,0 +1,8 @@
+from .device import SHCDevice
+from .device_helper import (SHCCameraEyes, SHCDeviceHelper,
+                            SHCIntrusionDetectionSystem, SHCShutterContact,
+                            SHCShutterControl, SHCSmartPlug, SHCSmokeDetector,
+                            SHCThermostat, SHCUniversalSwitch, SHCMotionDetector,
+                            SHCTwinguard, SHCBatteryDevice)
+from .scenario import SHCScenario
+from .session import SHCSession
