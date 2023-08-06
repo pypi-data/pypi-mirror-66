b0832aece608ef78a53e2c42ac5d94016b5f7753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:05 +0000

    Add package pdsf/0.1.9/pdsf-0.1.9-py2.py3-none-any.whl

---
+++ packages/pdsf/pdsf-0.1.9-py2.py3-none-any.whl/pdsf/util.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Utility functions for +other SQL modules.
+"""
+import numpy as np
+import pandas as pd
+import os
+import sqlalchemy
+
+
+get_pk_stmt = "SELECT ORDINAL_POSITION AS [index], COLUMN_NAME AS name FROM {db}.INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = '{table}' AND CONSTRAINT_NAME LIKE '%PK%' order by [index]"
+get_un_stmt = "SELECT ORDINAL_POSITION AS [index], COLUMN_NAME AS name FROM {db}.INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = '{table}' AND CONSTRAINT_NAME LIKE '%_UN' order by [index]"
+
+
+def compare_dfs(old_df, new_df, on):
+    """
+    Function to compare two DataFrames with nans and return a dict with rows that have changed (diff), rows that exist in new_df but not in old_df (new), and rows  that exist in old_df but not in new_df (remove).
+    Both DataFrame must have the same columns.
+
+    Parameters
+    ----------
+    old_df : DataFrame
+        The old DataFrame.
+    new_df : DataFrame
+        The new DataFrame.
+    on : str or list of str
+        The primary key(s) to index/merge the two DataFrames.
+
+    Returns
+    -------
+    dict of DataFrames
+        As described above, keys of 'diff', 'new', and 'remove'.
+    """
+    if ~np.in1d(old_df.columns, new_df.columns).any():
+        raise ValueError('Both DataFrames must have the same columns')
+
+    val_cols = [c for c in old_df.columns if not c in on]
+    all_cols = old_df.columns.tolist()
+
+    comp1 = pd.merge(old_df, new_df, on=on, how='outer', indicator=True, suffixes=('_x', ''))
+
+    rem1 = comp1.loc[comp1._merge == 'left_only', on].copy()
+    add1 = comp1.loc[comp1._merge == 'right_only', all_cols].copy()
+    comp2 = comp1[comp1._merge == 'both'].drop('_merge', axis=1).copy()
+    comp2[comp2.isnull()] = np.nan
+
+    old_cols = on.copy()
+    old_cols_map = {c: c[:-2] for c in comp2 if '_x' in c}
+    old_cols.extend(old_cols_map.keys())
+    old_set = comp2[old_cols].copy()
+    old_set.rename(columns=old_cols_map, inplace=True)
+    new_set = comp2[all_cols].copy()
+
+    comp_list = []
+    for c in val_cols:
+        if old_set[c].dtype.name == 'float64':
+            c1 = ~np.isclose(old_set[c], new_set[c])
+        elif old_set[c].dtype.name == 'object':
+            new_set[c] = new_set[c].astype(str)
+            c1 = old_set[c].astype(str) != new_set[c]
+        else:
+            c1 = old_set[c] != new_set[c]
+        notnan1 = old_set[c].notnull() | new_set[c].notnull()
+        c2 = c1 & notnan1
+        comp_list.append(c2)
+    comp_index = pd.concat(comp_list, axis=1).any(1)
+    diff_set = new_set[comp_index].copy()
+
+    dict1 = {'diff': diff_set, 'new': add1, 'remove': rem1}
+
+    return dict1
+
+
+def create_snowflake_engine(username, password, account, database, schema, warehouse=None, role=None):
+    """
+
+    """
+    conn_str = 'snowflake://{user}:{password}@{account}/{database}/{schema}'.format(
+        user=username,
+        password=password,
+        account=account,
+        database=database,
+        schema=schema
+    )
+    extra_params = []
+    if isinstance(warehouse, str):
+        extra_params.append('warehouse=' + warehouse)
+    if isinstance(role, str):
+        extra_params.append('role=' + role)
+    if extra_params:
+        conn_str = conn_str + '?' + '&'.join(extra_params)
+    engine = sqlalchemy.create_engine(conn_str)
+    return engine
+
+
+
+def save_df(df, path_str, index=True, header=True):
+    """
+    Function to save a dataframe based on the path_str extension. The path_str must  either end in csv or h5.
+
+    df -- Pandas DataFrame.\n
+    path_str -- File path (str).\n
+    index -- Should the row index be saved? Only necessary for csv.
+    """
+
+    path1 = os.path.splitext(os.path_str)
+
+    if path1[1] in '.h5':
+        df.to_hdf(path_str, 'df', mode='w')
+    if path1[1] in '.csv':
+        df.to_csv(path_str, index=index, header=header)
