2884f039fd7d81de3bc51e203b97da9cb1937d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:40 +0000

    Add package gitdir/1.2.2/gitdir-1.2.2.tar.gz

---
+++ packages/gitdir/gitdir-1.2.2.tar.gz/gitdir-1.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='gitdir',
+    version='1.2.2',
+    author='Siddharth Dushantha',
+    author_email='siddharth.dushantha@gmail.com',
+    description='Download a single directory/folder from a GitHub repo',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sdushantha/gitdir',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'gitdir = gitdir.__main__:main',
+        ]
+    },
+    install_requires=['colorama~=0.4']
+)
