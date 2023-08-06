83aad32100217a342cae803416732089914bae9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:37 +0000

    Add package jzrkingsta-prob/0.1/jzrkingsta_prob-0.1.tar.gz

---
+++ packages/jzrkingsta-prob/jzrkingsta_prob-0.1.tar.gz/jzrkingsta_prob-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jzrkingsta_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jzrkingsta_prob'],
+      author = 'James L Zhang',
+      author_email = 'jzr.kingsta@outlook.com',
+      zip_safe=False)
