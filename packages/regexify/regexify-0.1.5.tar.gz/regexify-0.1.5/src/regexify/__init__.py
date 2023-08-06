2155c8cd5e660702166a7d7f0657b55fd16bb6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:15 +0000

    Add package regexify/0.1.5/regexify-0.1.5.tar.gz

---
+++ packages/regexify/regexify-0.1.5.tar.gz/regexify-0.1.5/src/regexify/__init__.py
@@ -0,0 +1,5 @@
+"""Regular expression containers and helper functions to simplify the use and deployment of regular expressions."""
+__version__ = '0.1.5'
+
+from .pattern_trie import PatternTrie
+from .pattern import Match, Pattern, MatchCask
