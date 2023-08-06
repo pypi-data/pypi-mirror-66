091774cedc02e8be13681ea5659278771ac2dd59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:46 +0000

    Add package distributions-mh2065/1.0/distributions-mh2065-1.0.tar.gz

---
+++ packages/distributions-mh2065/distributions-mh2065-1.0.tar.gz/distributions-mh2065-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-mh2065',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-mh2065'],
+      author = 'Michael Hartmann',
+      author_email = 'mikehartmann@gmx.net',
+      zip_safe=False)
