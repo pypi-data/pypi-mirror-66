0aac14fd5c418de415047b1c4ab09335cb300a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:10 +0000

    Add package pandas-market-calendars/1.3.2/pandas_market_calendars-1.3.2-py3-none-any.whl

---
+++ packages/pandas-market-calendars/pandas_market_calendars-1.3.2-py3-none-any.whl/pandas_market_calendars/calendar_registry.py
@@ -0,0 +1,20 @@
+from .market_calendar import MarketCalendar
+
+
+def get_calendar(name, open_time=None, close_time=None):
+    """
+    Retrieves an instance of an MarketCalendar whose name is given.
+
+    :param name: The name of the MarketCalendar to be retrieved.
+    :param open_time: Market open time override as datetime.time object. If None then default is used.
+    :param close_time: Market close time override as datetime.time object. If None then default is used.
+    :return: MarketCalendar of the desired calendar.
+    """
+    return MarketCalendar.factory(name, open_time=open_time, close_time=close_time)
+
+
+def get_calendar_names():
+    """All Market Calendar names and aliases that can be used in "factory"
+    :return: list(str)
+    """
+    return MarketCalendar.calendar_names()
