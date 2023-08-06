c5a57f08fc6b3c10794f6eeaa84eae7e11658840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:11 +0000

    Add package django-helcim/0.8.0/django-helcim-0.8.0.tar.gz

---
+++ packages/django-helcim/django-helcim-0.8.0.tar.gz/django-helcim-0.8.0/helcim/migrations/0001_initial.py
@@ -0,0 +1,66 @@
+# pylint: disable=missing-docstring, line-too-long, invalid-name
+
+from __future__ import unicode_literals
+from uuid import uuid4
+
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='HelcimTransaction',
+            fields=[
+                ('id', models.UUIDField(default=uuid4, editable=False, primary_key=True, verbose_name='ID')),
+                ('raw_request', models.CharField(blank=True, help_text='The raw request used for this transaction', max_length=1024, null=True)),
+                ('raw_response', models.CharField(blank=True, help_text='The raw response returned for this transaction', max_length=1024, null=True)),
+                ('transaction_success', models.BooleanField(help_text='Whether the transaction was successful or not')),
+                ('response_message', models.CharField(blank=True, help_text='The response message with the API call', max_length=256, null=True)),
+                ('notice', models.CharField(blank=True, help_text='Any error or warning messages from Helcim', max_length=128, null=True)),
+                ('date_response', models.DateTimeField(help_text='The date and time of the API response')),
+                ('date_created', models.DateTimeField(auto_now_add=True, help_text='Date and time this transaction was recorded in database')),
+                ('transaction_type', models.CharField(choices=[('s', 'purchase (sale)'), ('p', 'pre-authorization'), ('c', 'capture'), ('r', 'refund')], help_text='The type of transaction', max_length=1)),
+                ('transaction_id', models.PositiveIntegerField(blank=True, help_text='The Helcim Commerce transaction ID', null=True)),
+                ('amount', models.DecimalField(blank=True, decimal_places=2, help_text='The transaction amount', max_digits=12, null=True)),
+                ('currency', models.CharField(blank=True, help_text='The transaction currency', max_length=8, null=True)),
+                ('cc_name', models.CharField(blank=True, help_text='The cardholder name', max_length=256, null=True)),
+                ('cc_number', models.CharField(blank=True, help_text='The first four and last 4 digits of the credit card number', max_length=16, null=True)),
+                ('cc_expiry', models.DateField(blank=True, help_text='The credit card expiry date', null=True)),
+                ('cc_type', models.CharField(blank=True, help_text='The credit card type', max_length=32, null=True)),
+                ('token', models.CharField(blank=True, help_text='The Helcim generated and stored credit card token', max_length=23, null=True)),
+                ('token_f4l4', models.CharField(blank=True, help_text='The first and last 4 digits of the credit card number', max_length=8, null=True)),
+                ('avs_response', models.CharField(blank=True, help_text='The address verification response', max_length=1, null=True)),
+                ('cvv_response', models.CharField(blank=True, help_text='The CVV verification response', max_length=1, null=True)),
+                ('approval_code', models.CharField(blank=True, help_text='The transaction approval code', max_length=16, null=True)),
+                ('order_number', models.CharField(blank=True, help_text='The Helcim order number', max_length=16, null=True)),
+                ('customer_code', models.CharField(blank=True, help_text='The Helcim customer code', max_length=16, null=True)),
+            ],
+            options={
+                'ordering': ('-date_response',),
+                'permissions': (('helcim_transactions', 'Can view and interact with Helcim transactions.'),),
+            },
+        ),
+        migrations.CreateModel(
+            name='HelcimToken',
+            fields=[
+                ('id', models.UUIDField(default=uuid4, editable=False, primary_key=True, verbose_name='ID')),
+                ('token', models.CharField(help_text='The Helcim card token number', max_length=23)),
+                ('token_f4l4', models.CharField(help_text='The first & last four digits of the credit card number', max_length=8)),
+                ('cc_type', models.CharField(blank=True, help_text='The credit card type', max_length=32, null=True)),
+                ('date_added', models.DateTimeField(auto_now_add=True, help_text='Date and time this token was added to database')),
+                ('customer_code', models.CharField(blank=True, help_text='The Helcim customer code', max_length=16, null=True)),
+                ('django_user', models.ForeignKey(blank=True, null=True, on_delete=models.deletion.CASCADE, related_name='tokens', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'permissions': (('helcim_tokens', 'Can view and interact with Helcim tokens.'),),
+                'unique_together': {('token', 'token_f4l4', 'customer_code', 'django_user')},
+            },
+        )
+    ]
---
+++ packages/django-helcim/django-helcim-0.8.0.tar.gz/django-helcim-0.8.0/helcim/views.py
@@ -0,0 +1,145 @@
+"""Views for Helcim Commerce API transactions."""
+from django.contrib import messages
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+
+from helcim import exceptions, models, gateway
+
+
+class TransactionListView(PermissionRequiredMixin, generic.ListView):
+    """List of all transactions submitted made by django-helcim."""
+    model = models.HelcimTransaction
+    permission_required = 'helcim.helcim_transactions'
+    raise_exception = True
+    context_object_name = 'transactions'
+    template_name = 'helcim/transaction_list.html'
+
+class TransactionDetailView(PermissionRequiredMixin, generic.DetailView):
+    """Details of a specific transaction made by django-helcim."""
+    model = models.HelcimTransaction
+    pk_url_kwarg = 'transaction_id'
+    permission_required = 'helcim.helcim_transactions'
+    raise_exception = True
+    context_object_name = 'transaction'
+    template_name = 'helcim/transaction_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TransactionDetailView, self).get_context_data(**kwargs)
+
+        # Check which actions are enabled
+        settings = gateway.SETTINGS
+        context['capture_enabled'] = settings['enable_transaction_capture']
+        context['refund_enabled'] = settings['enable_transaction_refund']
+
+        return context
+
+    def post(self, request, *args, **kwargs): # pylint: disable=unused-argument
+        """Handles any capture and refund requests."""
+        # Determines which action was submitted
+        action = request.POST.get('action', None)
+
+        if action == 'refund':
+            if gateway.SETTINGS['enable_transaction_refund']:
+                return self.refund()
+
+            transaction = self.get_object()
+            messages.error(self.request, 'Transactions cannot be refunded')
+
+            return HttpResponseRedirect(
+                reverse(
+                    'helcim_transaction_detail',
+                    kwargs={'transaction_id': transaction.id}
+                )
+            )
+
+        if action == 'capture':
+            if gateway.SETTINGS['enable_transaction_capture']:
+                return self.capture()
+
+            transaction = self.get_object()
+            messages.error(self.request, 'Transactions cannot be captured')
+
+            return HttpResponseRedirect(
+                reverse(
+                    'helcim_transaction_detail',
+                    kwargs={'transaction_id': transaction.id}
+                )
+            )
+
+        # Action not found - return bad request
+        return HttpResponseBadRequest('Unrecognized transaction action')
+
+    def refund(self):
+        """Processes a refund transaction."""
+        transaction = self.get_object()
+
+        refund = gateway.Refund({
+            'transaction_type': 'r',
+            'amount': transaction.amount,
+            'token': transaction.token,
+            'token_f4l4': transaction.token_f4l4,
+            'customer_code': transaction.customer_code,
+        })
+
+        try:
+            refund.process()
+        except exceptions.RefundError:
+            messages.error(self.request, 'Unable to refund transaction')
+        else:
+            messages.success(self.request, 'Transaction refunded')
+
+        return HttpResponseRedirect(
+            reverse(
+                'helcim_transaction_detail',
+                kwargs={'transaction_id': transaction.id}
+            )
+        )
+
+    def capture(self):
+        """Processes a capture transaction."""
+        transaction = self.get_object()
+
+        capture = gateway.Capture({
+            'transaction_type': 'c',
+            'transaction_id': transaction.transaction_id,
+        })
+
+        try:
+            capture.process()
+        except exceptions.PaymentError:
+            messages.error(self.request, 'Unable to capture transaction')
+        else:
+            messages.success(self.request, 'Transaction captured')
+
+        return HttpResponseRedirect(
+            reverse(
+                'helcim_transaction_detail',
+                kwargs={'transaction_id': transaction.id}
+            )
+        )
+
+class TokenListView(PermissionRequiredMixin, generic.ListView):
+    """List of all transactions submitted made by django-helcim."""
+    model = models.HelcimToken
+    permission_required = 'helcim.helcim_tokens'
+    raise_exception = True
+    context_object_name = 'tokens'
+    template_name = 'helcim/token_list.html'
+
+class TokenDeleteView(PermissionRequiredMixin, generic.DeleteView):
+    """Allows deletion of a Helcim API token."""
+    model = models.HelcimToken
+    permission_required = 'helcim.helcim_tokens'
+    raise_exception = True
+    pk_url_kwarg = 'token_id'
+    context_object_name = 'token'
+    success_message = 'Token successfully deleted.'
+    success_url = reverse_lazy('helcim_token_list')
+    template_name = 'helcim/token_delete.html'
+
+    def delete(self, request, *args, **kwargs):
+        """Override delete to allow success message to be added."""
+        messages.success(self.request, self.success_message)
+        return super(TokenDeleteView, self).delete(request, *args, **kwargs)
