d3a6ff2fb494f8a13e629f0603ac03a07a36a487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:33 +0000

    Add package meshio/4.0.11/meshio-4.0.11.tar.gz

---
+++ packages/meshio/meshio-4.0.11.tar.gz/meshio-4.0.11/test/test_medit.py
@@ -0,0 +1,27 @@
+import pytest
+
+import helpers
+import meshio
+
+
+@pytest.mark.parametrize(
+    "mesh",
+    [
+        helpers.line_mesh,
+        helpers.tri_mesh,
+        helpers.tri_mesh_2d,
+        helpers.quad_mesh,
+        helpers.tri_quad_mesh,
+        helpers.tet_mesh,
+        helpers.hex_mesh,
+        helpers.add_cell_data(helpers.tri_mesh, [("medit:ref", (), int)]),
+    ],
+)
+def test_io(mesh):
+    helpers.write_read(meshio.medit.write, meshio.medit.read, mesh, 1.0e-15)
+
+
+def test_generic_io():
+    helpers.generic_io("test.mesh")
+    # With additional, insignificant suffix:
+    helpers.generic_io("test.0.mesh")
