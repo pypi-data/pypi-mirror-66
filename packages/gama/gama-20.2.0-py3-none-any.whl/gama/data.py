5ed5dc04b26ce69d3e1792a9ca7c37eccc098031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:29 +0000

    Add package gama/20.2.0/gama-20.2.0-py3-none-any.whl

---
+++ packages/gama/gama-20.2.0-py3-none-any.whl/gama/data.py
@@ -0,0 +1,167 @@
+""" This module contains functions for loading data. """
+from collections import OrderedDict
+from typing import Tuple, Optional, Dict, Union, Type
+
+import arff
+import numpy as np
+import pandas as pd
+
+from gama.utilities.preprocessing import log
+
+
+def arff_to_pandas(file_path: str, encoding: Optional[str] = None) -> pd.DataFrame:
+    """ Load data from the ARFF file into a pd.DataFrame.
+
+    Parameters
+    ----------
+    file_path: str
+        Path of the ARFF file
+    encoding: str, optional
+        Encoding of the ARFF file.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A dataframe of the data in the ARFF file,
+        with categorical columns having category dtype.
+    """
+    if not isinstance(file_path, str):
+        raise TypeError(f"`file_path` must be of type `str` but is {type(file_path)}")
+
+    with open(file_path, "r", encoding=encoding) as arff_file:
+        arff_dict = arff.load(arff_file)
+
+    attribute_names, data_types = zip(*arff_dict["attributes"])
+    data = pd.DataFrame(arff_dict["data"], columns=attribute_names)
+    for attribute_name, dtype in arff_dict["attributes"]:
+        # 'real' and 'numeric' are probably interpreted correctly.
+        # Date support needs to be added.
+        if isinstance(dtype, list):
+            data[attribute_name] = data[attribute_name].astype("category")
+    return data
+
+
+def X_y_from_arff(
+    file_path: str, split_column: Optional[str] = None, encoding: Optional[str] = None
+) -> Tuple[pd.DataFrame, pd.Series]:
+    """ Load data from ARFF file into pd.DataFrame and specified column to pd.Series.
+
+    Parameters
+    ----------
+    file_path: str
+        path to the ARFF file.
+    split_column: str, optional (default=None)
+        Column to split and return separately (e.g. target column).
+        Value should either match a column name or None.
+        If None is specified, the last column is returned separately.
+    encoding: str, optional
+        Encoding of the ARFF file.
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.Series]
+        Features (everything except split_column) and targets (split_column).
+    """
+    data = arff_to_pandas(file_path, encoding)
+
+    if split_column is None:
+        return data.iloc[:, :-1], data.iloc[:, -1]
+    elif split_column in data.columns:
+        return data.loc[:, data.columns != split_column], data.loc[:, split_column]
+    else:
+        raise ValueError(f"No column named {split_column} found in {file_path}")
+
+
+def load_feature_metadata_from_arff(file_path: str) -> Dict[str, str]:
+    """ Load the header of the ARFF file and return the type of each attribute. """
+    data_header = "@data"
+    attribute_indicator = "@attribute"
+    attributes = OrderedDict()
+    with open(file_path, "r") as fh:
+        line = fh.readline()
+        while not line.lower().startswith(data_header):
+            if line.lower().startswith(attribute_indicator):
+                # arff uses a space separator, but allows spaces in
+                # feature name (name must be quoted) and feature type (if nominal).
+                indicator, name_and_type = line.split(" ", 1)
+                if name_and_type.startswith('"'):
+                    name, data_type = name_and_type[1:].split('" ', 1)
+                    name = name
+                else:
+                    name, data_type = name_and_type.split(" ", 1)
+                attributes[name] = data_type
+            line = fh.readline()[:-1]  # remove newline character
+    return attributes
+
+
+def heuristic_numpy_to_dataframe(
+    x: np.ndarray, max_unique_values_cat: int = 10
+) -> pd.DataFrame:
+    """ Transform a numpy array to a typed pd.DataFrame. """
+    x_df = pd.DataFrame(x)
+    for column, n_unique in x_df.nunique(dropna=True).items():
+        if n_unique <= max_unique_values_cat:
+            x_df[column] = x_df[column].astype("category")
+    return x_df
+
+
+def format_x_y(
+    x: Union[pd.DataFrame, np.ndarray],
+    y: Union[pd.DataFrame, pd.Series, np.ndarray],
+    y_type: Type = pd.Series,
+    remove_unlabeled: bool = True,
+) -> Tuple[pd.DataFrame, Union[pd.DataFrame, pd.Series]]:
+    """ Take (X,y) data and convert it to a (pd.DataFrame, pd.Series) tuple.
+
+    Parameters
+    ----------
+    x: pandas.DataFrame or numpy.ndarray
+    y: pandas.DataFrame, pandas.Series or numpy.ndarray
+    y_type: Type (default=pandas.Series)
+    remove_unlabeled: bool (default=True)
+        If true, remove all rows associated with unlabeled data (NaN in y).
+
+    Returns
+    -------
+    Tuple[pandas.DataFrame, pandas.DataFrame or pandas.Series]
+        X and y, where X is formatted as pd.DataFrame and y is formatted as `y_type`.
+    """
+    if not isinstance(x, (np.ndarray, pd.DataFrame)):
+        raise TypeError("X must be either np.ndarray or pd.DataFrame.")
+    if not isinstance(y, (np.ndarray, pd.Series, pd.DataFrame)):
+        raise TypeError("y must be np.ndarray, pd.Series or pd.DataFrame.")
+
+    if isinstance(x, np.ndarray):
+        x = heuristic_numpy_to_dataframe(x)
+    if isinstance(y, np.ndarray) and y.ndim == 2:
+        # Either indicator matrix or should be a vector.
+        if y.shape[1] > 1:
+            y = np.argmax(y, axis=1)
+        else:
+            y = y.squeeze()
+
+    if y_type == pd.Series:
+        if isinstance(y, pd.DataFrame):
+            y = y.squeeze()
+        elif isinstance(y, np.ndarray):
+            y = pd.Series(y)
+    elif y_type == pd.DataFrame:
+        if not isinstance(y, pd.DataFrame):
+            y = pd.DataFrame(y)
+    else:
+        raise ValueError(f"`y_type` must be pd.Series or pd.DataFrame but is {y_type}.")
+
+    if remove_unlabeled:
+        if isinstance(y, pd.DataFrame):
+            unlabeled = y.iloc[:, 0].isnull()
+        else:
+            unlabeled = y.isnull()
+
+        if unlabeled.any():
+            log.info(
+                f"Target vector has been found to contain {sum(unlabeled)} NaN-labels, "
+                f"these rows will be ignored."
+            )
+            x, y = x.loc[~unlabeled], y.loc[~unlabeled]
+
+    return x, y
---
+++ packages/gama/gama-20.2.0-py3-none-any.whl/gama/utilities/generic/async_evaluator.py
@@ -0,0 +1,259 @@
+"""
+I don't want to be reinventing the wheel but I can't find a satisfying implementation.
+I want to be able to execute arbitrary functions asynchronously on a different process.
+Any ongoing subprocesses must immediately be able to be terminated without errors.
+Results of cancelled subprocesses may be ignored.
+
+`concurrent.futures.ProcessPoolExecutor` gets very close to the desired implementation,
+but it has issues:
+    - by default it waits for subprocesses to close on __exit__.
+      Unfortunately it is possible the subprocesses can be running non-Python code,
+      e.g. a C implementation of SVC whose subprocess won't end until fit is complete.
+    - even if that is overwritten and no wait is performed,
+      the subprocess will raise an error when it is done.
+      Though that does not hinder the execution of the program,
+      I don't want errors for expected behavior.
+"""
+import datetime
+import logging
+import multiprocessing
+import os
+import queue
+import time
+import traceback
+import uuid
+from typing import Optional, Callable, Dict, List
+import gc
+
+try:
+    import resource
+except ModuleNotFoundError:
+    resource = None  # type: ignore
+
+import psutil
+
+from gama.logging import MACHINE_LOG_LEVEL
+
+log = logging.getLogger(__name__)
+
+
+class AsyncFuture:
+    """ Reference to a function call executed on a different process. """
+
+    def __init__(self, fn, *args, **kwargs):
+        self.id = uuid.uuid4()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.result = None
+        self.exception = None
+        self.traceback = None
+
+    def execute(self, extra_kwargs):
+        """ Execute the function call `fn(*args, **kwargs)` and record results. """
+        try:
+            # Don't update self.kwargs, as it will be pickled back to the main process
+            kwargs = {**self.kwargs, **extra_kwargs}
+            self.result = self.fn(*self.args, **kwargs)
+        except Exception as e:
+            self.exception = e
+            self.traceback = traceback.format_exc()
+
+
+class AsyncEvaluator:
+    """ Manages subprocesses on which arbitrary functions can be evaluated.
+
+    The function and all its arguments must be picklable.
+    Using the same AsyncEvaluator in two different contexts raises a `RuntimeError`.
+
+    class variables:
+    n_jobs: int (default=multiprocessing.cpu_count())
+        The default number of subprocesses to spawn.
+        Ignored if the `n_workers` parameter is specified on init.
+    defaults: Dict, optional (default=None)
+        Default parameter values shared between all submit calls.
+        This allows these defaults to be transferred only once per process,
+        instead of twice per call (to and from the subprocess).
+        Only supports keyword arguments.
+    """
+
+    n_jobs: int = multiprocessing.cpu_count()
+    memory_limit_mb: Optional[int] = None
+    defaults: Dict = {}
+
+    def __init__(
+        self, n_workers: Optional[int] = None,
+    ):
+        """
+        Parameters
+        ----------
+        n_workers : int, optional (default=None)
+            Maximum number of subprocesses to run for parallel evaluations.
+            Defaults to `AsyncEvaluator.n_jobs`, using all cores unless overwritten.
+        """
+        self._has_entered = False
+        self.futures: Dict[uuid.UUID, AsyncFuture] = {}
+        self._processes: List[psutil.Process] = []
+        self._n_jobs = n_workers if n_workers is not None else AsyncEvaluator.n_jobs
+
+        self._queue_manager = multiprocessing.Manager()
+        self._input = self._queue_manager.Queue()
+        self._output = self._queue_manager.Queue()
+        pid = os.getpid()
+        self._main_process = psutil.Process(pid)
+
+    def __enter__(self):
+        if self._has_entered:
+            raise RuntimeError(
+                "You can not use the same AsyncEvaluator in two different contexts."
+            )
+        self._has_entered = True
+
+        self._input = self._queue_manager.Queue()
+        self._output = self._queue_manager.Queue()
+
+        log.debug(
+            f"Process {self._main_process.pid} starting {self._n_jobs} subprocesses."
+        )
+        for _ in range(self._n_jobs):
+            mp_process = multiprocessing.Process(
+                target=evaluator_daemon,
+                args=(self._input, self._output, AsyncEvaluator.defaults),
+                daemon=True,
+            )
+            mp_process.start()
+
+            subprocess = psutil.Process(mp_process.pid)
+            self._processes.append(subprocess)
+
+            if resource and AsyncEvaluator.memory_limit_mb:
+                limit = AsyncEvaluator.memory_limit_mb * (2 ** 20)
+                resource.prlimit(subprocess.pid, resource.RLIMIT_AS, (limit, limit))
+
+        self._log_memory_usage()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        log.debug(f"Terminating {len(self._processes)} subprocesses.")
+        # This is ugly as the subprocesses use shared queues.
+        # It is in direct conflict with guidelines:
+        # https://docs.python.org/3/library/multiprocessing.html#all-start-methods
+        for subprocess in self._processes:
+            try:
+                subprocess.terminate()
+            except psutil.NoSuchProcess:
+                log.debug(f"Daemon {subprocess.pid} stopped prematurely.")
+        return False
+
+    def submit(self, fn: Callable, *args, **kwargs) -> AsyncFuture:
+        """ Submit fn(*args, **kwargs) to be evaluated on a subprocess.
+
+        Parameters
+        ----------
+        fn: Callable
+            Function to call on a subprocess.
+        args
+            Positional arguments to call `fn` with.
+        kwargs
+            Keyword arguments to call `fn` with.
+
+        Returns
+        -------
+        AsyncFuture
+            A Future of which the `result` or `exception` field will be populated
+            once evaluation is finished.
+        """
+        future = AsyncFuture(fn, *args, **kwargs)
+        self.futures[future.id] = future
+        self._input.put(future)
+        return future
+
+    def wait_next(self, poll_time: float = 0.05) -> AsyncFuture:
+        """ Wait until an AsyncFuture has been completed and return it.
+
+
+        Parameters
+        ----------
+        poll_time: float (default=0.05)
+            Time to sleep between checking if a future has been completed.
+
+        Returns
+        -------
+        AsyncFuture
+            The completed future that completed first.
+
+        Raises
+        ------
+        RuntimeError
+            If all futures have already been completed and returned.
+        """
+        if len(self.futures) == 0:
+            raise RuntimeError("No Futures queued, must call `submit` first.")
+
+        while True:
+            try:
+                completed_future = self._output.get(block=False)
+                match = self.futures.pop(completed_future.id)
+                match.result, match.exception, match.traceback = (
+                    completed_future.result,
+                    completed_future.exception,
+                    completed_future.traceback,
+                )
+                self._log_memory_usage()
+                return match
+            except queue.Empty:
+                time.sleep(poll_time)
+                continue
+
+    def _log_memory_usage(self):
+        processes = [self._main_process] + self._processes
+        mem_by_pid = [(p.pid, p.memory_info()[0] / (2 ** 20)) for p in processes]
+        mem_str = ",".join([f"{pid},{mem_mb}" for (pid, mem_mb) in mem_by_pid])
+        timestamp = datetime.datetime.now().isoformat()
+        log.log(MACHINE_LOG_LEVEL, f"M,{timestamp},{mem_str}")
+
+
+def evaluator_daemon(
+    input_queue: queue.Queue,
+    output_queue: queue.Queue,
+    default_parameters: Optional[Dict] = None,
+):
+    """ Function for daemon subprocess that evaluates functions from AsyncFutures.
+
+    Parameters
+    ----------
+    input_queue: queue.Queue[AsyncFuture]
+        Queue to get AsyncFuture from.
+        Queue should be managed by multiprocessing.manager.
+    output_queue: queue.Queue[AsyncFuture]
+        Queue to put AsyncFuture to.
+        Queue should be managed by multiprocessing.manager.
+    default_parameters: Dict, optional (default=None)
+        Additional parameters to pass to AsyncFuture.Execute.
+        This is useful to avoid passing lots of repetitive data through AsyncFuture.
+    """
+    try:
+        while True:
+            try:
+                future = input_queue.get()
+                future.execute(default_parameters)
+                if future.result:
+                    if isinstance(future.result, tuple):
+                        result = future.result[0]
+                    else:
+                        result = future.result
+                    if isinstance(result.error, MemoryError):
+                        # Can't pickle MemoryErrors. Should work around this later.
+                        result.error = "MemoryError"
+                        gc.collect()
+                output_queue.put(future)
+            except MemoryError:
+                future.result = None
+                future.exception = "ProcessMemoryError"
+                gc.collect()
+                output_queue.put(future)
+    except Exception as e:
+        # There are no plans currently for recovering from any exception:
+        print(f"Stopping daemon:{type(e)}:{str(e)}")
+    # Not sure if required: give main process time to process log message.
+    time.sleep(5)
