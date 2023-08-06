33fbb8a05463e1d19d42c95d614ee78974d8ceb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:36 +0000

    Add package delete-cli/1.1.3/delete_cli-1.1.3-py3-none-any.whl

---
+++ packages/delete-cli/delete_cli-1.1.3-py3-none-any.whl/delete/__init__.py
@@ -0,0 +1,15 @@
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the Apache License (v2.0) as published by the Apache Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the Apache License for more details.
+#
+# You should have received a copy of the Apache License along with this program.
+# If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
+
+"""delete-cli package initialization and metadata."""
+
+# metadata
+from delete.__meta__ import (__appname__, __version__, __authors__, __contact__,
+                             __license__, __website__, __keywords__, __description__)
