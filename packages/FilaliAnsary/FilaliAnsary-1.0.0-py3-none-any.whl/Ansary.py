32a555fec283ba85688a645714f17ad1309e98d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:44 +0000

    Add package FilaliAnsary/1.0.0/FilaliAnsary-1.0.0-py3-none-any.whl

---
+++ packages/FilaliAnsary/FilaliAnsary-1.0.0-py3-none-any.whl/Ansary.py
@@ -0,0 +1,29 @@
+import requests
+
+from scipy.spatial import distance
+
+def de(x,y):
+    return distance.euclidean(x,y)
+
+class Filali_KNN_Classifier():
+
+    def fit(self,x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def predict(self, x_test):
+        predictions=[]
+        for x in x_test :
+            l=self.plusProche(x)
+            predictions.append(l)
+        return predictions
+
+    def plusProche(self,x):
+        distMin = de(x, self.x_train[0])
+        mi=0
+        for i in range(1,len(self.x_train)):
+            dist=de(x, self.x_train[i])
+            if dist < distMin :
+                distMin = dist
+                mi = i
+        return self.y_train[mi]
