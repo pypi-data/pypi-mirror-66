2abf61623ccaa0f3447c3a7eec84b5902c91636b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:04 +0000

    Add package gapipy/2.26.2/gapipy-2.26.2.tar.gz

---
+++ packages/gapipy/gapipy-2.26.2.tar.gz/gapipy-2.26.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gapipy
+
+from setuptools import setup, find_packages
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = []
+with open('requirements.txt') as reqs:
+    lines = reqs.readlines()
+    for line in lines:
+        if line:
+            requirements.append(line.strip('\n'))
+
+test_requirements = [
+    'mock',
+    'nose',
+    'nose-parameterized',
+]
+
+setup(
+    name='gapipy',
+    version=gapipy.__version__,
+    description='Python client for the G Adventures REST API',
+    long_description=readme + '\n\n' + history,
+    author='G Adventures',
+    author_email='software@gadventures.com',
+    url='https://github.com/gadventures/gapipy',
+    packages=find_packages(exclude=('tests',)),
+    package_dir={'gapipy': 'gapipy'},
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False,
+    keywords='gapipy',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='nose.collector',
+    tests_require=test_requirements,
+)
